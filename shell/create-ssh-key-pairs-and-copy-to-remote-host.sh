b6716a06d6c311e484a4500101e8e61351662992
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-01-03 12:25:17 -0500

    Update create-ssh-key-pairs-and-copy-to-remote-host.sh

--- shell/create-ssh-key-pairs-and-copy-to-remote-host.sh
+++ shell/create-ssh-key-pairs-and-copy-to-remote-host.sh
@@ -10,7 +10,6 @@ exit_success_fn()
 {
     echo
     echo 'The script has finished.'
-    echo
     exit
 }
 
@@ -42,7 +41,19 @@ create_ssh_keypair_fn()
 
     clear
 
-    ssh-keygen -b '4096' -t 'rsa' -C 'jman@JDESKTOP-U' -f "${HOME}"/.ssh/id_rsa
+    echo 'User input is required to create the new ssh key pair.'
+    echo
+    echo 'Available options and examples are located inside the parenthesis.'
+    echo
+    read -p 'Enter the bit size ( 2048 | 4096 ): ' SSH_BITS
+    echo
+    read -p 'Enter the type ( dsa | rsa ): ' SSH_TYPE
+    echo
+    read -p "Enter a comment ( ${USER}@$(uname -n) ): " SSH_COMMENT
+    echo
+    read -p "Enter the full output file path ( ${HOME}/.ssh/id_rsa ): " SSH_DIR
+    echo
+    ssh-keygen -b "${SSH_BITS}" -t "${SSH_TYPE}" -C "${SSH_COMMENT}" -f "${SSH_DIR}"
 
     if [ ${?} -ne '0' ]; then
         exit_fail_ssh_keygen_fn
@@ -56,16 +67,18 @@ copy_ssh_key_fn()
     clear
 
     local SSH_IP SSH_PORT SSH_USER
-    echo 'Enter the remote PC'\''s information:'
+    echo 'User input is required to copy the public ssh key to a remote computer.'
+    echo
+    echo 'Available options and examples are located inside the parenthesis.'
+    echo
+    read -p 'Port (default is 22): ' SSH_PORT
+    echo
+    read -p "User ( $(whoami) ): " SSH_USER
+    echo
+    read -p 'IP Address ( 192.168.1.2 | 10.0.1.0 ): ' SSH_IP
+    echo
 
-    read -p 'Port: ' SSH_PORT
-    echo
-    read -p 'User: ' SSH_USER
-    echo
-    read -p 'IP Address: ' SSH_IP
-    echo
-
-    ssh-copy-id -p "${SSH_PORT}" -i "${HOME}"/.ssh/id_rsa.pub "${SSH_USER}"@"${SSH_IP}"
+    ssh-copy-id -p "${SSH_PORT}" -i "${HOME}/.ssh/id_rsa.pub" "${SSH_USER}"@"${SSH_IP}"
     echo
     read -p 'Press enter to continue.'
     clear
@@ -83,8 +96,8 @@ check_for_ssh_keys_fn()
 
     local ANSWER
 
-    # Verify that the public ssh key '~/.ssh/id_rsa.pub' exists before continuing
-    if [ -f "${HOME}"/.ssh/id_rsa.pub ]; then
+    # Verify that the ssh keys exist before continuing: '~/.ssh/id_rsa' and '~/.ssh/id_rsa.pub'
+    if [ -f "${HOME}"/.ssh/id_rsa ] && [ -f "${HOME}"/.ssh/id_rsa.pub ]; then
         echo
         echo 'The ssh key pair files were found. No need to recreate.'
         echo
@@ -94,30 +107,25 @@ check_for_ssh_keys_fn()
         clear
         return 0
     else
-        echo "The public ssh key is missing: ${HOME}/.ssh/id_rsa.pub"
+        printf "The ssh key pairs are missing:\n\n%s\n%s\n" "${HOME}"/.ssh/id_rsa "${HOME}"/.ssh/id_rsa.pub
         echo
-        echo 'You must generate a private keypair before you can run this script.'
+        echo '============================================='
         echo
-        echo 'Would you like to do that now?'
+        echo 'You must generate a new private keypair before'
+        echo 'attempting to copy the public key to another computer.'
+        echo
+        echo 'Would you like to create them now?'
         echo
         echo '[1] Yes'
         echo '[2] No'
-        echo '[3] Exit'
+        echo '[3] Main Menu'
         echo
         read -p 'You choices are (1 to 3): ' ANSWER
         clear
         if [ "${ANSWER}" -eq '1' ]; then
             create_ssh_keypair_fn
-
-            if [ ${?} -ne '0' ]; then
-                exit_fail_ssh_keygen_fn
-            else
-                main_menu_fn
-            fi
-
-            if [ "${ANSWER}" -eq '2' ] || [ "${ANSWER}" -eq '3' ]; then
-                exit_success_fn
-            fi
+        elif [ "${ANSWER}" -eq '2' ] || [ "${ANSWER}" -eq '3' ]; then
+            main_menu_fn
         fi
     fi
 }
@@ -137,7 +145,7 @@ main_menu_fn()
     echo
     echo '1) Check if ssh key pairs exist (id_rsa and id_rsa.pub)'
     echo '2) Copy this pc'\''s ssh public key to a remote computer'
-    echo '3) Exit back to terminal'
+    echo '3) Exit script'
     echo
 
     read -p 'Your choices (1 to 3): ' CHOICE
