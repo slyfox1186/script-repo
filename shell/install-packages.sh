cf62a624747b4aeb5387385449b0af81fe486f0d
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-01-03 07:30:06 -0500

    Update install-packages.sh

--- shell/install-packages.sh
+++ shell/install-packages.sh
@@ -56,13 +56,16 @@ installed()
 # function to exit the script
 exit_fn()
 {
-    echo 'Script complete.'
     echo
-    echo 'Removing the temporary install script in 5 seconds.'
+    echo 'Endo of Script.'
     echo
-    echo 'Press Ctrl+[Z or C] to prevent the install script from being removed.'
-    sleep 5
-    rm "${0}"
+    echo 'This script wil remove itself from your pc in 5 seconds.'
+    echo
+    echo 'Press Ctrl+Z to force exit the script and prevent it from being removed from your pc.'
+    echo
+    read -t 10 -p 'Press enter to skip waiting.'
+    clear
+    rm -r "${0}"
     exit
 }
 
@@ -86,28 +89,20 @@ cleanup_fn()
     # case code
     case ${1} in
         1)
-            echo '
-            You chose:
-
-            sudo apt autoremove -y
-            '
+            echo 'Running: sudo apt autoremove -y'
+            echo
             apt1_fn
             exit_fn
             ;;
         2)
-            echo '
-            You chose:
-
-            sudo apt clean
-            sudo apt autoclean
-            sudo apt autoremove -y
-            '
+            echo 'Running: sudo apt clean; sudo apt autoclean; sudo apt autoremove -y'
+            echo
             apt2_fn
             exit_fn
             ;;
         3)
-            echo 'You chose to exit.'
-            echo
+            echo 'This script is exiting.'
+            sleep 2
             exit_fn
             ;;
         *)
@@ -156,27 +151,28 @@ geforce_fn()
     echo "Installing: Geforce Video Driver ${PKGS:14}:"
     echo '=========================================='
     if [ -n "${MISSING_PKGS}" ]; then
-        apt -y install${MISSING_PKGS}
+        apt -y install"${MISSING_PKGS}"
         echo
         echo 'The Geforce video driver was successfully installed!'
         echo
         echo 'Do you want to reboot now?'
         echo
-        read -p 'Enter: [Y]es or [N]o: ' ANSWER1
+        read -p 'Enter: [Y]es or [N]o: ' ANSWER
 
-        if [ "${ANSWER1}" = 'Y' ]; then
+        if [ "${ANSWER}" = 'Y' ]; then
             reboot
         else
             echo
             echo 'Make sure to reboot asap to enable the newly installed video drivers!'
             echo
         fi
-
+        unset ANSWER
     else
         echo "The Geforce Video Driver ${PKGS:14} is already installed."
         echo
     fi
 }
+
 
 installed()
 {
@@ -206,17 +202,17 @@ done
 echo 'Installing: Standard Packages'
 echo '=========================================='
 if [ -n "${MISSING_PKGS}" ]; then
-    apt -y install${MISSING_PKGS}
+    apt -y install"${MISSING_PKGS}"
     echo
 else
-    echo 'All Standard runtimes are installed.'
+    echo 'The Standard Packages are already installed.'
     echo
 fi
 unset MISSING_PKGS
 
 PKGS=(autodep8 automake1.11 autopkgtest autoproject bcpp bind9-dev binutils-dev binutils-multiarch binutils-multiarch-dev bisonc++ build-essential calc-dev cargo ccbuild ccdiff clang clang-11 clang-12 clang-13 clang-14 clang-format clang-format-11 clang-format-12 clang-format-13 clang-format-14 clang-tidy clang-tidy-11 clang-tidy-12 clang-tidy-13 clang-tidy-14 clang-tools cmake cmake-extras copyright-update cppcheck-gui cpplint crossbuild-essential-amd64 cutils dbus-x11 debcargo device-tree-compiler devscripts diffstat disktype doxygen dpkg-dev dput erlang-base erlang-ssh erlang-ssl erlang-syntax-tools erlang-tools fasm fastboot fftw-dev flex g++ gawk gcc-10-cross-base-ports gcc-10-multilib gccgo-10 gccgo-11 gccgo-12 gccgo-9 gcc-multilib gcc-opt gengetopt gobjc++-10-multilib gobjc++-12 gobjc++-12-multilib golang gperf gtk-doc-tools intltool ir.lv2 lib32stdc++6 lib32z1 libbz2-dev libcppunit-dev libdmalloc-dev libfl-dev libgc-dev libghc-html-conduit-dev libghc-html-dev libghc-http2-dev libghc-http-api-data-dev libghc-http-client-dev libghc-http-client-tls-dev libghc-http-common-dev libghc-http-conduit-dev libghc-http-date-dev libghc-http-dev libghc-http-link-header-dev libghc-http-media-dev libghc-http-reverse-proxy-dev libghc-http-streams-dev libghc-http-types-dev libglib2.0-dev libgvc6 libgvc6-plugins-gtk libheif-dev libhttp-parser-dev libimage-librsvg-perl libjemalloc-dev libjxp-java libjxr0 libjxr-tools liblilv-dev liblvm2-dev liblz-dev liblzma-dev liblzo2-dev libmimalloc2.0 libmimalloc-dev libnabrit-dev libncurses5 libncurses5-dev libnet-ifconfig-wrapper-perl libnet-nslookup-perl libnghttp2-dev libperl-dev libpstoedit-dev libraqm0 libraqm-dev libraw20 libraw-dev librsvg2-bin librsvg2-dev librsvg2-doc librust-jemalloc-sys-dev librust-malloc-buf-dev libsdl-pango1 libsdl-pango-dev libsratom-dev libssl-dev libstdc++5 libsuil-0-0 libtalloc-dev libtbbmalloc2 libtool libtool-bin libvslvm-dev libzstd1 libzstd-dev libzzip-dev lilv-utils lintian linux-source llvm llvm-13 llvm-dev lv2-dev lv2file lv2vocoder lzma-dev make mono-devel mtd-utils python3-talloc-dev r-cran-rsvg repo ripper ruby-all-dev ruby-dev ruby-rsvg2 rustc rust-src shtool tcl-dev texinfo tk-dev tkpng tty-share u-boot-tools ui-auto uuid-dev wget2-dev zipalign)
 
-for PKG in ${PKGS[@]}
+for PKG in "${PKGS[@]}"
 do
 if ! installed "${PKG}"; then
         MISSING_PKGS+=" ${PKG}"
@@ -226,10 +222,10 @@ done
 echo 'Installing: General Dev Libraries'
 echo '=========================================='
 if [ -n "${MISSING_PKGS}" ]; then
-    apt -y install${MISSING_PKGS}
+    apt -y install"${MISSING_PKGS}"
     echo
 else
-    echo 'All General Dev Libraries are installed.'
+    echo 'The General Devlopment Packages are already installed.'
     echo
 fi
 unset MISSING_PKGS
@@ -249,10 +245,10 @@ done
 echo 'Installing: GParted Development Libraries'
 echo '=========================================='
 if [ -n "${MISSING_PKGS}" ]; then
-    apt -y install${MISSING_PKGS}
+    apt -y install"${MISSING_PKGS}"
     echo
 else
-    echo 'All GParted Development Libraries are installed.'
+    echo 'The GParted Development Packages are already installed.'
     echo
 fi
 unset MISSING_PKGS
@@ -267,14 +263,15 @@ do
     if ! installed "${PKG}"; then
         MISSING_PKGS+=" ${PKG}"
     fi
+done
 
 echo 'Installing: FFmpeg Development Libraries'
 echo '=========================================='
 if [ -n "${MISSING_PKGS}" ]; then
-    apt -y install${MISSING_PKGS}
+    apt -y install"${MISSING_PKGS}"
     echo
 else
-    echo 'All FFmpeg Development Libraries are installed.'
+    echo 'The FFmpeg Development Packages are already installed.'
     echo
 fi
 unset MISSING_PKGS
@@ -366,13 +363,15 @@ sudo -u jman bash -c '\
         "url-strip >= 0.2.1" \
         "url2text >= 0.7" \
         "url2word >= 0.1.5" \
-        "urllib3 >= 1.26.12"
+        "urllib3 >= 1.26.12" \
+        1>&- 2>&-
 '
-echo
 
 ##############################
 ## GEFORCE VIDEO DRIVER 520 ##
 ##############################
+
+clear
 
 PKGS=(nvidia-driver-520)
 
@@ -383,23 +382,26 @@ echo '
     2) No
 '
 
-read -p 'Your choices are (1 or 2): ' ANSWER2
+read -p 'Your choices are (1 or 2): ' ANSWER
 echo
 
-geforce_menu_fn "${ANSWER2}"
+geforce_menu_fn "${ANSWER}"
+unset ANSWER
 
 #####################################
 ## PROMPT USER TO CLEANUP PACKAGES ##
 #####################################
 
 clear
-echo '
-Do you want to run: sudo apt
-1) autoremove
-2) clean | autoclean | autoremove
-3) exit menu
-'
-read -p 'Your choices are (1 to 3): ' ANSWER3
+
+echo 'Do you want to run: sudo apt'
+echo
+echo '1) autoremove'
+echo '2) clean | autoclean | autoremove'
+echo '3) exit menu'
+
+read -p 'Your choices are (1 to 3): ' ANSWER
 echo
 
-cleanup_fn "${ANSWER3}"
+cleanup_fn "${ANSWER}"
+unset ANSWER
