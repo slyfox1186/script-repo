c64e6e2c201a6033118e4ac25ba357dff7a0b8fa
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-01-06 14:30:56 -0500

    Update install-7z.sh

--- shell/install-7z.sh
+++ shell/install-7z.sh
@@ -2,61 +2,55 @@
 
 clear
 
-# SET VARIABLES
-VERSION='7z2201'
-TAR_FILE="${PWD}/${VERSION}.tar.xz"
-OUPUT_DIR="${PWD}/${VERSION}"
-OUTPUT_FILE='/usr/bin/7z'
-
-echo "Installing 7zip v${VERSION:2}"
-
-if [ ! -f "${TAR_FILE}" ]; then
-    wget -cO "${TAR_FILE}" "https://www.7-zip.org/a/${VERSION}-linux-x64.tar.xz"
-fi
-
-# CHECK IF OUTPUT FOLDER EXISTS
-if [ ! -d "${OUPUT_DIR}" ]; then
-    mkdir -p "${OUPUT_DIR}"
-fi
-
-tar -xf "${TAR_FILE}" -C "${OUPUT_DIR}"
-
-if tar -xf "${TAR_FILE}" -C "${OUPUT_DIR}"; then
-    # CD INTO OUTPUT DIRECTORY
-    cd '7z2201' || exit 1
-
-    # COPY STATIC FILE TO /usr/bin/7z
-    sudo cp '7zzs' "${OUTPUT_FILE}"
-
-    # VERIFY THAT THE COPY COMMAND WAS SUCCESFULL
-    if ! sudo cp '7zzs' "${OUTPUT_FILE}"; then
-        echo "Failed to copy 7zzs file to ${OUTPUT_FILE}."
-        echo
-        exit 1
-    fi
-fi
-
-TEST_VERSION="$('/usr/bin/7z' | grep --color -Ewo '[0-9]+\.[0-9]+')"
-clear
-
-# PRINT THE NEW 7Z VERSION AND STATS
-if [ -z "${TEST_VERSION}" ]; then
-    # THE INSTALLATION HAS FAILED. PLEASE CREATE A SUPPORT TICKET TO ALERT THE REPO OWNER https://github.com/slyfox1186/script-repo/issues
-    echo 'Failed to install 7zip.'
+# VERIFY THE SCRIPT HAS ROOT ACCESS BEFORE CONTINUING
+if [[ "${EUID}" -gt '0' ]]; then
+    echo 'You must run this script as root/sudo'
     echo
-    echo 'Please create a support ticket to alert the repo owner: https://github.com/slyfox1186/script-repo/issues'
-
     exit 1
 fi
 
-# ALERT THE USER OF A SUCCESSFUL INSTALLATION
-echo "7-zip v${TEST_VERSION} was succesfully installed."
-echo
-echo 'The static file is located at /usr/bin/7z and requires no other files to execute.'
-echo
-echo 'Enter '\''7z'\'' to print the help menu.'
-echo
-echo
+# SET VARIABLES
+VERSION='7z2201'
+DOWNLOAD_FILE="${VERSION}.tar.xz"
+DONWLOAD_DIR='7z'
+OUTPUT_FILE='/usr/bin/7z'
 
-read -p 'Done. Script completed.'
-exit
+# DOWNLOAD THE CUDA DEBIAN FILE IF NOT EXIST
+if [ ! -f "${DOWNLOAD_FILE}" ]; then
+    wget -cqO "${DOWNLOAD_FILE}" "https://www.7-zip.org/a/${VERSION}-linux-x64.tar.xz"
+fi
+
+# UPACKAGE THE CUDA DEBIAN FILE
+if [ ! -f "${DOWNLOAD_FILE}" ]; then
+    echo 'The script was unable to find the downloaded file.'
+    echo
+    exit 1
+fi
+
+# CREATE OUTPUT DIRECOTRY BEFORE UNCOMPRESSING WITH THE TAR COMMAND
+if [ ! -d "${DONWLOAD_DIR}" ]; then
+    mkdir -p "${DONWLOAD_DIR}"
+fi
+
+# EXTRACT FILES INTO DIRECTORY '7Z'
+tar -xf "${DOWNLOAD_FILE}" -C "${DONWLOAD_DIR}"
+
+# CD INTO DIRECOTRY
+cd "${DONWLOAD_DIR}" || echo -e "\\nCan't cd into the 7z folder. Check your tar command.\\n"
+
+# THROW AN ERROR IF THE COPY FILE FAILS
+if ! cp -f '7zzs' "${OUTPUT_FILE}"; then
+    echo "The script was unable to copy the file '7zzs' to '${OUTPUT_FILE}'"
+    echo
+    exit 1
+fi
+
+# COPY THE FILE TO ITS DESTINATION
+cp -f '7zzs' "${OUTPUT_FILE}"
+clear
+
+cd ..
+
+# RUN THE COMMAND '7Z' TO SHOW ITS OUTPUT AND CONFIRM THAT EVERYTHING WORKED AS EXPECTED
+"${OUTPUT_FILE}" | head -n 2 | cut -d " " -f1,3 | awk 'NF' | xargs printf "%s: v%s\n" "${@}"
+rm -rf "${DOWNLOAD_FILE}" "${DONWLOAD_DIR}"
