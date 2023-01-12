b465199e7f8f34c4e72a35182cbfa656fbf91946
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-01-12 05:06:33 -0500

    Update install-7z.sh

--- shell/install-7z.sh
+++ shell/install-7z.sh
@@ -10,9 +10,9 @@ if [ "${EUID}" -gt '0' ]; then
 fi
 
 # SET VARIABLES
-VERSION='7z2201'
-DOWNLOAD_FILE="${VERSION}.tar.xz"
-DONWLOAD_DIR='7z'
+VER='7z2201'
+FILE="${VER}.tar.xz"
+DIR='7z'
 OUTPUT_FILE='/usr/bin/7z'
 
 echo '[i] Choose your os architechture'
@@ -37,44 +37,45 @@ elif [ "${OS_TYPE}" == '6' ]; then exit
 fi
 
 # DOWNLOAD THE CUDA DEBIAN FILE IF NOT EXIST
-if [ ! -f "${DOWNLOAD_FILE}" ]; then
-    wget -4cqO "${DOWNLOAD_FILE}" "https://www.7-zip.org/a/${VERSION}-${URL}"
+if [ ! -f "${FILE}" ]; then
+    wget -4cqO "${FILE}" "https://www.7-zip.org/a/${VER}-${URL}"
 fi
 
-# UPACKAGE THE CUDA DEBIAN FILE
-if [ ! -f "${DOWNLOAD_FILE}" ]; then
+# UNCOMPRESS THE XZ FILE
+if [ ! -f "${FILE}" ]; then
     echo 'The script was unable to find the downloaded file.'
     echo
     exit 1
 fi
 
 # DELETE ANY FILES LEFTOVER FROM PRIOR RUNS
-if [ -d "${DONWLOAD_DIR}" ]; then
-    rm -fr "${DONWLOAD_DIR}"
+if [ -d "${DIR}" ]; then
+    rm -fr "${DIR}"
 fi
 
-# CREATE OUTPUT DIRECOTRY BEFORE UNCOMPRESSING WITH THE TAR COMMAND
-mkdir -p "${DONWLOAD_DIR}"
+# CREATE AN OUTPUT DIRECTORY BEFORE UNCOMPRESSING WITH THE TAR COMMAND
+mkdir -p "${DIR}"
 
 # EXTRACT FILES INTO DIRECTORY '7Z'
-tar -xf "${DOWNLOAD_FILE}" -C "${DONWLOAD_DIR}"
+tar -xf "${FILE}" -C "${DIR}"
 
-# CD INTO DIRECOTRY
-cd "${DONWLOAD_DIR}" || echo -e "\\nCan't cd into the 7z folder. Check your tar command.\\n"
+# CD INTO DIRECTORY
+if ! cd "${DIR}"; then
+    echo
+    echo -e "Can't cd into the 7z folder. Check your tar command."
+    echo
+    exit 1
+fi
 
-# THROW AN ERROR IF THE COPY FILE FAILS
+# COPY THE FILE TO ITS DESTINATION OR THROW AN ERROR IF THE COPYING OF THE FILE FAILS
 if ! cp -f '7zzs' "${OUTPUT_FILE}"; then
     echo "The script was unable to copy the file '7zzs' to '${OUTPUT_FILE}'"
     echo
     exit 1
 fi
 
-# COPY THE FILE TO ITS DESTINATION
-cp -f '7zzs' "${OUTPUT_FILE}"
-clear
-
-cd ..
-
 # RUN THE COMMAND '7Z' TO SHOW ITS OUTPUT AND CONFIRM THAT EVERYTHING WORKED AS EXPECTED
 "${OUTPUT_FILE}" | head -n 2 | cut -d " " -f1,3 | awk 'NF' | xargs printf "%s: v%s\n" "${@}"
-rm -rf "${DOWNLOAD_FILE}" "${DONWLOAD_DIR}"
+
+# REMOVE LEFTOVER DIRECTORY
+rm -fr "${FILE}" "${DIR}"
