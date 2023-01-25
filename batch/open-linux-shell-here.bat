d9083d83af313b67f9158e6f0b17a13feaeceaec
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-01-25 17:31:52 -0500

    Create open-linux-shell-here.bat

---
+++ batch/open-linux-shell-here.bat
@@ -0,0 +1,108 @@
+@ECHO OFF
+SETLOCAL ENABLEEXTENSIONS
+COLOR 0A
+TITLE OPEN WINDOWS WSL HERE
+
+:------------------------------------------------------------------------------------------------------------------------------------
+
+PUSHD "%~dp0"
+IF NOT "%1"=="MAX" START /MAX CMD /D /C %0 MAX & GOTO :EOF
+
+:------------------------------------------------------------------------------------------------------------------------------------
+
+REM EDIT THIS VARIABLE TO MATCH THE LINUX DISTRIBUTION YOU WISH TO USE
+REM YOU CAN FIND THE DISTRIBUTION NAMES BY RUNNING wsl.exe -l --all
+ECHO Available Distros to choose from below
+ECHO Press enter to continue when done selecting
+ECHO=
+wsl.exe -l --all
+ECHO=
+SET /P DIST="Please input one of the listed distros to set as the default: "
+CLS
+
+:------------------------------------------------------------------------------------------------------------------------------------
+
+(
+ECHO Windows Registry Editor Version 5.00
+ECHO=
+ECHO ^; ADD OPEN LINUX HERE
+ECHO=
+ECHO ^; [ * ]
+ECHO=
+ECHO [HKEY_CLASSES_ROOT\*\shell\WSL]
+ECHO @^="Open WSL Here"
+ECHO "Icon"^="C:\\Windows\\System32\\wsl.exe"
+ECHO "NoWorkingDirectory"^=""
+ECHO "Extended"^=-
+ECHO=
+ECHO [HKEY_CLASSES_ROOT\*\shell\WSL\command]
+ECHO @^="pwsh.exe -NoP -NoL -W Hidden -C \"Start-Process wt.exe -Args ^'-w new-tab -M -d \"%%W\" wsl.exe -d %DIST%^' -Verb RunAs"
+ECHO=
+ECHO=
+ECHO ^; [ DesktopBackground ]
+ECHO=
+ECHO [HKEY_CLASSES_ROOT\DesktopBackground\shell\WSL]
+ECHO @^="Open WSL Here"
+ECHO "Icon"^="C:\\Windows\\System32\\wsl.exe"
+ECHO "NoWorkingDirectory"^=""
+ECHO "Extended"^=-
+ECHO=
+ECHO [HKEY_CLASSES_ROOT\DesktopBackground\shell\WSL\command]
+ECHO @^="pwsh.exe -NoP -NoL -W Hidden -C \"Start-Process wt.exe -Args ^'-w new-tab -M -d \"%%V\" wsl.exe -d %DIST%^' -Verb RunAs"
+ECHO=
+ECHO=
+ECHO ^; [ Directory\Background ]
+ECHO=
+ECHO [HKEY_CLASSES_ROOT\Directory\Background\shell\WSL]
+ECHO @^="Open WSL Here"
+ECHO "Icon"^="C:\\Windows\\System32\\wsl.exe"
+ECHO "NoWorkingDirectory"^=""
+ECHO "Extended"^=-
+ECHO=
+ECHO [HKEY_CLASSES_ROOT\Directory\Background\shell\WSL\command]
+ECHO @^="pwsh.exe -NoP -NoL -W Hidden -C \"Start-Process wt.exe -Args ^'-w new-tab -M -d \"%%V\" wsl.exe -d %DIST%^' -Verb RunAs"
+ECHO=
+ECHO=
+ECHO ^; [ Directory ]
+ECHO=
+ECHO [HKEY_CLASSES_ROOT\Directory\shell\WSL]
+ECHO @^="Open WSL Here"
+ECHO "Icon"^="C:\\Windows\\System32\\wsl.exe"
+ECHO "NoWorkingDirectory"^=""
+ECHO "Extended"^=-
+ECHO=
+ECHO [HKEY_CLASSES_ROOT\Directory\shell\WSL\command]
+ECHO @^="pwsh.exe -NoP -NoL -W Hidden -C \"Start-Process wt.exe -Args ^'-w new-tab -M -d \"%%V\" wsl.exe -d %DIST%^' -Verb RunAs"
+ECHO=
+ECHO=
+ECHO ^; [ Drive ]
+ECHO=
+ECHO [HKEY_CLASSES_ROOT\Drive\shell\WSL]
+ECHO @^="Open WSL Here"
+ECHO "Icon"^="C:\\Windows\\System32\\wsl.exe"
+ECHO "NoWorkingDirectory"^=""
+ECHO "Extended"^=-
+ECHO=
+ECHO [HKEY_CLASSES_ROOT\Drive\shell\WSL\command]
+ECHO @^="pwsh.exe -NoP -NoL -W Hidden -C \"Start-Process wt.exe -Args ^'-w new-tab -M -d \"%%V\\\" wsl.exe -d %DIST%^' -Verb RunAs"
+)>"%TMP%\wsl.reg"
+
+:------------------------------------------------------------------------------------------------------------------------------------
+
+REM ADD THE CREATED REG FILE TO WINDOWS REGISTRY AND THEN DELETE IT
+IF EXIST "%TMP%\wsl.reg" (
+    "%SystemRoot%\regedit.exe" /s "%TMP%\wsl.reg"
+    DEL /Q "%TMP%\wsl.reg"
+)
+
+:------------------------------------------------------------------------------------------------------------------------------------
+
+REM ECHO TO THE USER THAT THE SCRIPT HAS COMPLETED
+CLS
+ECHO The script has finished!
+ECHO=
+ECHO Give the context-menu a try by right clicking on a folder, file, drive, or desktop/folder background.
+ECHO
+ECHO The menu will say ""
+ECHO=
+SET /P "DUMMY=Press enter to exit: "
