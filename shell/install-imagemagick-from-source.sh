268f5dac941dcf44200b4704bdbf0f669c033b15
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-02-19 22:22:01 -0500

    Update install-imagemagick-from-source.sh

--- shell/install-imagemagick-from-source.sh
+++ shell/install-imagemagick-from-source.sh
@@ -278,7 +278,7 @@
     make install
 
     # LDCONFIG MUST BE RUN NEXT IN ORDER TO UPDATE FILE CHANGES OR THE MAGICK COMMAND WILL NOT WORK
-    ldconfig /usr/local/lib >dev/null
+    ldconfig /usr/local/lib >/dev/null
 
     # CD BACK TO THE PARENT FOLDER
     cd ../ || exit 1
