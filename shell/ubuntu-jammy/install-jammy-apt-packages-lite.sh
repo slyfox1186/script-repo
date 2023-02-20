1cc56cf71d15a94635f9b9cf5b94c0b607f2452d
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-02-19 22:01:26 -0500

    Update and rename install-apt-packages-lite.sh to install-jammy-apt-packages-lite.sh

--- shell/ubuntu-jammy/install-jammy-apt-packages-lite.sh
+++ shell/ubuntu-jammy/install-jammy-apt-packages-lite.sh
@@ -56,22 +56,22 @@ installed() { return $(dpkg-query -W -f '${Status}\n' "${1}" 2>&1 | awk '/ok ins
 ## INSTALL PPA REPOSITORIES ##
 ##############################
 
-PPA_REPO='danielrichter2007/grub-customizer'
+ppa_repo='danielrichter2007/grub-customizer'
 
-if ! grep -q "^deb .*${PPA_REPO}" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
-    echo "Now installing ppa:${PPA_REPO}"
+if ! grep -q "^deb .*${ppa_repo}" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+    echo "Now installing ppa:${ppa_repo}"
     echo
-    add-apt-repository ppa:"${PPA_REPO}"
+    add-apt-repository ppa:"${ppa_repo}"
     echo
-    echo 'Updating APT packages.'
-    sudo apt-get update
+    echo 'Updating: APT packages'
+    echo '======================='
     echo
-    echo "The PPA: ${PPA_REPO} has been installed."
-    echo
-else
-    echo
-    echo 'All PPA Repositories have been installed.'
-    echo
+    if sudo apt-get update &> /dev/null; then 
+        echo
+        echo "The PPA Repo has been installed: ${ppa_repo}"
+        echo '=============================================='
+        echo
+    fi
 fi
 sleep 2
 clear
@@ -82,21 +82,21 @@ clear
 
 echo
 echo 'Installing: APT Packages'
-echo '=============================='
+echo '=========================='
 echo
 sleep 2
 
 PKGS=(alien aptitude aria2 autoconf autogen automake bat binutils bison build-essential ccdiff clang clang-tools cmake cmake-extras colordiff crossbuild-essential-amd64 curl dbus-x11 ddclient disktype dos2unix flex g++ gawk gcc-multilib gedit gedit-plugins gir1.2-gtksource-3.0 git git-all gnome-tweaks gnustep-gui-runtime golang gparted gperf grub-customizer gufw htop idn2 iftop libbz2-dev libdmalloc-dev libglib2.0-dev libgvc6 libheif-dev libjemalloc-dev liblilv-dev liblvm2-dev liblz-dev liblzma-dev liblzo2-dev libmimalloc2.0 libmimalloc-dev libncurses5 libncurses5-dev libnet-nslookup-perl libperl-dev libpstoedit-dev libraqm-dev libraw-dev librsvg2-dev librust-jemalloc-sys-dev librust-malloc-buf-dev libsdl-pango-dev libsox-dev libsox-fmt-all libsoxr-dev libsratom-dev libssl-dev libtalloc-dev libtbbmalloc2 libtool libtool-bin libzstd-dev libzzip-dev linux-source llvm llvm-dev lshw lv2-dev lzma-dev make man-db mono-devel nano net-tools network-manager npm openssh-client openssh-server openssl pcregrep php-cli php-curl php-intl php-sqlite3 pipenv python3 python3-idna python3-pip python3-talloc-dev rpm ruby-all-dev sqlite3 synaptic texinfo tk-dev tty-share unzip uuid-dev wget wget2-dev)
 
-for PKG in ${PKGS[@]}
+for pkg in ${pkgs[@]}
 do
-    if ! installed "${PKG}"; then
-        MISSING_PKGS+=" ${PKG}"
+    if ! installed "${pkg}"; then
+        missing_pkgs+=" ${pkg}"
     fi
 done
 
-if [ -n "${MISSING_PKGS}" ]; then
-    for i in "${MISSING_PKGS}"
+if [ -n "${missing_pkgs}" ]; then
+    for i in "${missing_pkgs}"
     do
         apt -y install ${i}
     done
