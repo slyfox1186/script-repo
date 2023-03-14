a2269b34a283d1103e7839e5ba70af1b5602efab
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-03-13 22:05:43 -0400

    Update install-media-players.sh

--- shell/install-media-players.sh
+++ shell/install-media-players.sh
@@ -38,67 +38,34 @@ echo '[4] GNOME Videos (Totem)'
 echo '[5] Bomi'
 echo '[6] Exit'
 echo
-read -p 'Your choices are (1 to 5): ' ANSWER
+read -p 'Your choices are (1 to 5): ' answer
 clear
 
-if [[ "${ANSWER}" -eq '1' ]]; then
+if [[ "${answer}" -eq '1' ]]; then
     echo 'Installing VLC Media Player'
     echo
-    if ! which snap &> /dev/null; then
-        echo 'Snap package installer is required to install VLC.'
-        echo
-        echo 'Do you want to do that now?'
-        echo
-        echo '[1] Yes'
-        echo '[2] Exit'
-        echo
-        read -p 'Your choices are (1 or 2): ' VLC_ANSWER
-        clear
-        if [[ "${VLC_ANSWER}" -eq '1' ]]; then
-            sudo apt update
-            sudo apt -y install snapd
-        else
-            echo
-            exit 0
-        fi
-        sudo snap install vlc
-        if ! which vlc &> /dev/null; then
-            fail_fn 'VLC Media Player'
-        fi
-fi
-elif [[ "${ANSWER}" -eq '2' ]]; then
+    sudo snap install vlc
+elif [[ "${answer}" -eq '2' ]]; then
     echo 'Installing Kodi Media Player'
     echo
-    sudo apt install software-properties-common
+    sudo apt -y install software-properties-common
     sudo add-apt-repository -y ppa:team-xbmc/ppa
     sudo apt -y install kodi
-    if [ ! -d '/usr/share/kodi' ]; then
-        fail_fn 'Kodi Media Player'
-    fi
-elif [[ "${ANSWER}" -eq '3' ]]; then
+elif [[ "${answer}" -eq '3' ]]; then
     echo 'Installing SMPlayer'
     echo
     sudo apt -y install smplayer
-    if ! which smplayer &> /dev/null; then
-        fail_fn 'SMPlayer'
-    fi
-elif [[ "${ANSWER}" -eq '4' ]]; then
+elif [[ "${answer}" -eq '4' ]]; then
     echo 'Installing SMPlayer'
     echo
     sudo apt -y install totem
-    if ! which totem &> /dev/null; then
-        fail_fn 'GNOME Videos (Totem)'
-    fi
-elif [[ "${ANSWER}" -eq '5' ]]; then
+elif [[ "${answer}" -eq '5' ]]; then
     echo 'Installing Bomi'
     echo
     sudo add-apt-repository ppa:nemonein/bomi
     sudo apt update
     sudo apt -y install bomi
-    if ! which bomi &> /dev/null; then
-        fail_fn 'Bomi'
-    fi
-elif [[ "${ANSWER}" -eq '6' ]]; then
+elif [[ "${answer}" -eq '6' ]]; then
     echo
     exit 0
 else
@@ -110,5 +77,4 @@ fi
 echo
 echo 'Installation completed.'
 echo
-sudo rm "${0}"
 exit 0
