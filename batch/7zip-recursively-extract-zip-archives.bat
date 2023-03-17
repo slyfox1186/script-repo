c327eac009b18037900603736e7f51d5542e69f2
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-03-17 10:21:54 -0500

    Create 7zip-recursively-extract-zip-archives.bat

---
+++ batch/7zip-recursively-extract-zip-archives.bat
@@ -0,0 +1,28 @@
+@ECHO OFF
+SETLOCAL ENABLEEXTENSIONS
+COLOR 0A
+TITLE Recursively extract zip archives
+
+:--------------------------------------------------------------------------------
+
+PUSHD "%~dp0"
+IF NOT "%1"=="MAX" START /MAX CMD /D /C %0 MAX & GOTO :EOF
+
+:--------------------------------------------------------------------------------
+
+REM YOU CAN SET THE FULL PATH OF 7z.exe HERE IF YOU WISH.
+REM IF YOU DECIDE TO USE THIS VARIABLE YOU MUST REPLACE 7z.exe
+REM IN THE BELOW COMMAND WITH %SZIP%
+
+SET SZIP="%ProgramFiles%\7-Zip\7z.exe"
+
+:--------------------------------------------------------------------------------
+
+REM RECURSIVELY SEARCH FOR ALL ZIP FILES AND EXTRACT THEIR
+REM CONTENTS TO A FOLDER WITH THE SAME NAME AS THE ARCHIVE.
+REM UNCOMMENT DEL /Q "%%G" BELOW TO DELETE THE ARCHIVES AFTER EXTRACTION
+
+FOR /F "USEBACKQ TOKENS=*" %%G IN (`DIR /S /B *.zip`) DO (
+    %SZIP% x -y "%%G" -o"%%~dpnG"
+    %windir%\System32\recycle.exe -f "%%G"
+)
