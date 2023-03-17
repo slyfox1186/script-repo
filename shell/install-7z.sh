00aac28323354d28d6af8f98d224cbfcc229ee10
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-03-17 10:48:49 -0500

    Update install-7z.sh

--- shell/install-7z.sh
+++ shell/install-7z.sh
@@ -22,10 +22,10 @@ if [ "${EUID}" -gt '0' ]; then
 fi
 
 # SET VARIABLES
-VERSION='7z2201'
-TAR_FILE="${VERSION}.tar.xz"
-DIR='7z'
-OUTPUT_FILE='/usr/bin/7z'
+version='7z2201'
+tar_file="${version}.tar.xz"
+target_dir='7z'
+output_file='/usr/bin/7z'
 
 ##
 ## Create Functions
@@ -49,49 +49,49 @@ echo '[3] ARM x64'
 echo '[4] ARM x86'
 echo '[5] Exit'
 echo
-read -p 'Your choices are (1 to 5): ' OS_TYPE
+read -p 'Your choices are (1 to 5): ' os_type
 clear
 
 # Parse user input
-if [ "${OS_TYPE}" -eq '1' ]; then URL='linux-x64.tar.xz'
-elif [ "${OS_TYPE}" -eq '2' ]; then URL='linux-x86.tar.xz'
-elif [ "${OS_TYPE}" -eq '3' ]; then URL='linux-arm64.tar.xz'
-elif [ "${OS_TYPE}" -eq '4' ]; then URL='linux-arm.tar.xz'
-elif [ "${OS_TYPE}" -eq '5' ]; then exit
+if [ "${os_type}" -eq '1' ]; then url='linux-x64.tar.xz'
+elif [ "${os_type}" -eq '2' ]; then url='linux-x86.tar.xz'
+elif [ "${os_type}" -eq '3' ]; then url='linux-arm64.tar.xz'
+elif [ "${os_type}" -eq '4' ]; then url='linux-arm.tar.xz'
+elif [ "${os_type}" -eq '5' ]; then exit
 fi
 
 # DOWNLOAD THE TAR FILE IF MISSING
-if [ ! -f "${TAR_FILE}" ]; then
-    wget --show-progress -cqO "${TAR_FILE}" "https://www.7-zip.org/a/${VERSION}-${URL}"
+if [ ! -f "${tar_file}" ]; then
+    wget --show-progress -cqO "${tar_file}" "https://www.7-zip.org/a/${version}-${url}"
 fi
 
 # DELETE ANY FILES LEFTOVER FROM PRIOR RUNS
-if [ -d "${DIR}" ]; then rm -fr "${DIR}"; fi
+if [ -d "${target_dir}" ]; then rm -fr "${target_dir}"; fi
 
 # CREATE AN OUTPUT FOLDER FOR THE TAR COMMAND
-mkdir -p "${DIR}"
+mkdir -p "${target_dir}"
 
 # EXTRACT FILES INTO DIRECTORY '7Z'
-if ! tar -xf "${TAR_FILE}" -C "${DIR}"; then
+if ! tar -xf "${tar_file}" -C "${target_dir}"; then
     fail_fn 'The script was unable to find the download file.'
 fi
 
 # CD INTO DIRECTORY
-if ! cd "${DIR}"; then
-    fail_fn "The script was unable to cd into '${DIR}'."
+if ! cd "${target_dir}"; then
+    fail_fn "The script was unable to cd into '${target_dir}'."
 fi
 
 # COPY THE FILE TO ITS DESTINATION OR THROW AN ERROR IF THE COPYING OF THE FILE FAILS
-if ! cp -f '7zzs' "${OUTPUT_FILE}"; then
-    fail_fn "The script was unable to copy the static file '7zzs' to '${OUTPUT_FILE}'"
+if ! cp -f '7zzs' "${output_file}"; then
+    fail_fn "The script was unable to copy the static file '7zzs' to '${output_file}'"
 fi
 
 # RUN THE COMMAND '7Z' TO SHOW ITS OUTPUT AND CONFIRM THAT EVERTHING WORKS AS EXPECTED
 clear
 echo '7-zip has been updated to:'
-"${OUTPUT_FILE}" | head -n 2 | cut -d " " -f3 | awk 'NF' | xargs printf "v%s\n" "${@}"
+"${output_file}" | head -n 2 | cut -d " " -f3 | awk 'NF' | xargs printf "v%s\n" "${@}"
 echo
 
 # REMOVE LEFTOVER FILES
 cd ../ || exit 1
-rm -fr "${TAR_FILE}" "${DIR}" "${0}"
+rm -fr "${tar_file}" "${target_dir}" "${0}"
