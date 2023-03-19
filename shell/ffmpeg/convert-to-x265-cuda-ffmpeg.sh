021d5752be7461dbe271d72621483ea0d07a206c
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-03-19 18:55:33 -0400

    Update convert-to-x265-cuda-ffmpeg.sh

--- shell/ffmpeg/convert-to-x265-cuda-ffmpeg.sh
+++ shell/ffmpeg/convert-to-x265-cuda-ffmpeg.sh
@@ -7,12 +7,6 @@ if ! which bc &> /dev/null; then
     sudo apt -y install bc
 fi
 
-# INSTALL FFPB IF NOT INSTALLED
-get_pkg="$(pip list | grep -Eo '^ffpb')"
-if [ -z "${get_pkg}" ]; then
-    pip install ffpb
-fi
-
 # DELETE LEFTOVER FILES
 del_this="$(du -ah | grep -Eo '\..*\(.*\)\.mp4')"
 
@@ -21,7 +15,7 @@ if [ -n "${del_this:2}" ]; then
 fi
 
 # CAPTURE THE VIDEO WITHOUT (X265).MP4 AS THE ENDING
-video="$(find . -type f -name *.mp4 -exec echo '{}' +)"
+video="$(find . -type f -name '*.mp4' -exec echo '{}' +)"
 
 for v in "${video:2}"
 do
@@ -39,13 +33,12 @@ file_out="${file%.*} (x265).mp4"
 
 # TRIM THE STRINGS
 trim_back="${mr::-11}"
-trim_front="${trim_back:3}"
 
 # GETS THE INPUT VIDEOS MAX DATARATE AND APPLIES LOGIC TO DETERMINE BITRATE, BUFSIZE, AND MAXRATE VARIABLES
 trim_back="$(bc <<< "scale=2 ; ${trim_back} * 1000")"
 br="$(bc <<< "scale=2 ; ${trim_back} / 2")"
 bitrate="${br::-3}"
-maxrate="$(( ${bitrate} * 2 ))"
+maxrate="$(( bitrate * 2 ))"
 bc_bf="$(bc <<< "scale=2 ; ${br} * 2")"
 bufsize="${bc_bf::-3}"
 length="$(( ${ln::-7} / 60 ))"
@@ -54,54 +47,49 @@ length+=" Minutes"
 # ECHO THE STORED VARIABLES THAT CONTAIN THE VIDEOS STATS
 echo ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 echo
-echo "Input File:      ${file}"
+echo "INPUT FILE:      ${file}"
 echo
-echo "Output File:     ${file_out}"
+echo "OUTPUT FILE:     ${file_out}"
 echo
-echo "Dimensions:      ${vw}x${vh}"
-echo "Aspect Ratio:    ${ar}"
+echo "DIMENSIONS:      ${vw}x${vh}"
+echo "ASPECT RATIO:    ${ar}"
 echo
-echo "Maxrate:         ${maxrate}"k
-echo "Bufsize:         ${bufsize}"k
-echo "Bitrate:         ${bitrate}"k
+echo "MAXRATE:         ${maxrate}"k
+echo "BUFSIZE:         ${bufsize}"k
+echo "BITRATE:         ${bitrate}"k
 echo
-echo "Length:          ${length}"
+echo "LENGTH:          ${length}"
 echo
 echo ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 echo
 
 # EXECUTE FFMPEG
 
-ffmpeg \
-    -y \
-    -threads '0' \
-    -hide_banner \
-    -hwaccel_output_format 'cuda' \
-    -i "${file}" \
-    -pix_fmt 'p010le' \
-    -movflags 'frag_keyframe+empty_moov' \
-    -c:v 'hevc_nvenc' \
-    -preset:v 'medium' \
-    -profile:v 'main' \
-    -rc:v 'vbr' \
-    -b:v "${bitrate}"k \
-    -bufsize:v "${bufsize}"k \
-    -bf:v '3' \
-    -b_ref_mode:v 'middle' \
-    -qmin:v '0' \
-    -qmax:v '99' \
-    -temporal-aq:v '1' \
-    -rc-lookahead:v '20' \
-    -i_qfactor:v '0.75' \
-    -b_qfactor:v '1.1' \
-    -fps_mode vfr \
-    -c:a fdk_aac \
-    -qmin:a 1 \
-    -qmax:a 4 \
-    "${file_out}"
-
-if [ "${?}" -lt '1' ]; then
-    google_speech "Video conversion completed" 2>/dev/null
+if ! ffmpeg \
+        -y \
+        -threads '0' \
+        -hide_banner \
+        -hwaccel_output_format 'cuda' \
+        -i "${file}" \
+        -pix_fmt 'p010le' \
+        -movflags 'frag_keyframe+empty_moov' \
+        -c:v 'hevc_nvenc' \
+        -preset:v 'medium' \
+        -profile:v 'main' \
+        -rc:v 'vbr' \
+        -b:v "${bitrate}"k \
+        -bufsize:v "${bufsize}"k \
+        -bf:v '3' \
+        -b_ref_mode:v 'middle' \
+        -qmin:v '0' \
+        -qmax:v '99' \
+        -temporal-aq:v '1' \
+        -rc-lookahead:v '20' \
+        -i_qfactor:v '0.75' \
+        -b_qfactor:v '1.1' \
+        -c:a copy \
+        "${file_out}"; then
+        google_speech "Video conversion failed" 2>/dev/null
 else
-    google_speech "Video conversion failed" 2>/dev/null
+    google_speech "Video conversion completed" 2>/dev/null
 fi
