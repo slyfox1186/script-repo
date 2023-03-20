c0450b86478cbcb0c258f5f77eef62d6ba44e051
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-03-19 22:44:16 -0500

    Update magick-helper.sh

--- shell/imagemagick/magick-helper.sh
+++ shell/imagemagick/magick-helper.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-# Helper script to download and run the build-imagemagick.
+# Helper script to download and run build-magick.sh
 
 clear
 
-# verify the script does not have root access before continuing
+# Verify the script has root access before continuing
 if [ "${EUID}" -ne '0' ]; then
     exec sudo bash "${0}" "${@}"
 fi
 
-echo -e "\$ imagemagick-build-script downloader v1.0\\n"
+echo -e "Imagemagick Build Script Downloader v1.0\\n"
 
-echo -e "\$ creating the build directory\\n"
+echo -e "Creating the build directory\\n"
 
-if ! mkdir "${PWD}/imagick-build"; then
-    printf '\nfailed to create dir: %s' "${PWD}/imagick-build"
+if ! mkdir "${PWD}/build-magick"; then
+    printf '\nFailure to create the build directory: %s' "${PWD}/build-magick"
     echo
     exit 1
 fi
 
-cd "${PWD}/imagick-build" || exit 1
+# cd into the build directory
+cd "${PWD}/build-magick" || exit 1
 
-echo -e "\$ download and execute the build script\\n"
+echo -e "Downloading and executing the build script\\n"
 
 bash <(curl -sSL https://build-magick.optimizethis.net) --build
