e638885f3ddac61ea9b45727cb482767be0d187c
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-03-19 23:03:33 -0500

    Update magick.sh

--- shell/imagemagick/magick.sh
+++ shell/imagemagick/magick.sh
@@ -24,4 +24,4 @@ cd "${PWD}/build-magick" || exit 1
 
 echo -e "Downloading and executing the build script\\n"
 
-sudo bash <(curl -sSL https://build-magick.optimizethis.net) --build
+bash <(curl -sSL https://build-magick.optimizethis.net) --build
