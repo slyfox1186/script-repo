641db71267bf7ebbc1985179a93bdcc1894c48ac
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-04-13 23:02:22 -0400

    Update jammy-pkgs.sh

--- shell/ubuntu-jammy/installers/jammy-pkgs.sh
+++ shell/ubuntu-jammy/installers/jammy-pkgs.sh
@@ -43,22 +43,22 @@ exit_fn()
 
 pkgs_fn()
 {
-    pkgs=(alien aptitude aria2 autoconf autogen automake bat binutils bison build-essential \
-          ccdiff checkinstall clang clang-tools cmake cmake-extras colordiff curl cvs dbus \
-          dbus-x11 dconf-editor ddclient debhelper devscripts dh-make disktype dos2unix \
-          dpkg-dev exfat-fuse exfatprogs f2fs-tools fakeroot flex g++ gawk gcc gedit gedit-plugins \
-          gir1.2-gtksource-3.0 git git-all git-buildpackage gnome-tweaks gnustep-gui-runtime golang \
-          gparted gperf grub-customizer gufw hfsplus hfsprogs hfsutils htop idn2 iftop iw jfsutils jq \
-          libbz2-dev libdmalloc-dev libglib2.0-dev libgvc6 libheif-dev libjemalloc-dev liblz-dev liblzma-dev \
-          liblzo2-dev libmimalloc-dev libncurses5-dev libnet-nslookup-perl libnuma-dev libperl-dev \
-          libpstoedit-dev libraqm-dev libraw-dev librsvg2-dev librust-jemalloc-sys-dev librust-malloc-buf-dev \
-          libsdl-pango-dev libsox-dev libsoxr-dev libssl-dev libtalloc-dev libtbbmalloc2 libtool libtool-bin \
-          libzstd-dev libzzip-dev "linux-headers-$(uname -r)" linux-source lm-sensors lshw lvm2 lzma-dev make \
-          man-db mercurial mono-devel nano netplan.io net-tools network-manager nilfs-tools npm ntfs2btrfs ntfs-3g \
-          openssh-client openssh-server openssl patchutils pbuilder pcregrep php-cli php-sqlite3 pipenv plocate \
-          ppa-purge pristine-tar psensor python3 python3-pip quilt reiser4progs reiserfsprogs rpm ruby-all-dev \
-          samba shellcheck smbclient sox sqlite3 subversion synaptic texinfo tofrodos trash-cli tty-share udftools \
-          unzip uuid-dev wget xclip xfsprogs xsel yasm)
+    pkgs=(alien apt-config-auto-update apt-file aptitude aria2 apt-config-auto-update apt-file \
+          autoconf autogen automake bat binutils bison build-essential ccdiff checkinstall \
+          clang clang-tools cmake cmake-extras colordiff curl cvs dbus dbus-x11 dconf-editor \
+          ddclient debhelper devscripts dh-make disktype dos2unix fakeroot flex g++ gawk gcc \
+          gedit gedit-plugins dpkg-dev exfat-fuse exfatprogs f2fs-tools gir1.2-gtksource-3.0 git \
+          git-all git-buildpackage gnome-tweaks gnustep-gui-runtime golang gparted gperf grub-customizer \
+          gufw hfsplus hfsprogs hfsutils htop idn2 iftop iw jfsutils jq libbz2-dev libdmalloc-dev \
+          libglib2.0-dev libgvc6 libheif-dev libjemalloc-dev liblz-dev liblzma-dev liblzo2-dev libmimalloc-dev \
+          libncurses5-dev libnet-nslookup-perl libnuma-dev libperl-dev libpstoedit-dev libraqm-dev libraw-dev \
+          librsvg2-dev librust-jemalloc-sys-dev librust-malloc-buf-dev libsdl-pango-dev libsox-dev libsoxr-dev \
+          libssl-dev libtalloc-dev libtbbmalloc2 libtool libtool-bin libzstd-dev libzzip-dev "linux-headers-$(uname -r)" \
+          linux-source lm-sensors lshw lvm2 lzma-dev make man-db mercurial mono-devel nano netplan.io net-tools \
+          network-manager nilfs-tools npm ntfs2btrfs ntfs-3g openssh-client openssh-server openssl patchutils \
+          pbuilder pcregrep php-cli php-sqlite3 pipenv plocate ppa-purge pristine-tar psensor python3 python3-pip \
+          quilt reiser4progs reiserfsprogs rpm ruby-all-dev samba shellcheck smbclient sox sqlite3 subversion \
+          synaptic texinfo tofrodos trash-cli tty-share udftools unzip uuid-dev wget xclip xfsprogs xsel yasm)
 
     for pkg in ${pkgs[@]}
     do
