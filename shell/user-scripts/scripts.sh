9bddb276cbc90c3c10c73f97cccbad42345b9674
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-04-14 08:31:13 -0500

    Update scripts.sh

--- shell/user-scripts/scripts.sh
+++ shell/user-scripts/scripts.sh
@@ -2,21 +2,36 @@
 
 clear
 
-# Download the user scripts from github
-wget -qN - -i 'https://raw.githubusercontent.com/slyfox1186/script-repo/main/shell/ubuntu-jammy/user-scripts/scripts.txt'
+# make a tmporary random directory
+random_dir="/tmp/$(mktemp --directory)"
+
+if [ ! -d "$random_dir" ]; then
+    clear
+    printf "%s\n\n%s\n\n%s\n\n" \
+        "The temporary directory was not found: $random_dir" \
+        'Please create a support ticket.' \
+        'https://github.com/slyfox1186/script-repo/issues'
+    exit 1
+fi
+
+# Change the working directory into the random directory to avoid deleting unintended files
+cd "$random_dir"
+
+# Download the user scripts from GitHub
+wget -qN - -i 'https://raw.githubusercontent.com/slyfox1186/script-repo/main/shell/user-scripts/scripts.txt'
 
 # Delete all files except those that start with a '.' or end with '.sh'
 find . ! \( -name '\.*' -o -name '*.sh' \) -type f -delete 2>/dev/null
 
-# define variables
-scripts=(.bash_aliases .bash_functions .bashrc)
+# define script array
+scriptArray=(.bash_aliases .bash_functions .bashrc)
 
-# If the shell scripts exist, move them to the users home directory
-for script in ${scripts[@]}
+# If the scripts exist, move each one to the users home directory
+for script in ${scriptArray[@]}
 do
-    if [ -f "$PWD/$script" ]; then
-        mv -f "$PWD/$script" "$HOME"
-        if [ -f "$PWD/$HOME/$script" ]; then
+    if [ -f "$script" ]; then
+        mv -f "$script" "$HOME"
+        if [ -f "$HOME/$script" ]; then
             sudo chown "$USER":"$USER" "$HOME/$script"
         fi
     else
@@ -29,18 +44,18 @@ do
     fi
 done
 
-# execute all scripts in the pihole-regex folder
-for files in ${scripts[@]}
+# Open each script that is now in each user's home folder with an editor
+for script in "$HOME"/${scripts[@]}
 do
-    if [ -f "$HOME/$files" ]; then
+    if [ -f "$script" ]; then
         if which gedit &>/dev/null; then
-            gedit "$HOME/$files"
+            gedit "$script"
         elif which nano &>/dev/null; then
-            nano "$HOME/$files"
+            nano "$script"
         elif which vim &>/dev/null; then
-            vim "$HOME/$files"
+            vim "$script"
         else
-            vi "$files"
+            vi "$script"
         fi
     fi
 done
