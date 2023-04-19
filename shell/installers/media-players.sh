012f0b9ac8edac9e62d33e25a6d7e2d12a0013cf
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-04-19 17:30:02 -0500

    Update media-players.sh

--- shell/installers/media-players.sh
+++ shell/installers/media-players.sh
@@ -18,63 +18,105 @@ clear
 ## Functions
 ##
 
-fail_fn()
+exit_fn()
 {
-    clear
-    echo "${1} was not installed."
     echo
-    echo 'Please put in a support ticket.'
+    echo 'Please star this repository to show your support!'
     echo
-    sudo rm "${0}"
+    echo 'https://github.com/slyfox1186/script-repo/'
+    echo
+    sudo rm "$0"
     exit 1
 }
 
-echo 'Please choose a media player to install'
-echo
-echo '[1] VLC'
-echo '[2] Kodi'
-echo '[3] SMPlayer'
-echo '[4] GNOME Videos (Totem)'
-echo '[5] Bomi'
-echo '[6] Exit'
-echo
-read -p 'Your choices are (1 to 5): ' answer
-clear
-
-if [[ "${answer}" -eq '1' ]]; then
-    echo 'Installing VLC Media Player'
+fail_fn()
+{
+    clear
+    echo "APT was unable to install $1, possibly because it is already installed."
     echo
-    sudo snap install vlc
-elif [[ "${answer}" -eq '2' ]]; then
-    echo 'Installing Kodi Media Player'
+    echo 'If you think this is a bug please fill out a report.'
     echo
-    sudo apt -y install software-properties-common
-    sudo add-apt-repository -y ppa:team-xbmc/ppa
-    sudo apt -y install kodi
-elif [[ "${answer}" -eq '3' ]]; then
-    echo 'Installing SMPlayer'
-    echo
-    sudo apt -y install smplayer
-elif [[ "${answer}" -eq '4' ]]; then
-    echo 'Installing SMPlayer'
-    echo
-    sudo apt -y install totem
-elif [[ "${answer}" -eq '5' ]]; then
-    echo 'Installing Bomi'
-    echo
-    sudo add-apt-repository ppa:nemonein/bomi
-    sudo apt update
-    sudo apt -y install bomi
-elif [[ "${answer}" -eq '6' ]]; then
-    echo
-    exit 0
-else
-    echo 'Bad user input: Run the script again.'
+    echo 'https://github.com/slyfox1186/script-repo/issues'
     echo
     exit 1
-fi
+}
 
-echo
-echo 'Installation completed.'
-echo
-exit 0
+success_fn()
+{
+    clear
+    echo "$1 was succesfully installed."
+    exit_fn
+}
+
+printf "%s\n\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n" \
+    'Please choose a media player to install' \
+    '[1] VLC' \
+    '[2] Kodi' \
+    '[3] SMPlayer' \
+    '[4] GNOME Videos (Totem)' \
+    '[5] Bomi' \
+    '[6] Exit'
+
+read -p 'Your choices are (1 to 6): ' media_selection
+clear
+
+case "$media_selection" in 
+    1)
+        echo 'Installing VLC Media Player'
+        echo
+        if sudo snap install vlc; then
+            success_fn 'VLC Media Player'
+        else
+            fail_fn 'VLC Media Player'
+        fi
+        ;;
+    2)
+        echo 'Installing Kodi Media Player'
+        echo
+        sudo apt -y install software-properties-common 2>/dev/null
+        sudo add-apt-repository -y ppa:team-xbmc/ppa 2>/dev/null
+        if sudo apt -y install kodi; then
+             success_fn 'Kodi Media Player'
+        else
+            fail_fn 'Kodi Media Player'
+        fi
+        ;;
+    3)
+        echo 'Installing SMPlayer'
+        echo
+        if sudo apt -y install smplayer; then
+             success_fn 'SMPlayer'
+        else
+            fail_fn 'SMPlayer'
+        fi
+        ;;
+    4)
+        echo 'GNOME Videos (Totem)'
+        echo
+        if sudo apt -y install totem; then
+             success_fn 'GNOME Videos (Totem)'
+        else
+            fail_fn 'GNOME Videos (Totem)'
+        fi
+        ;;
+    5)
+        echo 'Installing Bomi'
+        echo
+        sudo add-apt-repository ppa:nemonein/bomi 2>/dev/null
+        sudo apt update 2>/dev/null
+        if sudo apt -y install bomi; then
+             success_fn 'Bomi'
+        else
+            fail_fn 'Bomi'
+        fi
+        ;;
+    6)
+        exit_fn
+        ;;
+
+    *)
+        echo 'Bad user input: Run the script again.'
+        echo
+        exit 1
+        ;;
+esac
