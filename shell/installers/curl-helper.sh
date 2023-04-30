8032b68729a15b07cec2ba2260ac52fd27d824ef
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-04-30 19:03:53 -0400

    Update curl-helper.sh

--- shell/installers/curl-helper.sh
+++ shell/installers/curl-helper.sh
@@ -2,8 +2,12 @@
 
 clear
 
+if [ -d 'script-repo' ]; then
+    sudo rm -fr 'script-repo'
+fi
+
 git clone -n --depth=1 --filter=tree:0 \
-  https://github.com/slyfox1186/script-repo
+  https://github.com/slyfox1186/script-repo script-repo
 cd script-repo || exit 1
 git sparse-checkout set --no-cone shell/installers/build-curl
 git checkout
