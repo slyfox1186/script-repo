79635e2d0a0590ab235c6c162cddc4540fb405dd
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-05-13 13:47:28 -0400

    Update jammy-pkgs.sh

--- shell/ubuntu-jammy/jammy-pkgs.sh
+++ shell/ubuntu-jammy/jammy-pkgs.sh
@@ -18,10 +18,22 @@
 clear
 
 # Verify the script has root access before continuing
-if [ $EUID -ne 0 ]; then
-    echo 'You must run this script as root/sudo.'
+if [ "$EUID" -eq '0' ]; then
+    echo 'You must run this script WITHOUT root/sudo.'
     echo
     exit 1
+fi
+
+##
+## DEFINE THE APT PACKAGE INSTALLER TO BE USED  
+##
+
+if which 'apt-fast' &>/dev/null; then
+    exec_apt='apt-fast'
+elif which 'aptitude' &>/dev/null; then
+    exec_apt='aptitude'
+else
+    exec_apt='apt'
 fi
 
 ######################
@@ -32,42 +44,32 @@ installed() { return $(dpkg-query -W -f '${Status}\n' "${1}" 2>&1 | awk '/ok ins
 
 exit_fn()
 {
-    printf "\n%s\n%s\n%s\n%s\n\n" \
-    "$1" \
-    "$2" \
-    '[i] Make sure to star this repository to show your support!' \
-    '[i] https://github.com/slyfox1186/script-repo/'
-    # rm "${0}"
+    printf "\n%s\n\n%s\n\n" \
+        '[i] Make sure to star this repository to show your support!' \
+        '[i] https://github.com/slyfox1186/script-repo/'
     exit 0
 }
 
-deb_fn()
-{
-    curl -fsS 'https://dlang.org/install.sh' | bash -s dmd
-}
+install_other_fn() { curl -Ssf 'https://dlang.org/install.sh' | bash -s dmd; }
 
-pkgs_fn()
+install_apt_fn()
 {
-    pkgs=(alien apt-file aptitude aria2 autoconf autogen automake bat binutils \
-          bison build-essential ccache ccdiff checkinstall clang clang-tools cmake \
-          cmake-extras cmake-qt-gui colordiff cpu-checker curl cvs dbus dbus-x11 \
-          dconf-editor ddclient debhelper devscripts dh-make disktype dos2unix dpkg-dev \
-          exfat-fuse exfatprogs f2fs-tools fakeroot flatpak flex gcc gcc-12 g++ g++-12 \
-          gawk gcc gedit gedit-plugins gh gir1.2-gtksource-3.0 git git-all git-buildpackage \
-          git-hub gnome-shell-extension-manager gnome-tweaks gnustep-gui-runtime golang gparted \
-          gperf grub-customizer gufw hfsplus hfsprogs hfsutils htop idn2 iftop iw jfsutils \
-          jq libbz2-dev libdmalloc-dev libglib2.0-dev libgvc6 libheif-dev libjemalloc-dev liblz-dev \
-          libasan6-amd64-cross liblzma-dev liblzo2-dev libboost-all-dev libmimalloc-dev libncurses5-dev \
-          libnet-nslookup-perl libnuma-dev libperl-dev libpstoedit-dev libraqm-dev libraw-dev \
-          librsvg2-dev librust-jemalloc-sys-dev librust-malloc-buf-dev libsdl-pango-dev libsox-dev \
-          libsoxr-dev libssl-dev libtalloc-dev libtbbmalloc2 libtool libtool-bin libzstd-dev \
-          libzzip-dev linux-headers-generic linux-source lm-sensors lsb-core lshw lvm2 lzma-dev \
-          mainline make man-db mercurial meson mono-devel nano netplan.io net-tools network-manager \
-          nilfs-tools npm ntfs2btrfs ntfs-3g nvme-cli openssh-client openssh-server openssl patchutils \
-          pbuilder pcregrep php-cli php-sqlite3 pipenv plank ppa-purge preload pristine-tar psensor python3 \
-          python3-pip quilt reiser4progs reiserfsprogs rpm ruby-all-dev samba shellcheck smbclient \
-          sox sqlite3 subversion synaptic texinfo tilix tofrodos trash-cli tty-share udftools unzip \
-          uuid-dev wget xclip xfsprogs xsel yasm)
+    pkgs=(alien apt-file aptitude aria2 autoconf autogen automake bat binutils bison build-essential ccache \
+          ccdiff checkinstall clang cmake cmake-extras cmake-qt-gui colordiff cpu-checker curl cvs dbus-x11 \
+          dconf-editor ddclient debhelper devscripts dh-make disktype dos2unix dpkg-dev exfat-fuse exfatprogs \
+          f2fs-tools fakeroot flatpak flex gawk gcc g++ gcc-12 g++-12 gedit gedit-plugins gh gir1.2-gtksource-3.0 \
+          git-all git-buildpackage gnome-shell-extension-manager gnome-tweaks gnustep-gui-runtime golang gparted \
+          gperf grub-customizer gufw hfsplus hfsprogs hfsutils htop idn2 iftop iw jfsutils jq libasan6-amd64-cross \
+          libboost-all-dev libbz2-dev libdmalloc-dev libglib2.0-dev libgvc6 libheif-dev libjemalloc-dev liblz-dev \
+          liblzma-dev liblzo2-dev libmimalloc-dev libncurses5-dev libnet-nslookup-perl libnuma-dev libperl-dev \
+          libpstoedit-dev libraqm-dev libraw-dev librsvg2-dev librust-jemalloc-sys-dev librust-malloc-buf-dev \
+          libsdl-pango-dev libsox-dev libssl-dev libtalloc-dev libtbbmalloc2 libtool libtool-bin libzstd-dev \
+          libzzip-dev linux-headers-generic linux-source lm-sensors lsb-core lshw lvm2 lzma-dev mainline make \
+          man-db mercurial meson mono-devel nano nasm netplan.io net-tools network-manager nilfs-tools \
+          ninja-build npm ntfs2btrfs ntfs-3g nvme-cli openssh-client openssh-server openssl patchutils pbuilder \
+          pcregrep pipenv plank ppa-purge preload pristine-tar psensor python3-pip quilt reiser4progs reiserfsprogs \
+          rpm ruby-all-dev samba shellcheck smbclient sox sqlite3 subversion synaptic texinfo tofrodos trash-cli \
+          tty-share udftools unzip uuid-dev wget xclip xfsprogs xsel yasm)
 
     for pkg in ${pkgs[@]}
     do
@@ -79,7 +81,7 @@ pkgs_fn()
     if [ -n "$missing_pkgs-" ]; then
         for i in "$missing_pkgs"
         do
-            apt -y install $i
+            $exec_apt -y install $i
         done
         printf "\n%s\n\n%s\n\n" \
             'The required packages were successfully installed.' \
@@ -90,48 +92,55 @@ pkgs_fn()
     fi
 }
 
-ppa_fn()
+install_ppa_fn()
 {
     local i missing_pkgs pkg pkgs ppa_repo
 
     if [ ! -d '/etc/apt/sources.list.d' ]; then
-        mkdir -p '/etc/apt/sources.list.d'
+        sudo mkdir -p '/etc/apt/sources.list.d'
     fi
 
-    ppa_repo='cappelikan/ppa danielrichter2007/grub-customizer git-core/ppa'
+    ppa_repo='apt-fast/stable cappelikan/ppa danielrichter2007/grub-customizer git-core/ppa'
 
     for pkg in ${ppa_repo[@]}
     do
         ppa_list="$(grep -Eo "^deb .*$pkg" /etc/apt/sources.list /etc/apt/sources.list.d/*)"
         if [ -z "$ppa_list" ]; then
-            add-apt-repository -y ppa:$pkg
+            sudo add-apt-repository -y ppa:$pkg
             for i in "$pkg"
             do
                 case "$i" in
-                        'danielrichter2007/grub-customizer')        apt_ppa+='grub-customizer';;
-                        'git')                                      apt_ppa+=' git';;
+                        'apt-fast/stable')                       apt_pkg='apt-fast';;
+                        'cappelikan/ppa')                        apt_pkg+=' mainline';;
+                        'danielrichter2007/grub-customizer')     apt_pkg+=' grub-customizer';;
+                        'git-core/ppa')                          apt_pkg+=' git';;
                  esac
             done
         fi
     done
 
-    if [ -n "$apt_ppa" ]; then
-        apt update
-        apt -y install $apt_ppa
-        exit_msg1='[i] Any missing ppa repositories were installed'
-    else
-        exit_msg1='[i] The ppa repositories are already installed'
+    if [ -n "$apt_pkg" ]; then
+        sudo $exec_apt update
+        if sudo $exec_apt -y install $apt_pkg; then
+            printf "%s\n\n" \
+                '$ Any missing ppa repositories were installed'
+                sleep 2
+        else
+            printf "%s\n\n" \
+                '$ The ppa repositories are already installed'
+                sleep 2
+        fi
     fi
 }
 
 # install missing ppa repositories
-ppa_fn
+install_ppa_fn
 
 # install missing apt packages
-pkgs_fn
+install_apt_fn
 
 # intall deb files
-deb_fn
+install_other_fn
 
 # show exit message
 exit_fn
