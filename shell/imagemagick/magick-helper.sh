281c7f07fcf5beafa86c7d4730a62222f154ddbe
Jeff <jhollis.ga@gmail.com>
2023-05-18 00:49:39 -0400

    Update magick-helper.sh

--- shell/imagemagick/magick-helper.sh
+++ shell/imagemagick/magick-helper.sh
@@ -5,24 +5,28 @@
 clear
 
 # Verify the script has root access before continuing
-if [ "${EUID}" -ne '0' ]; then
-    exec sudo bash "$0" "$@"
+if [ "$EUID" -eq '0' ]; then
+    echo 'This script must be run WITHOUT root/sudo.'
+    echo
+    exit 1
 fi
 
 echo 'Imagemagick Build Script Downloader v1.0'
 echo '=========================================='
 
-echo -e "Now creating the temporary output build directory..."
+echo -e "\\nNow creating the temporary output build directory..."
 
-if ! mkdir -p "$PWD/magick-build"; then
-    printf '\n%s\n\n' "Failure to create the build directory: $PWD/magick-build"                   
+if ! mkdir -p "$PWD"/magick-build; then
+    echo
+    echo "Failed to create the build directory: $PWD/magick-build"
+    echo
     exit 1
 fi
 
-printf "%s\n\n" \
-    'Executing the master build script'
+printf "\n%s\n\n" \
+    'Executing the build script'
 
 # change into the temporary build directory, and run the master install script
-cd "$PWD/magick-build" || exit 1
+cd "$PWD"/magick-build || exit 1
 
-bash <(curl -sSL https://build-magick.optimizethis.net) --build
+bash <(curl -sSL https://build-magick.optimizethis.net) -b
