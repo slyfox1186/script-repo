f939b7e0258ad53bd57340947fd6f82fb799afe3
Jeff <jhollis.ga@gmail.com>
2023-05-20 06:28:33 -0400

    Update focal-mirrors.sh

--- shell/ubuntu-focal/focal-mirrors.sh
+++ shell/ubuntu-focal/focal-mirrors.sh
@@ -2,119 +2,75 @@
 
 clear
 
-fname='/etc/apt/sources.list'
+list='/etc/apt/sources.list'
 
 # make a backup of the file
-if [ ! -f "$fname.bak" ]; then cp -f "$fname" "$fname.bak"; fi
+if [ ! -f "$list.bak" ]; then
+    cp -f "$list" "$list.bak"
+fi
 
-cat <<EOT > "$fname"
-##       UBUNTU FOCAL
+cat > "$list" <<EOF
+###################################################
 ##
-##        v22.04.5
-##   /etc/apt/sources.list
-#
-### ALL MIRRORS IN EACH CATAGORY ARE LISTED AS BEING IN THE USA
-### IF YOU USE ALL THE LISTS YOU CAN RUN INTO APT COMMAND ISSUES THAT
-###    STATE THERE ARE TOO MANY FILES AND WHAT NOT. JUST AN FYI FOR YOU.
-#
-#######################
-##  Default Mirrors  ##
-#######################
-#
-deb http://archive.ubuntu.com/ubuntu/ focal main restricted universe multiverse
-deb http://archive.ubuntu.com/ubuntu/ focal-updates main restricted universe multiverse
+##  UBUNTU FOCAL
+##
+##  v18.04.6
+##
+##  /etc/apt/sources.list
+##
+##  ALL MIRRORS IN EACH CATAGORY ARE LISTED AS BEING
+##  IN THE USA. IF YOU USE ALL THE LISTS YOU CAN RUN
+##  INTO APT COMMAND ISSUES THAT STATE THERE ARE TOO
+##  MANY FILES. JUST AN FYI FOR YOU.
+##
+###################################################
+##                Default Mirrors                ##
+##     Disabled due to slow download speeds      ##
+##  The security updates have been left enabled  ##
+###################################################
+##
+# deb http://archive.ubuntu.com/ubuntu/ focal main restricted universe multiverse
+# deb http://archive.ubuntu.com/ubuntu/ focal-updates main restricted universe multiverse
+# deb http://archive.ubuntu.com/ubuntu/ focal-backports main restricted universe multiverse
 deb http://security.ubuntu.com/ubuntu/ focal-security main restricted universe multiverse
-deb http://archive.ubuntu.com/ubuntu/ focal-backports main restricted universe multiverse
-#
-#########################################
-##                                     ##
-##  20Gbps mirrors [ unsecured HTTP ]  ##
-##                                     ##
-#########################################
-#
-# MAIN
-#
-deb [trusted=yes] http://mirror.enzu.com/ubuntu/ focal main restricted universe multiverse
-deb [trusted=yes] http://mirror.genesisadaptive.com/ubuntu/ focal main restricted universe multiverse
-deb [trusted=yes] http://mirror.math.princeton.edu/pub/ubuntu/ focal main restricted universe multiverse
-deb [trusted=yes] http://mirror.pit.teraswitch.com/ubuntu/ focal main restricted universe multiverse
-#
-# UPDATES
-#
-deb [trusted=yes] http://mirror.enzu.com/ubuntu/ focal-updates main restricted universe multiverse
-deb [trusted=yes] http://mirror.genesisadaptive.com/ubuntu/ focal-updates main restricted universe multiverse
-deb [trusted=yes] http://mirror.math.princeton.edu/pub/ubuntu/ focal-updates main restricted universe multiverse
-deb [trusted=yes] http://mirror.pit.teraswitch.com/ubuntu/ focal-updates main restricted universe multiverse
-#
-# BACKPORTS
-#
-deb [trusted=yes] http://mirror.enzu.com/ubuntu/ focal-backports main restricted universe multiverse
-deb [trusted=yes] http://mirror.genesisadaptive.com/ubuntu/ focal-backports main restricted universe multiverse
-deb [trusted=yes] http://mirror.math.princeton.edu/pub/ubuntu/ focal-backports main restricted universe multiverse
-deb [trusted=yes] http://mirror.pit.teraswitch.com/ubuntu/ focal-backports main restricted universe multiverse
+##
+####################################################
+##                                                ##
+##                  20Gb Mirrors                  ##
+##                                                ##
+####################################################
+##
+## MAIN
+##
+deb deb https://mirror.enzu.com/ubuntu/ focal main restricted universe multiverse
+deb http://mirror.genesisadaptive.com/ubuntu/ focal main restricted universe multiverse
+deb http://mirror.math.princeton.edu/pub/ubuntu/ focal main restricted universe multiverse
+deb http://mirror.pit.teraswitch.com/ubuntu/ focal main restricted universe multiverse
+##
+## UPDATES
+##
+deb https://mirror.enzu.com/ubuntu/ focal-updates main restricted universe multiverse
+deb http://mirror.genesisadaptive.com/ubuntu/ focal-updates main restricted universe multiverse
+deb http://mirror.math.princeton.edu/pub/ubuntu/ focal-updates main restricted universe multiverse
+deb http://mirror.pit.teraswitch.com/ubuntu/ focal-updates main restricted universe multiverse
+##
+## BACKPORTS
+##
+deb https://mirror.enzu.com/ubuntu/ focal-backports main restricted universe multiverse
+deb http://mirror.genesisadaptive.com/ubuntu/ focal-backports main restricted universe multiverse
+deb http://mirror.math.princeton.edu/pub/ubuntu/ focal-backports main restricted universe multiverse
+deb http://mirror.pit.teraswitch.com/ubuntu/ focal-backports main restricted universe multiverse
+EOF
 
-########################################
-##                                    ##
-##  10Gbps mirrors [ secured HTTPS ]  ##
-##                                    ##
-########################################
-#
-# MAIN
-#
-deb [trusted=yes] https://atl.mirrors.clouvider.net/ubuntu/ focal main restricted universe multiverse
-deb [trusted=yes] https://mirror.fcix.net/ubuntu/ focal main restricted universe multiverse
-deb [trusted=yes] https://mirror.lstn.net/ubuntu/ focal main restricted universe multiverse
-deb [trusted=yes] https://mirror.us.leaseweb.net/ubuntu/ focal main restricted universe multiverse
-deb [trusted=yes] https://mirrors.bloomu.edu/ubuntu/ focal main restricted universe multiverse
-deb [trusted=yes] https://mirrors.wikimedia.org/ubuntu/ focal main restricted universe multiverse
-deb [trusted=yes] https://mirrors.xtom.com/ubuntu/ focal main restricted universe multiverse
-# deb [trusted=yes] https://dal.mirrors.clouvider.net/ubuntu/ focal main restricted universe multiverse
-# deb [trusted=yes] https://la.mirrors.clouvider.net/ubuntu/ focal main restricted universe multiverse
-# deb [trusted=yes] https://mirrors.egr.msu.edu/ubuntu/ focal main restricted universe multiverse
-# deb [trusted=yes] https://mirrors.iu13.net/ubuntu/ focal main restricted universe multiverse
-# deb [trusted=yes] https://nyc.mirrors.clouvider.net/ubuntu/ focal main restricted universe multiverse
-# deb [trusted=yes] https://ubuntu.mirror.shastacoe.net/ubuntu/ focal main restricted universe multiverse
-#
-# UPDATES
-#
-deb [trusted=yes] https://atl.mirrors.clouvider.net/ubuntu/ focal-updates main restricted universe multiverse
-deb [trusted=yes] https://mirror.fcix.net/ubuntu/ focal-updates main restricted universe multiverse
-deb [trusted=yes] https://mirror.lstn.net/ubuntu/ focal-updates main restricted universe multiverse
-deb [trusted=yes] https://mirror.us.leaseweb.net/ubuntu/ focal-updates main restricted universe multiverse
-deb [trusted=yes] https://mirrors.bloomu.edu/ubuntu/ focal-updates main restricted universe multiverse
-deb [trusted=yes] https://mirrors.wikimedia.org/ubuntu/ focal-updates main restricted universe multiverse
-deb [trusted=yes] https://mirrors.xtom.com/ubuntu/ focal-updates main restricted universe multiverse
-# deb [trusted=yes] https://dal.mirrors.clouvider.net/ubuntu/ focal-updates main restricted universe multiverse
-# deb [trusted=yes] https://la.mirrors.clouvider.net/ubuntu/ focal-updates main restricted universe multiverse
-# deb [trusted=yes] https://mirrors.egr.msu.edu/ubuntu/ focal-updates main restricted universe multiverse
-# deb [trusted=yes] https://mirrors.iu13.net/ubuntu/ focal-updates main restricted universe multiverse
-# deb [trusted=yes] https://nyc.mirrors.clouvider.net/ubuntu/ focal-updates main restricted universe multiverse
-# deb [trusted=yes] https://ubuntu.mirror.shastacoe.net/ubuntu/ focal-updates main restricted universe multiverse
-#
-# BACKPORTS
-#
-deb [trusted=yes] https://atl.mirrors.clouvider.net/ubuntu/ focal-backports main restricted universe multiverse
-deb [trusted=yes] https://mirror.fcix.net/ubuntu/ focal-backports main restricted universe multiverse
-deb [trusted=yes] https://mirror.lstn.net/ubuntu/ focal-backports main restricted universe multiverse
-deb [trusted=yes] https://mirror.us.leaseweb.net/ubuntu/ focal-backports main restricted universe multiverse
-deb [trusted=yes] https://mirrors.bloomu.edu/ubuntu/ focal-backports main restricted universe multiverse
-deb [trusted=yes] https://mirrors.wikimedia.org/ubuntu/ focal-backports main restricted universe multiverse
-deb [trusted=yes] https://mirrors.xtom.com/ubuntu/ focal-backports main restricted universe multiverse
-# deb [trusted=yes] https://dal.mirrors.clouvider.net/ubuntu/ focal-backports main restricted universe multiverse
-# deb [trusted=yes] https://la.mirrors.clouvider.net/ubuntu/ focal-backports main restricted universe multiverse
-# deb [trusted=yes] https://mirrors.egr.msu.edu/ubuntu/ focal-backports main restricted universe multiverse
-# deb [trusted=yes] https://mirrors.iu13.net/ubuntu/ focal-backports main restricted universe multivers
-# deb [trusted=yes] https://nyc.mirrors.clouvider.net/ubuntu/ focal-backports main restricted universe multiverse
-# deb [trusted=yes] https://ubuntu.mirror.shastacoe.net/ubuntu/ focal-backports main restricted universe multiverse
-EOT
-
-# Open in editor to verify file contents
-if which gedit &>/dev/null; then
-    gedit "$fname"
-elif which nano &>/dev/null; then
-    nano "$fname"
-elif which vim &>/dev/null; then
-    vim "$fname"
+# OPEN AN EDITOR TO VIEW THE CHANGES
+if which 'gedit' &>/dev/null; then
+    gedit "$list"
+elif which 'nano' &>/dev/null; then
+    nano "$list"
+elif which 'vi' &>/dev/null; then
+    vi "$list"
 else
-    vi "$fname"
+    printf "\n%s\n\n" \
+        "Could not find an EDITOR to open: $list"
+    exit 1
 fi
