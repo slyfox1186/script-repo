0400f10d9cd559ce86bb5a0d3d8b7b8c64d159f3
Jeff <jhollis.ga@gmail.com>
2023-06-04 22:04:46 -0400

    Update jammy-mirrors.sh

--- shell/ubuntu-jammy/jammy-mirrors.sh
+++ shell/ubuntu-jammy/jammy-mirrors.sh
@@ -42,7 +42,7 @@ deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe m
 ##
 ## MAIN
 ##
-deb deb https://mirror.enzu.com/ubuntu/ jammy main restricted universe multiverse
+deb https://mirror.enzu.com/ubuntu/ jammy main restricted universe multiverse
 deb http://mirror.genesisadaptive.com/ubuntu/ jammy main restricted universe multiverse
 deb http://mirror.math.princeton.edu/pub/ubuntu/ jammy main restricted universe multiverse
 deb http://mirror.pit.teraswitch.com/ubuntu/ jammy main restricted universe multiverse
