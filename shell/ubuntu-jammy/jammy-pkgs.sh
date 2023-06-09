7e39e09887853472dd43cb279d5109bf17a5440f
Jeff <jhollis.ga@gmail.com>
2023-06-08 20:16:19 -0400

    Update jammy-pkgs.sh

--- shell/ubuntu-jammy/jammy-pkgs.sh
+++ shell/ubuntu-jammy/jammy-pkgs.sh
@@ -54,21 +54,21 @@ install_other_fn() { curl -Ssf 'https://dlang.org/install.sh' | bash -s dmd; }
 
 install_apt_fn()
 {
-    pkgs=(alien apt-file aptitude aria2 autoconf autogen automake bat binutils bison build-essential ccache \
-          ccdiff checkinstall clang cmake cmake-extras cmake-qt-gui colordiff cpu-checker curl cvs dbus-x11 \
-          dconf-editor ddclient debhelper devscripts dh-make disktype dos2unix dpkg-dev exfat-fuse exfatprogs \
-          f2fs-tools fakeroot flatpak flex gawk gcc g++ gcc-12 g++-12 gedit gedit-plugins gh gir1.2-gtksource-3.0 \
-          git-all git-buildpackage gnome-shell-extension-manager gnome-tweaks gnustep-gui-runtime golang gparted \
-          gperf grub-customizer gufw hfsplus hfsprogs hfsutils htop idn2 iftop iw jfsutils jq libasan6-amd64-cross \
-          libboost-all-dev libbz2-dev libdmalloc-dev libglib2.0-dev libgvc6 libheif-dev libjemalloc-dev liblz-dev \
-          liblzma-dev liblzo2-dev libmimalloc-dev libncurses5-dev libnet-nslookup-perl libnuma-dev libperl-dev \
-          libpstoedit-dev libraqm-dev libraw-dev librsvg2-dev librust-jemalloc-sys-dev librust-malloc-buf-dev \
-          libsdl-pango-dev libsox-dev libssl-dev libtalloc-dev libtbbmalloc2 libtool libtool-bin libzstd-dev \
-          libzzip-dev linux-headers-generic linux-source lm-sensors lsb-core lshw lvm2 lzma-dev mainline make \
-          man-db mercurial meson mono-devel nano nasm neofetch netplan.io net-tools network-manager nilfs-tools \
-          ninja-build ntfs2btrfs ntfs-3g nvme-cli openssh-client openssh-server openssl patchutils pbuilder \
-          pcregrep pipenv plank ppa-purge preload pristine-tar psensor python3-pip quilt reiser4progs reiserfsprogs \
-          rpm ruby-all-dev samba shellcheck smbclient sqlite3 subversion synaptic texinfo tofrodos trash-cli \
+    pkgs=(alien apt-file aptitude aria2 autoconf autogen automake bat binutils bison build-essential ccache
+          ccdiff checkinstall clang cmake cmake-extras cmake-qt-gui colordiff cpu-checker curl cvs dbus-x11
+          dconf-editor ddclient debhelper devscripts dh-make disktype dos2unix dpkg-dev exfat-fuse exfatprogs
+          f2fs-tools fakeroot flatpak flex gawk gcc g++ gcc-12 g++-12 gedit gedit-plugins gh gir1.2-gtksource-3.0
+          git-all git-buildpackage gnome-shell-extension-manager gnome-tweaks gnustep-gui-runtime golang gparted
+          gperf grub-customizer gufw hfsplus hfsprogs hfsutils htop idn2 iftop iw jfsutils jq libasan6-amd64-cross
+          libboost-all-dev libbz2-dev libdmalloc-dev libglib2.0-dev libgvc6 libheif-dev libjemalloc-dev liblz-dev
+          liblzma-dev liblzo2-dev libmimalloc-dev libncurses5-dev libnet-nslookup-perl libnuma-dev libperl-dev
+          libpstoedit-dev libraqm-dev libraw-dev librsvg2-dev librust-jemalloc-sys-dev librust-malloc-buf-dev
+          libsdl-pango-dev libsox-dev libssl-dev libtalloc-dev libtbbmalloc2 libtool libtool-bin libzstd-dev
+          libzzip-dev linux-headers-generic linux-source lm-sensors lsb-core lshw lvm2 lzma-dev mainline make
+          man-db mercurial meson mono-devel nano nasm neofetch netplan.io net-tools network-manager nilfs-tools
+          ninja-build ntfs2btrfs ntfs-3g nvme-cli openssh-client openssh-server openssl patchutils pbuilder
+          pcregrep pipenv plank ppa-purge preload pristine-tar psensor python3-pip quilt reiser4progs reiserfsprogs
+          rpm ruby-all-dev samba shellcheck smbclient sqlite3 subversion synaptic texinfo tofrodos trash-cli
           tty-share udftools unzip usb-creator-gtk uuid-dev wget xclip xfsprogs xsel yasm)
 
     for pkg in ${pkgs[@]}
