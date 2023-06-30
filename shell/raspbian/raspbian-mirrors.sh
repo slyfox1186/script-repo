44e702c3b18ef6bc224eef22e78623f6fda1e7be
Jeff <jhollis.ga@gmail.com>
2023-06-30 13:34:26 -0500

    Create raspbian-mirrors.sh

---
+++ shell/raspbian/raspbian-mirrors.sh
@@ -0,0 +1,8 @@
+# Official Raspbian Sources (Default)
+deb http://raspbian.raspberrypi.org/raspbian/ bullseye main contrib non-free rpi
+
+# Official Debian Bullseye Sources
+deb http://deb.debian.org/debian bullseye main contrib non-free
+deb http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
+deb http://deb.debian.org/debian bullseye-updates main contrib non-free
+deb http://deb.debian.org/debian bullseye-backports main contrib non-free
