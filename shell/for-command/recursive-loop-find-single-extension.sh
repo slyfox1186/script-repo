e65ea85965d4a7017f8f61f46f8206dbb97112d7
Jeff <jhollis.ga@gmail.com>
2023-07-06 20:57:43 -0500

    Update recursive-loop-find-single-extension.sh

--- shell/for-command/recursive-loop-find-single-extension.sh
+++ shell/for-command/recursive-loop-find-single-extension.sh
@@ -2,13 +2,28 @@
 
 clear
 
-fext=bat,sh
-prefix='./'
+fext=sh
 
-outfile="$(find . -type f -iname "*.$fext" -exec bash -c "echo {} | sed -E 's/.*\s(.*)/\1/'" \;)"
+printf "%s\n\n%s\n%s\n\n" \
+    'Include the parent folder?' \
+    '[1] Yes' \
+    '[2] No'
+read -p 'Your choices are (1 or 2): ' choice
+clear
 
-for i in ${outfile[@]}
+case "$choice" in
+    1)      store_paths="$(find . -type f -iname "*.$fext" -exec bash -c "echo {} | sed -E 's/.*\s(.*)/\1/'" \;)";;
+    2)      store_paths="$(find . -mindepth 2 -type f -iname "*.$fext" -exec bash -c "echo {} | sed -E 's/.*\s(.*)/\1/'" \;)";;
+    *)
+            clear
+            printf "%s\n\n" 'Bad user input.'
+            exit 1
+esac
+
+for i in ${store_paths[@]}
 do
     fpath="${i:2}"
     echo "$PWD/$fpath"
 done
+
+unset choice
