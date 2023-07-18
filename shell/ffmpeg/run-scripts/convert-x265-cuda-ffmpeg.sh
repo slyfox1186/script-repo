9c9418debebc7fe1f6977ea5f0d7d9dd982ad884
Jeff <jhollis.ga@gmail.com>
2023-07-18 10:00:51 -0400

    Update convert-x265-cuda-ffmpeg.sh

--- shell/ffmpeg/run-scripts/convert-x265-cuda-ffmpeg.sh
+++ shell/ffmpeg/run-scripts/convert-x265-cuda-ffmpeg.sh
@@ -1,39 +1,72 @@
 #!/bin/bash
+# shellcheck disable=SC2066,SC2068,SC2086,SC2162
 
 clear
 
+#
+# SET PATH
+#
+
+export PATH="$PATH:$HOME/.local/bin"
+
+#
 # REQUIRED APT PACKAGES
-apt_pkgs=(bc ffmpegthumbnailer ffmpegthumbs libffmpegthumbnailer4v5 sox trash-cli)
+#
+
+apt_pkgs=(bc ffmpegthumbnailer ffmpegthumbs libffmpegthumbnailer4v5 sox libsox-dev trash-cli)
 for i in ${apt_pkgs[@]}
 do
-    if ! dpkg -l | grep $i &>/dev/null; then
-        sudo apt -y install $i
+    missing_pkg="$(dpkg -l | grep "${i}")"
+    if [ -z "${missing_pkg}" ]; then
+        missing_pkgs+=" ${i}"
     fi
 done
 
+if [ -n "${missing_pkgs}" ]; then
+    sudo apt -y install ${missing_pkgs}
+    sudo apt -y autoremove
+    clear
+fi
+unset apt_pkgs i missing_pkg missing_pkgs
+
+#
 # REQUIRED PIP PACKAGES
+#
+
+pip_lock="$(find /usr/lib/python3* -name EXTERNALLY-MANAGED)"
+if [ -n "${pip_lock}" ]; then
+    sudo rm "${pip_lock}"
+fi
+
 pip_pkgs=(ffpb google_speech)
 for p in ${pip_pkgs[@]}
 do
-    if ! pip show $p &>/dev/null; then
-        pip install $p
+    missing_pkg="$(pip show "${p}")"
+    if [ -z "${missing_pkg}" ]; then
+        missing_pkgs+=" ${p}"
     fi
 done
 
+if [ -n "${missing_pkgs}" ]; then
+    pip install ${p}
+    clear
+fi
+unset p pip_lock pip_pkgs missing_pkg missing_pkgs
+
 # DELETE FILES PROM PRIOR RUNS
 del_this="$(du -ah --max-depth=1 | grep -Eo '[\/].*\(x265\)\.(mp4|mkv)$' | grep -Eo '[A-Za-z0-9].*\(x265\)\.(mp4|mkv)$')"
-
 clear
-if [ -n "$del_this" ]; then
+
+if [ -n "${del_this}" ]; then
     printf "%s\n\n%s\n%s\n%s\n\n" \
-        "Do you want to delete this video before continuing?: $del_this" \
+        "Do you want to delete this video before continuing?: ${del_this}" \
         '[1] Yes' \
         '[2] No' \
         '[3] Exit'
     read -p 'Your choices are (1 to 3): ' choice
 
-    case "$choice" in
-        1)      rm "$del_this"; clear;;
+    case "${choice}" in
+        1)      rm "${del_this}"; clear;;
         2)      clear;;
         3)      exit 0;;
         *)
@@ -44,70 +77,88 @@ if [ -n "$del_this" ]; then
     esac
 fi
 
+# MAKE SURE THERE ARE ACTUAL VIDEOS IN THE FOLDER WITH THIS SCRIPT BEFORE CONTINUING
+for vid in "$PWD"/*.{mp4,mkv}
+do
+    vid_exist="$(echo ${vid})"
+    if [ -n "${vid_exist}" ]; then
+        vid_list=" ${vid}"
+    fi
+    if [ -n "${vid_list}" ]; then
+        clear
+    else
+        printf "%s\n\n%s\n" \
+            'You dummy, there are no video files in this folder... what is this script supposed to do without those?!' \
+            'Fix this problem to continue.'
+        exit 1
+    fi
+done
+unset vid vid_exist vid_list
+
 # CAPTURE THE VIDEO WITHOUT (X265).MP4 AS THE ENDING
 video="$(find . -maxdepth 1 -type f \( -iname \*.mp4 -o -iname \*.mkv \) -exec echo '{}' +)"
 
-for i in "${video:2}"
+for v in "${video:2}"
 do
     # STORES THE CURRENT VIDEO WIDTH, ASPECT RATIO, PROFILE, BIT RATE, AND TOTAL DURATION IN VARIABLES FOR USE LATER IN THE FFMPEG COMMAND LINE
-    aspect_ratio="$(ffprobe -hide_banner -select_streams v:0 -show_entries stream=display_aspect_ratio -of default=nk=1:nw=1 -pretty "$i" 2>/dev/null)"
-    file_length="$(ffprobe -hide_banner -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$i" 2>/dev/null)"
-    max_rate="$(ffprobe -hide_banner -show_entries format=bit_rate -of default=nk=1:nw=1 -pretty "$i" 2>/dev/null)"
-    file_height="$(ffprobe -hide_banner -select_streams v:0 -show_entries stream=height -of csv=s=x:p=0 -pretty "$i" 2>/dev/null)"
-    file_width="$(ffprobe -hide_banner -select_streams v:0 -show_entries stream=width -of csv=s=x:p=0 -pretty "$i" 2>/dev/null)"
+    aspect_ratio="$(ffprobe -hide_banner -select_streams v:0 -show_entries stream=display_aspect_ratio -of default=nk=1:nw=1 -pretty "${v}" 2>/dev/null)"
+    file_length="$(ffprobe -hide_banner -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "${v}" 2>/dev/null)"
+    max_rate="$(ffprobe -hide_banner -show_entries format=bit_rate -of default=nk=1:nw=1 -pretty "${v}" 2>/dev/null)"
+    file_height="$(ffprobe -hide_banner -select_streams v:0 -show_entries stream=height -of csv=s=x:p=0 -pretty "${v}" 2>/dev/null)"
+    file_width="$(ffprobe -hide_banner -select_streams v:0 -show_entries stream=width -of csv=s=x:p=0 -pretty "${v}" 2>/dev/null)"
 done
 
 # MODIFY VARS TO GET FILE INPUT AND OUTPUT NAMES
-file_in="$i"
-file_ext="${file_in#*.}"
-file_out="${file_in%.*} (x265).$file_ext"
+file_in="${v}"
+fext="${file_in#*.}"
+file_out="${file_in%.*} (x265).${fext}"
 
 # TRIM THE STRINGS
 trim="${max_rate::-11}"
 
 # GETS THE INPUT VIDEOS MAX DATARATE AND APPLIES LOGIC TO DETERMINE bitrate, bufsize, AND MAXRATE VARIABLES
-trim="$(bc <<< "scale=2 ; $trim * 1000")"
-_bitrate="$(bc <<< "scale=2 ; $trim / 2")"
-bitrate="${_bitrate::-3}"
+trim="$(bc <<< "scale=2 ; ${trim} * 1000")"
+br="$(bc <<< "scale=2 ; ${trim} / 2")"
+bitrate="${br::-3}"
 maxrate="$(( bitrate * 2 ))"
-_bufsize="$(bc <<< "scale=2 ; $_bitrate * 2")"
-bufsize="${_bufsize::-3}"
+bs="$(bc <<< "scale=2 ; ${br} * 2")"
+bufsize="${bs::-3}"
 length="$(( ${file_length::-7} / 60 ))"
 length+=' Minutes'
 
 cat <<EOF
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-Input File:      $file_in
-Output File:     $file_out
+Input File:      ${file_in}
+Output File:     ${file_out}
 
-Dimensions:      ${file_width}x$file_height
-Aspect Ratio:    $aspect_ratio
+Dimensions:      ${file_width}x${file_height}
+Aspect Ratio:    ${aspect_ratio}
 
 Maxrate:         ${maxrate}k
 Bufsize:         ${bufsize}k
 Bitrate:         ${bitrate}k
 
-Length:          $length
+Length:          ${length}
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 EOF
 
-# EXECUTE FFMPEG
+# EXECUTE FFMPEG THROUGH FFPB
 if ffmpeg \
         -y \
         -threads 0 \
         -hide_banner \
         -hwaccel_output_format cuda \
-        -i "$file_in" \
+        -i "${file_in}" \
         -pix_fmt p010le \
         -movflags frag_keyframe+empty_moov \
         -c:v hevc_nvenc \
         -preset:v medium \
         -profile:v main \
         -rc:v vbr \
-        -b:v ${bitrate}k \
-        -bufsize:v ${bufsize}k \
+        -b:v "${bitrate}"k \
+        -bufsize:v "${bufsize}"k \
         -bf:v 4 \
         -b_ref_mode:v middle \
         -qmin:v 0 \
@@ -119,11 +170,14 @@ if ffmpeg \
         -c:a libfdk_aac \
         -qmin:a 1 \
         -qmax:a 4 \
-        "$file_out"; then
+        "${file_out}"; then
     google_speech 'Video conversion completed.' 2>/dev/null
+    # MOVE INPUT FILE TO TRASH
+    trash -f "${file_in}"
+    exit 0
 else
     google_speech 'Video conversion failed.' 2>/dev/null
+    echo
+    read -p 'Press enter to exit.'
+    exit 1
 fi
-
-# MOVE INPUT FILE TO TRASH
-trash -f "$file_in"
