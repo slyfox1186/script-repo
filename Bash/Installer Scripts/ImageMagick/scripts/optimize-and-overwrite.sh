26b4a42835bd17f00e787e7e9643db2dd765d832
Jeff <jhollis.ga@gmail.com>
2023-08-03 08:43:19 -0700

    Update optimize-and-overwrite.sh

--- Bash/Installer Scripts/ImageMagick/scripts/optimize-and-overwrite.sh
+++ Bash/Installer Scripts/ImageMagick/scripts/optimize-and-overwrite.sh
@@ -19,7 +19,7 @@ do
 done
 
 if [ -n "${missing_pkgs}" ]; then
-    sudo apt -y install ${missing_pkgs}
+    sudo apt -y install "${missing_pkgs}"
     sudo apt -y autoremove
     clear
 fi
@@ -46,41 +46,28 @@ find . -type f -name "*:Zone.Identifier" -delete 2>/dev/null
 cnt_queue=$(find . -maxdepth 2 -type f -iname "*.jpg" | wc -l)
 cnt_total=$(find . -maxdepth 2 -type f -iname "*.jpg" | wc -l)
 # GET THE UNMODIFIED PATH OF EACH MATCHING FILE
-get_path="$(find . -type f -iname "*.${fext}" -exec sh -c 'i="${1}"; echo "${i%*.}"' shell {} \;)"
 
-# FIND ALL JPG FILES AND CREATE TEMPORARY CACHE FILES FROM THEM
-for i in ${get_path[@]}
-do
-    fname_in="${i:2}"
-    fpath_full="${PWD}/${fname_in}"
-    fpath=${fpath_full%/*}
-    fdir="${fpath%%.*}"
-    # IF YOUR SUB-DIRECTORY IS NOT NAMED "Pics" THEN CHANGE THE BELOW COMMAND AS NEEDED
-    cd "${fdir//\/Pics\/Pics/\/Pics}" || exit 1
-done
-unset i
-
-for i in *.jpg
+for i in ./*."${fext}"
 do
     cnt_queue=$(( cnt_queue-1 ))
 
     cat <<EOF
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-File Path: ${fpath//\/Pics\/Pics/\/Pics}
+File Path: ${PWD}
 
-Folder: $(basename ${PWD})
+Folder: $(basename "${PWD}")
 
 Total Files:    ${cnt_total}
 Files in queue: ${cnt_queue}
 
 Converting:  ${i}
 
-             >> ${i%%.jpg}.mpc
-            
-                >> ${i%%.jpg}.cache
-               
-                   >> ${i%%.jpg}-IM.jpg
+>> ${i%%.jpg}.mpc
+
+>> ${i%%.jpg}.cache
+
+   >> ${i%%.jpg}-IM.jpg
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 EOF
@@ -91,29 +78,19 @@ EOF
         -unsharp '0.25x0.08+8.3+0.045' -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off \
         -auto-level -enhance -interlace none -colorspace sRGB "${random_dir}/${i%%.jpg}.mpc"
 
+
     for file in "${random_dir}"/*.mpc
     do
-        if [ -f "${file}" ]; then
-            if convert "${file}" -monitor "${file%%.mpc}.jpg"; then
-                if [ -f "${file%%.mpc}.jpg" ]; then
-                    cwd="$(echo "${file}" | sed 's:.*/::')"
-                    mv "${file%%.mpc}.jpg" "${PWD}/${cwd%%.*}-IM.jpg"
-                    rm -f "${PWD}/${cwd%%.*}.jpg"
-                    for v in ${file}
-                    do
-                        v_noslash="${v%/}"
-                        rm -fr "${v_noslash%/*}"
-                    done
-                fi
-            else
-                clear
-                printf "%s\n\n" 'Error: Unable to find the optimized image.'
-                read -p 'Press enter to exit.'
-                return 1
-            fi
-        fi
+        convert "${file}" -monitor "${file%%.mpc}.jpg"
+        tmp_file="$(echo "${file}" | sed 's:.*/::')"
+        mv "${file%%.mpc}.jpg" "${PWD}/${tmp_file%%.*}-IM.jpg"
+        rm -f "${PWD}/${tmp_file%%.*}.jpg"
+        for v in ${file}
+        do
+            v_noslash="${v%/}"
+            rm -fr "${v_noslash%/*}"
+        done
     done
-    clear
 done
 
 if [ "${?}" -eq '0' ]; then
