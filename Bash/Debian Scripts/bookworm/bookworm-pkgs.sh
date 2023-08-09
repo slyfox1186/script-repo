3ac92b6376913c4b16d02604c4f9c631c9a1f210
Jeff <jhollis.ga@gmail.com>
2023-08-09 15:37:22 -0700

    Update bookworm-pkgs.sh

--- Bash/Debian Scripts/bookworm/bookworm-pkgs.sh
+++ Bash/Debian Scripts/bookworm/bookworm-pkgs.sh
@@ -6,9 +6,9 @@ pkgs=(alien aptitude aria2 autoconf autoconf-archive autogen automake bat binuti
       ccdiff clang clang-tools cmake cmake-extras colordiff curl dbus dbus-x11 dconf-cli dconf-editor ddclient
       desktopfolder disktype dos2unix exfat-fuse exfatprogs f2fs-tools flatpak flex g++-multilib gawk gcc-multilib
       gedit gedit-plugins git-all gnome-shell-extension-manager gnome-tweaks gnustep-gui-runtime golang gparted gperf
-      grub-customizer gufw hfsplus hfsutils htop idn2 iftop iw jfsutils jq libbz2-dev libcurl4-openssl-dev libdmalloc-dev
-      libglib2.0-dev libgvc6 libheif-dev libjemalloc-dev liblz-dev liblzma-dev liblzo2-dev libncurses5-dev libnet-nslookup-perl
-      libnotify-bin libnuma-dev libperl-dev libpstoedit-dev libraqm-dev libraw-dev librsvg2-dev librust-malloc-buf-dev
+      grub-customizer gsettings-desktop-schemas gufw hfsplus hfsutils htop idn2 iftop iw jfsutils jq libbz2-dev libcurl4-openssl-dev
+      libdmalloc-dev libglib2.0-dev libgvc6 libheif-dev libjemalloc-dev liblz-dev liblzma-dev liblzo2-dev libncurses5-dev
+      libnet-nslookup-perl libnotify-bin libnuma-dev libperl-dev libpstoedit-dev libraqm-dev libraw-dev librsvg2-dev librust-malloc-buf-dev
       libsdl-pango-dev libsox-dev libssl-dev libtalloc-dev libtool libtool-bin libzstd-dev libzzip-dev linux-source lm-sensors
       lshw lvm2 lzma-dev m4 make man-db mono-devel nano net-tools netplan.io network-manager nilfs-tools npm ntfs-3g openssh-client
       openssh-server openssl pcregrep php-cli php-sqlite3 pipenv preload psensor python3 python3-pip reiser4progs reiserfsprogs
