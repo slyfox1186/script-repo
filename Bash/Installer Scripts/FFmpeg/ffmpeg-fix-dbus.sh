e4e10bb2bd599351988ccb896cce1d6c0779742f
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-08-31 23:17:06 -0400

    add-fix-dbus

---
+++ Bash/Installer Scripts/FFmpeg/ffmpeg-fix-dbus.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+clear
+
+if [ "${EUID}" -ne '0' ]; then
+    printf "%s\n\n" 'You must run this script with root/sudo.'
+    exit 1
+fi
+
+file=/usr/share/dbus-1/services/org.freedesktop.Notifications.service
+
+cat > "${file}" <<EOF
+[D-BUS Service]
+Name=org.freedesktop.Notifications
+Exec=/usr/lib/notification-daemon-1.0/notification-daemon
+EOF
