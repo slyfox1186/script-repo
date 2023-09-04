8eacf63f33777fb014b38c261c977c1b3f9e9c24
Jeff <jhollis.ga@gmail.com>
2023-09-03 22:09:01 -0700

    Create box-out-banner.sh

---
+++ Bash/misc/box-out-banner.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+clear
+
+box_out_banner()
+{
+    input_char=$(echo "${@}" | wc -c)
+    line=$(for i in $(seq 0 ${input_char}); do printf "-"; done)
+    tput bold
+    line="$(tput setaf 3)${line}"
+    space=${line//-/ }
+    echo " ${line}"
+    printf '|' ; echo -n "${space}" ; printf "%s\n" '|';
+    printf '| ' ;tput setaf 4; echo -n "${@}"; tput setaf 3 ; printf "%s\n" ' |';
+    printf '|' ; echo -n "${space}" ; printf "%s\n" '|';
+    echo " ${line}"
+    tput sgr 0
+}
+box_out_banner "YOUR MESSAGE HERE"
