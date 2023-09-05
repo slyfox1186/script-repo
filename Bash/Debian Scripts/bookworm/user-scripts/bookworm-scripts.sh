36819ebbaf1b026dcd5249ab5d83629849017577
Jeff <jhollis.ga@gmail.com>
2023-09-05 03:47:17 -0700

    Update bookworm-scripts.sh

--- Bash/Debian Scripts/bookworm/user-scripts/bookworm-scripts.sh
+++ Bash/Debian Scripts/bookworm/user-scripts/bookworm-scripts.sh
@@ -3,26 +3,47 @@
 clear
 
 #
+# DEFINE VARIABLES AND ARRAYS
+#
+
+randir="$(mktemp -d)"
+shell_array=(bash_aliases.sh bash_functions.sh)
+script_array=(.bash_aliases .bash_functions .bashrc)
+
+#
 # CREATE FUNCTIONS
 #
 
 fail_fn()
 {
-    printf "\n%s\n\n" "$1"
+    printf "\n%s\n\n" "${1}"
     exit 1
 }
 
-# Create and cd into a random directory
-cd "$(mktemp --directory)" || exit 1
+#
+# CREATE A RANDOM FOLDER TO HOLD AND EXECUTE THE TEMP FILES
+#
 
-# Download the user scripts from GitHub
+cd "${randir}" || exit 1
+
+#
+# DOWNLOAD THE USER SCRIPTS FROM GITHUB
+#
+
 wget -qN - -i 'https://raw.githubusercontent.com/slyfox1186/script-repo/main/Bash/Debian%20Scripts/bookworm/user-scripts/bookworm-scripts.txt'
 
-# Delete all files except those that start with a '.' or end with '.sh'
+#
+# DELETE ALL FILES EXCEPT THOSE THAT START WITH A "." OR END WITH ".sh"
+#
+
 find . ! \( -name '\.*' -o -name '*.sh' \) -type f -delete 2>/dev/null
 
-if ! mv -f '.bashrc' "$HOME"; then
-    fail_fn 'Failed to move the script .bashrc to the user'\''s $HOME directory.'
+#
+# MOVE ".bashrc" TO THE USERS HOME FOLDER
+#
+
+if ! mv -f '.bashrc' "${HOME}"; then
+    fail_fn "Failed to move the script \".bashrc\" to ${HOME}."
 fi
 
 #
@@ -30,19 +51,17 @@ fi
 #
 
 clear
-
 printf "%s\n\n%s\n%s\n\n" \
-    'Do you want to install/enable apt-fast as the primary download utility?' \
+    'Do you want to install and enable apt-fast as the primary download utility?' \
     '[1] Yes' \
     '[2] No'
 read -p 'Your choices are (1 or 2): ' answer
 clear
 
-case "$answer" in
+case "${answer}" in
     1)
             bash -c "$(curl -sL https://git.io/vokNn)"
             apt_flag=yes
-            clear
             ;;
     2)      apt_flag=no;;
     *)
@@ -52,46 +71,60 @@ case "$answer" in
             ;;
 esac
 unset answer
+clear
 
-shell_array=(bash_aliases.sh bash_functions.sh)
-script_array=(.bash_aliases .bash_functions .bashrc)
+#
+# RUN EACH SHELL SCRIPT TO INSTALL THE USER SCRIPTS
+#
 
 for i in ${shell_array[@]}
 do
-    bash "$i" "$apt_flag"
+    bash "${i}" "${apt_flag}"
 done
+unset i
 
-for f in ${script_array[@]}
+#
+# UPDATE THE OWNERSHIP OF EACH USER SCRIPT TO THE USER
+#
+
+for i in ${script_array[@]}
 do
-    if ! sudo chown "$USER":"$USER" "$HOME/$f"; then
-        fail_fn "Failed to update the file permissions for: $f"
+    if ! sudo chown "${USER}":"${USER}" "${HOME}/${i}"; then
+        fail_fn "Failed to update the file permissions for ${i}"
     fi
 done
+unset i
 
-# Open each script that is now in each user's home folder with an editor
-for v in ${script_array[@]}
+#
+# OPEN EACH SCRIPT WITH AN EDITOR
+#
+
+for i in "${script_array[@]}"
 do
-    if which gnome-text-editor &>/dev/null; then
-        cd "$HOME" || exit 1
-        gnome-text-editor "$v"
+    if which gted &>/dev/null; then
+        cd "${HOME}" || exit 1
+        gted "${i}"
     elif which gedit &>/dev/null; then
-        cd "$HOME" || exit 1
-        gedit "$v"
+        cd "${HOME}" || exit 1
+        gedit "${i}"
     elif which nano &>/dev/null; then
-        cd "$HOME" || exit 1
-        nano "$v"
+        cd "${HOME}" || exit 1
+        nano "${i}"
     elif which vim &>/dev/null; then
-        cd "$HOME" || exit 1
-        vim "$v"
+        cd "${HOME}" || exit 1
+        vim "${i}"
     elif which vi &>/dev/null; then
-        cd "$HOME" || exit 1
-        vi "$v"
+        cd "${HOME}" || exit 1
+        vi "${i}"
     else
         fail_fn 'Could not find an EDITOR to open the user scripts.'
     fi
 done
 
-# Remove the installer script itself
-#if [ -f "$0" ]; then
-#    sudo rm "$0"
-#fi
+#
+# DELETE THE LEFTOVER TEMP FILES
+#
+
+if [ -d "${randir}" ]; then
+    sudo rm -fr "${randir}"
+fi
