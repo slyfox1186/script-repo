8ce16b211fee3ec469358ab314945b0be7dec207
Jeff <jhollis.ga@gmail.com>
2023-09-04 21:39:36 -0400

    Update ffmpeg-scripts.sh

--- Bash/Installer Scripts/FFmpeg/run-scripts/ffmpeg-scripts.sh
+++ Bash/Installer Scripts/FFmpeg/run-scripts/ffmpeg-scripts.sh
@@ -7,32 +7,35 @@ parent_dir="${PWD}"
 tmp_dir="$(mktemp -d)"
 
 # CREATE AND CD INTO A RANDOM DIRECTORY
-cd "$tmp_dir" || exit 1
+cd "${tmp_dir}" || exit 1
 
 # DOWNLOAD THE SCRIPTS FROM GITHUB
 wget -qN - -i 'https://raw.githubusercontent.com/slyfox1186/script-repo/main/Bash/Installer%20Scripts/FFmpeg/run-scripts/ffmpeg-scripts.txt'
+
+scripts=(ffmpeg ffpb ffpb-loop)
+
 # RENAME THE SCRIPTS
 sudo mv convert-x265-cuda-ffpb.sh ffpb
-sudo mv convert-x265-cuda-ffmpeg.sh ffmpeg
 sudo mv ffpb-loop-mp4-files.sh ffpb-loop
+sudo mv convert-x265-cuda-ffmpeg.sh ffmpeg
 
 # MOVE THE SCRIPTS TO THE ORIGINAL DIRECTORY THE SCRIPT WAS EXECUTED FROM
-sudo mv ffpb ffmpeg "$parent_dir"
+sudo mv "${scripts[@]}" "${parent_dir}"
 
 # CD BACK INTO THE ORIGINAL DIRECTORY
-cd "$parent_dir" || exit 1
-
-# DELETE THE RANDOM DIRECTORY
-sudo rm -fr "$tmp_dir"
+cd "${parent_dir}" || exit 1
 
 # CHANGE THE FILE PERMISSIONS OF EACH SCRIPT
-sudo chown "$USER":"$USER" -R ffmpeg ffpb
-sudo chmod +rwx -R ffmpeg ffpb
+for script in "${scripts[@]}"
+do
+    sudo chown "${USER}":"${USER}" -R "${script}"
+    sudo chmod +rwx -R "${script}"
+done
 
-# UNSET THE SCRIPT VARIABLES
-unset parent_dir tmp_dir
+# DELETE THE RANDOM DIRECTORY
+sudo rm -fr "${tmp_dir}"
 
-# REMOVE THE INSTALLER SCRIPT ITSELF
+# DELETE THIS SCRIPT
 if [ -f "${0}" ]; then
     sudo rm "${0}"
 fi
