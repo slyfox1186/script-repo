7b9dfe780b16d09dad71c7d2c7d7075855aeb535
Jeff <jhollis.ga@gmail.com>
2023-09-04 21:55:57 -0400

    Update adobe-source-code-pro-fonts.sh

--- Bash/Installer Scripts/SlyFox1186 Scripts/adobe-source-code-pro-fonts.sh
+++ Bash/Installer Scripts/SlyFox1186 Scripts/adobe-source-code-pro-fonts.sh
@@ -2,6 +2,11 @@
 
 clear
 
+if [ "$EUID" -ne '0' ]; then
+    printf "%s\n\n" 'You must run this script with root/sudo.'
+    exit 1
+fi
+
 printf "%s\n\n" "Installing fonts in /usr/local/share/fonts/adobe-fonts"
 
 cd /usr/local/share/fonts || exit 1
@@ -16,5 +21,3 @@ git clone --depth 1 https://github.com/adobe-fonts/source-code-pro.git adobe-fon
 fc-cache -f -v adobe-fonts
 
 printf "\n%s\n\n" 'Font installation completed.'
-
-# find . -iname '*.ttf' -exec echo \{\} \;
