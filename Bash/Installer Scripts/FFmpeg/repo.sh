54aa5f22e5194e5c9f7071e77de385df42f80307
Jeff <jhollis.ga@gmail.com>
2023-09-07 10:54:32 -0400

    Update repo.sh

--- Bash/Installer Scripts/FFmpeg/repo.sh
+++ Bash/Installer Scripts/FFmpeg/repo.sh
@@ -1,6 +1,9 @@
+user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+
     git_token=''
 
     if curl_cmd="$(curl \
+                        -A "${user_agent}" \
                         -m 10 \
                         --request GET \
                         --url "https://api.github.com/slyfox1186" \
