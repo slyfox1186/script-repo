52cd7c5cf9dde7286497e9c1c8184afd9fb6e8e3
Jeff <jhollis.ga@gmail.com>
2023-09-10 03:49:03 -0700

    Update fix-dbus.sh

--- Bash/Installer Scripts/SlyFox1186 Scripts/fix-dbus.sh
+++ Bash/Installer Scripts/SlyFox1186 Scripts/fix-dbus.sh
@@ -11,5 +11,5 @@ file=/usr/share/dbus-1/services/org.freedesktop.Notifications.service
 cat > "${file}" <<'EOF'
 [D-BUS Service]
 Name=org.freedesktop.Notifications
-Exec=/usr/lib/notification-daemon-1.0/notification-daemon
+Exec=/usr/lib/notification-daemon/notification-daemon
 EOF
