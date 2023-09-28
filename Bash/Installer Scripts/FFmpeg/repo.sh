a488a1dd245fc7e1350bb7f2e8988c12039774a2
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-09-28 16:07:52 -0400

    Update repo.sh

--- Bash/Installer Scripts/FFmpeg/repo.sh
+++ Bash/Installer Scripts/FFmpeg/repo.sh
@@ -1,4 +1,4 @@
-user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
 
     git_token=''
 
