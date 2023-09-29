c4109fcb393e51bcfd41af0c45ec4779e33e7ff0
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-09-28 23:28:25 -0700

    Update imagick-run-script.sh

--- Bash/Installer Scripts/ImageMagick/scripts/imagick-run-script.sh
+++ Bash/Installer Scripts/ImageMagick/scripts/imagick-run-script.sh
@@ -8,23 +8,23 @@ tmp_dir="$(mktemp -d)"
 user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
 
 # CREATE AND CD INTO A RANDOM DIRECTORY
-cd "$tmp_dir" || exit 1
+cd "${tmp_dir}" || exit 1
 
 # DOWNLOAD THE SCRIPTS FROM GITHUB
 curl -A "${user_agent}" -m 10 -Lso 'imow' 'https://raw.githubusercontent.com/slyfox1186/script-repo/main/Bash/Installer%20Scripts/ImageMagick/scripts/optimize-and-overwrite.sh'
 
 # MOVE THE SCRIPTS TO THE ORIGINAL DIRECTORY THE SCRIPT WAS EXECUTED FROM
-sudo mv imow "$cwd"
+sudo mv 'imow' "${cwd}"
 
 # DELETE THE RANDOM DIRECTORY
-sudo rm -fr "$tmp_dir"
+sudo rm -fr "${tmp_dir}"
 
 # CD BACK INTO THE ORIGINAL DIRECTORY
-cd "$cwd" || exit 1
+cd "${cwd}" || exit 1
 
 # CHANGE THE FILE PERMISSIONS OF EACH SCRIPT
-sudo chown "$USER":"$USER" -R imow
-sudo chmod +rwx -R imow
+sudo chown "${USER}":"${USER}" 'imow'
+sudo chmod +rwx 'imow'
 
 # REMOVE THE INSTALLER SCRIPT ITSELF
 if [ -f "${0}" ]; then
