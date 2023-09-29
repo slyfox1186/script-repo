ef126131ff4e0166dc8269f17e12a2379588ba52
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-09-28 23:26:25 -0700

    Update imagick-run-script.sh

--- Bash/Installer Scripts/ImageMagick/scripts/imagick-run-script.sh
+++ Bash/Installer Scripts/ImageMagick/scripts/imagick-run-script.sh
@@ -11,7 +11,7 @@ user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Geck
 cd "$tmp_dir" || exit 1
 
 # DOWNLOAD THE SCRIPTS FROM GITHUB
-curl -A "${user_agent}" -Lso 'imow' 'https://raw.githubusercontent.com/slyfox1186/script-repo/main/Bash/Installer%20Scripts/ImageMagick/scripts/optimize-and-overwrite.sh'
+curl -A "${user_agent}" -m 10 -Lso 'imow' 'https://raw.githubusercontent.com/slyfox1186/script-repo/main/Bash/Installer%20Scripts/ImageMagick/scripts/optimize-and-overwrite.sh'
 
 # MOVE THE SCRIPTS TO THE ORIGINAL DIRECTORY THE SCRIPT WAS EXECUTED FROM
 sudo mv imow "$cwd"
