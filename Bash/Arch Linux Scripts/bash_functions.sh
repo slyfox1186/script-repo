4199a7345b94d52cb001ac306b8c01a76d5fd37d
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-11-06 01:19:14 -0500

    Update bash_functions.sh

--- Bash/Arch Linux Scripts/bash_functions.sh
+++ Bash/Arch Linux Scripts/bash_functions.sh
@@ -1340,7 +1340,7 @@ adl()
 
     aria2c \
         --console-log-level=notice \
-        --user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36' \
+        --user-agent="${user_agent}" \
         -x16 \
         -j5 \
         --split=32 \
