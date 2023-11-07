61df3fbcaf924ed035dff19c44fdec83fe3db682
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-11-07 15:11:29 -0500

    Update bullseye-mirrors.sh

--- Bash/Debian Scripts/bullseye/bullseye-mirrors.sh
+++ Bash/Debian Scripts/bullseye/bullseye-mirrors.sh
@@ -10,46 +10,14 @@ if [ ! -f "$list".bak ]; then
 fi
 
 cat > "$list" <<EOF
-##################################################################
-##
-##  DEBIAN BULLSEYE MIRRORS
-##
-##  /etc/apt/sources.list
-##
-##  ALL MIRRORS IN EACH CATEGORY ARE LISTED AS BEING IN THE USA.
-##
-##################################################################
-##
-## DEFAULT
-##
-# deb http://deb.debian.org/debian bullseye main contrib non-free
-# deb http://deb.debian.org/debian bullseye-updates main contrib non-free
-# deb http://deb.debian.org/debian bullseye-backports main contrib non-free
 deb http://security.debian.org/debian-security bullseye-security main contrib non-free
-##
-## MAIN
-##
-deb http://mirror.cogentco.com/debian/ bullseye main contrib non-free
 deb http://atl.mirrors.clouvider.net/debian/ bullseye main contrib non-free
-deb http://mirrors.wikimedia.org/debian/ bullseye main contrib non-free
-##
-## UPDATES
-##
-deb http://mirror.cogentco.com/debian/ bullseye-updates main contrib non-free
 deb http://atl.mirrors.clouvider.net/debian/ bullseye-updates main contrib non-free
-deb http://mirrors.wikimedia.org/debian/ bullseye-updates main contrib non-free
-##
-## BACKPORTS
-##
-deb http://mirror.cogentco.com/debian/ bullseye-backports main contrib non-free
 deb http://atl.mirrors.clouvider.net/debian/ bullseye-backports main contrib non-free
-deb http://mirrors.wikimedia.org/debian/ bullseye-backports main contrib non-free
 EOF
 
 # OPEN AN EDITOR TO VIEW THE CHANGES
-if which gnome-text-editor &>/dev/null; then
-    sudo gnome-text-editor "$list"
-elif which gedit &>/dev/null; then
+if which gedit &>/dev/null; then
     sudo gedit "$list"
 elif which nano &>/dev/null; then
     sudo nano "$list"
@@ -62,4 +30,4 @@ else
     exit 1
 fi
 
-sudo rm "$0"
+sudo rm "${0}"
