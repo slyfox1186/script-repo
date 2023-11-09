4b18c526f51c3ec028ff9a958b98e535be8742e0
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-11-09 01:09:30 -0500

    Update bash_functions.sh

--- Bash/Debian Scripts/bookworm/user-scripts/bash_functions.sh
+++ Bash/Debian Scripts/bookworm/user-scripts/bash_functions.sh
@@ -737,7 +737,7 @@ imdl()
     clear
     cwd="${PWD}"
     tmp_dir="$(mktemp -d)"
-    user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+    user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
     cd "${tmp_dir}" || exit 1
     curl -A "${user_agent}" -Lso 'imow' 'https://raw.githubusercontent.com/slyfox1186/script-repo/main/Bash/Installer%20Scripts/ImageMagick/scripts/optimize-and-overwrite.sh'
     sudo mv imow "${cwd}"
