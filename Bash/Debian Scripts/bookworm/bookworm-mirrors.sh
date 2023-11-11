d3ee7633a885f00a3eaa230d2aa53ab6cb30241a
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-11-10 20:29:18 -0500

    Update bookworm-mirrors.sh

--- Bash/Debian Scripts/bookworm/bookworm-mirrors.sh
+++ Bash/Debian Scripts/bookworm/bookworm-mirrors.sh
@@ -5,28 +5,28 @@ clear
 list=/etc/apt/sources.list
 
 # Create a backup of the sources.list file
-if [ ! -f "$list.bak" ]; then
-    sudo cp -f "$list" "$list.bak"
+if [ ! -f "${list}.bak" ]; then
+    sudo cp -f "${list}" "${list}.bak"
 fi
 
-cat > /etc/apt/sources.list <<'EOF'
-deb http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
-deb http://atl.mirrors.clouvider.net/debian/ bookworm main contrib non-free non-free-firmware
-deb http://atl.mirrors.clouvider.net/debian/ bookworm-updates main contrib non-free non-free-firmware
-deb http://atl.mirrors.clouvider.net/debian/ bookworm-backports main contrib non-free non-free-firmware
+cat > "${list}" <<'EOF'
+deb https://security.debian.org/debian-security/ bookworm-security main contrib non-free non-free-firmware
+deb https://atl.mirrors.clouvider.net/debian/ bookworm main contrib non-free non-free-firmware
+deb https://atl.mirrors.clouvider.net/debian/ bookworm-updates main contrib non-free non-free-firmware
+deb https://atl.mirrors.clouvider.net/debian/ bookworm-backports main contrib non-free non-free-firmware
 EOF
 
 # Open the sources.list file for review
 if which gnome-text-editor &>/dev/null; then
-    sudo gnome-text-editor "$list"
+    sudo gnome-text-editor "${list}"
 elif which gedit &>/dev/null; then
-    sudo gedit "$list"
+    sudo gedit "${list}"
 elif which nano &>/dev/null; then
-    sudo nano "$list"
+    sudo nano "${list}"
 elif which vim &>/dev/null; then
-    sudo vim "$list"
+    sudo vim "${list}"
 elif which vi &>/dev/null; then
-    sudo vi "$list"
+    sudo vi "${list}"
 else
     fail_fn 'Could not find an EDITOR to open the updated sources.list'
 fi
