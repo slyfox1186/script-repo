6c9d9ecb7ce8af7bc8d9d3bf027ceb8d4cd84072
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-11-10 23:28:20 -0500

    updated browser user_agent

--- Bash/Debian Scripts/bookworm/user-scripts/bookworm-scripts.sh
+++ Bash/Debian Scripts/bookworm/user-scripts/bookworm-scripts.sh
@@ -7,7 +7,7 @@ clear
 #
 
 random_dir="$(mktemp -d)"
-user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
 
 shell_scripts=(bash_aliases.sh bash_functions.sh)
 script_array=(.bash_aliases .bash_functions .bashrc)
