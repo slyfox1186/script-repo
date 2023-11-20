730d379a7d7f76622c1d5eca4cc53f22fe472086
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-11-20 14:54:07 -0500

    Update fix-dbus.sh

--- Bash/Installer Scripts/SlyFox1186 Scripts/fix-dbus.sh
+++ Bash/Installer Scripts/SlyFox1186 Scripts/fix-dbus.sh
@@ -3,13 +3,14 @@
 clear
 
 if [ "${EUID}" -ne '0' ]; then
-    printf "%s\n\n" 'You must run this script WITH root/sudo.'
+    printf "%s\n\n" 'You must run this script with root/sudo.'
+    exit 1
 fi
 
 file=/usr/share/dbus-1/services/org.freedesktop.Notifications.service
 
-cat > "${file}" <<'EOF'
+cat > "${file}" <<EOF
 [D-BUS Service]
 Name=org.freedesktop.Notifications
-Exec=/usr/lib/notification-daemon/notification-daemon
+Exec=/usr/lib/notification-daemon-1.0/notification-daemon
 EOF
