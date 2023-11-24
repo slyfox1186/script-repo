b14e88b8880ab171e5779d67155f10fb03e105c7
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-11-24 12:44:14 -0500

    Update raspi-scripts.sh

--- Bash/Raspbian OS/user-scripts/raspi-scripts.sh
+++ Bash/Raspbian OS/user-scripts/raspi-scripts.sh
@@ -10,8 +10,7 @@ random_dir="$(mktemp -d)"
 dl_url='https://raw.githubusercontent.com/slyfox1186/script-repo/main/Bash/Raspbian%20OS/user-scripts/raspi-scripts.txt'
 user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
 
-shell_array=(bash_aliases.sh bash_functions.sh)
-script_array=(.bash_aliases .bash_functions .bashrc)
+scripts=('.bashrc' '.bash_aliases' '.bash_functions')
 
 #
 # CREATE FUNCTIONS
@@ -39,7 +38,7 @@ cd "${random_dir}" || exit 1
 # DOWNLOAD THE USER SCRIPTS FROM GITHUB
 #
 
-wget -U "${user_agent}" -qN - -i "${dl_url}"
+wget --show-progress -U "${user_agent}" -qN - -i "${dl_url}"
 
 #
 # DELETE ALL FILES EXCEPT THOSE THAT START WITH A "." OR END WITH ".sh"
@@ -48,48 +47,41 @@ wget -U "${user_agent}" -qN - -i "${dl_url}"
 find . ! \( -name '\.*' -o -name '*.sh' \) -type f -delete 2>/dev/null
 
 #
-# MOVE ".bashrc" TO THE USERS HOME FOLDER
+# MOVE SCRIPTS TO THE USER'S HOME FOLDER
 #
 
-if ! mv -f '.bashrc' "${HOME}"; then
-    fail_fn "Failed to move the script \".bashrc\" to ${HOME}."
-fi
-
-#
-# RUN EACH SHELL SCRIPT TO INSTALL THE USER SCRIPTS
-#
-
-for files in ${shell_array[@]}
+for script in ${scripts[@]}
 do
-    bash "${files}"
+    sudo cp -f "${script}" "${HOME}"
 done
+unset script
 
 #
-# UPDATE THE OWNERSHIP OF EACH USER SCRIPT TO THE USER
+# GRANT THE OWNERSHIP OF EACH USER SCRIPT BACK TO THE USER
 #
 
-for scripts in ${script_array[@]}
+for script in ${scripts[@]}
 do
-    if ! sudo chown "${USER}":"${USER}" "${HOME}/${scripts}"; then
-        fail_fn "Failed to update the file permissions for ${scripts}"
+    if ! sudo chown "${USER}":"${USER}" "${HOME}/${script}"; then
+        fail_fn "Failed to update the file permissions for: ${script}"
     fi
 done
+unset script
 
 #
 # OPEN EACH SCRIPT WITH AN EDITOR
 #
 
-for i in ${script_array[@]}
+cd "${HOME}" || exit 1
+
+for script in ${scripts[@]}
 do
-    if which nano &>/dev/null; then
-        cd "${HOME}" || exit 1
-        nano "${i}"
-    elif which vim &>/dev/null; then
-        cd "${HOME}" || exit 1
-        vim "${i}"
-    elif which vi &>/dev/null; then
-        cd "${HOME}" || exit 1
-        vi "${i}"
+    if type -P nano &>/dev/null; then
+        nano "${script}"
+    elif type -P vim &>/dev/null; then
+        vim "${script}"
+    elif type -P vi &>/dev/null; then
+        vi "${script}"
     else
         fail_fn 'Could not find an EDITOR to open the user scripts.'
     fi
