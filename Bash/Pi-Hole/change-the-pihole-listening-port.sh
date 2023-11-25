49d299cf7d1d89221d1b44dfb9d0f8bf04f0d9d1
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-11-25 11:20:47 -0500

    Create change-the-pihole-listening-port.sh

---
+++ Bash/Pi-Hole/change-the-pihole-listening-port.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+clear
+
+if [ "${EUID}" -eq '0' ]; then
+    printf "%s\n\n" 'You must run this script WITHOUT root/sudo.'
+    exit 1
+fi
+
+#
+# IF NOT INSTALLED, INSTALL LIGHTTPD
+#
+
+if ! sudo dpkg -l | grep -o 'lighttpd' &>/dev/null; then
+    sudo apt -y install lighttpd
+    clear
+fi
+
+pihole_ip="$(ip route get 1.2.3.4 | awk '{print $7}')"
+
+# CHECK TO SEE IF YOU PASSED THE NEW PORT NUMBER AS THE FIRST ARGUMENT TO THE SCRIPT
+if [ -z "${1}" ]; then
+    clear
+    read -p 'Please enter the new listening port for Pi-Hole (example: 20000): ' custom_port
+    clear
+else
+    custom_port="${1}"
+fi
+
+#
+# MODIFY LIGHTHTTPD'S CONFIG FILE PORT NUMBER
+#
+
+if ! sudo sed -E -i "s/^server\.port                 \= [0-9]+/server\.port                 = ${custom_port}/g" '/etc/lighttpd/lighttpd.conf'; then
+    printf "%s\n\n" 'Failed to modify: /etc/lighttpd/lighttpd.conf'
+    exit 1
+fi
+
+#
+# WHITELIST THE NEW PORT NUMBER IN UFW SO SSH DOESN'T STOP WORKING
+#
+
+clear
+if ! sudo dpkg -l | grep -o 'ufw' &>/dev/null; then
+    sudo apt -y install ufw
+fi
+sudo ufw allow "${custom_port}"/tcp
+
+#
+# RESTART APACHE2 IF INSTALLED
+#
+
+if sudo dpkg -l | grep -o 'apache2' &>/dev/null; then
+    sudo service apache2 restart
+fi
+
+#
+# RESTART LIGHTTPD
+#
+
+if ! sudo service lighttpd restart; then
+    sleep 3
+    sudo service lighttpd restart
+    clear
+fi
+
+# THE NEW WEB GUI WILL LOOK SOMETHING LIKE DEPENDING
+# ON IF YOU CHANGED THE NUMBER 100 TO SOMETHING ELSE
+
+clear
+printf "%s\n\n%s\n\n"                            \
+    'Pi-Hole'\''s new web address is show below' \
+    "http://${pihole_ip}:${custom_port}/admin"
