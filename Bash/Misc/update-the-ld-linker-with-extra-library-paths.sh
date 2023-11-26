fb6fa4b0ba8bfb1379c5bd3b8e30ad8f6d379fe0
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-11-26 03:50:05 -0500

    Update update-the-ld-linker-with-extra-library-paths.sh

--- Bash/Misc/update-the-ld-linker-with-extra-library-paths.sh
+++ Bash/Misc/update-the-ld-linker-with-extra-library-paths.sh
@@ -8,7 +8,7 @@ if [ "${EUID}" -ne '0' ]; then
 fi
 
 printf "%s\n%s\n\n"                                           \
-    'Creating a custom config file for the ld linker command' \
+    'Creating a custom config file for the ld linker'         \
     '======================================================='
 sleep 2
 
@@ -19,6 +19,7 @@ cat > '/etc/ld.so.conf.d/jman-custom-libraries.conf' <<'EOF'
 /usr/local/x86_64-linux-gnu/lib
 /usr/local/ssl/lib
 /usr/local/lib64
+/usr/local/lib/gprofng
 /usr/local/lib
 /usr/local/cuda-12.3/targets/x86_64-linux/lib
 /usr/local/cuda-12.3/nvvm/lib64
@@ -28,13 +29,6 @@ cat > '/etc/ld.so.conf.d/jman-custom-libraries.conf' <<'EOF'
 /lib64
 /lib
 EOF
-
-random_dir="$(mktemp -d)"
-
-# REMOVE ANY DUPLICATE LINES TO AVOIDE UNWATED LINKER OVERLOAD
-awk '!NF || !seen[$0]++' '/etc/ld.so.conf.d/jman-custom-libraries.conf' > "${random_dir}/jman-custom-libraries.conf"
-sudo cp -f "${random_dir}/jman-custom-libraries.conf" '/etc/ld.so.conf.d/'
-sudo rm -fr "${random_dir}"
 
 clear
 sudo ldconfig -v
