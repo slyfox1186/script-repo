26a626dd9d6635e9778dd381b20f911abc1670f2
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-11-29 17:23:35 -0500

    Update untar.sh

--- Bash/Misc/untar.sh
+++ Bash/Misc/untar.sh
@@ -16,7 +16,7 @@
 ##  2) Add the untar function to your .bash_functions or .bashrc file and source it or restart your terminal
 ##
 ##  3) Simply type "untar" in the same directory as the above archive files
-##     - Second method is to run this script in the same direcotry with the archive files using the command "bash untar.sh"
+##     - The second method is to run this script in the same directory with the archive files using the command "bash untar.sh"
 ##
 ##  Result: All archives will be extracted and ready to use
 ##
