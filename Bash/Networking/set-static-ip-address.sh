d4ed69dbad367bd368d21384ac6aa5109821c8d2
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-12-02 21:10:29 -0500

    Update set-static-ip-address.sh

--- Bash/Networking/set-static-ip-address.sh
+++ Bash/Networking/set-static-ip-address.sh
@@ -26,6 +26,11 @@ activate_fn()
     fi
 
 cat > "${fname}" <<EOF
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+source /etc/network/interfaces.d/*
+
 # The loopback network interface
 auto lo
 iface lo inet loopback
