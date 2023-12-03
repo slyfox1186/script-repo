99c240588b8c5ffe84644971cb33e42258f9e939
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-12-03 10:24:33 -0500

    Update ld-linker-search-paths.sh

--- Bash/Misc/ld-linker-search-paths.sh
+++ Bash/Misc/ld-linker-search-paths.sh
@@ -16,6 +16,12 @@ cat > '/etc/ld.so.conf.d/user-local-libs.conf' <<'EOF'
 /usr/local/x86_64-linux-gnu/lib
 /usr/local/cuda/nvvm/lib64
 /usr/local/cuda/targets/x86_64-linux/lib
+/usr/local/lib64
+/usr/local/lib
+/usr/lib64
+/usr/lib
+/lib64
+/lib
 EOF
 
 clear
