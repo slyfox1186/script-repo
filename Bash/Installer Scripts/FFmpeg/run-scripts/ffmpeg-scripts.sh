8784c2561a371a080888dd7c9a98e7ce93f0046e
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-12-03 02:05:20 -0500

    Update ffmpeg-scripts.sh

--- Bash/Installer Scripts/FFmpeg/run-scripts/ffmpeg-scripts.sh
+++ Bash/Installer Scripts/FFmpeg/run-scripts/ffmpeg-scripts.sh
@@ -5,7 +5,7 @@ clear
 # CREATE VARIABLES
 parent_dir="${PWD}"
 tmp_dir="$(mktemp -d)"
-scripts=(ffmpeg ffpb ffpb-loop loop-paths.sh)
+scripts=('ffmpeg' 'ffpb' 'ffpb-loop' 'ffpb-loop-paths')
 
 # CREATE AND CD INTO A RANDOM DIRECTORY
 cd "${tmp_dir}" || exit 1
@@ -17,6 +17,7 @@ wget -qN - -i 'https://raw.githubusercontent.com/slyfox1186/script-repo/main/Bas
 sudo mv 'convert-x265-cuda-ffmpeg.sh' 'ffmpeg'
 sudo mv 'convert-x265-cuda-ffpb.sh' 'ffpb'
 sudo mv 'convert-x265-cuda-ffpb-loop.sh' 'ffpb-loop'
+sudo mv 'convert-x265-cuda-ffpb-loop-paths.sh' 'ffpb-loop-paths'
 
 # MOVE THE SCRIPTS TO THE ORIGINAL DIRECTORY THE SCRIPT WAS EXECUTED FROM
 sudo mv "${scripts[@]}" "${parent_dir}"
@@ -28,13 +29,13 @@ cd "${parent_dir}" || exit 1
 for script in "${scripts[@]}"
 do
     sudo chown "${USER}":"${USER}" -R "${script}"
-    sudo chmod +rwx -R "${script}"
+    sudo chmod a+rwx -R "${script}"
 done
+
+# DELETE THIS SCRIPT
+if [ -f 'ffmpeg-scripts.sh' ]; then
+    sudo rm 'ffmpeg-scripts.sh'
+fi
 
 # DELETE THE RANDOM DIRECTORY
 sudo rm -fr "${tmp_dir}"
-
-# DELETE THIS SCRIPT
-if [ -f "${0}" ]; then
-    sudo rm "${0}"
-fi
