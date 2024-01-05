3be40f371c46bdfabefd5a948511d667b5ce38ae
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-05 08:45:57 -0500

    Update youtube-downloader.sh

--- Bash/Installer Scripts/FFmpeg/run-scripts/youtube-downloader.sh
+++ Bash/Installer Scripts/FFmpeg/run-scripts/youtube-downloader.sh
@@ -2,14 +2,33 @@
 
 clear
 
-ouput_dir='/path/to/Youtube-Download'
+ouput_dir='/path/to/directory'
+filename="$ouput_dir/%(title)s.%(ext)s"
+regex='\.txt$'
 user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
 ff="$(type -P ffmpeg)"
-regex='\.txt$'
+format='bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b'
 
-# Check if the first argument being passed ends in '.txt'
+# Check if the first argument passed ends in ".txt"
 if [[ $1 =~ $regex ]]; then
-    yt-dlp --ffmpeg-location "$ff" --audio-quality 0 -f "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b" --windows-filenames --user-agent "$user_agent" --progress --paths "all:$ouput_dir" --batch-file "$1"
+    yt-dlp --ffmpeg-location $ff    \
+           --audio-quality 3        \
+           -f $format               \
+           --embed-thumbnail        \
+           --windows-filenames      \
+           --user-agent $user_agent \
+           --progress               \
+           --paths "$ouput_dir"     \
+           --batch-file             \
+           "$1"
 else
-    yt-dlp --ffmpeg-location "$ff" --audio-quality 0 -f "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b" --windows-filenames --user-agent "$user_agent" --progress -o "$ouput_dir/%(title)s.%(ext)s" "$@"
+    yt-dlp --ffmpeg-location $ff    \
+           --audio-quality 3        \
+           -f $format               \
+           --embed-thumbnail        \
+           --windows-filenames      \
+           --user-agent $user_agent \
+           --progress               \
+           -o "$filename"           \
+           "$@"
 fi
