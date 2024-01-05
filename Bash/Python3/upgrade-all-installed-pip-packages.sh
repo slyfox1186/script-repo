69d5b5ccf8d0c38654efa518abf785aba3383090
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-05 03:11:43 -0500

    Update and rename upgrade-all-installed-pip-packages.sh to upgrade-all-installed-pip-packages.sh

--- Bash/Python3/upgrade-all-installed-pip-packages.sh
+++ Bash/Python3/upgrade-all-installed-pip-packages.sh
@@ -6,6 +6,8 @@ list_pkgs="$(pip list | awk '{print $1}')"
 
 for p in ${list_pkgs[@]}
 do
-    pip install --upgrade $p
+    if [ $p != wxPython ]; then
+        pip install --user --upgrade $p
+    fi
     echo
 done
