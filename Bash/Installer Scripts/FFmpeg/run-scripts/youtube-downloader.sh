834c4af30ec87d144fa188ba46c8be6351ddf20b
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-05 08:23:46 -0500

    Create youtube-downloader.sh

---
+++ Bash/Installer Scripts/FFmpeg/run-scripts/youtube-downloader.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+clear
+
+ouput_dir='/path/to/Youtube-Download'
+regex='\.txt$'
+user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+ff="$(type -P ffmpeg)"
+
+if [[ $1 =~ $regex ]]; then
+    yt-dlp --ffmpeg-location "$ff" --audio-quality 0 -f "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b" --windows-filenames --user-agent "$user_agent" --progress --paths "all:$ouput_dir" --batch-file "$1"
+else
+    yt-dlp --ffmpeg-location "$ff" --audio-quality 0 -f "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b" --windows-filenames --user-agent "$user_agent" --progress -o "$ouput_dir/%(title)s.%(ext)s" "$@"
+fi
