6b0f0460b818bc06cc6b88b1872d40bafe961411
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-05 18:57:08 -0800

    Update upgrade-all-installed-pip-packages.sh

--- Bash/Python3/upgrade-all-installed-pip-packages.sh
+++ Bash/Python3/upgrade-all-installed-pip-packages.sh
@@ -4,10 +4,11 @@ clear
 
 list_pkgs="$(pip list | awk '{print $1}')"
 
+pip install --user --upgrade pip
+
 for p in ${list_pkgs[@]}
 do
     if [ $p != wxPython ]; then
-        pip install --user --upgrade pip
         pip install --user --upgrade $p
     fi
     echo
