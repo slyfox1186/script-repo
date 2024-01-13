4039bc6064d676a7bf2f694a1daf3b49f3f3b618
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-13 13:32:52 -0800

    Update jammy-mirrors.sh

--- Bash/Ubuntu Scripts/jammy/jammy-mirrors.sh
+++ Bash/Ubuntu Scripts/jammy/jammy-mirrors.sh
@@ -2,36 +2,41 @@
 
 clear
 
-fname='/etc/apt/sources.fname'
+fname='/etc/apt/sources.list'
 
 # make a backup of the file
-if [ ! -f "${fname}.bak" ]; then
-    cp -f "${fname}" "${fname}.bak"
+if [ ! -f "$fname".bak ]; then
+    cp -f "$fname" "$fname".bak
 fi
 
-cat > "${fname}" <<EOF
-deb https://mirror.enzu.com/ubuntu/ jammy main restricted universe multiverse
-deb https://mirror.enzu.com/ubuntu/ jammy-updates main restricted universe multiverse
-deb https://mirror.enzu.com/ubuntu/ jammy-backports main restricted universe multiverse
+cat > "$fname" <<EOF
+deb https://atl.mirrors.clouvider.net/ubuntu/ jammy main restricted universe multiverse
+deb https://atl.mirrors.clouvider.net/ubuntu/ jammy-updates main restricted universe multiverse
+deb https://atl.mirrors.clouvider.net/ubuntu/ jammy-backports main restricted universe multiverse
 deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
 EOF
 
 # OPEN AN EDITOR TO VIEW THE CHANGES
 if type -P gnome-text-editor &>/dev/null; then
-    sudo gnome-text-editor "${fname}"
+    sudo gnome-text-editor "$fname"
 elif type -P gedit &>/dev/null; then
-    sudo gedit "${fname}"
+    sudo gedit "$fname"
 elif type -P nano &>/dev/null; then
-    sudo nano "${fname}"
+    sudo nano "$fname"
+elif type -P vim &>/dev/null; then
+    sudo vim "$fname"
 elif type -P vi &>/dev/null; then
-    sudo vi "${fname}"
+    sudo vi "$fname"
 else
-    printf "\n%s\n\n" "Could not find an EDITOR to open the file: ${fname}"
+    printf "\n%s\n\n" "Could not find an EDITOR to open: $fname"
     exit 1
 fi
 
 if [[ "${0}" == 'jammy-mirrors' ]]; then
     sudo rm 'jammy-mirrors'
-else
+elif [[ "${0}" == 'jammy-mirrors.sh' ]]; then
     sudo rm 'jammy-mirrors.sh'
+else
+    printf "%s\n\n" 'Unable to find and delete the sources.list shell script.'
+    exit 1
 fi
