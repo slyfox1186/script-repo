e95f7981db185eb160e4ca7818c3a1fb886809d1
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-13 18:29:21 -0800

    Update raspi-mirrors.sh

--- Bash/Raspbian OS/raspi-mirrors.sh
+++ Bash/Raspbian OS/raspi-mirrors.sh
@@ -5,11 +5,11 @@ clear
 fname='/etc/apt/sources.list'
 
 # Create a backup of the sources.list file
-if [ ! -f "${fname}.bak" ]; then
-    sudo cp -f "${fname}" "${fname}.bak"
+if [ ! -f "$fname".bak ]; then
+    sudo cp -f "$fname" "$fname".bak
 fi
 
-cat > "${fname}" <<'EOF'
+cat > "$fname" <<'EOF'
 # Custom Atlanta GA Mirrors
 deb http://atl.mirrors.clouvider.net/debian/ bookworm main contrib non-free non-free-firmware
 deb http://atl.mirrors.clouvider.net/debian/ bookworm-updates main contrib non-free non-free-firmware
@@ -22,13 +22,22 @@ deb http://deb.debian.org/debian bookworm-backports main contrib non-free non-fr
 deb http://deb.debian.org/debian-security/ bookworm-security main contrib non-free non-free-firmware
 EOF
 
-# Open the sources.list file for review
+# OPEN AN EDITOR TO VIEW THE CHANGES
 if type -P gnome-text-editor &>/dev/null; then
-    sudo gnome-text-editor "${fname}"
+    sudo gnome-text-editor "$fname"
 elif type -P gedit &>/dev/null; then
-    sudo gedit "${fname}"
+    sudo gedit "$fname"
 elif type -P nano &>/dev/null; then
-    sudo nano "${fname}"
+    sudo nano "$fname"
+elif type -P vim &>/dev/null; then
+    sudo vim "$fname"
+elif type -P vi &>/dev/null; then
+    sudo vi "$fname"
 else
-    fail_fn 'Could not find an EDITOR to open the updated sources.list'
+    printf "\n%s\n\n" "Could not find an EDITOR to open \"$fname\""
+    exit 1
+fi
+
+if [[ "${0}" == 'raspi-mirrors' ]]; then
+    sudo rm "${0}"
 fi
