2dab1e88a371555d93e600beee2e84e66a540ff0
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-16 07:40:24 -0800

    Update mirrors-menu.sh

--- Bash/Installer Scripts/SlyFox1186 Scripts/mirrors-menu.sh
+++ Bash/Installer Scripts/SlyFox1186 Scripts/mirrors-menu.sh
@@ -1,172 +1,67 @@
 #!/usr/bin/env bash
-#shellcheck disable=SC2000,SC2034,2086,SC2162
 
 clear
 
-user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+echo -e "Starting Linux Mirrors Script...\n"
 
-#
-# MENU COLOR FUNCTIONS
-#
+# Define Color functions
+ColorGreen() { echo -ne "\e[32m${1}\e[0m"; }
+ColorBlue() { echo -ne "\e[34m${1}\e[0m"; }
 
-green='\e[32m'
-blue='\e[34m'
-clear='\e[0m'
-ColorGreen() { echo -ne "${green}${1}${clear}"; }
-ColorBlue() { echo -ne "${blue}${1}${clear}"; }
-
-#
-# SHOW SCRIPT BANNER
-#
-
-box_out_banner() {
-    input_char=$(echo "${@}" | wc -c)
-    line=$(for i in $(seq 0 ${input_char}); do printf '-'; done)
-    tput bold
-    line="$(tput setaf 3)${line}"
-    space=${line//-/ }
-    echo " ${line}"
-    printf '|' ; echo -n "$space" ; printf "%s\n" '|';
-    printf '| ' ;tput setaf 4; echo -n "${@}"; tput setaf 3 ; printf "%s\n" ' |';
-    printf '|' ; echo -n "$space" ; printf "%s\n" '|';
-    echo " ${line}"
-    tput sgr 0
-}
-box_out_banner 'Linux Mirrors Script'
-
-#
-# DETERMINE WHAT DOWNLOADER TO USE
-#
-
-if ! sudo dpkg -l | grep -o curl &>/dev/null; then
-    sudo apt update
-    sudo apt -y install curl
-    clear
-fi
-
-#
-# DISPLAY DEBIAN MIRRORS MENU
-#
-
-debian_mirrors_fn() {
-    local answer
-echo -ne "
-$(ColorGreen '1)') Debian 11 - Bullseye
-$(ColorGreen '2)') Debian 12 - Bookworm
-$(ColorGreen '3)') Back
-$(ColorBlue 'Choose the Debian release version:') "
-    read answer
-    clear
-    case "${answer}" in
-        1)
-                curl -A "$user_agent" -Lso 'bullseye-mirrors' 'https://bullseye-mirrors.optimizethis.net'
-                sudo bash 'bullseye-mirrors'
-                rm 'bullseye-mirrors'
-                return 0
-                ;;
-        2)
-                curl -A "$user_agent" -Lso 'bookworm-mirrors' 'https://bookworm-mirrors.optimizethis.net'
-                sudo bash 'bookworm-mirrors'
-                rm 'bookworm-mirrors'
-                return 0
-                ;;
-        3)      main_menu;;
-        *)
-                unset answer
-                clear
-                debian_mirrors_fn
-                ;;
-    esac
-    clear
-}
-clear
-
-#
-# DISPLAY UBUNTU MIRRORS MENU
-#
-
-ubuntu_mirrors_fn() {
-    local answer
-echo -ne "
-$(ColorGreen '1)') Ubuntu 23.04 - Lunar Lobster
-$(ColorGreen '2)') Ubuntu 22.04 - Jammy Jellyfish
-$(ColorGreen '3)') Ubuntu 20.04 - Focal Fossa
-$(ColorGreen '4)') Ubuntu 18.04 - Bionic Beaver
-$(ColorGreen '0)') Back
-$(ColorBlue 'Choose the Ubuntu release version:') "
-    read answer
-    clear
-    case "${answer}" in
-        1)
-                curl -A "$user_agent" -Lso 'lunar-mirrors' 'https://lunar-mirrors.optimizethis.net'
-                sudo bash 'lunar-mirrors'
-                rm 'lunar-mirrors'
-                return 0
-                ;;
-        2)
-                curl -A "$user_agent" -Lso 'jammy-mirrors' 'https://jammy.optimizethis.net'
-                sudo bash 'jammy-mirrors'
-                rm 'jammy-mirrors'
-                return 0
-                ;;
-        3)
-                curl -A "$user_agent" -Lso 'focal-mirrors' 'https://focal-mirrors.optimizethis.net'
-                sudo bash 'focal-mirrors'
-                rm 'focal-mirrors'
-                return 0
-                ;;
-        4)
-                curl -A "$user_agent" -Lso 'bionic-mirrors' 'https://bionic-mirrors.optimizethis.net'
-                sudo bash 'bionic-mirrors'
-                rm 'bionic-mirrors'
-                return 0
-                ;;
-        0)      main_menu;;
-        *)
-                unset answer
-                clear
-                ubuntu_mirrors_fn
-                ;;
-    esac
-    clear
+# Function to download and execute mirrors script
+execute() {
+    local url="$1"
+    local output_file="$2"
+    curl -Lso "$output_file" "$url"
+    sudo bash "$output_file"
+    if [ -f "$output_file" ]; then
+        rm "$output_file"
+    fi
 }
 
-#
-# DISPLAY THE MAIN MENU
-#
-
+# Function to display the main menu
 main_menu() {
-    local answer
-echo -ne "
-$(ColorGreen '1)') Ubuntu
-$(ColorGreen '2)') Debian
-$(ColorGreen '3)') Raspberry Pi (Bookworm)
-$(ColorGreen '4)') Arch Linux
-$(ColorGreen '5)') Exit
-$(ColorBlue 'Choose an operating system:') "
-    read answer
+    echo -e "$(ColorGreen '1)') Ubuntu\n$(ColorGreen '2)') Debian\n$(ColorGreen '3)') Raspberry Pi (Bookworm)\n$(ColorGreen '4)') Arch Linux\n$(ColorGreen '5)') Exit"
+    read -p "$(ColorBlue 'Choose an operating system: ')" answer
+    echo -e "\nYou selected option $answer."
     clear
     case "${answer}" in
-        1)      ubuntu_mirrors_fn;;
-        2)      debian_mirrors_fn;;
-        3)
-                curl -A "$user_agent" -Lso 'raspi-mirrors' 'https://raspi-mirrors.optimizethis.net'
-                sudo bash 'raspi-mirrors'
-                return 0
-                ;;
+        1) 
+            echo -e "$(ColorGreen '1)') Ubuntu 23.04 - Lunar Lobster\n$(ColorGreen '2)') Ubuntu 22.04 - Jammy Jellyfish\n$(ColorGreen '3)') Ubuntu 20.04 - Focal Fossa\n$(ColorGreen '4)') Ubuntu 18.04 - Bionic Beaver\n$(ColorGreen '0)') Back"
+            read -p "$(ColorBlue 'Choose the Ubuntu release version: ')" answer
+            echo -e "\nYou selected option $answer."
+            clear
+            case "${answer}" in
+                1) execute 'https://lunar-mirrors.optimizethis.net' 'lunar-mirrors';;
+                2) execute 'https://jammy.optimizethis.net' 'jammy-mirrors';;
+                3) execute 'https://focal-mirrors.optimizethis.net' 'focal-mirrors';;
+                4) execute 'https://bionic-mirrors.optimizethis.net' 'bionic-mirrors';;
+                0) main_menu;;
+                *) unset answer; clear; main_menu;;
+            esac
+            ;;
+        2)
+            echo -e "$(ColorGreen '1)') Debian 11 - Bullseye\n$(ColorGreen '2)') Debian 12 - Bookworm\n$(ColorGreen '3)') Back"
+            read -p "$(ColorBlue 'Choose the Debian release version: ')" answer
+            echo -e "\nYou selected option $answer."
+            clear
+            case "${answer}" in
+                1) execute 'https://bullseye-mirrors.optimizethis.net' 'bullseye-mirrors';;
+                2) execute 'https://bookworm-mirrors.optimizethis.net' 'bookworm-mirrors';;
+                3) main_menu;;
+                *) unset answer; clear; main_menu;;
+            esac
+            ;;
+        3) execute 'https://raspi-mirrors.optimizethis.net' 'raspi-mirrors';;
         4)
-                curl -A "$user_agent" -Lso 'archlinux-mirrors' 'https://raw.githubusercontent.com/slyfox1186/script-repo/main/Bash/Arch%20Linux%20Scripts/archlinux-mirrors.sh'
-                sudo bash 'archlinux-mirrors'
-                return 0
-                ;;
-        5)      return 0;;
-        *)
-                unset answer
-                clear
-                main_menu
-                ;;
+            sudo mkdir -p /etc/pacman.d # Create the directory if it doesn't exist
+            execute 'https://raw.githubusercontent.com/slyfox1186/script-repo/main/Bash/Arch%20Linux%20Scripts/archlinux-mirrors.sh' 'archlinux-mirrors'
+            ;;
+        5) return 0;;
+        *) unset answer; clear; main_menu;;
     esac
     clear
 }
 
+# Initial call to the main menu
 main_menu
