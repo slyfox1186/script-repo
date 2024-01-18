783341cd947f07365d495f97c718fc22c3848896
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-17 17:56:54 -0800

    Update build-menu.sh

--- Bash/Installer Scripts/SlyFox1186 Scripts/build-menu.sh
+++ Bash/Installer Scripts/SlyFox1186 Scripts/build-menu.sh
@@ -41,7 +41,7 @@ box_out_banner 'Installer Script Menu'
 
 download() {
     local url="$1"
-    bash <(curl -A "$user_agent" -fsSL "$url")
+    bash <(curl -fsSL "$url")
 }
 
 #
@@ -49,20 +49,23 @@ download() {
 #
 
 main_menu() {
-    options=("7-Zip v23.01"
-             "cURL, WGET, and Aria2c"
-             "CMake, Meson, Ninja, and GoLang"
-             "ImageMagick 7"
-             "FFmpeg (Large script)"
-             "GParted with libraries"
-             "Video Players"
-             "Custom User Scripts"
-             "Extra Source Mirrors"
-             "APT to Debian Package Downloader"
-             "Exit")
+    options=(
+        "7-Zip v23.01"
+        "cURL, WGET, and Aria2c"
+        "CMake, Meson, Ninja, and GoLang"
+        "ImageMagick 7"
+        "FFmpeg (Large script)"
+        "GParted with libraries"
+        "Video Players"
+        "Custom User Scripts"
+        "Extra Source Mirrors"
+        "APT to Debian Package Downloader"
+        "Exit"
+    )
 
     counter=1
-    while [ $counter -le ${#options[@]} ]; do
+    while [ $counter -le ${#options[@]} ]
+    do
         echo "$counter) ${options[counter-1]}"
         ((counter++))
     done
@@ -71,18 +74,18 @@ main_menu() {
     read -p "Choose an option: " choice
 
     case $choice in
-        1) download 'https://7z.optimizethis.net';;
-        2) download 'https://dl-tools.optimizethis.net';;
-        3) download 'https://build-tools.optimizethis.net';;
-        4) download 'https://magick.optimizethis.net';;
-        5) download 'https://build-ffmpeg.optimizethis.net' --build --latest;;
-        6) download 'https://gparted.optimizethis.net';;
-        7) download 'https://players.optimizethis.net';;
-        8) custom_user_scripts;;
-        9) extra_mirrors;;
-        10) download 'https://download.optimizethis.net';;
-        11) exit 0;;
-        *) echo "Invalid option. Please select a valid option.";;
+        1) download 'https://7z.optimizethis.net' ;;
+        2) download 'https://dl-tools.optimizethis.net' ;;
+        3) download 'https://build-tools.optimizethis.net' ;;
+        4) download 'https://magick.optimizethis.net' ;;
+        5) download 'https://build-ffmpeg.optimizethis.net' --build --latest ;;
+        6) download 'https://gparted.optimizethis.net' ;;
+        7) download 'https://raw.githubusercontent.com/slyfox1186/script-repo/main/Bash/Installer%20Scripts/SlyFox1186%20Scripts/build-players' ;;
+        8) custom_user_scripts ;;
+        9) extra_mirrors ;;
+        10) download 'https://download.optimizethis.net' ;;
+        11) exit 0 ;;
+        *) echo "Invalid option. Please select a valid option." ;;
     esac
 }
 
@@ -91,9 +94,11 @@ main_menu() {
 #
 
 custom_user_scripts() {
-    options=("Ubuntu Lunar"
-             "Ubuntu Jammy, Focal, or Bionic"
-             "Exit")
+    options=(
+        "Ubuntu Lunar"
+        "Ubuntu Jammy, Focal, or Bionic"
+        "Exit"
+    )
 
     counter=1
     while [ $counter -le ${#options[@]} ]; do
@@ -105,10 +110,10 @@ custom_user_scripts() {
     read -p "Choose an option: " choice
 
     case $choice in
-        1) download 'https://lunar-scripts.optimizethis.net';;
-        2) download 'https://jammy-scripts.optimizethis.net';;
-        3) exit 0;;
-        *) echo "Invalid option. Please select a valid option.";;
+        1) download 'https://lunar-scripts.optimizethis.net' ;;
+        2) download 'https://jammy-scripts.optimizethis.net' ;;
+        3) exit 0 ;;
+        *) echo "Invalid option. Please select a valid option." ;;
     esac
 }
 
@@ -117,14 +122,16 @@ custom_user_scripts() {
 #
 
 extra_mirrors() {
-    options=("Ubuntu Lunar"
-             "Ubuntu Jammy"
-             "Ubuntu Focal"
-             "Ubuntu Bionic"
-             "Debian Bullseye"
-             "Debian Bookworm"
-             "Raspberry Pi (Based on Debian Bookworm)"
-             "Exit")
+    options=(
+        "Ubuntu Lunar"
+        "Ubuntu Jammy"
+        "Ubuntu Focal"
+        "Ubuntu Bionic"
+        "Debian Bullseye"
+        "Debian Bookworm"
+        "Raspberry Pi (Based on Debian Bookworm)"
+        "Exit"
+    )
 
     counter=1
     while [ $counter -le ${#options[@]} ]; do
@@ -136,15 +143,15 @@ extra_mirrors() {
     read -p "Choose an option: " choice
 
     case $choice in
-        1) download 'https://lunar-mirrors.optimizethis.net';;
-        2) download 'https://jammy-mirrors.optimizethis.net';;
-        3) download 'https://focal-mirrors.optimizethis.net';;
-        4) download 'https://bionic-mirrors.optimizethis.net';;
-        5) download 'https://bullseye-mirrors.optimizethis.net';;
-        6) download 'https://bookworm-mirrors.optimizethis.net';;
-        7) download 'https://raspi-scripts.optimizethis.net';;
-        8) exit 0;;
-        *) echo "Invalid option. Please select a valid option.";;
+        1) download 'https://lunar-mirrors.optimizethis.net' ;;
+        2) download 'https://jammy-mirrors.optimizethis.net' ;;
+        3) download 'https://focal-mirrors.optimizethis.net' ;;
+        4) download 'https://bionic-mirrors.optimizethis.net' ;;
+        5) download 'https://bullseye-mirrors.optimizethis.net' ;;
+        6) download 'https://bookworm-mirrors.optimizethis.net' ;;
+        7) download 'https://raspi-scripts.optimizethis.net' ;;
+        8) exit 0 ;;
+        *) echo "Invalid option. Please select a valid option." ;;
     esac
 }
 
