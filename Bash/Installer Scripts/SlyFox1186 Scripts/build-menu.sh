f60b15dbaac417fcd8bee6013f3f7b0b76f19c08
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-24 17:18:08 -0500

    update browser user agents

--- Bash/Installer Scripts/SlyFox1186 Scripts/build-menu.sh
+++ Bash/Installer Scripts/SlyFox1186 Scripts/build-menu.sh
@@ -2,7 +2,7 @@
 
 clear
 
-user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
 
 #
 # MENU COLOR FUNCTIONS
