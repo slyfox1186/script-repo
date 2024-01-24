6394d9f3afdf9f5bb2a53cd691d4d73148f315f2
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-24 17:18:41 -0500

    update browser user agents

--- Bash/Aria2/aria2-start-stop-deamon.sh
+++ Bash/Aria2/aria2-start-stop-deamon.sh
@@ -370,7 +370,7 @@ use-head=false
 # Set user agent for HTTP(S) downloads. Default: aria2/$VERSION, $VERSION is replaced by package version.
 user-agent=${user_agent}
 # Set user agent for HTTP(S) downloads. Default: aria2/$VERSION, $VERSION is replaced by package version.
-user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
 EOF
 
     # CD INTO DIRECTORIES
--- Bash/Debian Scripts/bookworm/user-scripts/bookworm-scripts.sh
+++ Bash/Debian Scripts/bookworm/user-scripts/bookworm-scripts.sh
@@ -9,7 +9,7 @@ clear
 
 random_dir="$(mktemp -d)"
 url='https://raw.githubusercontent.com/slyfox1186/script-repo/main/Bash/Debian%20Scripts/bookworm/user-scripts/bookworm-scripts.txt'
-user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
 script_array=('.bashrc' '.bash_aliases' '.bash_functions')
 
 #
