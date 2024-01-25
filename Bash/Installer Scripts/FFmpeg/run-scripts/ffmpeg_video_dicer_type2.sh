b122a9617c22de104d861642d505018c97e161d5
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-24 19:38:45 -0800

    Update ffmpeg_video_dicer_type2.sh

--- Bash/Installer Scripts/FFmpeg/run-scripts/ffmpeg_video_dicer_type2.sh
+++ Bash/Installer Scripts/FFmpeg/run-scripts/ffmpeg_video_dicer_type2.sh
@@ -2,8 +2,8 @@
 
 concat_video_segments() {
     local video_path=$1
-    local starts=("$2")
-    local stops=("$3")
+    local starts=("${@:2:$(($#/2))}") # First half of arguments are 'starts'
+    local stops=("${@:$(($#/2 + 2))}") # Second half of arguments are 'stops'
 
     local ext="${video_path##*.}"
     local output_name="$(basename "$video_path" ."$ext")-OUT.$ext"
@@ -25,21 +25,33 @@ concat_video_segments() {
     cat "$concat_file"
 
     echo "Concatenating to $output_name"
-    ffmpeg -hide_banner -y -f concat -safe 0 -i "$concat_file" -c copy "$output_name"
+    ffmpeg -hide_banner -y -f concat -safe 0 -i "$concat_file" -c copy "$PWD/$output_name"
 
     # Clean up temporary files
     rm -r "$tmp_dir"
 }
 
 START_TIMES=(
-00:12:12
-00:16:00
-00:16:32
+00:12:12 # PAIR 01
+00:16:00 # PAIR 02
+00:16:32 # ECT...
+00:19:36
+00:20:19
+00:23:48
+00:26:44
+00:27:36
+00:29:18
 )
 STOP_TIMES=(
-00:14:51
-00:16:28
-00:17:12
+00:14:51 # PAIR 01
+00:16:28 # PAIR 02
+00:17:12 # ECT...
+00:20:14
+00:20:42
+00:26:37
+00:27:31
+00:29:10
+00:30:27
 )
 
 # Video path and start/stop times
