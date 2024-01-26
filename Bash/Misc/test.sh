0e41c91660a1fc8a671489a7e7730b19664541cb
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-26 13:08:51 -0800

    Update test.sh

--- Bash/Misc/test.sh
+++ Bash/Misc/test.sh
@@ -1,23 +1,31 @@
-#!/usr/bin/env bash
+# Clear the terminal
+clear
 
-# Function to retrieve the latest Git tag version from a repository
-get_latest_git_tag() {
-    local url="$1"
-    local tag_version
-
-    tag_version=$(git ls-remote --tags "$url" | awk -F/ '/[0-9]+\.[0-9]+[0-9\.]*$/ {print $NF}' | grep -Eo '[0-9\.]+' | sort -V | tail -n1)
-    
-    echo "$tag_version"
+# Function to retrieve the latest Git tag version from multiple repositories
+git_tag() {
+    for url in "$@"
+    do
+        result=$(git ls-remote --tags "$url" | \
+        awk -F/ '/[0-9]+[0-9_.]+[0-9_.]*$/ {print $NF}' | \
+        grep -Eo '[0-9.\_]+' | \
+        sort -V | \
+        tail -n1)
+        
+        # Replace underscores with dots
+        result=${result//_/\.}
+        
+        echo "$result"
+    done
 }
 
-# Check if a URL is provided as an argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <repo_url>"
-    exit 1
+# Check if script is being run directly and not sourced
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    # Check if at least one URL is provided as an argument
+    if [ $# -eq 0 ]; then
+        echo "Usage: $0 <repo_url1> [<repo_url2> ...]"
+        exit 1
+    fi
+
+    # Call the git_tag function with provided repository URLs
+    git_tag "$@"
 fi
-
-repo_url="$1"
-latest_tag="$(get_latest_git_tag "$repo_url")"
-
-# Return the latest tag to the caller script
-echo "$latest_tag"
