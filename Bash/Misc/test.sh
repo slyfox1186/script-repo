b05603e5e5cc68bed86129d166c1c79dc8ac2d71
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-26 04:48:32 -0800

    Update test.sh

--- Bash/Misc/test.sh
+++ Bash/Misc/test.sh
@@ -17,7 +17,7 @@ if [ -z "$1" ]; then
 fi
 
 repo_url="$1"
-latest_tag="$(get_latest_git_tag)"
+latest_tag="$(get_latest_git_tag "$repo_url")"
 
 # Return the latest tag to the caller script
 echo "$latest_tag"
