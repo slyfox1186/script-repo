aa379329a42d6cecf39a4c22ccff9608917834c9
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-01-26 12:47:45 -0800

    Update source-git-version.sh

--- Bash/Misc/source-git-version.sh
+++ Bash/Misc/source-git-version.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
 
-# Clear the terminal
-clear
-
 # Function to retrieve the latest Git tag version from a repository
 get_latest_git_tag() {
     local url="$1"
     local tag_version
 
-    # Extracting the version number using a regular expression
-    tag_version=$(git ls-remote --tags "$url" | awk -F/ '{print $NF}' | grep -Eo '([0-9]+[-_])+[0-9]+' | sort -V | tail -n1)
-
-    # Replace underscores with dots in the version number
-    tag_version=${tag_version//_/.}
+    tag_version=$(git ls-remote --tags "$url" | awk -F/ '/[0-9]+[0-9_.]+[0-9_.]*$/ {print $NF}' | grep -Eo '[0-9.]+' | sort -V | tail -n1)
     
     echo "$tag_version"
 }
