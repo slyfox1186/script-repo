aa82870eda3efbd66920aaff8fdced6374f1a691
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-02-20 17:58:56 -0500

    Update ffmpeg-scripts.sh

--- Bash/Installer Scripts/FFmpeg/run-scripts/ffmpeg-scripts.sh
+++ Bash/Installer Scripts/FFmpeg/run-scripts/ffmpeg-scripts.sh
@@ -1,39 +1,34 @@
 #!/usr/bin/env bash
 
-clear
-
 # CREATE VARIABLES
-parent_dir="${PWD}"
+parent_dir="$PWD"
 tmp_dir="$(mktemp -d)"
-scripts=('ffpb-loop' 'ffpb-loop-paths')
+scripts=("ffpb-loop" "ffpb-loop-paths" "convert-wmv-to-mp4")
 
 # CREATE AND CD INTO A RANDOM DIRECTORY
-cd "${tmp_dir}" || exit 1
+cd "$tmp_dir" || exit 1
 
 # DOWNLOAD THE SCRIPTS FROM GITHUB
-wget -qN - -i 'https://raw.githubusercontent.com/slyfox1186/script-repo/main/Bash/Installer%20Scripts/FFmpeg/run-scripts/ffmpeg-scripts.txt'
+wget -qN - -i "https://raw.githubusercontent.com/slyfox1186/script-repo/main/Bash/Installer%20Scripts/FFmpeg/run-scripts/ffmpeg-scripts.txt"
 
 # RENAME THE SCRIPTS
-sudo mv 'convert-x265-cuda-ffpb-loop.sh' 'ffpb-loop'
-sudo mv 'convert-x265-cuda-ffpb-loop-paths.sh' 'ffpb-loop-paths'
+sudo mv "convert-x265-cuda-ffpb-loop.sh" "ffpb-loop"
+sudo mv "convert-x265-cuda-ffpb-loop-paths.sh" "ffpb-loop-paths"
+sudo mv "convert-wmv-to-mp4.sh" "convert-wmv-to-mp4"
 
 # MOVE THE SCRIPTS TO THE ORIGINAL DIRECTORY THE SCRIPT WAS EXECUTED FROM
-sudo mv "${scripts[@]}" "${parent_dir}"
+sudo mv "${scripts[@]}" "$parent_dir"
 
 # CD BACK INTO THE ORIGINAL DIRECTORY
-cd "${parent_dir}" || exit 1
+cd "$parent_dir" || exit 1
 
 # CHANGE THE FILE PERMISSIONS OF EACH SCRIPT
 for script in "${scripts[@]}"
 do
-    sudo chown "${USER}":"${USER}" -R "${script}"
-    sudo chmod a+rwx -R "${script}"
+    sudo chown "$USER":"$USER" -R "$script"
+    sudo chmod a+rwx -R "$script"
 done
 
-# DELETE THIS SCRIPT
-if [ -f 'ffmpeg-scripts.sh' ]; then
-    sudo rm 'ffmpeg-scripts.sh'
-fi
-
-# DELETE THE RANDOM DIRECTORY
-sudo rm -fr "${tmp_dir}"
+# DELETE THIS SCRIPT and THE RANDOM TEMP DIRECTORY
+[[ -f "ffmpeg-scripts.sh" ]] && sudo rm "ffmpeg-scripts.sh"
+[[ -d "$tmp_dir" ]] && sudo rm -fr "$tmp_dir"
