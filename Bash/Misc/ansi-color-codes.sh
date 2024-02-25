f75ffccdf7adde285926569e65ca788c3b171f9a
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-02-24 21:39:07 -0500

    Create ansi-color-codes.sh

---
+++ Bash/Misc/ansi-color-codes.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Color codes
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT_GRAY='\033[0;37m'
+DARK_GRAY='\033[1;30m'
+LIGHT_RED='\033[1;31m'
+LIGHT_GREEN='\033[1;32m'
+LIGHT_YELLOW='\033[1;33m'
+LIGHT_BLUE='\033[1;34m'
+LIGHT_PURPLE='\033[1;35m'
+LIGHT_CYAN='\033[1;36m'
+WHITE='\033[1;37m'
+NC='\033[0m' # No Color
+
+# Example of using colors
+echo -e "${GREEN}This text is green.${NC}"
+echo -e "${RED}This text is red.${NC}"
+echo -e "${BLUE}This text is blue.${NC}"
