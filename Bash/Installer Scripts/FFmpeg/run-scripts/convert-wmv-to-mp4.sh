4a048c905a801d72d3268b1b88521c6b1f10b7c8
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-02-24 23:26:43 -0500

    Update convert-wmv-to-mp4.sh

--- Bash/Installer Scripts/FFmpeg/run-scripts/convert-wmv-to-mp4.sh
+++ Bash/Installer Scripts/FFmpeg/run-scripts/convert-wmv-to-mp4.sh
@@ -2,13 +2,23 @@
 # Recursively search and re-encode all found WMV files to MP4
 
 echo "Searching for .wmv files to convert to .mp4 with NVIDIA CUDA acceleration..."
+echo
 
 # Define the conversion process as a function for clarity
 convert_to_mp4() {
     local file_in="$1"
     local file_out="${file_in%.wmv}.mp4"
 
-    echo "Converting: $file_in to $file_out"
+    local filename_in="${file_in##*/}"
+    local filename_out="${file_out##*/}"
+    local folder_path="${file_in%/*}"
+
+    echo "Name in:  $filename_in"
+    echo "Name out: $filename_out"
+    echo
+    echo "CWD:      $PWD/${folder_path//.\//}"
+    echo
+
     if ffpb -y -hide_banner -hwaccel cuda -hwaccel_output_format cuda -fflags '+genpts' -i "$file_in" -c:v h264_nvenc -preset slow -c:a libfdk_aac "$file_out"; then
         echo "Conversion complete: $file_out"
         rm "$file_in"
