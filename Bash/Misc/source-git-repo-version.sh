dca85c113a565b2919314f2a7115361349fb01c6
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-03-03 10:03:03 -0500

    Update source-git-repo-version.sh

--- Bash/Misc/source-git-repo-version.sh
+++ Bash/Misc/source-git-repo-version.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Updated: 03.03.2024
+# Improved the RegEx parsing which has allowed more results
+
 # Function to retrieve the latest Git tag version from a repository
 find_latest_git_tag() {
     local url="$1"
@@ -17,20 +20,22 @@ find_latest_git_tag() {
 }
 
 parse_version() {
-    grep -Eo 'href="[^"]*\/tag\/[a-z\.\-]*[0-9][0-9\.\_]+[a-zA-Z0-9\.\-]*"' |
-    grep -Eo '[0-9][0-9\.\_]+[a-zA-Z0-9\.\-]*' |
+    grep -oP 'href="[^"]*\/[a-z]*\/tag(s)?([a-z\/]*)\/[a-z\.\-]*[0-9][0-9\.\_]+[a-zA-Z0-9\.\-]*"' |
+    grep -oP '[0-9][0-9\.\_]+[a-zA-Z0-9\.\-]*' |
     grep -Eiv 'alpha|beta|init|next|pre|rc|tentative' |
+    grep -oP '([0-9\.]*)\.[0-9]+[0-9\-]*' | 
     sort -rV |
-    head -n1
+    head -n1 |
+    sed 's/\-$//'
 }
 
-# Check if a URL is provided as an argument
+# Check if a URL is provided as an url
 if [ -z "$1" ]; then
-    echo "Usage: $0 <repo_url>"
+    echo "Usage: $0 <url>"
     exit 1
 fi
 
-repo_url="$1"
-latest_tag="$(find_latest_git_tag "$repo_url")"
+url="$1"
+latest_tag="$(find_latest_git_tag "$url")"
 
 echo "$latest_tag"
