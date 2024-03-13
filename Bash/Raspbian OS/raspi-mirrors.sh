b5c16b1eb203b8ae66e50a561f8388ecf18b858c
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-03-13 05:47:18 -0400

    Update raspi-mirrors.sh

--- Bash/Raspbian OS/raspi-mirrors.sh
+++ Bash/Raspbian OS/raspi-mirrors.sh
@@ -1,43 +1,38 @@
 #!/usr/bin/env bash
 
-clear
-
-fname='/etc/apt/sources.list'
-
-# Create a backup of the sources.list file
-if [ ! -f "$fname".bak ]; then
-    sudo cp -f "$fname" "$fname".bak
+if [[ "$EUID" -ne 0 ]]; then
+    echo "You must run this script with root or sudo."
+    exit 1
 fi
 
-cat > "$fname" <<'EOF'
-# Custom Atlanta GA Mirrors
+script_path=$(readlink -f "${BASH_SOURCE[0]}")
+script_name=$(basename "$script_path")
+
+fname="/etc/apt/sources.list"
+
+# Make a backup of the file
+if [ ! -f "${fname}.bak" ]; then
+    cp -f "$fname" "${fname}.bak"
+fi
+
+cat > "$fname" <<EOF
+# Atlanta, GA
 deb http://atl.mirrors.clouvider.net/debian/ bookworm main contrib non-free non-free-firmware
 deb http://atl.mirrors.clouvider.net/debian/ bookworm-updates main contrib non-free non-free-firmware
 deb http://atl.mirrors.clouvider.net/debian/ bookworm-backports main contrib non-free non-free-firmware
 
-# Official Raspbian Sources (Default)
+# Official (Default)
 deb http://deb.debian.org/debian bookworm main contrib non-free non-free-firmware
 deb http://deb.debian.org/debian bookworm-updates main contrib non-free non-free-firmware
 deb http://deb.debian.org/debian bookworm-backports main contrib non-free non-free-firmware
 deb http://deb.debian.org/debian-security/ bookworm-security main contrib non-free non-free-firmware
 EOF
 
-# OPEN AN EDITOR TO VIEW THE CHANGES
-if type -P gnome-text-editor &>/dev/null; then
-    sudo gnome-text-editor "$fname"
-elif type -P gedit &>/dev/null; then
-    sudo gedit "$fname"
-elif type -P nano &>/dev/null; then
+# Open an editor to view the changes
+if command -v nano &>/dev/null; then
     sudo nano "$fname"
-elif type -P vim &>/dev/null; then
-    sudo vim "$fname"
-elif type -P vi &>/dev/null; then
-    sudo vi "$fname"
 else
-    printf "\n%s\n\n" "Could not find an EDITOR to open \"$fname\""
-    exit 1
+    echo -e "\\nThe script failed to locate nano to open the file...\\n"
 fi
 
-if [[ "${0}" == 'raspi-mirrors' ]]; then
-    sudo rm "${0}"
-fi
+sudo rm "$script_name"
