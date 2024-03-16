747cb097ead1f57d60024d1cb8c3313ef96b3c0d
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-03-16 09:18:15 -0400

    Update and rename Bash/Misc/ld-linker-search-paths.sh to Bash/Misc/System/edit-ld-linker-search-paths.sh

--- Bash/Misc/System/edit-ld-linker-search-paths.sh
+++ Bash/Misc/System/edit-ld-linker-search-paths.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env bash
 
-clear
+file="/etc/ld.so.conf.d/$USER-custom.conf"
 
-if [ "${EUID}" -ne '0' ]; then
-    printf "%s\n\n" 'You must run this script with root/sudo permissions.'
+if [ "$EUID" -ne 0 ]; then
+    echo "You must run this script with root or sudo."
     exit 1
 fi
 
-cat > '/etc/ld.so.conf.d/user-added.conf' <<'EOF'
+cat > "file" <<'EOF'
 /usr/local/x86_64-linux-gnu/lib
 /usr/local/cuda/nvvm/lib64
 /usr/local/cuda/targets/x86_64-linux/lib
+/usr/local/lib64/x86_64-linux-gnu
 /usr/local/lib64
-/usr/local/x86_64-linux-gnu/lib/ldscripts
 /usr/local/lib/x86_64-linux-gnu
 /usr/local/lib
 /usr/lib64
 /usr/lib/x86_64-linux-gnu
 /usr/lib
+/lib64/x86_64-linux-gnu
 /lib64
 /lib/x86_64-linux-gnu
 /lib
 EOF
 
-sudo ldconfig
+# Update the ld linker paths
+ldconfig
