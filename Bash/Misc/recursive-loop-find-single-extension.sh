92962b42faabf5aa271a68a55884fbe16ead52e6
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-03-16 09:23:59 -0400

    Update recursive-loop-find-single-extension.sh

--- Bash/Misc/recursive-loop-find-single-extension.sh
+++ Bash/Misc/recursive-loop-find-single-extension.sh
@@ -1,29 +1,26 @@
 #!/usr/bin/env bash
 
-clear
+ext="sh"
 
-fext=sh
+prompt_user() {
+    echo "Include the parent folder?"
+    echo
+    echo "[1] Yes"
+    echo "[2] No"
+    echo
+    read -p "Your choices are (1 or 2): " choice
+    clear
 
-printf "%s\n\n%s\n%s\n\n" \
-    'Include the parent folder?' \
-    '[1] Yes' \
-    '[2] No'
-read -p 'Your choices are (1 or 2): ' choice
-clear
+    case "$choice" in
+        1) store_paths="$(find . -type f -iname "*.$ext" -exec bash -c "echo {} | sed -E 's/.*\s(.*)/\1/'" \;)" ;;
+        2) store_paths="$(find . -mindepth 2 -type f -iname "*.$ext" -exec bash -c "echo {} | sed -E 's/.*\s(.*)/\1/'" \;)" ;;
+        *) unset choice
+           clear
+           prompt_user
+           ;;
+    esac
+}
 
-case "${choice}" in
-    1)      store_paths="$(find . -type f -iname "*.$fext" -exec bash -c "echo {} | sed -E 's/.*\s(.*)/\1/'" \;)";;
-    2)      store_paths="$(find . -mindepth 2 -type f -iname "*.$fext" -exec bash -c "echo {} | sed -E 's/.*\s(.*)/\1/'" \;)";;
-    *)
-            clear
-            printf "%s\n\n" 'Bad user input.'
-            exit 1
-esac
-
-for i in ${store_paths[@]}
-do
-    fpath="${i:2}"
-    echo "$PWD/$fpath"
+for i in ${store_paths[@]}; do
+    echo "$PWD/${i:2}"
 done
-
-unset choice
