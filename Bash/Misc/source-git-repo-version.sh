b51c882d9b475263338132bc8ce58a4e076d75e9
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-03-16 10:29:34 -0400

    Update source-git-repo-version.sh

--- Bash/Misc/source-git-repo-version.sh
+++ Bash/Misc/source-git-repo-version.sh
@@ -4,19 +4,16 @@
 # Improved the RegEx parsing which has allowed more results
 
 # Function to retrieve the latest Git tag version from a repository
-find_latest_release_version() {
+get_latest_release_version() {
     local url="$1"
     local tags_url="${url/.git/}/tags"
-    local tag_version
 
     # Try fetching from the original URL
-    tag_version=$(curl -sSL "$url" | parse_version)
-    if [[ -z "$tag_version" ]]; then
+    latest_version=$(curl -fsSL "$url" | parse_version)
+    if [[ -z "$latest_version" ]]; then
         # If no version found, try the alternative URL
-        tag_version=$(curl -sSL "$tags_url" | parse_version)
+        latest_version=$(curl -fsSL "$tags_url" | parse_version)
     fi
-
-    echo "$tag_version"
 }
 
 parse_version() {
@@ -36,6 +33,6 @@ if [ -z "$1" ]; then
 fi
 
 url="$1"
-latest_tag="$(find_latest_release_version "$url")"
+get_latest_release_version
 
-echo "$latest_tag"
+echo "$latest_version"
