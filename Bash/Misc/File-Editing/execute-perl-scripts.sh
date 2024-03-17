0ca2a8beca01693a02a7d6204e7c1836c743081f
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-03-17 12:24:02 -0400

    fix code

--- Bash/Misc/File-Editing/execute-perl-scripts.sh
+++ Bash/Misc/File-Editing/execute-perl-scripts.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# Define the directory to search in and the pattern for file names
 search_directory="/c/Users/jholl/OneDrive/Documents/GitHub/script-repo/Bash"
 
 if [[ -z "$search_directory" ]]; then
@@ -11,13 +10,10 @@ fi
 scripts=(minimize_comments.pl fix_function_braces.pl replace-variables-curly-brackets.pl)
 
 for file in ${scripts[@]}; do
-    # Path to your Perl script
     perl_script_path="$file"
-    # Check if the Perl script exists
     if [[ ! -f "$perl_script_path" ]]; then
         echo "Perl script not found at: $perl_script_path"
     else
-        # Use find to locate files and execute the Perl script on each
         find "$search_directory" -type f -exec perl "$perl_script_path" {} \;
     fi
     echo
