c402b48b2394ad979e21294e29253e1e3a97e58a
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-03-16 21:06:19 -0400

    Update mirrors-menu.sh

--- Bash/Installer Scripts/SlyFox1186 Scripts/mirrors-menu.sh
+++ Bash/Installer Scripts/SlyFox1186 Scripts/mirrors-menu.sh
@@ -1,45 +1,29 @@
 #!/usr/bin/env bash
 
 # Define color variables
-GREEN='\e[0;32m'
-CYAN='\e[0;36m'
-MAGENTA='\e[0;35m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+MAGENTA='\033[0;35m'
 RED='\033[0;31m'
-NC='\e[0m' # No Color
+NC='\033[0m' # No Color
 
 # Create a temporary directory for storing files
-dir=$(mktemp -d /tmp/mirrors-script-XXXXXX)
+dir=$(mktemp -d)
 
 # Function to download and execute mirrors script
 execute() {
-    local url=$1
-    local file=$2
-    if curl -Lso "$dir/$file" "$url"; then
-        if [[ -f "$dir/$file" ]]; then
-            if sudo bash "$dir/$file"; then
-                sudo rm -rf "$dir"
-                echo -e "${GREEN}[SUCCESS]${NC} Execution completed successfully.\\n"
-                exit 0
-            else
-                echo -e "${RED}[ERROR]${NC} Failed to execute: \"$file\"\\n"
-                read -p "Press any key to exit."
-                echo
-                sudo rm -rf "$dir"
-                exit 1
-            fi
+    local url="$1"
+    local fname="$2"
+
+    if curl -fsSLo "$dir/$fname" "$url"; then
+        cd "$dir" || exit 1
+        if ! sudo bash "$fname"; then
+            echo "Failed to execute: $fname"
         else
-            echo -e "${RED}[ERROR]${NC} File not found: \"$file\"\\n"
-            read -p "Press any key to exit."
-            echo
-            sudo rm -rf "$dir"
-            exit 1
+            echo "Successfully executed: $fname"
         fi
     else
-        echo -e "${RED}[ERROR]${NC} Failed to download: \"$file\"\\n"
-        read -p "Press any key to exit."
-        echo
-        sudo rm -rf "$dir"
-        exit 1
+        echo "Failed to execute: $fname"
     fi
 }
 
@@ -64,10 +48,7 @@ main_menu() {
             4) sudo mkdir -p "/etc/pacman.d"
                execute "https://raw.githubusercontent.com/slyfox1186/script-repo/main/Bash/Arch%20Linux%20Scripts/archlinux-mirrors.sh" "archlinux-mirrors.sh"
                ;;
-            0)
-               rm -rf "$dir"
-               exit 0
-               ;;
+            0) exit 0 ;;
             *) clear
                main_menu
                ;;
