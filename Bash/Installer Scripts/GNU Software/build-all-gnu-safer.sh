ead1150e252e47f39bfe7ae512aa8e81bf95bb42
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-03-18 11:50:36 -0400

    Update build-all-gnu-safer.sh

--- Bash/Installer Scripts/GNU Software/build-all-gnu-safer.sh
+++ Bash/Installer Scripts/GNU Software/build-all-gnu-safer.sh
@@ -1,29 +1,39 @@
 #!/usr/bin/env bash
 
+##  GitHub Script: https://github.com/slyfox1186/script-repo/blob/main/Bash/Installer%20Scripts/GNU%20Software/build-all-gnu-safer
+##  Purpose: Loops multiple build scripts to optimize efficiency. This is the safer of the two scripts.
+##  Updated: 03.17.24
+##  Script version: 1.3
 
 if [ "$EUID" -eq 0 ]; then
-    echo "You must run this script without root or sudo."
+    printf "%s\n\n" "You must run this script without root/sudo."
     exit 1
 fi
 
-count=0
+#
+# PRINT THE SCRIPT BANNER
+#
 
-working="$PWD/build-gnu-safer-scripts"
+script_ver=1.2
+cwd="$PWD/build-all-gnu-safer-script"
+web_repo="https://github.com/slyfox1186/script-repo"
 
-[[ -d "$working" ]] && sudo rm -fr "$working"
-mkdir -p "$working/completed"
+if [ -d "$cwd" ]; then
+    sudo rm -fr "$cwd"
+fi
+mkdir -p "$cwd/completed"
 
 exit_fn() {
-    echo
-    echo "Make sure to star this repository to show your support!"
-    echo "https://github.com/slyfox1186/script-repo"
+    printf "\n%s\n\n%s\n\n" \
+        "Make sure to star this repository to show your support!" \
+        "$web_repo"
     exit 0
 }
 
-fail() {
-    echo
-    echo "$1"
-    echo "To report a bug create an issue at: https://github.com/slyfox1186/script-repo/issues"
+fail_fn() {
+    printf "\n%s\n\n%s\n\n" \
+        "$1" \
+        "To report a bug create an issue at: $web_repo/issues"
     exit 1
 }
 
@@ -34,7 +44,8 @@ pkgs=(
     texinfo xmlto yasm wget zlib1g-dev
 )
 
-for pkg in "${pkgs[@]}"; do
+for pkg in "${pkgs[@]}"
+do
     missing_pkg="$(sudo dpkg -l | grep -o "$pkg")"
 
     if [ -z "$missing_pkg" ]; then
@@ -42,49 +53,65 @@ for pkg in "${pkgs[@]}"; do
     fi
 done
 
-if [[ -n "$missing_pkgs" ]]; then
-    sudo apt-get install $missing_pkgs
+if [ -n "$missing_pkgs" ]; then
+    sudo apt install $missing_pkgs
+    sudo apt -y autoremove
+    clear
 fi
 
-sudo bash -c 'bash <(curl -fsSL https://ld.optimizethis.net)'
+#
+# ADD ADDITIONAL SEARCH PATHS TO THE LD LIBRARY LINKER
+#
 
-case $(uname -m) in
-    x86_64)                     arch_ver="pkg-config" ;;
-    aarch64*|armv8*|arm|armv7*) arch_ver="pkg-config-arm" ;;
-    *)                          fail "Unrecognized architecture: $(uname -m)" ;;
+sudo bash -c "bash <(curl -sSL https://ld-linker.optimizethis.net)"
+
+#
+# DETECT THE PC ARCHITECTURE
+#
+
+case "$(uname -m)" in
+    x86_64)                        arch_ver="pkg-config" ;;
+    aarch64*|armv8*|arm|armv7*)    arch_ver="pkg-config-arm" ;;
+    *)                             fail_fn "Unrecognized architecture: $(uname -m)" ;;
 esac
 
-cd "$working" || exit 1
+cd "$cwd" || exit 1
 
 scripts=(
-    "$arch_ver" coreutils.sh m4 autoconf-2.71 autoconf-archive libtool bash
-    make sed tar gawk grep nano parallel.sh get
+    "$arch_ver" "coreutils.sh" "m4" "autoconf-2.71" "autoconf-archive" "libtool" "bash"
+    "make" "sed" "tar" "gawk" "grep" "nano" "parallel" "get"
 )
 
-for script in "${scripts[@]}"; do
-    ((count++))
-    wget --show-progress -t 2 -cq "https://raw.githubusercontent.com/slyfox1186/script-repo/main/Bash/Installer%20Scripts/GNU%20Software/build-$script.sh"
+count=0
+
+for script in "${scripts[@]}"
+do
+    let count=count+1
+    wget --show-progress -t 2 -cq "https://raw.githubusercontent.com/slyfox1186/script-repo/main/Bash/Installer%20Scripts/GNU%20Software/build-$script"
     mv "build-$script" "$count-build-$script.sh" 2>/dev/null
 done
 
-for file in $(find ./ -maxdepth 1 -type f | sort -V | sed 's/\.\///g'); do
-    if echo "1" | sudo bash "$file"; then
-        sudo mv "$file" "$working/completed"
+for f in $(find ./ -maxdepth 1 -type f | sort -V | sed 's/\.\///g')
+do
+    if echo "1" | sudo bash "$f"; then
+        sudo mv "$f" "$cwd/completed"
     else
-        if [ ! -d "$working/failed" ]; then
-            mkdir -p "$working/failed"
+        if [ ! -d "$cwd/failed" ]; then
+            mkdir -p "$cwd/failed"
         fi
-        sudo mv "$file" "$working/failed"
+        sudo mv "${f}" "$cwd/failed"
     fi
 done
 
-if [ -d "$working/failed" ]; then
-    echo "One of the scripts failed to build successfully."
-    echo
-    echo "You can find the failed script at: $working/failed"
+if [ -d "$cwd/failed" ]; then
+    printf "%s\n\n%s\n\n" \
+        "One of the scripts failed to build successfully." \
+        "You can find the failed script at: $cwd/failed"
     exit_fn
 fi
 
-sudo rm -fr "$working"
+# CLEANUP LEFTOVER FILES
+sudo rm -fr "$cwd"
 
+# DISPLAY EXIT MESSAGE
 exit_fn
