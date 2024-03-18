9f99cf502650f28989f9918f93cf3110eab8e348
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-03-18 16:11:38 -0400

    Update build-autoconf.sh

--- Bash/Installer Scripts/GNU Software/build-autoconf.sh
+++ Bash/Installer Scripts/GNU Software/build-autoconf.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
-# Build GNU Autoconf - v1.1 - 03.08.24
-# GitHub: https://github.com/slyfox1186/script-repo/blob/main/Bash/Installer%20Scripts/GNU%20Software/build-autoconf
 
-set -euo pipefail
-trap 'fail_fn "Error occurred on line ${LINENO}"' ERR
+# Build GNU Autoconf - v1.1 - 03.08.24
+# GitHub: https://github.com/slyfox1186/script-repo/blob/main/Bash/Installer%20Scripts/GNU%20Software/build-autoconf.sh
+
+set -eo pipefail
+trap 'fail "Error occurred on line: $LINENO".' ERR
 
 version=1.1
 program_name=autoconf
-archive_url=https://ftp.gnu.org/gnu/autoconf/"$program_name"-latest.tar.xz
+archive_url="https://ftp.gnu.org/gnu/autoconf/$program_name-latest.tar.xz"
 install_prefix=/usr/local
-build_dir=/tmp/"$program_name-$version-build"
-repo_url=https://github.com/slyfox1186/script-repo
+build_dir="/tmp/$program_name-$version-build"
 verbose=0
 
 usage() {
-    printf "%s\n" "Usage: ./build-autoconf.sh [OPTIONS]"
-    printf "%s\n" "Options:"
+    echo "Usage: ./build-autoconf.sh [OPTIONS]"
+    echo "Options:"
     printf "  %-25s %s\n" "-p, --prefix DIR" "Set the installation prefix (default: $install_prefix)"
     printf "  %-25s %s\n" "-v, --verbose" "Enable verbose logging"
     printf "  %-25s %s\n" "-h, --help" "Show this help message"
@@ -37,7 +37,7 @@ parse_args() {
                 usage
                 ;;
             *)
-                fail_fn "Unknown option: $1. Use -h or --help for usage information."
+                fail "Unknown option: $1. Use -h or --help for usage information."
                 ;;
         esac
     done
@@ -45,40 +45,41 @@ parse_args() {
 
 log_msg() {
     if [[ $verbose -eq 1 ]]; then
-        printf "%s\n" "$1"
+        echo "$1"
     fi
 }
 
-fail_fn() {
-    printf "%s\n" "$1"
-    printf "%s\n" "To report a bug create an issue at: $repo_url/issues"
+fail() {
+    echo "$1"
+    echo "To report a bug create an issue at: https://github.com/slyfox1186/script-repo/issues"
     exit 1
 }
 
 install_deps() {
     log_msg "Installing dependencies..."
-    if command -v apt-get >/dev/null 2>&1; then
+    if command -v apt-get &>/dev/null; then
         apt-get update
         apt-get install -y --no-install-recommends autoconf autoconf-archive autogen automake autopoint autotools-dev binutils bison build-essential bzip2 ccache curl libc6-dev libpth-dev libtool libtool-bin lzip lzma-dev m4 nasm texinfo zlib1g-dev yasm
-    elif command -v dnf >/dev/null 2>&1; then
+    elif command -v dnf &>/dev/null; then
         dnf install -y autoconf autoconf-archive autogen automake autopoint autotools-dev binutils bison bzip2 ccache curl gcc gcc-c++ kernel-devel libpth-devel libtool libtool-ltdl-devel lzip lzma-devel m4 make nasm perl-Thread-Queue tar texinfo xz yasm zlib-devel
-    elif command -v pacman >/dev/null 2>&1; then
+    elif command -v pacman &>/dev/null; then
         pacman -Sy --noconfirm --needed autoconf autoconf-archive autogen automake gettext binutils bison bzip2 ccache curl gcc libtool m4 make nasm texinfo xz yasm zlib
     else
-        fail_fn "Unsupported package manager. Please install the required dependencies manually."
+        fail "Unsupported package manager. Please install the required dependencies manually."
     fi
 }
 
 set_env_vars() {
     log_msg "Setting environment variables..."
-    export CC="ccache gcc"
-    export CXX="ccache g++"
-    export CFLAGS="-O3 -pipe -fno-plt -march=native"
-    export CXXFLAGS="-O3 -pipe -fno-plt -march=native"
-    export CPPFLAGS="-D_FORTIFY_SOURCE=2"
-    export LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now,-rpath,$install_prefix/$program_name/lib"
-    export PATH="/usr/lib/ccache:$HOME/perl5/bin:$HOME/.cargo/bin:$HOME/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-    export PKG_CONFIG_PATH="/usr/local/lib64/pkgconfig:/usr/local/lib/pkgconfig:/usr/lib64/pkgconfig:/usr/lib/pkgconfig:/lib64/pkgconfig:/lib/pkgconfig"
+    CC="ccache gcc"
+    CXX="ccache g++"
+    CFLAGS="-O3 -pipe -fno-plt -march=native"
+    CXXFLAGS="-O3 -pipe -fno-plt -march=native"
+    CPPFLAGS="-D_FORTIFY_SOURCE=2"
+    LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now,-rpath,$install_prefix/$program_name/lib"
+    PATH="/usr/lib/ccache:$HOME/perl5/bin:$HOME/.cargo/bin:$HOME/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+    PKG_CONFIG_PATH="/usr/local/lib64/pkgconfig:/usr/local/lib/pkgconfig:/usr/lib64/pkgconfig:/usr/lib/pkgconfig:/lib64/pkgconfig:/lib/pkgconfig"
+    export CC CFLAGS CPPFLAGS CXX CXXFLAGS LDFLAGS PATH PKG_CONFIG_PATH
 }
 
 download_archive() {
@@ -124,7 +125,7 @@ create_symlinks() {
 cleanup() {
     log_msg "Cleaning up..."
     read -rp "Remove temporary build directory '$build_dir'? [y/N] " response
-    if [[ $response =~ ^[Yy]$ ]]; then
+    if [[ "$response" =~ ^[Yy]$ ]]; then
         rm -rf "$build_dir"
     fi
 }
@@ -132,13 +133,11 @@ cleanup() {
 main() {
     parse_args "$@"
 
-    if [[ $EUID -ne 0 ]]; then
-        fail_fn "You must run this script with root/sudo."
+    if [[ "$EUID" -ne 0 ]]; then
+        fail "You must run this script with root/sudo."
     fi
 
-    if [[ -d "$build_dir" ]]; then
-        rm -rf "$build_dir"
-    fi
+    [[ -d "$build_dir" ]] && rm -rf "$build_dir"
     mkdir -p "$build_dir"
 
     install_deps
@@ -153,7 +152,7 @@ main() {
 
     log_msg "Build completed successfully!"
     log_msg "Make sure to star this repository to show your support!"
-    log_msg "$repo_url"
+    log_msg "https://github.com/slyfox1186/script-repo"
 }
 
 main "$@"
