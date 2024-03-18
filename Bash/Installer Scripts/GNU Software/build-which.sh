b931e155ab1279efb26257215dbfcd0c7da33ab2
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-03-18 11:30:54 -0400

    Update build-which.sh

--- Bash/Installer Scripts/GNU Software/build-which.sh
+++ Bash/Installer Scripts/GNU Software/build-which.sh
@@ -1,124 +1,113 @@
-#!/Usr/bin/env bash
+#!/usr/bin/env bash
 
+##  GitHub Script: https://github.com/slyfox1186/script-repo/blob/main/Bash/Installer%20Scripts/GNU%20Software/build-
+##  Purpose: build gnu which
+##  Updated: 12.10.23
+##  Script version: 1.0
 
-clear
-
-if [ "$EUID" -eq '0' ]; then
+if [ "$EUID" -eq 0 ]; then
     echo "You must run this script without root or sudo."
     exit 1
 fi
 
-
-script_ver=1.0
+# SET THE VARIABLES
+script_ver="1.0"
 archive_dir=which-2.21
-archive_url=https://carlowood.github.io/which/"$archive_dir".tar.gz
-archive_ext="$archive_url//*."
+archive_url="https://carlowood.github.io/which/$archive_dir.tar.gz"
+archive_ext="${archive_url//*.}"
 archive_name="$archive_dir.tar.$archive_ext"
-cwd="$PWD"/which-build-script
+cwd="$PWD/which-build-script"
 install_dir=/usr/local
-user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
-web_repo=https://github.com/slyfox1186/script-repo
 
-printf "%s\n%s\n\n" \
-    "which build script - v$script_ver" \
-    '==============================================='
+echo "which build script version $script_ver"
+echo "==============================================="
+echo
 
-
-if [ -d "$cwd" ]; then
-    sudo rm -fr "$cwd"
-fi
+# CREATE OUTPUT DIRECTORY
+[[ -d "$cwd" ]] && sudo rm -fr "$cwd"
 mkdir -p "$cwd"
 
+# Set the C + CPP compilers and their compiler optimization flags
+CC="gcc"
+CXX="g++"
+CFLAGS="-O3 -pipe -march=native"
+CXXFLAGS="-O3 -pipe -march=native"
+export CC CFLAGS CXX CXXFLAGS
 
-export CC=gcc CXX=g++
-
-
-export {CFLAGS,CXXFLAGS}='-g -O3 -pipe -fno-plt -march=native'
-
-
+# SET THE PATH VARIABLE
 PATH="\
 /usr/lib/ccache:\
 $HOME/perl5/bin:\
 $HOME/.cargo/bin:\
 $HOME/.local/bin:\
 /usr/local/sbin:\
-/usr/local/cuda/bin:\
-/usr/local/x86_64-linux-gnu/bin:\
 /usr/local/bin:\
 /usr/sbin:\
 /usr/bin:\
 /sbin:\
-/bin:\
-/usr/local/games:\
-/usr/games:\
-/snap/bin\
+/bin\
 "
 export PATH
 
-
+# SET THE PKG_CONFIG_PATH VARIABLE
 PKG_CONFIG_PATH="\
 /usr/local/lib64/pkgconfig:\
 /usr/local/lib/pkgconfig:\
-/usr/local/lib/x86_64-linux-gnu/pkgconfig:\
 /usr/local/share/pkgconfig:\
 /usr/lib64/pkgconfig:\
 /usr/lib/pkgconfig:\
-/usr/lib/x86_64-linux-gnu/pkgconfig:\
-/usr/share/pkgconfig:\
-/lib64/pkgconfig:\
-/lib/pkgconfig:\
-/lib/x86_64-linux-gnu/pkgconfig\
+/usr/share/pkgconfig\
 "
 export PKG_CONFIG_PATH
 
-
+# CREATE FUNCTIONS
 exit_fn() {
-    printf "\n%s\n\n%s\n\n" \
-        'Make sure to star this repository to show your support!' \
-        "$web_repo"
+    echo
+    echo "Make sure to star this repository to show your support!"
+    echo "https://github.com/slyfox1186/script-repo"
+    echo
     exit 0
 }
 
-fail_fn() {
-    printf "\n%s\n\n%s\n\n" \
-        "$1" \
-        "To report a bug create an issue at: $web_repo/issues"
+fail() {
+    echo
+    echo "$1"
+    echo "To report a bug create an issue at: https://github.com/slyfox1186/script-repo/issues"
+    echo
     exit 1
 }
 
 cleanup_fn() {
     local choice
 
-    printf "%s\n%s\n%s\n\n%s\n%s\n\n" \
-        '============================================' \
-        '  Do you want to clean up the build files?  ' \
-        '============================================' \
-        '[1] Yes' \
-        '[2] No'
+    echo "============================================"
+    echo "  Do you want to clean up the build files?  "
+    echo "============================================"
+    echo '[1] Yes'
+    echo '[2] No'
+    echo
     read -p 'Your choices are (1 or 2): ' choice
 
     case "$choice" in
-        1)      sudo rm -fr "$cwd";;
-        2)      echo;;
-        *)
-                unset choice
-                clear
-                cleanup_fn
-                ;;
+        1) sudo rm -fr "$cwd" ;;
+        2) echo ;;
+        *) unset choice
+           clear
+           cleanup_fn
+           ;;
     esac
 }
 
-
+# INSTALL REQUIRED APT PACKAGES
 pkgs=(autoconf autoconf-archive autogen automake autopoint autotools-dev binutils
       bison build-essential bzip2 bzip2 ccache curl libc6-dev libpth-dev libtool
       libtool-bin lzip lzma-dev m4 nasm texinfo zlib1g-dev yasm)
 
-for i in ${pkgs[@]}
-do
-    missing_pkg="$(sudo dpkg -l | grep -o "$i")"
+for pkg in ${pkgs[@]}; do
+    missing_pkg="$(sudo dpkg -l | grep -o "$pkg")"
 
     if [ -z "$missing_pkg" ]; then
-        missing_pkgs+=" $i"
+        missing_pkgs+=" $pkg"
     fi
 done
 
@@ -128,24 +117,24 @@ if [ -n "$missing_pkgs" ]; then
     clear
 fi
 
-
+# DOWNLOAD THE ARCHIVE FILE
 if [ ! -f "$cwd/$archive_name" ]; then
-    curl -A "$user_agent" -Lso "$cwd/$archive_name" "$archive_url"
+    curl -Lso "$cwd/$archive_name" "$archive_url"
 fi
 
-
+# CREATE OUTPUT DIRECTORY
 if [ -d "$cwd/$archive_dir" ]; then
     sudo rm -fr "$cwd/$archive_dir"
 fi
 mkdir -p "$cwd/$archive_dir/build"
 
-
+# EXTRACT ARCHIVE FILES
 if ! tar -xf "$cwd/$archive_name" -C "$cwd/$archive_dir" --strip-components 1; then
     printf "%s\n\n" "Failed to extract: $cwd/$archive_name"
     exit 1
 fi
 
-
+# BUILD PROGRAM FROM SOURCE
 cd "$cwd/$archive_dir" || exit 1
 autoupdate
 autoreconf -fi
@@ -154,15 +143,15 @@ cd build || exit 1
              --enable-silent-rules
 echo
 if ! make "-j$(nproc --all)"; then
-    fail_fn "Failed to execute: make -j$(nproc --all). Line: $LINENO"
-    exit 1
+    fail "Failed to execute: make -j$(nproc --all). Line: $LINENO"
 fi
 echo
 if ! sudo make install; then
-    fail_fn "Failed to execute: sudo make install. Line: $LINENO"
-    exit 1
+    fail "Failed to execute: sudo make install. Line: $LINENO"
 fi
 
+# PROMPT USER TO CLEAN UP FILES
 cleanup_fn
 
+# SHOW EXIT MESSAGE
 exit_fn
