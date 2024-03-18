f42ba8d8cfe20950ad8183747bdd04824b05ec7c
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-03-18 11:54:30 -0400

    Update build-all-gnu-safer.sh

--- Bash/Installer Scripts/GNU Software/build-all-gnu-safer.sh
+++ Bash/Installer Scripts/GNU Software/build-all-gnu-safer.sh
@@ -10,30 +10,25 @@ if [ "$EUID" -eq 0 ]; then
     exit 1
 fi
 
-#
-# PRINT THE SCRIPT BANNER
-#
-
-script_ver=1.2
+# Print the script banner
 cwd="$PWD/build-all-gnu-safer-script"
-web_repo="https://github.com/slyfox1186/script-repo"
 
-if [ -d "$cwd" ]; then
-    sudo rm -fr "$cwd"
-fi
+[[ -d "$cwd" ]] && sudo rm -fr "$cwd"
 mkdir -p "$cwd/completed"
 
 exit_fn() {
-    printf "\n%s\n\n%s\n\n" \
-        "Make sure to star this repository to show your support!" \
-        "$web_repo"
+    echo
+    echo "Make sure to star this repository to show your support!"
+    echo "https://github.com/slyfox1186/script-repo"
+    echo
     exit 0
 }
 
 fail_fn() {
-    printf "\n%s\n\n%s\n\n" \
-        "$1" \
-        "To report a bug create an issue at: $web_repo/issues"
+    echo
+    echo "$1"
+    echo "To report a bug create an issue at: https://github.com/slyfox1186/script-repo/issues"
+    echo
     exit 1
 }
 
@@ -44,8 +39,7 @@ pkgs=(
     texinfo xmlto yasm wget zlib1g-dev
 )
 
-for pkg in "${pkgs[@]}"
-do
+for pkg in "${pkgs[@]}"; do
     missing_pkg="$(sudo dpkg -l | grep -o "$pkg")"
 
     if [ -z "$missing_pkg" ]; then
@@ -55,20 +49,12 @@ done
 
 if [ -n "$missing_pkgs" ]; then
     sudo apt install $missing_pkgs
-    sudo apt -y autoremove
-    clear
 fi
 
-#
-# ADD ADDITIONAL SEARCH PATHS TO THE LD LIBRARY LINKER
-#
-
+# Add additional search paths to the ld library linker
 sudo bash -c "bash <(curl -sSL https://ld-linker.optimizethis.net)"
 
-#
-# DETECT THE PC ARCHITECTURE
-#
-
+# Detect the pc architecture
 case "$(uname -m)" in
     x86_64)                        arch_ver="pkg-config" ;;
     aarch64*|armv8*|arm|armv7*)    arch_ver="pkg-config-arm" ;;
@@ -84,15 +70,13 @@ scripts=(
 
 count=0
 
-for script in "${scripts[@]}"
-do
-    let count=count+1
+for script in "${scripts[@]}"; do
+    ((count++))
     wget --show-progress -t 2 -cq "https://raw.githubusercontent.com/slyfox1186/script-repo/main/Bash/Installer%20Scripts/GNU%20Software/build-$script"
     mv "build-$script" "$count-build-$script.sh" 2>/dev/null
 done
 
-for f in $(find ./ -maxdepth 1 -type f | sort -V | sed 's/\.\///g')
-do
+for f in $(find ./ -maxdepth 1 -type f | sort -V | sed 's/\.\///g'); do
     if echo "1" | sudo bash "$f"; then
         sudo mv "$f" "$cwd/completed"
     else
@@ -110,8 +94,8 @@ if [ -d "$cwd/failed" ]; then
     exit_fn
 fi
 
-# CLEANUP LEFTOVER FILES
+# Cleanup leftover files
 sudo rm -fr "$cwd"
 
-# DISPLAY EXIT MESSAGE
+# Display exit message
 exit_fn
