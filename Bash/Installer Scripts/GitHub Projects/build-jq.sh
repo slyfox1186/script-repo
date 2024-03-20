9d0f2cdba3fcfecaed744540ebb744f512315731
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-03-19 21:53:01 -0400

    Update build-jq.sh

--- Bash/Installer Scripts/GitHub Projects/build-jq.sh
+++ Bash/Installer Scripts/GitHub Projects/build-jq.sh
@@ -1,62 +1,93 @@
 #!/usr/bin/env bash
 
-#################################################################################################################################
-##
 ##  Github Script: https://github.com/slyfox1186/script-repo/edit/main/util-linux/Installer%20Scripts/GNU%20Software/build-jq
-##
 ##  Purpose: Build jq
-##
 ##  Updated: 12.04.23
-##
 ##  Script version: 1.0
-##
-#################################################################################################################################
 
-clear
-
-if [ "${EUID}" -eq '0' ]; then
+if [[ "$EUID" -eq 0 ]]; then
     echo "You must run this script without root or sudo."
     exit 1
 fi
 
 # Set the variables
+script_ver=1.1
+jq_ver=1.7.1
+archive_dir="jq-$jq_ver"
+archive_url="https://github.com/jqlang/jq/releases/download/$archive_dir/$archive_dir.tar.gz"
+cwd="$PWD/jq-build-script"
+install_dir="/usr/local/$archive_dir"
 
-script_ver=1.0
-jq_ver=1.7
-archive_dir="jq-${jq_ver}"
-archive_url="https://github.com/jqlang/jq/releases/download/jq-1.7/jq-linux-amd64"
-cwd="$PWD"/jq-build-script
-install_dir=/usr/local
-user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+GREEN='\033[32m'
+RED='\033[31m'
+NC='\033[0m'
+
+log() {
+    echo -e "${GREEN}[INFO]${NC} $1"
+}
+
+fail() {
+    echo -e "${RED}[ERROR]${NC} $1"
+}
+
+set_compiler_flags() {
+    CC="gcc"
+    CXX="g++"
+    CFLAGS="-O3 -pipe -fno-plt -march=native"
+    CXXFLAGS="-O3 -pipe -fno-plt -march=native"
+    CPPFLAGS="-D_FORTIFY_SOURCE=2"
+    LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now,-rpath,$install_dir/lib"
+    export CC CFLAGS CPPFLAGS CXX CXXFLAGS LDFLAGS
+
+}
 
 # Create output directory jemalloc
+log "Creating output directories"
+echo
+[[ -d "$cwd/$archive_dir" ]] && sudo rm -fr "$cwd/$archive_dir" "$install_dir"
+mkdir -p "$cwd/$archive_dir/build"
+sudo mkdir -p "$install_dir/bin"
 
-if [ -d "$cwd/$archive_dir" ]; then
-    sudo rm -fr "$cwd/$archive_dir"
+# Change into the working directory
+cd "$cwd" || exit 1
+
+# Download the archive file
+if [[ ! -f "$cwd/$archive_dir.tar.gz" ]]; then
+    log "Downloading the source code"
+    wget -cqO "$cwd/$archive_dir.tar.gz" "$archive_url"
+    echo
+else
+    log "Source code already downloaded"
+    echo
 fi
-mkdir -p "$cwd/$archive_dir"
 
-# Download the archive file jemalloc
+# Extract the archive file
+log "Extacting archive file"
+tar -zxf "$cwd/$archive_dir.tar.gz" -C "$cwd/$archive_dir" --strip-components 1
+echo
 
-if [ ! -f "$cwd/$archive_dir" ]; then
-    wget -U "$user_agent" -cq "${archive_url}"
-fi
+cd "$cwd/$archive_dir" || exit 1
+log "Configuring JQ"
+echo
+set_compiler_flags
+autoreconf -fi
+cd build || exit 1
+../configure --prefix="$install_dir" || fail "Failed to configure JQ"
+log "Building JQ"
+make "-j$(nproc --all)" || fail "Failed to build JQ"
+log "Installing JQ"
+sudo make install || fail "Failed to install JQ"
 
-# Install jemalloc
+# Create soft links to a common path
+echo
+log "Creating soft links"
+sudo ln -sf "$install_dir/bin/jq" /usr/local/bin/ || fail "Failed to create soft links"
 
-printf "\n%s\n%s\n\n"                        \
-    "Installing JQ - v${jq_ver}" \
-    '==============================================='
-sleep 2
+# Print the updated jq version
+echo
+log "The updated jq file is located at: $(type -P jq)"
+echo
 
-sudo chmod 755 'jq-linux-amd64'
-sudo chown root:root 'jq-linux-amd64'
-
-sudo cp -f 'jq-linux-amd64' "$install_dir"/bin/jq
-
-clear
-printf "%s\n\n" "The updated jq file is located at: $(type -P jq)"
-jq --version
-
-sudo rm -fr "$cwd"
-
+log "Removing leftover files"
+echo
+sudo rm -fr "$cwd" || fail "Failed to remove leftover files"
