326acf8144b0cc5ba6abe92311f91d217a442a31
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-03-25 10:22:26 -0400

    Update find-fastest-archlinux-mirrors.sh

--- Bash/Arch Linux Scripts/find-fastest-archlinux-mirrors.sh
+++ Bash/Arch Linux Scripts/find-fastest-archlinux-mirrors.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# The config file is located at: https://github.com/slyfox1186/script-repo/blob/main/Bash/Arch%20Linux%20Scripts/pacman-mirror-update.conf
+# An example config file is located at: https://github.com/slyfox1186/script-repo/blob/main/Bash/Arch%20Linux%20Scripts/pacman-mirror-update.conf
 
 if [[ "$EUID" -ne 0 ]]; then
     echo "You must run this script with root or sudo."
