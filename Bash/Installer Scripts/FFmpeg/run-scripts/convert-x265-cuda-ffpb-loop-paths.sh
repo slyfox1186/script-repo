329cb3a94b02ba1cf62bdd886236b2cab04636f7
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-03-26 19:39:18 -0400

    Update convert-x265-cuda-ffpb-loop-paths.sh

--- Bash/Installer Scripts/FFmpeg/run-scripts/convert-x265-cuda-ffpb-loop-paths.sh
+++ Bash/Installer Scripts/FFmpeg/run-scripts/convert-x265-cuda-ffpb-loop-paths.sh
@@ -1,93 +1,30 @@
 #!/usr/bin/env bash
-# Shellcheck disable=sc2066,sc2068,sc2086,sc2162
+# shellcheck disable=SC2066,SC2068,SC2086,SC2162
 
 # Set the path variable
-if [ -d "$HOME/.local/bin" ]; then
-    export PATH="$PATH:$HOME/.local/bin"
-fi
-
-# Installl the required apt packages
-pkgs=(
-    ffmpegthumbnailer ffmpegthumbs libffmpegthumbnailer4v5
-    libsox-dev python3-pip sox trash-cli
-)
-
-for pkg in ${pkgs[@]}
-do
-    missing_pkg=$(sudo dpkg -l | grep -o "$pkg")
-    if [ -z "$missing_pkg" ]; then
-        missing_pkgs+=" $pkg"
-    fi
-done
-
-if [ -n "$missing_pkgs" ]; then
-    printf "%s\n\n" "$ Installing: $missing_pkgs"
-    if ! sudo apt -y install $missing_pkgs; then
-        fail_fn "Failed to install the required APT packages: $missing_pkgs"
-    else
-        sudo apt -y install bc
-        printf "\n%s\n\n" "The required APT packages were successfully installed!"
-    fi
-fi
-clear
-
-# Install the required pip packages
-# Specify a permanent location for the virtual environment
-venv_dir="$HOME/my_venv"
-
-# Check if the virtual environment already exists
-if [ ! -d "$venv_dir" ]; then
-    echo "Creating a virtual environment in $venv_dir"
-    python3 -m venv "$venv_dir"
-else
-    echo "Using existing virtual environment in $venv_dir"
-fi
-
-# Activate the virtual environment
-source "$venv_dir/bin/activate"
-
-# Function to check if a package is installed
-is_package_installed() {
-    pip list | grep "^$1 " > /dev/null
-}
-
-# List of required packages
-required_packages=(ffpb google_speech)
-
-# Iterate over the required packages and install if not already installed
-for pkg in "${required_packages[@]}"; do
-    if is_package_installed "$pkg"; then
-        echo "$pkg is already installed."
-    else
-        echo "Installing $pkg..."
-        pip install "$pkg" > /dev/null && echo "$pkg installed successfully." || { echo "Failed to install $pkg."; exit 1; }
-    fi
-done
-
-echo "Setup complete. The python virtual environment is ready to use."
+[[ -d "$HOME/.local/bin" ]] && export PATH="$PATH:$HOME/.local/bin"
 
 # Create an output file that contains all of the video paths and use it to loop the contents
-tmp_list_dir="$(mktemp -d)"
-cat > "$tmp_list_dir/list.txt" <<'EOF'
-/path/to/video.mkv
+temp_file=$(mktemp)
+cat > "$temp_file" <<'EOF'
 /path/to/video.mp4
+/path/to/video.mkv
 EOF
 
-while read -u 9 video
-do
-# Stores the current video width, aspect ratio, profile, bit rate, and total duration in variables for use later in the ffmpeg command line
+while read -u 9 video; do
+    # Stores the current video width, aspect ratio, profile, bit rate, and total duration in variables for use later in the ffmpeg command line
     aspect_ratio=$(ffprobe -hide_banner -select_streams v:0 -show_entries stream=display_aspect_ratio -of default=nk=1:nw=1 -pretty "$video" 2>/dev/null)
     length=$(ffprobe -hide_banner -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$video" 2>/dev/null)
     maxrate=$(ffprobe -hide_banner -show_entries format=bit_rate -of default=nk=1:nw=1 -pretty "$video" 2>/dev/null)
     height=$(ffprobe -hide_banner -select_streams v:0 -show_entries stream=height -of csv=s=x:p=0 -pretty "$video" 2>/dev/null)
     width=$(ffprobe -hide_banner -select_streams v:0 -show_entries stream=width -of csv=s=x:p=0 -pretty "$video" 2>/dev/null)
 
-# Modify vars to get file input and output names
+    # Modify vars to get file input and output names
     file_in="$video"
     fext="${video#*.}"
     file_out="${video%.*} (x265).$fext"
 
-# Gets the input videos max datarate and applies logic to determine bitrate, bufsize, and maxrate variables
+    # Gets the input videos max datarate and applies logic to determine bitrate, bufsize, and maxrate variables
     trim=$(bc <<< "scale=2 ; ${maxrate::-11} * 1000")
     btr=$(bc <<< "scale=2 ; $trim / 2")
     bitrate="${btr::-3}"
@@ -96,7 +33,7 @@ do
     bufsize="${bfs::-3}"
     length=$((${length::-7} / 60))
 
-# Print the video stats in the terminal
+    # Print the video stats in the terminal
     cat <<EOF
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
@@ -143,13 +80,13 @@ EOF
             -c:a copy \
             "$file_out"; then
         google_speech "Video conversion completed." 2>/dev/null
-        if [ -f "$file_out" ]; then
-            sudo rm "$file_in"
-        fi
+        [[ -f "$file_out" ]] && sudo rm "$file_in"
+
+        # Remove the successfully encoded video's file path from the temporary file
+        sed -i "\|^$video\$|d" "$temp_file"
     else
         google_speech "Video conversion failed." 2>/dev/null
-        echo
         exit 1
     fi
     clear
-done 9< "$tmp_list_dir/list.txt"
+done 9< "$temp_file"
