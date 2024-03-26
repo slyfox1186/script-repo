b4875311cc6b235f5459d769116cf193edfe2770
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-03-26 00:27:46 -0400

    Update pip-virtual-installer.sh

--- Bash/Misc/Python3/pip-virtual-installer.sh
+++ Bash/Misc/Python3/pip-virtual-installer.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Help Menu: ./pip-virtual-installer.sh -h
+
 # Define the master folder path
 MASTER_FOLDER="$HOME/python-venv"
 VENV_NAME="myenv"
@@ -7,84 +9,102 @@ REQUIREMENTS_FILE="$MASTER_FOLDER/requirements.txt"
 
 # Define an array of pip package names
 pip_packages=(
-    "adal" "aiohttp" "aiohttpx" "aiosignal" "annotated-types" "anyio" "appdirs" "asgiref" "async-generator"
-    "async-lru" "async-openai" "async-timeout" "attrs" "avro" "Babel" "backoff" "bcrypt" "beautifulsoup4" "beniget"
-    "blinker" "boto3" "botocore" "cachetools" "certifi" "cffi" "chardet" "charset-normalizer" "click" "cmarkgfm"
-    "colorama" "colorlog" "commonmark" "cryptography" "dbus-python" "decorator" "Deprecated" "distro" "Django"
-    "exceptiongroup" "ffpb" "fixedint" "Flask" "frozendict" "frozenlist" "future" "fuzzywuzzy" "gast" "gcovr"
-    "gensim" "google-api-core" "google-auth" "google-speech" "googleapis-common-protos" "h11" "html5lib" "httpcore"
-    "httpx" "idna" "importlib-metadata" "iniconfig" "invoke" "isodate" "itsdangerous" "Jinja2" "jmespath" "joblib"
-    "jsonpointer" "jsonschema" "jsonschema-specifications" "lazyops" "Levenshtein" "loguru" "lxml" "Mako" "Markdown"
-    "MarkupSafe" "marshmallow" "meson" "more-itertools" "msal" "msal-extensions" "msrest" "msrestazure" "multidict"
-    "nltk" "npx" "numpy" "oauthlib" "olefile" "openai" "opencensus" "opencensus-context" "opencensus-ext-azure"
-    "opencensus-ext-logging" "opentelemetry-api" "opentelemetry-sdk" "opentelemetry-semantic-conventions" "outcome"
-    "packaging" "paramiko" "pillow" "pluggy" "ply" "portalocker" "proto-plus" "protobuf" "psutil" "py" "pyasn1"
-    "pyasn1-modules" "pycairo" "pycparser" "pycups" "pydantic" "pydantic-settings" "pydantic_core" "pydash" "Pygments"
-    "PyGObject" "pyinotify" "PyJWT" "PyNaCl" "pyOpenSSL" "pyparsing" "pyrsistent" "pysmbc" "PySocks" "pytest"
-    "python-dateutil" "python-dotenv" "python-Levenshtein" "python-whois" "pythran" "pytz" "PyYAML" "rapidfuzz"
-    "referencing" "regex" "requests" "requests-oauthlib" "rfc3987" "robot-detection" "rpds-py" "rsa" "ruamel.yaml"
-    "ruamel.yaml.clib" "s3transfer" "scipy" "SCons" "selenium" "selenium-stealth" "simplejson" "six" "smart-open"
-    "sniffio" "sortedcontainers" "soupsieve" "sqlparse" "strictyaml" "termcolor" "tiktoken" "tqdm" "trash-cli" "trio"
-    "trio-websocket" "typing_extensions" "uamqp" "uritemplate" "urllib3" "web-cache" "webcolors" "webencodings" "Werkzeug"
-    "whois" "wrapt" "wsproto" "yarl" "zipp"
+    "async-lru" "async-openai" "async-timeout" "attrs" "avro" "Babel" "backoff" "bcrypt" "beautifulsoup4"
+    "colorama" "ffpb" "Flask" "fuzzywuzzy" "google-speech" "jsonschema" "nltk" "openai" "python-dateutil"
+    "python-dotenv" "python-Levenshtein" "python-whois" "regex" "requests" "setuptools" "termcolor" "wheel"
+    "whois"
 )
+
+# Function to install specific packages
+install_specific_packages() {
+    activate_venv
+    for package in "${@}"; do
+        pip install "$package"
+    done
+    deactivate_venv
+}
+
+# Function to upgrade specific packages
+upgrade_specific_packages() {
+    activate_venv
+    for package in "${@}"; do
+        pip install --upgrade "$package"
+    done
+    deactivate_venv
+}
+
+# Function to remove specific packages
+remove_specific_packages() {
+    activate_venv
+    for package in "${@}"; do
+        pip uninstall -y "$package"
+    done
+    deactivate_venv
+}
+
+# Function to activate the virtual environment
+activate_venv() {
+    source "$MASTER_FOLDER/$VENV_NAME/bin/activate"
+}
+
+# Function to deactivate the virtual environment
+deactivate_venv() {
+    deactivate
+}
+
+# Function to install packages from requirements.txt or default pip_packages array
+install_packages() {
+    pip install --upgrade pip
+
+    if [[ -f "$REQUIREMENTS_FILE" ]]; then
+        pip install -r "$REQUIREMENTS_FILE"
+    else
+        printf "%s\n" "${pip_packages[@]}" > "$REQUIREMENTS_FILE"
+        pip install -r "$REQUIREMENTS_FILE"
+    fi
+}
 
 # Function to import packages from pip freeze
 import_packages() {
     if [[ -d "$MASTER_FOLDER/$VENV_NAME" ]]; then
         printf "\n%s\n\n" "Importing packages from pip freeze..."
-        source "$MASTER_FOLDER/$VENV_NAME/bin/activate"
+        activate_venv
         pip freeze | awk -F'=' '{print $1}' > "$REQUIREMENTS_FILE"
         printf "\n%s\n\n" "Packages imported successfully and stored in $REQUIREMENTS_FILE."
-        deactivate
+        deactivate_venv
     else
         printf "\n%s\n\n" "Virtual environment does not exist. Create one using the -c or --create option."
     fi
 }
 
-# Function to create virtual environment and install packages
+# Function to create a virtual environment and install packages
 create_venv() {
     echo "Creating Python virtual environment..."
     mkdir -p "$MASTER_FOLDER"
     python3 -m venv "$MASTER_FOLDER/$VENV_NAME"
 
     echo "Activating virtual environment..."
-    source "$MASTER_FOLDER/$VENV_NAME/bin/activate"
+    activate_venv
 
     echo "Installing pip packages..."
-    pip install --upgrade pip
-
-    # Install packages from requirements.txt if it exists, otherwise use the default pip_packages array
-    if [[ -f "$REQUIREMENTS_FILE" ]]; then
-        pip install -r "$REQUIREMENTS_FILE"
-    else
-        # Output array content into the requirements.txt file
-        printf "%s\n" "${pip_packages[@]}" > "$REQUIREMENTS_FILE"
-        pip install -r "$REQUIREMENTS_FILE"
-    fi
+    install_packages
 
     echo "Installation completed."
-    deactivate
-
-    # Delete leftover files
-    [[ -f "$REQUIREMENTS_FILE" ]] && rm -f "$REQUIREMENTS_FILE"
+    deactivate_venv
 }
 
 # Function to update virtual environment
 update_venv() {
     if [[ -d "$MASTER_FOLDER/$VENV_NAME" ]]; then
         printf "\n%s\n\n" "Updating Python virtual environment..."
-        source "$MASTER_FOLDER/$VENV_NAME/bin/activate"
-        pip install --upgrade pip
-        pip install -r "$REQUIREMENTS_FILE"
+        activate_venv
+        install_packages
         printf "\n%s\n\n" "Update completed."
-        deactivate
+        deactivate_venv
     else
         printf "\n%s\n\n" "Virtual environment does not exist. Creating one now..."
         create_venv
     fi
-    # Delete leftover files
-    [[ -f "$REQUIREMENTS_FILE" ]] && rm -f "$REQUIREMENTS_FILE"
 }
 
 # Function to delete virtual environment
@@ -92,8 +112,6 @@ delete_venv() {
     printf "\n%s\n\n" "Deleting Python virtual environment..."
     rm -rf "$MASTER_FOLDER/$VENV_NAME"
     printf "\n%s\n\n" "Deletion completed."
-
-    # Delete leftover files
     [[ -f "$REQUIREMENTS_FILE" ]] && rm -f "$REQUIREMENTS_FILE"
 }
 
@@ -101,9 +119,9 @@ delete_venv() {
 list_packages() {
     if [[ -d "$MASTER_FOLDER/$VENV_NAME" ]]; then
         printf "\n%s\n\n" "Listing installed packages in the virtual environment..."
-        source "$MASTER_FOLDER/$VENV_NAME/bin/activate"
+        activate_venv
         pip list
-        deactivate
+        deactivate_venv
     else
         printf "\n%s\n\n" "Virtual environment does not exist. Create one using the -c or --create option."
     fi
@@ -112,37 +130,65 @@ list_packages() {
 # Function to display help
 display_help() {
     echo "Usage: $0 [OPTIONS]"
+    echo
     echo "Options:"
-    echo "  -h, --help       Display this help message."
-    echo "  -c, --create     Create a new Python virtual environment."
-    echo "  -u, --update     Update the existing Python virtual environment."
-    echo "  -d, --delete     Delete the Python virtual environment."
-    echo "  -l, --list       List the installed packages in the virtual environment."
-    echo "  -i, --import     Import packages from pip freeze into the requirements.txt file."
+    echo
+    echo "  -h, --help                 Display this help message."
+    echo "  -l, --list                 List the installed packages in the virtual environment."
+    echo "  -i, --import               Import packages from pip freeze into the requirements.txt file."
+    echo "  -c, --create               Create a new Python virtual environment."
+    echo "  -u, --update               Update the existing Python virtual environment."
+    echo "  -d, --delete               Delete the Python virtual environment."
+    echo "  -a, --add PACKAGES         Install specific packages (comma-separated) in the virtual environment."
+    echo "  -U, --upgrade PACKAGES     Upgrade specific packages (comma-separated) in the virtual environment."
+    echo "  -r, --remove PACKAGES      Remove specific packages (comma-separated) from the virtual environment."
+    echo
+    echo "Examples: ./$0 --help"
+    echo "Examples: ./$0 --create"
+    echo "Examples: ./$0 -i && ./$0 -U setuptools,wheel"
 }
 
 # Handling arguments
-case "$1" in
-    -h|--help)
-        display_help
-        ;;
-    -c|--create)
-        create_venv
-        ;;
-    -u|--update)
-        update_venv
-        ;;
-    -d|--delete)
-        delete_venv
-        ;;
-    -l|--list)
-        list_packages
-        ;;
-    -i|--import)
-        import_packages
-        ;;
-    *)
-        echo "Invalid option. Use -h for help."
-        exit 1
-        ;;
-esac
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h|--help)
+            display_help
+            exit 0
+            ;;
+        -c|--create)
+            create_venv
+            ;;
+        -u|--update)
+            update_venv
+            ;;
+        -d|--delete)
+            delete_venv
+            ;;
+        -l|--list)
+            list_packages
+            ;;
+        -i|--import)
+            import_packages
+            ;;
+        -a|--add)
+            IFS=',' read -ra PACKAGES <<< "$2"
+            install_specific_packages "${PACKAGES[@]}"
+            shift
+            ;;
+        -U|--upgrade)
+            IFS=',' read -ra PACKAGES <<< "$2"
+            upgrade_specific_packages "${PACKAGES[@]}"
+            shift
+            ;;
+        -r|--remove)
+            IFS=',' read -ra PACKAGES <<< "$2"
+            remove_specific_packages "${PACKAGES[@]}"
+            shift
+            ;;
+        *)
+            echo "Invalid option: $1. Use -h for help."
+            exit 1
+            ;;
+    esac
+    shift
+done
