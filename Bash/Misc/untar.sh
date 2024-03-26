551feee736e81460a32d9bc92bb7faeda4bde421
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-03-26 04:05:42 -0400

    Update untar.sh

--- Bash/Misc/untar.sh
+++ Bash/Misc/untar.sh
@@ -1,24 +1,44 @@
 #!/usr/bin/env bash
 
-
 untar() {
-    clear
-    local archive ext gflag jflag xflag
+    local archive dirname ext USER=$(whoami) approved="7z zip gz tgz bz2 xz lz"
 
-    for archive in *.*
-    do
+    for archive in *.*; do
+        ext="${archive##*.}"
+        if [[ ! " $approved " =~ " $ext " ]]; then
+            continue
+        fi
 
-        [[ ! -d "$PWD"/"$archive%%.*" ]] && mkdir -p "$PWD"/"$archive%%.*"
+        dirname="${archive%.*}"
+        if [[ "$archive" =~ \.tar\.(gz|bz2|xz|lz)$ ]]; then
+            dirname="${dirname%.*}"
+        fi
 
-        unset flag
+        mkdir -p "$dirname"
+
         case "$ext" in
-            7z|zip) 7z x -o./"$archive%%.*" ./"$archive";;
-            bz2)    flag='jxf';;
-            gz|tgz) flag='zxf';;
-            xz|lz)  flag='xf';;
+            7z) sudo 7z x -y "$archive" -o"$dirname" ;;
+            zip) temp_dir=$(mktemp -d)
+                 sudo unzip "$archive" -d "$temp_dir"
+                 items=("$temp_dir"/* "$temp_dir"/.*) # Include hidden files
+                 if [[ ${#items[@]} -eq 3 && -d "${items[0]}" && "${items[0]##*/}" == "$dirname" ]]; then
+                     sudo mv "${items[0]}"/* "${items[0]}/".* "$dirname" 2>/dev/null
+                 else
+                     sudo mv "$temp_dir"/* "$temp_dir"/.* "$dirname" 2>/dev/null
+                 fi
+                 sudo rm -rf "$temp_dir"
+                 ;;
+            gz|tgz|bz2|xz|lz)
+                 sudo tar -xf "$archive" -C "$dirname" --strip-components 1 ;;
         esac
+    done
 
-        [ -n "$flag" ] && tar $flag ./"$archive" -C ./"$archive%%.*" --strip-components 1
+    for dir in *; do
+        if [ -d "$dir" ]; then
+            sudo chown -R "$USER":"$USER" "$dir"
+            sudo chmod -R 755 "$dir"
+        fi
     done
 }
+
 untar
