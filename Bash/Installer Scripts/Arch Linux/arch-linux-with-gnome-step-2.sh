841f3f844873a9443116cd5367627fe4cbfea896
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-04-05 12:56:43 -0400

    Update arch-linux-with-gnome-step-2.sh

--- Bash/Installer Scripts/Arch Linux/arch-linux-with-gnome-step-2.sh
+++ Bash/Installer Scripts/Arch Linux/arch-linux-with-gnome-step-2.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-clear
-
 set -e
 
 ####################################
 ## After YOU LOAD INTO ARCH LINUX ##
 ####################################
+
+#IMPORTANT - Login as the root user before running this script
 
 # Install required software using pacman
 pacman -Sy gnome lightdm lightdm-gtk-greeter nano gnome-terminal gnome-text-editor gedit gedit-plugins git nvidia pulseaudio pulseaudio-alsa sudo xorg xorg-xinit xorg-server
@@ -14,7 +14,12 @@ pacman -Sy gnome lightdm lightdm-gtk-greeter nano gnome-terminal gnome-text-edit
 # Set visudo env var
 EDITOR=nano visudo
 
-# Login to gnome desktop
+# Enable the gnome desktop
 systemctl enable gdm.service
+# Enable and start the networkmanager
+systemctl enable NetworkManager
+systemctl start NetworkManager
 
-printf "\n%s\n\n" 'The script has finished. Please reboot.'
+echo
+echo "The script has finished. Please reboot."
+echo
