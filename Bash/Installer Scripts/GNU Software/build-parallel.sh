98e9886e8f801efd0c3f8a32c16554b18f717122
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-04-04 23:36:46 -0400

    Update build-parallel.sh

--- Bash/Installer Scripts/GNU Software/build-parallel.sh
+++ Bash/Installer Scripts/GNU Software/build-parallel.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Purpose: Build GNU Parallel from source code
-# Updated: 03.16.24
-# Script version: 2.1
+# Updated: 04.04.24
+# Script version: 2.6
 # Optimizations: Efficiency, readability, functionality, error handling, best practices
 
 set -euo pipefail
@@ -14,25 +14,75 @@ YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
 # Log functions
-log() { echo -e "${GREEN}[INFO] Bash${NC} $*"; }
-warn() { echo -e "${YELLOW}[WARNING] Bash${NC} $*"; }
+log() {
+    echo -e "${GREEN}[INFO]${NC} $1"
+}
+
+warn() {
+    echo -e "${YELLOW}[WARNING]${NC} $1"
+}
+
 fail() {
-    echo -e "${RED}[ERROR] Bash${NC} $*"
+    echo -e "${RED}[ERROR]${NC} $1"
     exit 1
 }
 
-# Verify the script is not run as root
-if [[ "$EUID" -eq 0 ]]; then
-    fail "Do not run this script as root. Use sudo for necessary commands."
-fi
+# Help menu
+display_help() {
+    echo "Build GNU Parallel from source code"
+    echo
+    echo "Usage: $0 [options]"
+    echo
+    echo "Options:"
+    echo "  -v, --version <version>   Specify the version of GNU Parallel to install"
+    echo "  -l, --latest              Install the latest version of GNU Parallel"
+    echo "  -c, --cleanup             Automatically clean up the build files after installation"
+    echo "  -h, --help                Display this help menu"
+    echo
+    echo "This script builds and installs GNU Parallel from source code."
+    echo "It downloads the specified version (or the latest version) of GNU Parallel,"
+    echo "compiles it, and installs it in the /usr/local directory."
+    echo
+    echo "Dependencies:"
+    echo "  The script automatically checks and installs the required dependencies"
+    echo "  using the package manager (apt)."
+    echo
+    echo "Note: This script should not be run as root or with sudo."
+}
 
-# Dependencies
-DEPENDENCIES=(
-    autoconf autoconf-archive autogen automake binutils bison
-    build-essential bzip2 ccache curl libc6-dev libpth-dev
-    libtool libtool-bin lzip lzma-dev m4 nasm texinfo zlib1g-dev
-    yasm
-)
+# Parse command line arguments
+parse_arguments() {
+    while [[ "$#" -gt 0 ]]; do
+        case "$1" in
+            -v|--version)
+                DOWNLOAD_VERSION="$2"
+                shift 2
+                ;;
+            -l|--latest)
+                DOWNLOAD_VERSION="latest"
+                shift
+                ;;
+            -c|--cleanup)
+                CLEANUP=true
+                shift
+                ;;
+            -h|--help)
+                display_help
+                exit 0
+                ;;
+            *)
+                fail "You did not enter a valid version: $1"
+                ;;
+        esac
+    done
+}
+
+# Verify the script is not run as root
+verify_not_root() {
+    if [[ "$EUID" -eq 0 ]]; then
+        fail "You must not run this script as root or with sudo."
+    fi
+}
 
 # Check and install missing dependencies
 check_dependencies() {
@@ -44,29 +94,31 @@ check_dependencies() {
     done
     if [ ${#missing_deps[@]} -ne 0 ]; then
         log "Installing missing dependencies: ${missing_deps[*]}"
-        sudo apt-get update
-        sudo apt-get install -y "${missing_deps[@]}"
+        sudo apt update
+        sudo apt install "${missing_deps[@]}"
     else
         log "All dependencies are satisfied."
     fi
 }
 
-# Variables
-PROGRAM_NAME="parallel"
-DOWNLOAD_VERSION="latest"
-INSTALL_DIR_VERSION="20240222"
-ARCHIVE_URL="https://ftp.gnu.org/gnu/parallel/${PROGRAM_NAME}-${DOWNLOAD_VERSION}.tar.bz2"
-CWD="$PWD/${PROGRAM_NAME}-build-script"
-INSTALL_DIR="/usr/local/${PROGRAM_NAME}-${INSTALL_DIR_VERSION}"
-
 # Cleanup function
 cleanup() {
-    echo
-    read -r -p "Do you want to clean up the build files? [y/N] " choice
-    case "$choice" in
-        [yY]*|"") rm -fr "$CWD"; echo; log "Cleanup completed." ;;
-        * ) log "Cleanup skipped." ;;
-    esac
+    if [[ "$CLEANUP" == true ]]; then
+        sudo rm -fr "$CWD"
+        echo
+        log "Cleanup completed."
+    else
+        echo
+        read -p "Do you want to clean up the build files? (yes/no) " choice
+        case "$choice" in
+            [yY]*|[yY][eE][sS]*|"")
+                sudo rm -fr "$CWD"
+                echo
+                log "Cleanup completed."
+                ;;
+            * ) log "Cleanup skipped." ;;
+        esac
+    fi
 }
 
 # Exit function
@@ -76,30 +128,64 @@ exit_fn() {
     log "Make sure to star the repository to show your support: https://github.com/slyfox1186/script-repo"
 }
 
-# Main script
-main() {
-    check_dependencies
-    log "Building GNU ${PROGRAM_NAME} from source."
-    echo
-
+# Download and extract source code
+download_and_extract() {
     # Create build directory
     mkdir -p "$CWD"
     cd "$CWD" || exit 1
 
-    # Download and extract source
-    curl -sSfLo "${PROGRAM_NAME}.tar.bz2" "$ARCHIVE_URL"
-    tar -jxf "${PROGRAM_NAME}.tar.bz2" --strip-components 1
+    # Download the source code files
+    if [[ "$DOWNLOAD_VERSION" == "latest" ]]; then
+        ARCHIVE_URL="https://ftp.gnu.org/gnu/parallel/parallel-latest.tar.bz2"
+    else
+        ARCHIVE_URL="https://ftp.gnu.org/gnu/parallel/parallel-$DOWNLOAD_VERSION.tar.bz2"
+    fi
 
+    curl --connect-timeout 2 --retry 2 -LSso "$PROGRAM_NAME-$DOWNLOAD_VERSION.tar.bz2" "$ARCHIVE_URL"
+ 
+    # Extract source code
+    tar -jxf "$PROGRAM_NAME-$DOWNLOAD_VERSION.tar.bz2" --strip-components 1
+}
+
+# Build and install
+build_and_install() {
     # Build process
     ./configure --prefix="$INSTALL_DIR"
-    make "-j$(nproc)"
+    make "-j$(nproc --all)"
     sudo make install
 
     # Create symbolic links
-    find "$INSTALL_DIR/bin" -type f -exec sudo ln -s {} /usr/local/bin \;
+    sudo ln -sf "$INSTALL_DIR/bin/parallel" "/usr/local/bin/parallel"
+    sudo ln -sf "$INSTALL_DIR/share/man/man1/parallel.1" "/usr/local/share/man/man1/parallel.1"
+}
 
+# Main script
+main() {
+    parse_arguments "$@"
+    verify_not_root
+    check_dependencies
+    log "Building GNU $PROGRAM_NAME from source."
+    echo
+    download_and_extract
+    build_and_install
     cleanup
     exit_fn
 }
+
+# Variables
+PROGRAM_NAME="parallel"
+[[ -z ${DOWNLOAD_VERSION+x} ]] && DOWNLOAD_VERSION="20240322"
+ARCHIVE_URL="https://ftp.gnu.org/gnu/parallel/$PROGRAM_NAME-$DOWNLOAD_VERSION.tar.bz2"
+CWD="$PWD/$PROGRAM_NAME-build-script"
+INSTALL_DIR="/usr/local/$PROGRAM_NAME-$DOWNLOAD_VERSION"
+CLEANUP=false
+
+# Dependencies
+DEPENDENCIES=(
+    autoconf autoconf-archive autogen automake binutils bison
+    build-essential bzip2 ccache curl libc6-dev libpth-dev
+    libtool libtool-bin lzip lzma-dev m4 nasm texinfo zlib1g-dev
+    yasm
+)
 
 main "$@"
