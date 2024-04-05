284ea3d1cbe3fb7aa90550a8d257a7b3c60252e4
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-04-05 13:47:58 -0400

    Update mirrors-menu.sh

--- Bash/Installer Scripts/SlyFox1186 Scripts/mirrors-menu.sh
+++ Bash/Installer Scripts/SlyFox1186 Scripts/mirrors-menu.sh
@@ -3,42 +3,12 @@
 # Define color variables
 GREEN='\e[0;32m'
 CYAN='\e[0;36m'
-MAGENTA='\e[0;35m'
 RED='\033[0;31m'
 NC='\e[0m' # No Color
 
 # Create a temporary directory for storing files
 dir=$(mktemp -d /tmp/mirrors-script-XXXXXX)
 
-execute_arch() {
-    local arch_flags="$1"
-    if curl -Lso "$dir/$file" "$url"; then
-        if [[ -f "$dir/$file" ]]; then
-            if sudo bash "$dir/$file" $arch_flags; then
-                sudo rm -rf "$dir"
-                echo -e "${GREEN}[SUCCESS]${NC} Execution completed successfully.\n"
-                exit 0
-            else
-                echo -e "${RED}[ERROR]${NC} Failed to execute: \"$file $flag\"\n"
-                read -p "Press any key to exit."
-                sudo rm -rf "$dir"
-                exit 1
-            fi
-        else
-            echo -e "${RED}[ERROR]${NC} File not found: \"$file\"\n"
-            read -p "Press any key to exit."
-            sudo rm -rf "$dir"
-            exit 1
-        fi
-    else
-        echo -e "${RED}[ERROR]${NC} Failed to download: \"$file\"\n"
-        read -p "Press any key to exit."
-        sudo rm -rf "$dir"
-        exit 1
-    fi
-}
-
-# Function to download and execute mirrors script
 execute() {
     local url=$1
     local file=$2
@@ -46,80 +16,65 @@ execute() {
     
     if [[ "$flag" == "arch" ]]; then
         flag_args="-m 30 -f daily -c US"
-        execute_arch "$flag_args"
+        file="$file $flag_args"
+    elif [[ -n "$flag" ]]; then
+        file="$file $flag"
     fi
 
     if curl -Lso "$dir/$file" "$url"; then
         if [[ -f "$dir/$file" ]]; then
-            if sudo bash "$dir/$file $flag"; then
+            if sudo bash "$dir/$file"; then
                 sudo rm -rf "$dir"
                 echo -e "${GREEN}[SUCCESS]${NC} Execution completed successfully.\n"
                 exit 0
             else
-                echo -e "${RED}[ERROR]${NC} Failed to execute: \"$file $flag\"\n"
-                read -p "Press any key to exit."
-                sudo rm -rf "$dir"
-                exit 1
+                echo -e "${RED}[ERROR]${NC} Failed to execute: \"$file\"\n"
             fi
         else
             echo -e "${RED}[ERROR]${NC} File not found: \"$file\"\n"
-            read -p "Press any key to exit."
-            sudo rm -rf "$dir"
-            exit 1
         fi
     else
         echo -e "${RED}[ERROR]${NC} Failed to download: \"$file\"\n"
-        read -p "Press any key to exit."
-        sudo rm -rf "$dir"
-        exit 1
     fi
+    read -p "Press any key to exit."
+    sudo rm -rf "$dir"
+    exit 1
 }
 
-# Function to display the ubuntu menu
-ubuntu_menu() {
+display_menu() {
+    local os=$1
     local choice
-    echo -e "${GREEN}1)${NC} Ubuntu 23.04 - Lunar Lobster"
-    echo -e "${GREEN}2)${NC} Ubuntu 22.04 - Jammy Jellyfish"
-    echo -e "${GREEN}3)${NC} Ubuntu 20.04 - Focal Fossa"
-    echo -e "${GREEN}4)${NC} Ubuntu 18.04 - Bionic Beaver"
+    case "$os" in
+        ubuntu)
+            echo -e "${GREEN}1)${NC} Ubuntu 23.04 - Lunar Lobster"
+            echo -e "${GREEN}2)${NC} Ubuntu 22.04 - Jammy Jellyfish"
+            echo -e "${GREEN}3)${NC} Ubuntu 20.04 - Focal Fossa"
+            echo -e "${GREEN}4)${NC} Ubuntu 18.04 - Bionic Beaver"
+            ;;
+        debian)
+            echo -e "${GREEN}1)${NC} Debian 11 (Bullseye)"
+            echo -e "${GREEN}2)${NC} Debian 12 (Bookworm)"
+            ;;
+    esac
     echo -e "${GREEN}0)${NC} Back"
     echo
-    echo -en "${CYAN}Choose the Ubuntu release version: ${NC}"
+    echo -en "${CYAN}Choose the $os release version: ${NC}"
     read -n 1 choice
     clear
-    case "$choice" in
-        1) execute "https://lunar-mirrors.optimizethis.net" "lunar-mirrors.sh" ;;
-        2) execute "https://jammy.optimizethis.net" "jammy-mirrors.sh" ;;
-        3) execute "https://focal-mirrors.optimizethis.net" "focal-mirrors.sh" ;;
-        4) execute "https://bionic-mirrors.optimizethis.net" "bionic-mirrors.sh" ;;
-        0) clear ;;
+    case "$os:$choice" in
+        ubuntu:1) execute "https://lunar-mirrors.optimizethis.net" "lunar-mirrors.sh" ;;
+        ubuntu:2) execute "https://jammy.optimizethis.net" "jammy-mirrors.sh" ;;
+        ubuntu:3) execute "https://focal-mirrors.optimizethis.net" "focal-mirrors.sh" ;;
+        ubuntu:4) execute "https://bionic-mirrors.optimizethis.net" "bionic-mirrors.sh" ;;
+        debian:1) execute "https://bullseye-mirrors.optimizethis.net" "bullseye-mirrors.sh" ;;
+        debian:2) execute "https://bookworm-mirrors.optimizethis.net" "bookworm-mirrors.sh" ;;
+        *:0) clear ;;
         *) clear
-           ubuntu_menu
+           display_menu "$os"
            ;;
     esac
 }
 
-# Function to display the debian menu
-debian_menu() {
-    local choice
-    echo -e "${GREEN}1)${NC} Debian 11 (Bullseye)"
-    echo -e "${GREEN}2)${NC} Debian 12 (Bookworm)"
-    echo -e "${GREEN}0)${NC} Back"
-    echo
-    echo -en "${CYAN}Choose the Debian release version: "
-    read -n 1 choice
-    clear
-    case "$choice" in
-        1) execute "https://bullseye-mirrors.optimizethis.net" "bullseye-mirrors.sh" ;;
-        2) execute "https://bookworm-mirrors.optimizethis.net" "bookworm-mirrors.sh" ;;
-        0) clear ;;
-        *) clear
-           debian_menu
-           ;;
-    esac
-}
-
-# Function to display the main menu
 main_menu() {
     local choice
     while true; do
@@ -134,8 +89,8 @@ main_menu() {
         read -n 1 choice
         clear
         case "$choice" in
-            1) ubuntu_menu ;;
-            2) debian_menu ;;
+            1) display_menu "ubuntu" ;;
+            2) display_menu "debian" ;;
             3) execute "https://raspi-mirrors.optimizethis.net" "raspi-mirrors.sh" ;;
             4) sudo mkdir -p "/etc/pacman.d"
                execute "https://raw.githubusercontent.com/slyfox1186/script-repo/main/Bash/Arch%20Linux%20Scripts/update_mirrorlist.sh" "update_mirrorlist.sh" "arch"
