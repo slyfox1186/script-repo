73660cec6f8da7e5bac92ac1d00d7a664011efcf
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-04-08 13:19:45 -0400

    Update create-icon.sh

--- Bash/Installer Scripts/ImageMagick/scripts/create-icon.sh
+++ Bash/Installer Scripts/ImageMagick/scripts/create-icon.sh
@@ -1,34 +1,35 @@
 #!/usr/bin/env bash
 
-# Required dependencies: imagemagick
-
 # Color and logging setup
 GREEN='\033[0;32m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
-echo -e "${GREEN}Converting images to ICONS...${NC}"
-
-# Navigate to the script's directory
-cd "$PWD"
+echo -e "${GREEN}Converting images to icons.${NC}"
 
 # Sizes for the icon
 SIZES="256,128,64,48,32,16"
 
 # Cleanup old output directory and create a new one
-[ -d "output" ] && rm -fr "output"
+[[ -d "output" ]] && rm -fr "output"
 mkdir "output"
 
 # Convert images
 find ./ -type f \( -iname "*.jpg" -o -iname "*.png" \) -print0 |
 while IFS= read -r -d $'\0' file; do
-    filename="${file##*/}"          # Extracts the basename (filename.extension)
-    filename_without_ext="${filename%.*}"  # Removes the extension
-    output_path="Output/${filename_without_ext}.ico"
+    filename="${file##*/}"
+    no_ext="${filename%.*}"
+    outpath="Output/${no_ext}.ico"
 
-    convert -background none "$file" -define icon:auto-resize="$SIZES" "$output_path"
-    if [ $? -eq 0 ]; then
+    if convert -background none "$file" -define icon:auto-resize="$SIZES" "$outpath"; then
         echo -e "${GREEN}Convert success: ${file}${NC}"
     else
         echo -e "${RED}Convert failed: ${file}${NC}"
     fi
 done
+
+echo
+read -p "Do you want to delete in input JPG and/or PNG file? (y/n): " choice
+case "$choice" in
+    [yY]*) sudo rm -f *.jpg *.png ;;
+    [nN]*) ;;
+esac
