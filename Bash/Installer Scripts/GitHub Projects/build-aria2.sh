048531e3268e597bceee11999bef3754360ae48a
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-04-10 15:55:37 -0400

    Update build-aria2.sh

--- Bash/Installer Scripts/GitHub Projects/build-aria2.sh
+++ Bash/Installer Scripts/GitHub Projects/build-aria2.sh
@@ -2,8 +2,8 @@
 
 ##  Github Script: https://github.com/slyfox1186/script-repo/blob/main/Bash/Installer%20Scripts/GitHub%20Projects/build-aria2
 ##  Purpose: Build aria2 from source code
-##  Updated: 03.07.24
-##  Script version: 2.0
+##  Updated: 04.10.24
+##  Script version: 2.1
 ##  Features:
 ##            - static build
 ##            - openssl backend
@@ -18,15 +18,21 @@
 ##         - runpath to LDFLAGS
 ##  Fixed: Soft linking error
 
-script_ver="2.0"
+script_ver="2.1"
 echo "GitHub Script for building aria2 from source. Version: $script_ver"
 
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
-log() { echo -e "${GREEN}[LOG]${NC} $(date +'%Y-%m-%d %H:%M:%S') - $*"; }
-handle_error() { echo -e "${RED}[ERROR]${NC} $(date +'%Y-%m-%d %H:%M:%S') - $*"; exit 1; }
+log() {
+    echo -e "${GREEN}[LOG]${NC} $(date +'%Y-%m-%d %H:%M:%S') - $*"
+}
+
+handle_error() {
+    echo -e "${RED}[ERROR]${NC} $(date +'%Y-%m-%d %H:%M:%S') - $*"
+    exit 1
+}
 
 display_help() {
     echo "Usage: $0 [OPTION]..."
@@ -40,12 +46,40 @@ display_help() {
 }
 
 set_compiler_options() {
-    CC="ccache gcc"
-    CXX="ccache g++"
-    CFLAGS="-g -O3 -pipe -fno-plt -march=native"
-    CXXFLAGS="-g -O3 -pipe -fno-plt -march=native"
+    CC="gcc"
+    CXX="g++"
+    CFLAGS="-O2 -pipe -march=native -mtune=native"
+    CXXFLAGS="$CFLAGS"
     export CC CFLAGS CXX CXXFLAGS
 }
+
+PATH="\
+/usr/lib/ccache:\
+$HOME/perl5/bin:\
+$HOME/.cargo/bin:\
+$HOME/.local/bin:\
+/usr/local/sbin:\
+/usr/local/cuda/bin:\
+/usr/local/x86_64-linux-gnu/bin:\
+/usr/local/bin:\
+/usr/sbin:\
+/usr/bin:\
+/sbin:\
+/bin\
+"
+export PATH
+
+PKG_CONFIG_PATH="\
+/usr/local/lib64/pkgconfig:\
+/usr/local/lib/pkgconfig:\
+/usr/local/lib/x86_64-linux-gnu/pkgconfig:\
+/usr/local/share/pkgconfig:\
+/usr/lib64/pkgconfig:\
+/usr/lib/pkgconfig:\
+/usr/lib/x86_64-linux-gnu/pkgconfig:\
+/usr/share/pkgconfig\
+"
+export PKG_CONFIG_PATH
 
 # Check if help argument is passed
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
@@ -56,20 +90,27 @@ install_packages() {
     local pkgs=(autoconf autoconf-archive autogen automake build-essential ca-certificates
                 ccache curl libssl-dev libtool libtool-bin m4 pkg-config zlib1g-dev)
     log "Attempting to install required packages..."
-    apt install -y "${pkgs[@]}"
+    sudo apt install -y "${pkgs[@]}"
     for pkg in "${pkgs[@]}"; do
-        dpkg-query -W -f='${Status}' "$pkg" 2>/dev/null | grep -q "ok installed" || handle_error "Failed to install: $pkg"
+        sudo dpkg-query -W -f='${Status}' "$pkg" 2>/dev/null | grep -q "ok installed" || handle_error "Failed to install: $pkg"
     done
     echo
     log "Installation of required packages completed."
 }
 
-github_latest_release_version() { curl -sLH "Content-Type: text/plain" "https://gitver.optimizethis.net" | bash -s "$1"; }
+github_latest_release_version() {
+    curl -LSs "https://gitver.optimizethis.net" | bash -s "$1"
+}
 
 libgpg_latest_release_version() {
     local url="$1"
-    local latest_version=$(curl -s "$url" | grep -Eo 'href="libgpg-error-[0-9]+\.[0-9]+\.tar\.bz2"' | head -n1 | grep -Eo '[0-9]+\.[0-9]+')
-    [[ -z "$latest_version" ]] && { echo "Failed to find the latest version of libgpg-error."; exit 1; } || echo "$latest_version"
+    local latest_version=$(curl -fsS "$url" | grep -Eo 'href="libgpg-error-[0-9]+\.[0-9]+\.tar\.bz2"' | head -n1 | grep -Eo '[0-9]+\.[0-9]+')
+    if [[ -z "$latest_version" ]]; then
+        echo "Failed to find the latest version of libgpg-error."
+        exit 1
+    else
+        echo "$latest_version"
+    fi
 }
 
 prepare_build_environment() {
@@ -88,9 +129,10 @@ build_jemalloc() {
     local jemalloc_version=$(github_latest_release_version "https://github.com/jemalloc/jemalloc.git")
     local jemalloc_url="https://github.com/jemalloc/jemalloc/releases/download/$jemalloc_version/jemalloc-$jemalloc_version.tar.bz2"
     curl -Ls "$jemalloc_url" | tar -xj
-    cd "jemalloc-$jemalloc_version"
+    cd "jemalloc-$jemalloc_version" || exit 1
     ./configure --prefix="$temp_dir/jemalloc"
-    make "-j$(nproc)" && make install
+    make "-j$(nproc)" && \
+    sudo make install
     cd ../
 }
 
@@ -101,9 +143,10 @@ build_libgpg_error() {
     local libgpg_error_version=$(libgpg_latest_release_version "https://gnupg.org/ftp/gcrypt/libgpg-error/")
     local libgpg_error_url="https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-$libgpg_error_version.tar.bz2"
     curl -Ls "$libgpg_error_url" | tar -xj
-    cd "libgpg-error-${libgpg_error_version}"
+    cd "libgpg-error-${libgpg_error_version}" || exit 1
     ./configure --prefix="$temp_dir/libgpg-error"
-    make "-j$(nproc)" && make install
+    make "-j$(nproc)" && \
+    sudo make install
     cd ../
 }
 
@@ -114,7 +157,7 @@ compile_aria2() {
     local aria2_version=$(github_latest_release_version "https://github.com/aria2/aria2.git")
     local aria2_url="https://github.com/aria2/aria2/releases/download/release-$aria2_version/aria2-$aria2_version.tar.xz"
     curl -Ls "$aria2_url" | tar -xJ
-    cd "aria2-$aria2_version"
+    cd "aria2-$aria2_version" || exit 1
     sed -i "s/1, 16/1, 128/g" "src/OptionHandlerFactory.cc"
     PATH="$temp_dir/jemalloc/bin:$temp_dir/libgpg-error/bin:$PATH" \
     ./configure --prefix=/usr/local \
@@ -127,7 +170,7 @@ compile_aria2() {
                 LDFLAGS="-L$temp_dir/jemalloc/lib -L$temp_dir/libgpg-error/lib" \
                 CPPFLAGS="-I$temp_dir/jemalloc/include -I$temp_dir/libgpg-error/include"
     make "-j$(nproc)" && \
-    make install
+    sudo make install
     cd ../
 }
 
@@ -135,7 +178,7 @@ create_aria2_service() {
     echo
     log "Creating aria2 service..."
     echo
-    tee /etc/systemd/system/aria2.service > /dev/null <<EOT
+    sudo tee /etc/systemd/system/aria2.service >/dev/null <<EOT
 [Unit]
 Description=Aria2 Service
 After=network.target
@@ -149,22 +192,22 @@ Restart=on-abort
 WantedBy=multi-user.target
 EOT
 
-    systemctl daemon-reload
-    systemctl enable aria2
-    systemctl start aria2
+    sudo systemctl daemon-reload
+    sudo systemctl enable aria2
+    sudo systemctl start aria2
     log "Aria2 service created and started."
     echo
 }
 
 cleanup() {
     log "Cleaning up..."
-    rm -fr "$build_dir" "$temp_dir"
+    sudo rm -fr "$build_dir" "$temp_dir"
     log "Cleanup completed."
 }
 
 main() {
-    if [[ "$EUID" -ne 0 ]]; then
-        echo "This script must be run as root or with sudo."
+    if [[ "$EUID" -eq 0 ]]; then
+        echo "This script must be run without root or with sudo."
         exit 1
     fi
 
@@ -183,8 +226,7 @@ main() {
             -s|--service)
                 create_aria2_service
                 ;;
-            *)
-                ;;
+            *)  ;;
         esac
     done
     
