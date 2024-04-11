69bd791082dbcef4e31637afb821794db6e67b6a
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-04-11 06:07:10 -0400

    Update build-libtool.sh

--- Bash/Installer Scripts/GNU Software/build-libtool.sh
+++ Bash/Installer Scripts/GNU Software/build-libtool.sh
@@ -7,8 +7,8 @@
 # Github repository: https://github.com/slyfox1186/script-repo
 
 # Ensure the script is run as root
-if [ "$EUID" -ne 0 ]; then
-    echo -e "\nThis script must be run with root privileges. Please run it again with 'sudo' or as root.\n"
+if [ "$EUID" -eq 0 ]; then
+    echo "This script must be run without root or with sudo."
     exit 1
 fi
 
@@ -19,42 +19,48 @@ archive_url="https://ftp.gnu.org/gnu/libtool/$archive_dir.tar.xz"
 cwd="$PWD/libtool-build-script"
 
 # Display script banner
-echo -e "\n========== libtool Build Script v$script_ver ==========\n"
+echo "======================================="
+echo "   libtool Build Script v$script_ver   "
+echo "======================================="
+echo
 
 # Create the build directory
 echo "Preparing build environment..."
-rm -fr "$cwd" && mkdir -p "$cwd"
+[[ -d "$cwd" ]] && sudo rm -fr "$cwd"
+mkdir -p "$cwd"
 
 # Set environment variables for building
-export CC="gcc"
-export CXX="g++"
-export CFLAGS="-g -O3 -pipe -fno-plt -march=native"
-export CXXFLAGS="$CFLAGS"
+CC="ccache gcc"
+CXX="ccache g++"
+CFLAGS="-O2 -pipe -fno-plt -march=native -mtune=native -D_FORTIFY_SOURCE=2"
+CXXFLAGS="$CFLAGS"
+export CC CXX CFLAGS CXXFLAGS
 
 # Install required packages
 echo "Installing required packages..."
-apt update && apt install autoconf autoconf-archive autogen automake build-essential ccache cmake curl git libltdl-dev
+sudo apt update
+sudo apt install autoconf autoconf-archive autogen automake build-essential ccache cmake curl git libltdl-dev
 
 # Download libtool archive
 echo "Downloading libtool archive..."
 if [ ! -f "$cwd/$archive_dir.tar.xz" ]; then
-    curl -Lso "$cwd/$archive_dir.tar.xz" "$archive_url" || { echo "Failed to download libtool archive."; exit 1; }
+    curl -Lso "$cwd/$archive_dir.tar.xz" "$archive_url" || echo "Failed to download libtool archive."; exit 1
 fi
 
 # Extract the archive
 echo "Extracting archive..."
 mkdir -p "$cwd/$archive_dir/build"
-tar -xf "$cwd/$archive_dir.tar.xz" -C "$cwd/$archive_dir" --strip-components=1 || { echo "Failed to extract libtool archive."; exit 1; }
+tar -xf "$cwd/$archive_dir.tar.xz" -C "$cwd/$archive_dir" --strip-components=1 || echo "Failed to extract libtool archive."; exit 1
 
 # Build libtool from source
 echo "Building libtool from source..."
 cd "$cwd/$archive_dir/build" || exit 1
-../configure --prefix=/usr/local --enable-ltdl-install
-if ! make "-j$(nproc)"; then
+../configure --prefix="/usr/local" --enable-ltdl-install
+if ! make "-j$(nproc --all)"; then
      echo "Failed to build and install libtool. Line: $LINENO"
      exit 1
 fi
-if ! make install; then
+if ! sudo make install; then
      echo "Failed to build and install libtool. Line: $LINENO"
      exit 1
 fi
@@ -64,10 +70,12 @@ echo
 read -p "Do you want to clean up the build files? [Y/n]: " choice
 if [[ "$choice" =~ ^(yes|y| ) ]] || [[ -z "$choice" ]]; then
     echo "Cleaning up..."
-    rm -fr "$cwd"
+    sudo rm -fr "$cwd"
 fi
 
 # Completion message
-echo -e "\nlibtool has been successfully built and installed.\n"
+echo
+echo "libtool has been successfully built and installed."
+echo
 echo "Thank you for using this script. For more tools and scripts, visit our GitHub repository:"
 echo "https://github.com/slyfox1186/script-repo"
