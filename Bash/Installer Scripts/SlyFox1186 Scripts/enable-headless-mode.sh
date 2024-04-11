342fb35a9ed8cf64ab784d69e49be0a9ca05bd58
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-04-11 07:59:31 -0400

    Update enable-headless-mode.sh

--- Bash/Installer Scripts/SlyFox1186 Scripts/enable-headless-mode.sh
+++ Bash/Installer Scripts/SlyFox1186 Scripts/enable-headless-mode.sh
@@ -1,59 +1,106 @@
 #!/usr/bin/env bash
 
-clear
+# ANSI color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
 
-echo 'This script will allow the user to quickly switch Ubuntu Desktop'
-echo 'into it'\''s VGA or Headless Client mode as dictated by the startup'
-echo 'commands located in /etc/default/grub.'
-echo
-echo 'This has been only tested on my pc using Ubuntu Jammy 22.04.1'
-echo
-echo 'Run this script at your own risk as I assume none.'
-echo
-read -p 'Press enter to continue.'
-clear
+# Logging functions
+function log {
+    echo -e "${GREEN}[INFO]${NC} $1"
+}
 
-file='/etc/default/grub'
+function warn {
+    echo -e "${YELLOW}[WARNING]${NC} $1"
+}
 
-if [ ! "${file}" ]; then
-    echo "The main file: ${file} was not found."
-    echo
+function fail {
+    echo -e "${RED}[ERROR]${NC} $1"
     exit 1
+}
+
+# Check if script is run as root
+if [[ $EUID -eq 0 ]]; then
+    fail "This script must not be run as root."
 fi
 
-echo '[1] Headless Mode'
-echo '[2] VGA Mode'
-echo '[3] Exit'
+log "This script will allow the user to quickly switch Ubuntu Desktop"
+log "into its VGA or Headless Client mode as dictated by the startup"
+log "commands located in /etc/default/grub."
 echo
-read -p 'Your choices are ( 1 to 3): ' i
 
-clear
+grub_config="/etc/default/grub"
 
-if [[ "${i}" -eq '1'  ]]; then sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="3"/g' "${file}"
-elif [[ "${i}" -eq '2'  ]]; then sed -i 's/GRUB_CMDLINE_LINUX="3"/GRUB_CMDLINE_LINUX=""/g' "${file}"
-elif [[ "${i}" -eq '3'  ]]; then clear; \ls -1AhFv --group-directories-first --color; exit
-else
-    echo 'Input error: enter a number (1 to 3)'
+if [[ ! -f $grub_config ]]; then
+    fail "The main grub_config: $grub_config was not found."
+fi
+
+function display_menu {
+    echo "[1] Headless Mode"
+    echo "[2] VGA Mode"
+    echo "[3] Exit"
     echo
-    read -p 'Press enter to start over or Ctrl+Z to exit: '
-    clear
-    bash "${0}"
-fi
+    read -p "Your choices are (1 to 3): " choice
+}
 
-clear
-unset i
+function update_grub {
+    log "Updating grub..."
+    if ! sudo update-grub; then
+        fail "The update-grub command failed."
+    fi
+}
 
-echo 'Do you want to update grub?'
-echo
-echo '[1] Yes'
-echo '[2] No'
-echo
-read -p 'Enter a number: ' i
+function enable_headless_mode {
+    log "Enabling headless mode..."
+    sudo sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="3"/g' $grub_config
+    update_grub
+}
 
-clear
+function enable_vga_mode {
+    log "Enabling VGA mode..."
+    sudo sed -i 's/GRUB_CMDLINE_LINUX="3"/GRUB_CMDLINE_LINUX=""/g' $grub_config
+    update_grub
+}
 
-if ! update-grub; then
-    echo 'The update-grub command failed.'
+function prompt_reboot {
     echo
-    exit 1
-fi
+    read -p "Do you want to reboot the PC now? [y/N]: " reboot_choice
+    case $reboot_choice in
+        [Yy]*)
+            log "Rebooting the PC..."
+            sudo reboot
+            ;;
+        *)
+            log "Skipping reboot."
+            ;;
+    esac
+}
+
+function main {
+    while true; do
+        display_menu
+        case $choice in
+            1)
+                enable_headless_mode
+                prompt_reboot
+                break
+                ;;
+            2)
+                enable_vga_mode
+                prompt_reboot
+                break
+                ;;
+            3)
+                log "Exiting..."
+                exit 0
+                ;;
+            *)
+                warn "Input error: enter a number (1 to 3)"
+                read -p "Press enter to start over or Ctrl+Z to exit: "
+                ;;
+        esac
+    done
+}
+
+main
