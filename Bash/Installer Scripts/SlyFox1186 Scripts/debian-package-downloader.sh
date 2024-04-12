960369ce9cd85943bf845a90a9d4b517429449d6
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-04-12 05:36:24 -0400

    Update debian-package-downloader.sh

--- Bash/Installer Scripts/SlyFox1186 Scripts/debian-package-downloader.sh
+++ Bash/Installer Scripts/SlyFox1186 Scripts/debian-package-downloader.sh
@@ -1,39 +1,36 @@
 #!/usr/bin/env bash
 
-clear
-
-function box_out_banner()
+box_out_banner()
 {
-    input_char=$(echo "${@}" | wc -c)
-    line=$(for i in `seq 0 ${input_char}`; do printf "-"; done)
+    input_char=$(echo "$@" | wc -c)
+    line=$(for i in $(seq 0 $input_char); do printf "-"; done)
     tput bold
-    line="$(tput setaf 3)${line}"
+    line="$(tput setaf 3)$line"
     space=${line//-/ }
-    echo " ${line}"
-    printf '|' ; echo -n "$space" ; printf "%s\n" '|';
-    printf '| ' ;tput setaf 4; echo -n "${@}"; tput setaf 3 ; printf "%s\n" ' |';
-    printf '|' ; echo -n "$space" ; printf "%s\n" '|';
-    echo " ${line}"
+    echo " $line"
+    printf "|" ; echo -n "$space" ; printf "%s\n" "|";
+    printf "| " ;tput setaf 4; echo -n "$@"; tput setaf 3 ; printf "%s\n" " |";
+    printf "|" ; echo -n "$space" ; printf "%s\n" "|";
+    echo " $line"
     tput sgr 0
 }
 
-box_out_banner 'Debian Package Downloader'
+box_out_banner "Debian Package Downloader"
 
 if [ -z "${1}" ]; then
     printf "\n%s\n\n%s\n\n%s\n\n" \
-        'This script will download debian packages to the current directory.' \
-        'To download, enter a space separated list of APT packages' \
-        'Example: aptitude curl gedit gedit-plugins synaptic'
-    read -p 'Enter your list here: ' pkgs
+        "This script will download debian packages to the current directory." \
+        "To download, enter a space-separated list of APT packages" \
+        "Example: aptitude curl gedit gedit-plugins synaptic"
+    read -p "Enter your list here: " pkgs
     
 else
     pkgs="${1}"
 fi
 
-for i in ${pkgs[@]}
-do
-    printf "\n%s\n" 'Downloading...'
-    wget -cq "$(apt-get install --reinstall --print-uris -qq ${i} 2>/dev/null | cut -d"'" -f2)"
+for pkg in ${pkgs[@]}; do
+    printf "\n%s\n" "Downloading..."
+    wget -cq "$(apt-get install --reinstall --print-uris -qq $pkg 2>/dev/null | cut -d""" -f2)"
 done
 
-printf "\n%s\n\n" 'The script has finished.'
+printf "\n%s\n\n" "The script has finished."
