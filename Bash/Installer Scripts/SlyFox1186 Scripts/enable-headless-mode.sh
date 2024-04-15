2f5f8964f2df8f88a3f7f935fd36aaad323325a5
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-04-15 00:45:19 -0400

    changes

--- Bash/Installer Scripts/SlyFox1186 Scripts/enable-headless-mode.sh
+++ Bash/Installer Scripts/SlyFox1186 Scripts/enable-headless-mode.sh
@@ -7,15 +7,15 @@ YELLOW='\033[1;33m'
 NC='\033[0m'
 
 # Logging functions
-function log {
+log() {
     echo -e "${GREEN}[INFO]${NC} $1"
 }
 
-function warn {
+warn() {
     echo -e "${YELLOW}[WARNING]${NC} $1"
 }
 
-function fail {
+fail() {
     echo -e "${RED}[ERROR]${NC} $1"
     exit 1
 }
@@ -36,7 +36,7 @@ if [[ ! -f $grub_config ]]; then
     fail "The main grub_config: $grub_config was not found."
 fi
 
-function display_menu {
+display_menu() {
     echo "[1] Headless Mode"
     echo "[2] VGA Mode"
     echo "[3] Exit"
@@ -44,26 +44,26 @@ function display_menu {
     read -p "Your choices are (1 to 3): " choice
 }
 
-function update_grub {
+update_grub() {
     log "Updating grub..."
     if ! sudo update-grub; then
         fail "The update-grub command failed."
     fi
 }
 
-function enable_headless_mode {
+enable_headless_mode() {
     log "Enabling headless mode..."
     sudo sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="3"/g' $grub_config
     update_grub
 }
 
-function enable_vga_mode {
+enable_vga_mode() {
     log "Enabling VGA mode..."
     sudo sed -i 's/GRUB_CMDLINE_LINUX="3"/GRUB_CMDLINE_LINUX=""/g' $grub_config
     update_grub
 }
 
-function prompt_reboot {
+prompt_reboot() {
     echo
     read -p "Do you want to reboot the PC now? [y/N]: " reboot_choice
     case $reboot_choice in
@@ -77,7 +77,7 @@ function prompt_reboot {
     esac
 }
 
-function main {
+main() {
     while true; do
         display_menu
         case $choice in
