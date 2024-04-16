fdbe4c0e05727c2c706c29787f92ac94d7b5de02
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-04-15 21:56:15 -0400

    Update copy-windows-path-as-linux.bat

--- Batch/copy-windows-path-as-linux.bat
+++ Batch/copy-windows-path-as-linux.bat
@@ -5,8 +5,8 @@ COLOR 0A
 CLS
 
 ECHO Select an option: & ECHO=
-ECHO [1] Add "Copy Linux Path" to context menu
-ECHO [2] Remove "Copy Linux Path" from context menu & ECHO=
+ECHO [1] Add "Copy Linux Path" to the context menu
+ECHO [2] Remove "Copy Linux Path" from the context menu & ECHO=
 
 CHOICE /C 12 /N /M "Your choices are (1 or 2): " & CLS
 
@@ -18,8 +18,10 @@ PAUSE
 GOTO :EOF
 
 :addReg
+REG ADD "HKCR\Directory\shell\CopyLinuxPath" /ve /d "Copy Linux Path" /f
+REG ADD "HKCR\Directory\shell\CopyLinuxPath" /v "Extended" /d "" /f
 REG ADD "HKCR\Directory\shell\CopyLinuxPath" /v "Icon" /d "C:\Program Files\WSL\wsl.exe" /f
-REG ADD "HKCR\Directory\shell\CopyLinuxPath\command" /d "%windir%\System32\cmd.exe /d /c \"wsl.exe wslpath -u '%%V' ^| %windir%\System32\clip.exe\"" /f
+REG ADD "HKCR\Directory\shell\CopyLinuxPath\command" /d "%windir%\System32\cmd.exe /d /c \"wsl.exe wslpath -u '%%1' ^| %windir%\System32\clip.exe\"" /f
 REG ADD "HKCR\*\shell\CopyLinuxPath" /ve /d "Copy Linux Path" /f
 REG ADD "HKCR\*\shell\CopyLinuxPath" /v "Extended" /d "" /f
 REG ADD "HKCR\*\shell\CopyLinuxPath" /v "Icon" /d "C:\Program Files\WSL\wsl.exe" /f
