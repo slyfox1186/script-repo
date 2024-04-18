9f88a95a0fa184f5a7e06ac24d62ba7c405155bc
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-04-18 12:43:06 -0400

    Update add-colors-to-nano.sh

--- Bash/Misc/System/add-colors-to-nano.sh
+++ Bash/Misc/System/add-colors-to-nano.sh
@@ -2,7 +2,15 @@
 
 # Execute like this: ./script.sh
 
-configs=(asm.nanorc autoconf.nanorc awk.nanorc c.nanorc changelog.nanorc cmake.nanorc css.nanorc default.nanorc elisp.nanorc email.nanorc go.nanorc groff.nanorc guile.nanorc html.nanorc java.nanorc javascript.nanorc json.nanorc lua.nanorc makefile.nanorc man.nanorc markdown.nanorc nanohelp.nanorc nanorc.nanorc nftables.nanorc objc.nanorc ocaml.nanorc patch.nanorc perl.nanorc php.nanorc po.nanorc python.nanorc ruby.nanorc rust.nanorc sh.nanorc sql.nanorc tcl.nanorc tex.nanorc texinfo.nanorc xml.nanorc yaml.nanorc)
+configs=(
+    asm.nanorc autoconf.nanorc awk.nanorc c.nanorc changelog.nanorc cmake.nanorc
+    css.nanorc default.nanorc elisp.nanorc email.nanorc go.nanorc groff.nanorc
+    guile.nanorc html.nanorc java.nanorc javascript.nanorc json.nanorc lua.nanorc
+    makefile.nanorc man.nanorc markdown.nanorc nanohelp.nanorc nanorc.nanorc
+    nftables.nanorc objc.nanorc ocaml.nanorc patch.nanorc perl.nanorc php.nanorc
+    po.nanorc python.nanorc ruby.nanorc rust.nanorc sh.nanorc sql.nanorc tcl.nanorc
+    tex.nanorc texinfo.nanorc xml.nanorc yaml.nanorc
+)
 
 [[ ! -f "$HOME/.nanorc" ]] && touch "$HOME/.nanorc"
 for config in ${configs[@]}; do
