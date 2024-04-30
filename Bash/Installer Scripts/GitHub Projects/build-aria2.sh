814e286d740fd61948c9185fa1054896e6f92d47
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-04-29 20:02:10 -0400

    Update build-aria2.sh

--- Bash/Installer Scripts/GitHub Projects/build-aria2.sh
+++ Bash/Installer Scripts/GitHub Projects/build-aria2.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 
 ##  Github Script: https://github.com/slyfox1186/script-repo/blob/main/Bash/Installer%20Scripts/GitHub%20Projects/build-aria2.sh
-##  Purpose: Build aria2 from source code
-##  Updated: 04.17.24
-##  Script version: 2.2
+##  Purpose: Build aria2 from source code with hardening options
+##  Updated: 04.29.24
+##  Script version: 2.3
 
-script_ver="2.2"
-echo "GitHub Script for building aria2 from source. Version: $script_ver"
+script_ver="2.3"
+echo "GitHub Script for building aria2 from source with hardening options. Version: $script_ver"
 
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
 log() {
-    echo -e "${GREEN}[LOG]${NC} $(date +'%Y-%m-%d %H:%M:%S') - $*"
+    echo -e "${GREEN}[LOG]${NC} $*"
 }
 
-handle_error() {
-    echo -e "${RED}[ERROR]${NC} $(date +'%Y-%m-%d %H:%M:%S') - $*"
+fail() {
+    echo -e "${RED}[ERROR]${NC} $*"
     exit 1
 }
 
 display_help() {
     echo "Usage: $0 [OPTION]..."
-    echo "Build aria2 from source code."
+    echo "Build aria2 from source code with hardening options."
     echo
     echo "Options:"
     echo "  -s, --service     Create aria2 service"
@@ -32,8 +32,8 @@ display_help() {
     echo "  -h, --help        Display this help message and exit"
     echo
     echo "Examples:"
-    echo "  $0                  # Build aria2 from source"
-    echo "  $0 -s               # Build aria2 and create a systemd service"
+    echo "  $0                  # Build aria2 from source with hardening options"
+    echo "  $0 -s               # Build aria2 with hardening options and create a systemd service"
     echo "  $0 -d -c            # Build aria2 with debug mode and cleanup enabled"
     echo
     exit 0
@@ -42,9 +42,10 @@ display_help() {
 set_compiler_options() {
     CC="ccache gcc"
     CXX="ccache g++"
-    CFLAGS="-O3 -pipe -march=native"
+    CFLAGS="-O3 -march=native -mtune=native -pipe -fstack-protector-strong -D_FORTIFY_SOURCE=2"
     CXXFLAGS="$CFLAGS"
-    export CC CFLAGS CXX CXXFLAGS
+    LDFLAGS="-Wl,-z,relro,-z,now -pie"
+    export CC CXX CFLAGS CXXFLAGS LDFLAGS
 }
 
 PATH="\
@@ -81,19 +82,19 @@ install_packages() {
     log "Attempting to install required packages..."
     sudo apt install -y "${pkgs[@]}"
     for pkg in "${pkgs[@]}"; do
-        sudo dpkg-query -W -f='${Status}' "$pkg" 2>/dev/null | grep -q "ok installed" || handle_error "Failed to install: $pkg"
+        sudo dpkg-query -W -f='${Status}' "$pkg" 2>/dev/null | grep -q "ok installed" || fail "Failed to install: $pkg"
     done
     echo
     log "Installation of required packages completed."
 }
 
 github_latest_release_version() {
-    curl -LSs "https://gitver.optimizethis.net" | bash -s "$1"
+    curl -sSL "https://gitver.optimizethis.net" | bash -s "$1"
 }
 
 libgpg_latest_release_version() {
     local url="$1"
-    local latest_version=$(curl -fsS "$url" | grep -Eo 'href="libgpg-error-[0-9]+\.[0-9]+\.tar\.bz2"' | head -n1 | grep -Eo '[0-9]+\.[0-9]+')
+    local latest_version=$(curl -fsS "$url" | grep -oP 'href="libgpg-error-[0-9]+\.[0-9]+\.tar\.bz2"' | head -n1 | grep -oP '[0-9]+\.[0-9]+')
     if [[ -z "$latest_version" ]]; then
         echo "Failed to find the latest version of libgpg-error."
         exit 1
@@ -107,82 +108,94 @@ prepare_build_environment() {
     temp_dir="/tmp/aria2-temp-$(date +%s)"
     log "Creating build directory: $build_dir"
     mkdir -p "$build_dir"
-    cd "$build_dir" || handle_error "Failed to create or navigate to build directory."
+    cd "$build_dir" || fail "Failed to create or navigate to build directory."
     log "Creating temporary directory: $temp_dir"
     mkdir -p "$temp_dir"
 }
 
 build_jemalloc() {
+    local jemalloc_url jemalloc_version
+    echo
     log "Compiling jemalloc..."
     echo
-    local jemalloc_version=$(github_latest_release_version "https://github.com/jemalloc/jemalloc.git")
-    local jemalloc_url="https://github.com/jemalloc/jemalloc/releases/download/$jemalloc_version/jemalloc-$jemalloc_version.tar.bz2"
-    curl -Ls "$jemalloc_url" | tar -xj
+    jemalloc_version=$(github_latest_release_version "https://github.com/jemalloc/jemalloc.git")
+    jemalloc_url="https://github.com/jemalloc/jemalloc/releases/download/$jemalloc_version/jemalloc-$jemalloc_version.tar.bz2"
+    curl -sSL "$jemalloc_url" | tar -jx
     cd "jemalloc-$jemalloc_version" || exit 1
-    ./configure --prefix="$temp_dir/jemalloc"
-    make "-j$(nproc)" && \
+    ./configure --prefix="$temp_dir/jemalloc" --enable-static --disable-shared CFLAGS="-fPIE" CXXFLAGS="-fPIE" LDFLAGS="-pie"
+    make "-j$(nproc --all)" && \
     sudo make install
     cd ../
 }
 
 build_libgpg_error() {
+    local libgpg_error_url libgpg_error_version
     echo
     log "Compiling libgpg-error..."
     echo
-    local libgpg_error_version=$(libgpg_latest_release_version "https://gnupg.org/ftp/gcrypt/libgpg-error/")
-    local libgpg_error_url="https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-$libgpg_error_version.tar.bz2"
-    curl -Ls "$libgpg_error_url" | tar -xj
+    libgpg_error_version=$(libgpg_latest_release_version "https://gnupg.org/ftp/gcrypt/libgpg-error/")
+    libgpg_error_url="https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-$libgpg_error_version.tar.bz2"
+    curl -sSL "$libgpg_error_url" | tar -jx
     cd "libgpg-error-$libgpg_error_version" || exit 1
-    ./configure --prefix="$temp_dir/libgpg-error"
-    make "-j$(nproc)" && \
+    ./configure --prefix="$temp_dir/libgpg-error" --enable-static --disable-shared CFLAGS="-fPIE" CXXFLAGS="-fPIE" LDFLAGS="-pie"
+    make "-j$(nproc --all)" && \
     sudo make install
     cd ../
 }
 
 build_c_ares() {
-    echo "Compiling c-ares..."
-    local c_ares_version=$(github_latest_release_version "https://github.com/c-ares/c-ares.git")
-    local c_ares_url="https://github.com/c-ares/c-ares/releases/download/cares-${c_ares_version//./_}/c-ares-$c_ares_version.tar.gz"
-    curl -Ls "$c_ares_url" | tar -xz
-    cd "c-ares-$c_ares_version"
-    ./configure --prefix="$temp_dir/c-ares"
-    make "-j$(nproc)" && make install
+    local c_ares_url c_ares_version
+    echo
+    log "Compiling c-ares..."
+    echo
+    c_ares_version=$(github_latest_release_version "https://github.com/c-ares/c-ares.git")
+    c_ares_url="https://github.com/c-ares/c-ares/archive/refs/tags/cares-${c_ares_version//./_}.tar.gz"
+    curl -sSL "$c_ares_url" | tar -zx
+    cd "c-ares-cares-$c_ares_version" || exit 1
+    autoreconf -fi
+    ./configure --prefix="$temp_dir/c-ares" --enable-static --disable-shared CFLAGS="-fPIE" CXXFLAGS="-fPIE" LDFLAGS="-pie"
+    make "-j$(nproc --all)" && \
+    sudo make install
     cd ../
 }
 
 build_sqlite3() {
-    echo "Compiling sqlite3..."
-    local sqlite_version=$(github_latest_release_version "https://github.com/sqlite/sqlite.git" "version-")
-    local sqlite_url="https://www.sqlite.org/2023/sqlite-autoconf-${sqlite_version//./0}.tar.gz"
-    curl -Ls "$sqlite_url" | tar -xz
-    cd "sqlite-autoconf-${sqlite_version//./0}"
-    ./configure --prefix="$temp_dir/sqlite3"
-    make "-j$(nproc)" && make install
+    local sqlite_url sqlite_version
+    echo
+    log "Compiling sqlite3..."
+    echo
+    sqlite_version=$(github_latest_release_version "https://github.com/sqlite/sqlite.git")
+    sqlite_url="https://github.com/sqlite/sqlite/archive/refs/tags/version-$sqlite_version.tar.gz"
+    curl -sSL "https://github.com/sqlite/sqlite/archive/refs/tags/version-3.45.3.tar.gz" | tar -zx
+    cd "sqlite-version-$sqlite_version" || exit 1
+    ./configure --prefix="$temp_dir/sqlite3" --enable-static --disable-shared CFLAGS="-fPIE" CXXFLAGS="-fPIE" LDFLAGS="-pie"
+    make "-j$(nproc --all)" && \
+    sudo make install
     cd ../
 }
 
-compile_aria2() {
+build_aria2() {
+    local aria2_url aria2_version
     echo
     log "Compiling Aria2..."
     echo
-    local aria2_version=$(github_latest_release_version "https://github.com/aria2/aria2.git")
-    local aria2_url="https://github.com/aria2/aria2/releases/download/release-$aria2_version/aria2-$aria2_version.tar.xz"
-    curl -Ls "$aria2_url" | tar -xJ
+    aria2_version=$(github_latest_release_version "https://github.com/aria2/aria2.git")
+    aria2_url="https://github.com/aria2/aria2/releases/download/release-$aria2_version/aria2-$aria2_version.tar.xz"
+    curl -sSL "$aria2_url" | tar -Jx
     cd "aria2-$aria2_version" || exit 1
     sed -i "s/1, 16/1, 128/g" "src/OptionHandlerFactory.cc"
     PATH="$temp_dir/jemalloc/bin:$temp_dir/libgpg-error/bin:$PATH" \
-    ./configure --prefix=/usr/local \
-                --enable-static \
-                --disable-shared \
-                --without-gnutls \
-                --with-openssl \
-                --with-ca-bundle=$(find /etc/ -type f -name ca-certificates.crt) \
-                --with-jemalloc="$temp_dir/jemalloc" \
-                --with-libcares="$temp_dir/c-ares" \
-                --with-sqlite3="$temp_dir/sqlite3" \
-                LDFLAGS="-L$temp_dir/jemalloc/lib -L$temp_dir/libgpg-error/lib -L$temp_dir/c-ares/lib -L$temp_dir/sqlite3/lib" \
+    ./configure --prefix=/usr/local --enable-static --disable-shared --without-gnutls --with-openssl \
+                --with-ca-bundle=$(sudo find /etc/ -type f -name ca-certificates.crt) \
+                --with-jemalloc="$temp_dir/jemalloc" --with-libcares="$temp_dir/c-ares" \
+                --with-sqlite3="$temp_dir/sqlite3" --enable-lto --enable-profile-guided-optimization \
+                LDFLAGS="-L$temp_dir/jemalloc/lib -L$temp_dir/libgpg-error/lib -L$temp_dir/c-ares/lib -L$temp_dir/sqlite3/lib $LDFLAGS" \
                 CPPFLAGS="-I$temp_dir/jemalloc/include -I$temp_dir/libgpg-error/include -I$temp_dir/c-ares/include -I$temp_dir/sqlite3/include"
-    make "-j$(nproc)" && \
+    make "-j$(nproc --all)"
+    sudo make install
+    sudo /usr/local/bin/aria2c --optimize-profile
+    make clean
+    make "-j$(nproc --all)" && \
     sudo make install
     cd ../
 }
@@ -191,45 +204,54 @@ create_aria2_service() {
     echo
     log "Creating aria2 service..."
     echo
-    sudo tee /etc/systemd/system/aria2.service >/dev/null <<EOT
+    sudo tee "/etc/systemd/system/aria2.service" >/dev/null <<EOT
 [Unit]
 Description=Aria2 Service
 After=network.target
 
 [Service]
 Type=simple
-ExecStart=/usr/local/bin/aria2c --enable-rpc --rpc-listen-all
+ExecStart=/usr/local/bin/aria2c --enable-rpc --rpc-listen-all --rpc-secure --rpc-certificate=/path/to/cert.pem --rpc-private-key=/path/to/key.pem --rpc-secure-policy-file=/etc/aria2/aria2.seccomp
 Restart=on-abort
 
 [Install]
 WantedBy=multi-user.target
 EOT
 
+    sudo mkdir -p "/etc/aria2"
+    sudo tee "/etc/aria2/aria2.seccomp" >/dev/null <<EOT
+POLICY aria2
+ALLOW {BASIC_SYSCALLS}
+ALLOW {FILESYSTEM_SYSCALLS}
+ALLOW {PROCESS_CONTROL_SYSCALLS}
+ALLOW {MEMORY_SYSCALLS}
+ALLOW {SELECT_SYSCALLS}
+ALLOW {TIME_SYSCALLS}
+ALLOW {SOCKET_SYSCALLS}
+ALLOW {SIGNAL_SYSCALLS}
+EOT
+
     sudo systemctl daemon-reload
     sudo systemctl enable aria2
     sudo systemctl start aria2
+    echo
     log "Aria2 service created and started."
     echo
 }
 
 cleanup() {
-    echo "Cleaning up..."
+    echo
+    log "Cleaning up..."
+    echo
     sudo rm -fr "$build_dir" "$temp_dir"
-    
-    if [[ "$create_service" = true && "$cleanup" = true ]]; then
-        echo "Removing aria2 service..."
-        sudo systemctl stop aria2
-        sudo systemctl disable aria2
-        sudo rm /etc/systemd/system/aria2.service
-        sudo systemctl daemon-reload
-    fi
-    
-    echo "Cleanup completed."
+    echo
+    log "Cleanup completed."
+    echo
 }
 
 main() {
     if [[ "$EUID" -eq 0 ]]; then
-        echo "This script must be run without root or with sudo."
+        echo "This script must be run without root or sudo."
         exit 1
     fi
     
@@ -259,28 +281,26 @@ main() {
         shift
     done
 
+    echo
     log "Starting aria2 build process..."
     echo
 
     set_compiler_options
     install_packages
     prepare_build_environment
+    cd "$temp_dir" || exit 1
     build_jemalloc
     build_libgpg_error
     build_c_ares
     build_sqlite3
-    compile_aria2
+    build_aria2
     
-    if [[ "$create_service" = true ]]; then
-        create_aria2_service
-    fi
-    
-    if [[ "$cleanup" = true ]]; then
-        cleanup
-    fi
+    [[ "$create_service" = true ]] && create_aria2_service    
+    [[ "$cleanup" = true ]] && cleanup
     
     echo
     log "Aria2 build process completed successfully."
+    echo
 }
 
 main "$@"
