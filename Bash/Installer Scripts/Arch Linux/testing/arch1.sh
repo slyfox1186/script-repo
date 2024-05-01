ba693edbe36495681d1921cf14439402060fc6d5
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-05-01 17:06:38 -0400

    Update arch1.sh

--- Bash/Installer Scripts/Arch Linux/testing/arch1.sh
+++ Bash/Installer Scripts/Arch Linux/testing/arch1.sh
@@ -3,23 +3,54 @@
 GREEN='\033[0;32m'
 NC='\033[0m'
 
+# Verbose logging function
 log() {
     echo -e "${GREEN}[LOG]${NC} $1"
 }
 
+# Variables with placeholders for command line arguments
 USERNAME=""
 USER_PASSWORD=""
 ROOT_PASSWORD=""
 COMPUTER_NAME=""
-TIMEZONE="US/Eastern"
+TIMEZONE="US/Eastern"  # Default value for TIMEZONE
 DISK=""
 
+# Helper function to prompt for missing variables
+prompt_variable() {
+    local var_name="$1"
+    local prompt_msg="$2"
+    local var_value
+    eval var_value=\$$var_name
+
+    while [[ -z "$var_value" ]]; do
+        read -p "$prompt_msg: " var_value
+        if [[ -z "$var_value" ]]; then
+            echo "This is a required field. Please enter a value."
+        else
+            eval $var_name='$var_value'
+        fi
+    done
+}
+
+# Check and prompt for each required variable
+[[ -z "$USERNAME" ]] && prompt_variable USERNAME "Enter the non-root username"
+[[ -z "$USER_PASSWORD" ]] && prompt_variable USER_PASSWORD "Enter the non-root user password"
+[[ -z "$ROOT_PASSWORD" ]] && prompt_variable ROOT_PASSWORD "Enter the root password"
+[[ -z "$COMPUTER_NAME" ]] && prompt_variable COMPUTER_NAME "Enter the computer name"
+[[ -z "$DISK" ]] && prompt_variable DISK "Enter the target disk (e.g., sdX or nvmeXn1)"
+
+# Append '/dev/' to DISK for internal use
+FULL_DISK_PATH="/dev/$DISK"
+
+# Partition variables
 PARTITION_COUNT=3
 PARTITION1_SIZE="500M"
 PARTITION2_SIZE="2G"
 PARTITION_SIZES=()
 PARTITION_TYPES=()
 
+# Help function
 help() {
     echo "Arch Linux Installation Script"
     echo "This script automates the installation of Arch Linux."
@@ -32,7 +63,7 @@ help() {
     echo "  -r ROOT_PASSWORD  Set the root password"
     echo "  -c COMPUTER_NAME  Set the computer name"
     echo "  -t TIMEZONE       Set the timezone (default: US/Eastern)"
-    echo "  -d DISK           Set the target disk (e.g., /dev/sdX or /dev/nvmeXn1)"
+    echo "  -d DISK           Set the target disk (e.g., sdX or nvmeXn1)"
     echo "  -h                Display this help message"
     echo
     echo "Examples:"
@@ -41,6 +72,7 @@ help() {
     exit 0
 }
 
+# Parse command line arguments
 while getopts ":u:p:r:c:t:d:h" opt; do
     case "$opt" in
         u) USERNAME="$OPTARG" ;;
@@ -55,6 +87,7 @@ while getopts ":u:p:r:c:t:d:h" opt; do
     esac
 done
 
+# Prompt for missing variables
 prompt_variable() {
     local prompt var_name var_value
     var_name="$1"
@@ -66,22 +99,25 @@ prompt_variable() {
     done
 }
 
+# Set values for the prompt_variable function
 prompt_variable USERNAME "Enter the non-root username"
 prompt_variable USER_PASSWORD "Enter the non-root user password"
 prompt_variable ROOT_PASSWORD "Enter the root password"
 prompt_variable COMPUTER_NAME "Enter the computer name"
 prompt_variable DISK "Enter the target disk (e.g., /dev/sda or /dev/nvme0n1)"
 
-if [[ "$DISK" == *"nvme"* ]]; then
-    DISK1="${DISK}p1"
-    DISK2="${DISK}p2"
-    DISK3="${DISK}p3"
+# Determine disk partition naming convention
+if [[ "$FULL_DISK_PATH" == *"nvme"* ]]; then
+    DISK1="${FULL_DISK_PATH}p1"
+    DISK2="${FULL_DISK_PATH}p2"
+    DISK3="${FULL_DISK_PATH}p3"
 else
-    DISK1="${DISK}1"
-    DISK2="${DISK}2"
-    DISK3="${DISK}3"
+    DISK1="${FULL_DISK_PATH}1"
+    DISK2="${FULL_DISK_PATH}2"
+    DISK3="${FULL_DISK_PATH}3"
 fi
 
+# Partition the disk
 setup_disk() {
     local disk_parts end SIZE start type
     disk_parts=("$DISK1" "$DISK2" "$DISK3")
@@ -92,11 +128,21 @@ setup_disk() {
         read -p "Enter the number of partitions (minimum 3): " PARTITION_COUNT
     done
 
+    # Store the default partition size set at the top of the script in another
+    # variable in case the user wants to just hit enter and use the default value 
+    DEFAULT_PARTITION1_SIZE="$PARTITION1_SIZE"
     echo "Partition 1 will be set as GPT and EFI."
-    read -p "Enter partition 1 SIZE (e.g., 550M): " PARTITION1_SIZE
+    read -p "Enter partition 1 size or hit enter to use the default value (default: 500M): " PARTITION1_SIZE
 
+    [[ -z "$PARTITION1_SIZE" ]] && PARTITION1_SIZE="$DEFAULT_PARTITION1_SIZE"
+
+    # Store the default partition size set at the top of the script in another
+    # variable in case the user wants to just hit enter and use the default value
+    DEFAULT_PARTITION2_SIZE="$PARTITION2_SIZE"
     echo "Partition 2 will be set as swap."
-    read -p "Enter partition 2 SIZE (e.g., 2G): " PARTITION2_SIZE
+    read -p "Enter partition 2 size or hit enter to use the default value (default: 2G): " PARTITION2_SIZE
+
+    [[ -z "$PARTITION2_SIZE" ]] && PARTITION2_SIZE="$DEFAULT_PARTITION2_SIZE"
 
     for ((i=3; i<PARTITION_COUNT; i++)); do
         read -p "Enter SIZE for partition $i: " SIZE
@@ -153,30 +199,30 @@ setup_disk() {
     echo "The last partition will be set as Linux x86-64 root and use the remaining disk space."
 
     echo
-    log "Partitioning disk $DISK..."
-    parted -s "$DISK" mklabel gpt
+    log "Partitioning disk $FULL_DISK_PATH..."
+    parted -s "$FULL_DISK_PATH" mklabel gpt
 
-    parted -s "$DISK" mkpart primary fat32 1 $(echo "$PARTITION1_SIZE" | sed 's/[^0-9]*//g')
-    parted -s "$DISK" set 1 esp on
-    parted -s "$DISK" mkpart primary linux-swap $(echo "$PARTITION1_SIZE" | sed 's/[^0-9]*//g') $(echo "$(echo "$PARTITION2_SIZE" | sed 's/[^0-9]*//g') * 1024" | bc)
+    parted -s "$FULL_DISK_PATH" mkpart primary fat32 1 $(echo "$PARTITION1_SIZE" | sed 's/[^0-9]*//g')
+    parted -s "$FULL_DISK_PATH" set 1 esp on
+    parted -s "$FULL_DISK_PATH" mkpart primary linux-swap $(echo "$PARTITION1_SIZE" | sed 's/[^0-9]*//g') $(echo "$(echo "$PARTITION2_SIZE" | sed 's/[^0-9]*//g') * 1024" | bc)
 
     start=$(echo "$(echo "$PARTITION2_SIZE" | sed 's/[^0-9]*//g') * 1024" | bc)
     for ((i=0; i<${#PARTITION_SIZES[@]}; i++)); do
         SIZE=$(echo "$(echo "${PARTITION_SIZES[i]}" | sed 's/[^0-9]*//g') * 1024" | bc)
         end=$((start + SIZE))
-        parted -s "$DISK" mkpart primary $start $end
+        parted -s "$FULL_DISK_PATH" mkpart primary $start $end
         type=${PARTITION_TYPES[i]}
         case $type in
-            1) parted -s "$DISK" set $((i+3)) esp on ;;
-            2) parted -s "$DISK" set $((i+3)) bios_grub on ;;
-            4) parted -s "$DISK" set $((i+3)) boot on ;;
-            19) parted -s "$DISK" set $((i+3)) swap on ;;
+            1) parted -s "$FULL_DISK_PATH" set $((i+3)) esp on ;;
+            2) parted -s "$FULL_DISK_PATH" set $((i+3)) bios_grub on ;;
+            4) parted -s "$FULL_DISK_PATH" set $((i+3)) boot on ;;
+            19) parted -s "$FULL_DISK_PATH" set $((i+3)) swap on ;;
         esac
         start=$end
     done
 
-    parted -s "$DISK" mkpart primary $start 100%
-    parted -s "$DISK" set $PARTITION_COUNT 23
+    parted -s "$FULL_DISK_PATH" mkpart primary $start 100%
+    parted -s "$FULL_DISK_PATH" set $PARTITION_COUNT 23
 
     echo
     log "Creating filesystems..."
@@ -186,6 +232,7 @@ setup_disk() {
     mkfs.ext4 "${DISK}${PARTITION_COUNT}"
 }
 
+# Mount the partitions
 mount_partitions() {
     log "Enabling swap and mounting partitions..."
     swapon "$DISK2"
@@ -194,6 +241,7 @@ mount_partitions() {
     mount --mkdir "$DISK1" /mnt/boot/efi
 }
 
+# Prompt for loadkeys
 prompt_loadkeys() {
     local loadkeys_value
     while [[ -z "$loadkeys_value" ]]; do
@@ -221,6 +269,7 @@ prompt_loadkeys() {
     done
 }
 
+# Package installation
 install_packages() {
     local PACKAGES
     PACKAGES="base efibootmgr linux linux-firmware linux-headers nano networkmanager os-prober reflector sudo"
@@ -239,42 +288,66 @@ install_packages() {
     pacstrap -K /mnt $PACKAGES
 }
 
-configure_chroot() {
-    log "Entering chroot to configure system..."
-    arch-chroot /mnt /bin/bash -c "
-ln -sf '/usr/share/zoneinfo/$TIMEZONE' /etc/localtime;
-hwclock --systohc;
-echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen;
-locale-gen;
-echo 'LANG=en_US.UTF-8' > /etc/locale.conf;
-echo '$COMPUTER_NAME' > /etc/hostname;
-mkinitcpio -P;
-echo '127.0.1.1 myarch.localdomain $COMPUTER_NAME' >> /etc/hosts;
-echo root:'$ROOT_PASSWORD' | chpasswd;
-useradd -m -G wheel -s /bin/bash $USERNAME;
-echo '$USERNAME:$USER_PASSWORD' | chpasswd;
-echo '' >> /etc/sudoers;
-echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers;
-bootctl --path=/boot/efi install;
-echo 'default arch.conf' > /boot/efi/loader/loader.conf;
-echo 'timeout 4' >> /boot/efi/loader/loader.conf;
-echo 'console-mode max' >> /boot/efi/loader/loader.conf;
-echo 'editor no' >> /boot/efi/loader/loader.conf;
-echo 'title Arch Linux' > /boot/efi/loader/entries/arch.conf;
-echo 'linux /vmlinuz-linux' >> /boot/efi/loader/entries/arch.conf;
-echo 'initrd /initramfs-linux.img' >> /boot/efi/loader/entries/arch.conf;
-systemctl enable NetworkManager.service;
-systemctl start NetworkManager.service;
-"
+# Create the fstab file in Arch Linux /etc
+generate_fstab() {
+    echo
+    log "Generating fstab..."
+    genfstab -U /mnt >> /mnt/etc/fstab
 }
 
-# After disk and partitions are configured, fetch the PARTUUID
-PARTUUID=$(blkid -s PARTUUID -o value "${DISK3}") # Adjust as needed to target the correct root partition
-export PARTUUID
+# Use a heredoc to execute commands using the arch-chroot command
+configure_chroot() {
+    log "Configuring installed system..."
 
+    arch-chroot /mnt /bin/bash -c "
+        ln -sf '/usr/share/zoneinfo/$TIMEZONE' /etc/localtime
+        hwclock --systohc
+
+        echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
+        locale-gen
+        echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+
+        echo '$COMPUTER_NAME' > /etc/hostname
+        echo '127.0.1.1 myarch.localdomain $COMPUTER_NAME' >> /etc/hosts
+
+        echo 'root:$ROOT_PASSWORD' | chpasswd
+
+        useradd -m -G wheel -s /bin/bash '$USERNAME'
+        echo '$USERNAME:$USER_PASSWORD' | chpasswd
+
+        echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
+
+        # Copy kernel and initramfs to the ESP
+        cp /boot/vmlinuz-linux /boot/efi/
+        cp /boot/initramfs-linux.img /boot/efi/
+
+        # Install and configure bootloader
+        bootctl --path=/boot/efi install
+
+        echo 'default arch' > /boot/efi/loader/loader.conf
+        echo 'timeout 4' >> /boot/efi/loader/loader.conf
+        echo 'editor 0' >> /boot/efi/loader/loader.conf
+
+        echo 'title Arch Linux' > /boot/efi/loader/entries/arch.conf
+        echo 'linux /vmlinuz-linux' >> /boot/efi/loader/entries/arch.conf
+        echo 'initrd /initramfs-linux.img' >> /boot/efi/loader/entries/arch.conf
+
+        # Enable NetworkManager so you have access to the internet after rebooting
+        systemctl enable NetworkManager
+        systemctl start NetworkManager.service
+    "
+}
+
+# Fetch PARTUUID after exiting arch-chroot and export it to the arch.conf file
+generate_and_set_partuuid() {
+    PARTUUID=$(blkid -s PARTUUID -o value "${DISK}${PARTITION_COUNT}")
+    echo "options root=PARTUUID=$PARTUUID rw" >> /mnt/boot/efi/loader/entries/arch.conf
+}
+
+# Prompt to unmount all of the partitions
 prompt_umount() {
     local choice
-    clear
+    echo
     read -p "Installation complete. Do you want to unmount all partitions? (y/n): " choice
     case "$choice" in
         [yY]*|[yY][eE][sS]*)
@@ -290,9 +363,10 @@ prompt_umount() {
     esac
 }
 
+# Prompt to reboot the pc
 prompt_reboot() {
     local choice
-    clear
+    echo
     read -p "Do you want to reboot now? (y/n): " choice
     case "$choice" in
         [yY]*|[yY][eE][sS]*)
@@ -306,25 +380,16 @@ prompt_reboot() {
     esac
 }
 
+# Start the installation
 log "Starting installation..."
 prompt_loadkeys
 timedatectl set-ntp true
 log "System clock synchronized."
-
 setup_disk
 mount_partitions
 install_packages
-
-echo
-log "Generating fstab..."
-genfstab -U /mnt >> /mnt/etc/fstab
-
+generate_fstab
 configure_chroot
-
-# Fetch PARTUUID after exiting arch-chroot
-PARTUUID=$(blkid -s PARTUUID -o value "${DISK}${PARTITION_COUNT}")
-export PARTUUID
-echo "options root=PARTUUID=$PARTUUID rw" >> /mnt/boot/efi/loader/entries/arch.conf
-
+generate_and_set_partuuid
 prompt_umount
 prompt_reboot
