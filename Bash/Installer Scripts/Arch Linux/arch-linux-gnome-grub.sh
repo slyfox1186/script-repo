59d7741cfc09c7678425ad0c6f6bccbf962fb6ba
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-05-01 23:20:34 -0400

    Update arch-linux-gnome-grub.sh

--- Bash/Installer Scripts/Arch Linux/arch-linux-gnome-grub.sh
+++ Bash/Installer Scripts/Arch Linux/arch-linux-gnome-grub.sh
@@ -16,6 +16,23 @@ COMPUTER_NAME=""
 TIMEZONE="US/Eastern"
 DISK=""
 
+# Helper function to prompt for missing variables
+prompt_variable() {
+    local prompt_msg var_name var_value
+    var_name="$1"
+    prompt_msg="$2"
+    eval var_value=\$$var_name
+
+    while [[ -z "$var_value" ]]; do
+        read -p "$prompt_msg: " var_value
+        if [[ -z "$var_value" ]]; then
+            printf "\n%s\n\n" "This is a required field. Please enter a value."
+        else
+            eval $var_name='$var_value'
+        fi
+    done
+}
+
 # Partition variables
 PARTITION_COUNT=3
 PARTITION1_SIZE="500M"
@@ -36,12 +53,12 @@ help() {
     echo "  -r ROOT_PASSWORD  Set the root password"
     echo "  -c COMPUTER_NAME  Set the computer name"
     echo "  -t TIMEZONE       Set the timezone (default: US/Eastern)"
-    echo "  -d DISK           Set the target disk (e.g., /dev/sdX or /dev/nvmeXn1)"
+    echo "  -d DISK           Set the target disk (e.g., sdX or nvmeXn1)"
     echo "  -h                Display this help message"
     echo
     echo "Examples:"
-    echo "  $0 -u john -p password123 -r rootpass -c myarch -t Europe/London -d /dev/sda"
-    echo "  $0 -u jane -p pass456 -r rootpass789 -c janepc -d /dev/nvme0n1"
+    echo "  $0 -u john -p password123 -r rootpass -c myarch -t Europe/London -d sda"
+    echo "  $0 -u jane -p pass456 -r rootpass789 -c janepc -d nvme0n1"
     exit 0
 }
 
@@ -60,61 +77,46 @@ while getopts ":u:p:r:c:t:d:h" opt; do
     esac
 done
 
-# Prompt for missing variables
-prompt_variable() {
-    local var_name="$1"
-    local prompt="$2"
-    local var_value=$(eval echo \$$var_name)
-    while [[ -z "$var_value" ]]; do
-        read -p "$prompt: " var_value
-        eval $var_name="$var_value"
-    done
-}
+# Append '/dev/' to DISK for internal use
+FULL_DISK_PATH="/dev/$DISK"
 
-# Set values for the prompt_variable function
-prompt_variable USERNAME "Enter the non-root username"
-prompt_variable USER_PASSWORD "Enter the non-root user password"
-prompt_variable ROOT_PASSWORD "Enter the root password"
-prompt_variable COMPUTER_NAME "Enter the computer name"
-prompt_variable DISK "Enter the target disk (e.g., /dev/sda or /dev/nvme0n1)"
+# Check and prompt for each required variable
+[[ -z "$USERNAME" ]] && clear; prompt_variable USERNAME "Enter the non-root username"
+[[ -z "$USER_PASSWORD" ]] && clear; prompt_variable USER_PASSWORD "Enter the non-root user password"
+[[ -z "$ROOT_PASSWORD" ]] && clear; prompt_variable ROOT_PASSWORD "Enter the root password"
+[[ -z "$COMPUTER_NAME" ]] && clear; prompt_variable COMPUTER_NAME "Enter the computer name"
+[[ -z "$DISK" ]] && clear; prompt_variable DISK "Enter the target disk (e.g., sdX or nvmeXn1)"
 
 # Determine disk partition naming convention
-if [[ "$DISK" == *"nvme"* ]]; then
-    DISK1="${DISK}p1"
-    DISK2="${DISK}p2"
-    DISK3="${DISK}p3"
+if [[ "$FULL_DISK_PATH" == *"nvme"* ]]; then
+    DISK1="${FULL_DISK_PATH}p1"
+    DISK2="${FULL_DISK_PATH}p2"
+    DISK3="${FULL_DISK_PATH}p3"
 else
-    DISK1="${DISK}1"
-    DISK2="${DISK}2"
-    DISK3="${DISK}3"
+    DISK1="${FULL_DISK_PATH}1"
+    DISK2="${FULL_DISK_PATH}2"
+    DISK3="${FULL_DISK_PATH}3"
 fi
 
 # Partition the disk
 setup_disk() {
-    local disk_parts end SIZE start type
-    disk_parts=("$DISK1" "$DISK2" "$DISK3")
+    local part1_size part2_size
+    echo "Partition 1 will be set as GPT and EFI."
+    read -p "Enter partition 1 size or hit enter to use the default value (default: 500M): " part1_size
+    PARTITION1_SIZE=${part1_size:-$PARTITION1_SIZE}
 
-    read -p "Enter the number of partitions (minimum 3): " PARTITION_COUNT
+    echo "Partition 2 will be set as swap."
+    read -p "Enter partition 2 size or hit enter to use the default value (default: 2G): " part2_size
+    PARTITION2_SIZE=${part2_size:-$PARTITION2_SIZE}
+
+    echo "Enter the number of partitions (minimum 3, default 3):"
+    read -p "Number of partitions: " input_partition_count
+    PARTITION_COUNT=${input_partition_count:-$PARTITION_COUNT}
+
     while [[ "$PARTITION_COUNT" -lt 3 ]]; do
         echo "The minimum number of partitions is 3."
         read -p "Enter the number of partitions (minimum 3): " PARTITION_COUNT
     done
-
-    # Store the default partition size set at the top of the script in another
-    # variable in case the user wants to just hit enter and use the default value 
-    DEFAULT_PARTITION1_SIZE="$PARTITION1_SIZE"
-    echo "Partition 1 will be set as GPT and EFI."
-    read -p "Enter partition 1 size or hit enter to use the default value (default: 500M): " PARTITION1_SIZE
-
-    [[ -z "$PARTITION1_SIZE" ]] && PARTITION1_SIZE="$DEFAULT_PARTITION1_SIZE"
-
-    # Store the default partition size set at the top of the script in another
-    # variable in case the user wants to just hit enter and use the default value
-    DEFAULT_PARTITION2_SIZE="$PARTITION2_SIZE"
-    echo "Partition 2 will be set as swap."
-    read -p "Enter partition 2 size or hit enter to use the default value (default: 2G): " PARTITION2_SIZE
-
-    [[ -z "$PARTITION2_SIZE" ]] && PARTITION2_SIZE="$DEFAULT_PARTITION2_SIZE"
 
     for ((i=3; i<PARTITION_COUNT; i++)); do
         read -p "Enter SIZE for partition $i: " SIZE
@@ -201,7 +203,11 @@ setup_disk() {
 
     mkfs.fat -F32 "$DISK1"
     mkswap "$DISK2"
-    mkfs.ext4 "${DISK}${PARTITION_COUNT}"
+    if [[ "$DISK" == *"nvme"* ]]; then
+        mkfs.ext4 "${DISK}$p{PARTITION_COUNT}"
+    else
+        mkfs.ext4 "${DISK}${PARTITION_COUNT}"
+    fi
 }
 
 # Mount the partitions
@@ -209,7 +215,12 @@ mount_partitions() {
     log "Enabling swap and mounting partitions..."
     swapon "$DISK2"
 
-    mount "${DISK}${PARTITION_COUNT}" /mnt
+    if [[ "$DISK" == *"nvme"* ]]; then
+        mount "${DISK}$p{PARTITION_COUNT}" /mnt
+    else
+        mount "${DISK}${PARTITION_COUNT}" /mnt
+    fi
+    
     mount --mkdir "$DISK1" /mnt/boot/efi
 }
 
@@ -218,7 +229,7 @@ prompt_loadkeys() {
     local loadkeys_value
     while [[ -z "$loadkeys_value" ]]; do
         clear
-        read -p "Enter the value for loadkeys (press 'l' to list available options or Enter to continue): " loadkeys_value
+        read -p "Enter the value for loadkeys (press 'l' to list available options): " loadkeys_value
         if [[ "$loadkeys_value" == "l" ]]; then
             echo "Available keymaps:"
             tempfile=$(mktemp)
@@ -228,6 +239,11 @@ prompt_loadkeys() {
             read -p "Enter the value for loadkeys: " loadkeys_value
             if [[ -n "$loadkeys_value" ]]; then
                 loadkeys "$loadkeys_value"
+            else
+                echo "You must enter a value to continue. Press l to get a list of available options."
+                echo
+                read -p "Press enter to try again."
+                prompt_loadkeys
             fi
         elif [[ -n "$loadkeys_value" ]]; then
             loadkeys "$loadkeys_value"
@@ -239,7 +255,8 @@ prompt_loadkeys() {
 
 # Package installation
 install_packages() {
-    local PACKAGES="base efibootmgr grub linux linux-headers linux-firmware nano networkmanager reflector sudo" # Package list
+    local PACKAGES
+    PACKAGES="base efibootmgr linux linux-firmware linux-headers nano networkmanager os-prober reflector sudo"
     echo
     log "Installing essential packages..."
     echo "Current package list: $PACKAGES"
@@ -253,6 +270,13 @@ install_packages() {
         fi
     done
     pacstrap -K /mnt $PACKAGES
+}
+
+# Set the time and date for the system clock
+set_time_date() {
+    log "Synchronizing system clock..."
+    timedatectl set-ntp true
+    log "System clock synchronized!"
 }
 
 # Create the fstab file in Arch Linux /etc
@@ -271,25 +295,27 @@ ln -sf "/usr/share/zoneinfo/$TIMEZONE" /etc/localtime
 hwclock --systohc
 
 # Localization
-echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 
 # Network configuration
 echo "$COMPUTER_NAME" > /etc/hostname
 mkinitcpio -P
-echo "127.0.1.1 myarch.localdomain $COMPUTER_NAME" >> /etc/hosts
-
-# Set root password
-echo root:"$ROOT_PASSWORD" | chpasswd
+echo "127.0.1.1 localhost.localdomain $COMPUTER_NAME" >> /etc/hosts
 
 # Create a new user with user variables
-useradd -m -G wheel -s /bin/bash $USERNAME
+useradd -mG wheel -s /bin/bash $USERNAME
+
+# Set non-root user password
 echo "$USERNAME:$USER_PASSWORD" | chpasswd
+
+# Set root password
+echo "root:$ROOT_PASSWORD" | chpasswd
 
 # Enable sudo for wheel group
 echo "" >> /etc/sudoers
-echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+echo '%wheel ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 
 # Grub installation and configuration
 grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
@@ -321,7 +347,10 @@ prompt_umount() {
             ;;
         [nN]*|[nN][oO]*)
             ;;
-        *)  unset choice
+        *)  echo "Bad user choice... the script will let you try again..."
+            sleep 4
+            unset choice
+            clear
             prompt_umount
             ;;
     esac
@@ -338,7 +367,10 @@ prompt_reboot() {
             ;;
         [nN]*|[nN][oO]*)
             ;;
-        *)  unset choice
+        *)  echo "Bad user choice... the script will let you try again..."
+            sleep 4
+            unset choice
+            clear
             prompt_reboot
             ;;
     esac
@@ -347,8 +379,7 @@ prompt_reboot() {
 # Start the installation
 log "Starting installation..."
 prompt_loadkeys
-timedatectl set-ntp true
-log "System clock synchronized."
+set_time_date
 setup_disk
 mount_partitions
 install_packages
