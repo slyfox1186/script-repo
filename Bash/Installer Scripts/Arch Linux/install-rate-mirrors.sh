3dee211f28d6f7ec0862816046ece9cc03f97c02
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-05-02 04:21:26 -0400

    Update install-rate-mirrors.sh

--- Bash/Installer Scripts/Arch Linux/install-rate-mirrors.sh
+++ Bash/Installer Scripts/Arch Linux/install-rate-mirrors.sh
@@ -5,9 +5,32 @@ if [[ "$EUID" -eq 0 ]]; then
     exit
 fi
 
+update_now() {
+    echo
+    alias ua-drop-caches='sudo paccache -rk3; yay -Sc --aur --noconfirm'
+    alias ua-update-all='export TMPFILE="$(mktemp)"; sudo true; \
+    rate-mirrors --save=$TMPFILE arch --max-delay=21600 && \
+    sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup && \
+    sudo mv $TMPFILE /etc/pacman.d/mirrorlist && ua-drop-caches && \
+    yay -Syyu --noconfirm'
+    if ua-update-all; then
+        printf "\n%s\n\n" "Update successful!"
+    else
+        printf "\n%s\n\n" "Update failed!"
+    fi
+}
+
 echo
 read -p "To link static choose yes else choose no (y/n): " choice
+echo
 case "$choice" in
     [yY]) yay -S rate-mirrors-bin ;;
     [nN]) yay -S rate-mirrors ;;
+esac
+
+echo
+read -p "Do you want to update the mirror list now? (recommended on first use): " choice_run
+case "$choice_run" in
+    [yY]) update_now ;;
+    [nN]) ;;
 esac
