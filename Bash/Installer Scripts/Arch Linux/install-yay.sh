e9174f9021940b1ee4bb0377b46baaffdebd240d
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-05-02 20:36:33 -0400

    Update install-yay.sh

--- Bash/Installer Scripts/Arch Linux/install-yay.sh
+++ Bash/Installer Scripts/Arch Linux/install-yay.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-if [[ "$EUID" -ne 0 ]]; then
-    echo "You must run this as root or with sudo."
+if [[ "$EUID" -eq 0 ]]; then
+    echo "You must run this without root or sudo."
     exit
 fi
 
 # Update arch
-pacman -Syu
+sudo pacman -Syu
 
 # Install required packages
-pacman -S --needed --noconfirm base-devel git
+sudo pacman -S --needed --noconfirm base-devel git
 
 # Git clone the yay AUR repository
-git clone https://aur.archlinux.org/yay.git
+git clone "https://aur.archlinux.org/yay.git"
 cd yay || exit 1
 
 # edit the mirrorlist file permissions or the makepkg command will fail
-chmod 644 /etc/pacman.d/mirrorlist
-chown jman:root /etc/pacman.d/mirrorlist
+sudo chmod 644 /etc/pacman.d/mirrorlist
+sudo chown jman:root /etc/pacman.d/mirrorlist
 
 # Install yay
-makepkg -sif
+makepkg -Csif
