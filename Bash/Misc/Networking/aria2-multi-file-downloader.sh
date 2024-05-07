d2d80a53c56353ec0f5db68f30c43a26500cdd87
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-05-07 01:09:23 -0400

    Create aria2-multi-file-downloader.sh

---
+++ Bash/Misc/Networking/aria2-multi-file-downloader.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Define the arrays
+filenames=(
+    ""
+    ""
+    ""
+)
+
+urls=(
+    ""
+    ""
+    ""
+)
+
+paths=(
+    ""
+    ""
+    ""
+)
+
+# Create the output file
+random=$(mktemp)
+output_file="${random}.sh"
+cat > "$output_file" <<EOL
+for i in {0..2}; do
+    cd "${paths[i]}" || exit 1
+    aria2c --conf-path="$HOME/.aria2/aria2.conf" --out="${filenames[i]}.mp4" '${urls[i]}'
+done
+EOL
+
+# Execute the output file
+if bash "$output_file"; then
+    google_speech "Batch video download completed." &>/dev/null
+else
+    google_speech "Batch video download failed." &>/dev/null
+fi
+
+# Delete the temporary file
+rm "$output_file"
