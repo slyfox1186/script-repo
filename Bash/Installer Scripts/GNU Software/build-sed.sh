3c9e575aa3079070b4749f7af10de0981c7c6dd2
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-05-11 17:31:38 -0400

    Update build-sed.sh

--- Bash/Installer Scripts/GNU Software/build-sed.sh
+++ Bash/Installer Scripts/GNU Software/build-sed.sh
@@ -1,140 +1,190 @@
 #!/usr/bin/env bash
 
-##  Github Script: https://github.com/slyfox1186/script-repo/edit/main/Bash/Installer%20Scripts/GNU%20Software/build-sed
+##  Github Script: https://github.com/slyfox1186/script-repo/blob/main/Bash/Installer%20Scripts/GNU%20Software/build-sed.sh
 ##  Purpose: build gnu sed
-##  Updated: 05.29.24
-##  Script version: 1.2
+##  Updated: 05.11.24
+##  Script version: 1.3
 
 set -euo pipefail
 
-# Set color variables
-RED='\033[0;31m'
+if [[ "$EUID" -eq 0 ]]; then
+    echo "You must run this script without root or sudo."
+    exit 1
+fi
+
+CYAN='\033[0;36m'
 GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
 NC='\033[0m'
 
-# Set variables
-script_ver="1.2"
-archive_dir="sed-4.9"
-archive_url="https://ftp.gnu.org/gnu/sed/$archive_dir.tar.xz"
-archive_name="$archive_dir.tar.${archive_url##*.}"
-cwd="$PWD/sed-build-script"
-install_dir="/usr/local/$archive_dir"
-
-[[ -d "$cwd" ]] && sudo rm -fr "$cwd"
-mkdir -p "$cwd"
-
-# Create logging functions
+# Enhanced logging and error handling
 log() {
-    echo -e "${GREEN}[INFO] $1${NC}"
+    echo -e "${GREEN}[INFO]${NC} $1"
 }
 
 fail() {
-    echo -e "${RED}[ERROR] $1${NC}"
-    echo "To report a bug, create an issue at: https://github.com/slyfox1186/script-repo/issues"
+    echo -e "${RED}[ERROR]${NC} $1"
+    echo -e "To report a bug, create an issue at: https://github.com/slyfox1186/script-repo/issues"
     exit 1
 }
 
-# Check if running as root or with sudo
-if [[ "$EUID" -eq 0 ]]; then
-    fail "You must run this script without root or with sudo."
-fi
-
-echo
-echo -e "${GREEN}===============================================${NC}"
-echo -e "${GREEN}            Sed build script - v${script_ver}"
-echo -e "${GREEN}===============================================${NC}"
-echo
-
-# Set the C and C++ compilers
-CC="gcc"
-CXX="g++"
-CFLAGS="-O3 -pipe -fno-plt -march=native -mtune=native -D_FORTIFY_SOURCE=2"
-CXXFLAGS="$CFLAGS"
-LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now,-rpath,$install_dir/lib"
-export CC CFLAGS CXX CXXFLAGS LDFLAGS
-
-# Set the path variable
-PATH="/usr/lib/ccache:${HOME}/perl5/bin:${HOME}/.cargo/bin:${HOME}/.local/bin:/usr/local/sbin:/usr/local/cuda/bin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-export PATH
-
-# Set the pkg_config_path variable
-PKG_CONFIG_PATH="/usr/local/lib64/pkgconfig:/usr/local/lib/pkgconfig:/usr/local/share/pkgconfig:/usr/lib64/pkgconfig:/usr/lib/pkgconfig:/usr/share/pkgconfig"
-export PKG_CONFIG_PATH
-
 # Create functions
-exit_fn() {
+exit_function() {
     echo
-    log "Make sure to star this repository to show your support!"
-    log "https://github.com/slyfox1186/script-repo"
+    log "The script has completed"
+    log "${GREEN}Make sure to ${YELLOW}star ${GREEN}this repository to show your support!${NC}"
+    log "${CYAN}https://github.com/slyfox1186/script-repo${NC}"
     exit 0
 }
 
 cleanup() {
-    local choice
-    echo
-    read -p "Remove temporary build directory '$cwd'? [y/N] " choice
-    case "$choice" in
-        [yY]*|"")
-        sudo rm -rf "$cwd"
-        log "Build directory removed."
-        ;;
-        [nN]*) ;;
-    esac
+    sudo rm -fr "$cwd"
 }
 
-# Install required apt packages
-pkgs=(
-    autoconf autoconf-archive autogen automake autopoint autotools-dev build-essential bzip2
-    ccache curl git libaudit-dev libintl-perl libticonv-dev libtool libtool-bin lzip pkg-config
-    valgrind zlib1g-dev librust-polling-dev
-)
+required_packages() {
+    local -a missing_pkgs pkgs
+    local pkg
+    log "Installing dependencies..."
+    pkgs=(
+        autoconf automake autopoint autotools-dev build-essential
+        bzip2 ccache curl libaudit-dev libintl-perl libticonv-dev
+        libtool pkg-config valgrind zlib1g-dev librust-polling-dev
+    )
 
-missing_pkgs=""
-for pkg in "${pkgs[@]}"; do
-    if ! dpkg -s "$pkg" &> /dev/null; then
-        missing_pkgs+=" $pkg"
+    missing_pkgs=()
+    for pkg in "${pkgs[@]}"; do
+        if ! dpkg-query -W -f='${Status}' "$pkg" 2>/dev/null | grep -q "ok installed"; then
+            missing_pkgs+=("$pkg")
+        fi
+    done
+
+    if [[ "${#missing_pkgs[@]}" -gt 0 ]]; then
+        sudo apt update
+        sudo apt install "${missing_pkgs[@]}"
     fi
+}
+
+set_compiler_flags() {
+    CC="gcc"
+    CXX="g++"
+    CFLAGS="-O2 -pipe -march=native"
+    CXXFLAGS="$CFLAGS"
+    CPPFLAGS="-D_FORTIFY_SOURCE=2"
+    LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now,-rpath,$install_dir/lib"
+    PATH="/usr/lib/ccache:$PATH"
+    PKG_CONFIG="$(command -v pkg-config)"
+    PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig:/usr/lib/pkgconfig"
+    export CC CXX CFLAGS CPPFLAGS CXXFLAGS LDFLAGS PATH PKG_CONFIG PKG_CONFIG_PATH
+}
+
+download_archive() {
+    wget --show-progress -cqO "$cwd/$tar_file" "$archive_url" || fail "Failed to download archive with WGET. Line: $LINENO"
+}
+
+extract_archive() {
+    tar -Jxf "$cwd/$tar_file" -C "$cwd/$archive_name" --strip-components 1 || fail "Failed to extract: $cwd/$tar_file"
+}
+
+configure_build() {
+    cd "$cwd/$archive_name" || fail "Failed to cd into $cwd/$archive_name. Line: $LINENO"
+    autoreconf -fi
+    cd build || fail "Failed to cd into the build directory. Line: $LINENO"
+    ../configure --prefix="$install_dir" --enable-threads=posix --disable-nls \
+                 --with-libiconv-prefix=/usr --with-libintl-prefix=/usr || fail "Failed to execute: configure. Line: $LINENO"
+}
+
+compile_build() {
+    make "-j$(nproc --all)" || fail "Failed to execute: make build. Line: $LINENO"
+}
+
+install_build() {
+    sudo make install || fail "Failed execute: make install. Line: $LINENO"
+}
+
+ld_linker_path() {
+    if ! echo "$install_dir/lib" | sudo tee "/etc/ld.so.conf.d/custom_$prog_name.conf" >/dev/null; then
+        echo "LD linker failed."
+        exit 1
+    fi
+    sudo ldconfig
+}
+
+create_soft_links() {
+    sudo ln -sf "$install_dir/bin/"* "/usr/local/bin/"
+    sudo ln -sf "$install_dir/lib/pkgconfig/"*.pc "/usr/local/lib/pkgconfig/"
+    sudo ln -sf "$install_dir/include/"* "/usr/local/include/"
+}
+
+show_usage() {
+    echo "Usage: ${0##*/} [OPTIONS]"
+    echo "Build GNU Make from source."
+    echo
+    echo "Options:"
+    echo "  -h, --help       Show this help message and exit"
+    echo "  -v, --version    Specify the version of sed to build (default: 4.4.1)"
+    echo
+    echo "Example:"
+    echo "  $0 -v 4.3 -c"
+}
+
+# Parse command-line options
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        -h|--help)
+            show_usage
+            exit 0
+            ;;
+        -v|--version)
+            shift
+            sed_version="$1"
+            ;;
+        *)
+            warn "Unknown option: $1"
+            show_usage
+            exit 1
+            ;;
+    esac
+    shift
 done
 
-if [[ -n "$missing_pkgs" ]]; then
-    sudo apt install $missing_pkgs
-fi
+main_menu() {
+    sed_version=""
+    script_ver=2.2
+    prog_name="sed"
+    version=$(curl -fsS "https://ftp.gnu.org/gnu/$prog_name/" | grep -oP 'sed-\K([0-9.])+(?=\.tar\..*)' | sort -ruV | head -n1)
+    archive_name="$prog_name-$version"
+    install_dir="/usr/local/$archive_name"
+    cwd="$PWD/$archive_name-build-script"
 
-# Download the archive file
-if [[ ! -f "$cwd/$archive_name" ]]; then
-    curl -Lso "$cwd/$archive_name" "$archive_url"
-fi
+    echo "$prog_name build script - version $script_ver"
+    echo "================================================="
+    echo
 
-# Create output directory
-[[ -d "$cwd/$archive_dir" ]] && sudo rm -fr "$cwd/$archive_dir"
-mkdir -p "$cwd/$archive_dir/build"
+    # Create output directory
+    [[ -d "$cwd/$archive_name" ]] && sudo rm -fr "$cwd/$archive_name"
+    mkdir -p "$cwd/$archive_name/build"
 
-# Extract archive files
-if ! tar -xf "$cwd/$archive_name" -C "$cwd/$archive_dir" --strip-components 1; then
-    fail "Failed to extract: $cwd/$archive_name"
-fi
+    if [[ -n "$sed_version" ]]; then
+        archive_url="https://ftp.gnu.org/gnu/sed/sed-$sed_version.tar.xz"
+        archive_ext="${archive_url//*.}"
+    else
+        archive_url="https://ftp.gnu.org/gnu/sed/sed-$version.tar.xz"
+        archive_ext="${archive_url//*.}"
+    fi
+    tar_file="$archive_name.tar.$archive_ext"
 
-# Build program from source
-cd "$cwd/$archive_dir" || fail "Failed to change directory to: $cwd/$archive_dir/build"
-autoreconf -fi
-cd build || exit 1
-../configure --prefix="$install_dir" \
-             --enable-threads=posix \
-             --disable-nls \
-             --with-libiconv-prefix=/usr \
-             --with-libintl-prefix=/usr
-if ! make "-j$(nproc --all)"; then
-    fail "Failed to execute: make -j$(nproc --all). Line: $LINENO"
-fi
-if ! sudo make install; then
-    fail "Failed to execute: sudo make install. Line: $LINENO"
-fi
+    required_packages
+    set_compiler_flags
+    download_archive
+    extract_archive
+    configure_build
+    compile_build
+    install_build
+    ld_linker_path
+    create_soft_links
+    cleanup
+    exit_function
+}
 
-# Create soft links
-sudo ln -sf "$install_dir"/bin/* /usr/local/bin/
-
-# Prompt user to clean up files
-cleanup
-
-# Show exit message
-exit_fn
+main_menu "$@"
