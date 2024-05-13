bfc2843626353e260c8ad1472d41af0a7a535427
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-05-13 08:44:02 -0400

    Update build-sed.sh

--- Bash/Installer Scripts/GNU Software/build-sed.sh
+++ Bash/Installer Scripts/GNU Software/build-sed.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
-##  Github Script: https://github.com/slyfox1186/script-repo/blob/main/Bash/Installer%20Scripts/GNU%20Software/build-sed.sh
-##  Purpose: build gnu sed
-##  Updated: 05.11.24
-##  Script version: 1.3
-
-set -euo pipefail
-
-if [[ "$EUID" -eq 0 ]]; then
-    echo "You must run this script without root or sudo."
-    exit 1
-fi
+# Github Script: https://github.com/slyfox1186/script-repo/blob/main/Bash/Installer%20Scripts/GNU%20Software/build-sed.sh
+# Purpose: build gnu sed
+# Updated: 05.13.24
+# Script version: 1.4
 
 CYAN='\033[0;36m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
+
+# Set the variables
+script_ver=1.4
+prog_name="sed"
+default_version=$(curl -fsS "https://ftp.gnu.org/gnu/$prog_name/" | grep -oP '\d\.[\d.]+(?=\.tar\.[a-z]+)' | sort -ruV | head -n1)
+install_dir="/usr/local"
+cwd="$PWD/$prog_name-build-script"
+compiler="gcc"
 
 # Enhanced logging and error handling
 log() {
@@ -29,7 +30,15 @@ fail() {
     exit 1
 }
 
-# Create functions
+print_usage() {
+    echo "Usage: $0 [OPTIONS]"
+    echo "  -v, --version VERSION       Set the version of $prog_name to install (default: $default_version)"
+    echo "  -l, --list                  List available versions of $prog_name"
+    echo "  -u, --uninstall             Uninstall $prog_name"
+    echo "  -c, --compiler COMPILER     Set the compiler to use (clang) instead of the default: $compiler"
+    echo "  -h, --help                  Display this help and exit"
+}
+
 exit_function() {
     echo
     log "The script has completed"
@@ -45,7 +54,6 @@ cleanup() {
 required_packages() {
     local -a missing_pkgs pkgs
     local pkg
-    log "Installing dependencies..."
     pkgs=(
         autoconf automake autopoint autotools-dev build-essential
         bzip2 ccache curl libaudit-dev libintl-perl libticonv-dev
@@ -66,16 +74,14 @@ required_packages() {
 }
 
 set_compiler_flags() {
-    CC="gcc"
-    CXX="g++"
+    CC="$compiler"
+    CXX="$compiler++"
     CFLAGS="-O2 -pipe -march=native"
     CXXFLAGS="$CFLAGS"
-    CPPFLAGS="-D_FORTIFY_SOURCE=2"
-    LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now,-rpath,$install_dir/lib"
+    LDFLAGS="-Wl,-rpath,$install_dir/lib"
     PATH="/usr/lib/ccache:$PATH"
-    PKG_CONFIG="$(command -v pkg-config)"
     PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig:/usr/lib/pkgconfig"
-    export CC CXX CFLAGS CPPFLAGS CXXFLAGS LDFLAGS PATH PKG_CONFIG PKG_CONFIG_PATH
+    export CC CXX CFLAGS CXXFLAGS LDFLAGS PATH PKG_CONFIG_PATH
 }
 
 download_archive() {
@@ -88,8 +94,9 @@ extract_archive() {
 
 configure_build() {
     cd "$cwd/$archive_name" || fail "Failed to cd into $cwd/$archive_name. Line: $LINENO"
+
     autoreconf -fi
-    cd build || fail "Failed to cd into the build directory. Line: $LINENO"
+    cd build || exit 1
     ../configure --prefix="$install_dir" --enable-threads=posix --disable-nls \
                  --with-libiconv-prefix=/usr --with-libintl-prefix=/usr || fail "Failed to execute: configure. Line: $LINENO"
 }
@@ -103,76 +110,78 @@ install_build() {
 }
 
 ld_linker_path() {
-    if ! echo "$install_dir/lib" | sudo tee "/etc/ld.so.conf.d/custom_$prog_name.conf" >/dev/null; then
-        echo "LD linker failed."
-        exit 1
-    fi
+    echo "$install_dir/$archive_name/lib" | sudo tee "/etc/ld.so.conf.d/custom_$prog_name.conf" >/dev/null
     sudo ldconfig
 }
 
 create_soft_links() {
-    sudo ln -sf "$install_dir/bin/"* "/usr/local/bin/"
-    sudo ln -sf "$install_dir/lib/pkgconfig/"*.pc "/usr/local/lib/pkgconfig/"
-    sudo ln -sf "$install_dir/include/"* "/usr/local/include/"
+    [[ -d "$install_dir/$archive_name/bin" ]] && sudo ln -sf "$install_dir/$archive_name/bin/"* "/usr/local/bin/"
+    [[ -d "$install_dir/$archive_name/lib/pkgconfig" ]] && sudo ln -sf "$install_dir/$archive_name/lib/pkgconfig/"*.pc "/usr/local/lib/pkgconfig/"
+    [[ -d "$install_dir/$archive_name/include" ]] && sudo ln -sf "$install_dir/$archive_name/include/"* "/usr/local/include/"
 }
 
-show_usage() {
-    echo "Usage: ${0##*/} [OPTIONS]"
-    echo "Build GNU Make from source."
-    echo
-    echo "Options:"
-    echo "  -h, --help       Show this help message and exit"
-    echo "  -v, --version    Specify the version of sed to build (default: 4.4.1)"
-    echo
-    echo "Example:"
-    echo "  $0 -v 4.3 -c"
+uninstall_sed() {
+    local sed_dir
+    sed_dir="$install_dir/$archive_name"
+    if [[ -d "$sed_dir" ]]; then
+        log "Uninstalling $prog_name from $sed_dir"
+        sudo rm -rf "$sed_dir"
+        sudo rm "/etc/ld.so.conf.d/custom_$prog_name.conf"
+        sudo ldconfig
+        log "$prog_name has been uninstalled"
+    else
+        log "$prog_name is not installed"
+    fi
 }
 
-# Parse command-line options
-while [[ "$#" -gt 0 ]]; do
-    case "$1" in
-        -h|--help)
-            show_usage
-            exit 0
-            ;;
-        -v|--version)
-            shift
-            sed_version="$1"
-            ;;
-        *)
-            warn "Unknown option: $1"
-            show_usage
-            exit 1
-            ;;
-    esac
-    shift
-done
+list_versions() {
+    log "Available versions of $prog_name:"
+    curl -fsS "https://ftp.gnu.org/gnu/$prog_name/" | grep -oP '\d\.[\d.]+(?=\.tar\.[a-z]+)' | sort -ruV
+}
 
 main_menu() {
-    sed_version=""
-    script_ver=2.2
-    prog_name="sed"
-    version=$(curl -fsS "https://ftp.gnu.org/gnu/$prog_name/" | grep -oP 'sed-\K([0-9.])+(?=\.tar\..*)' | sort -ruV | head -n1)
-    archive_name="$prog_name-$version"
-    install_dir="/usr/local/$archive_name"
-    cwd="$PWD/$archive_name-build-script"
+    # Parse command-line arguments
+    while [[ "$#" -gt 0 ]]; do
+        case "$1" in
+            -v|--version)
+                version="$2"
+                shift 2
+                ;;
+            -l|--list)
+                list_versions
+                exit 0
+                ;;
+            -u|--uninstall)
+                uninstall_sed
+                exit 0
+                ;;
+            -c|--compiler)
+                compiler="$2"
+                shift 2
+                ;;
+            -h|--help)
+                print_usage
+                exit 0
+                ;;
+            *)
+                fail "Invalid option: $1"
+                ;;
+        esac
+    done
 
-    echo "$prog_name build script - version $script_ver"
-    echo "================================================="
-    echo
+    if [[ -z "$version" ]]; then
+        version="$default_version"
+        log "No version specified, using default version: $version"
+    fi
+
+    archive_name="$prog_name-$version"
+    archive_url="https://ftp.gnu.org/gnu/$prog_name/$prog_name-$version.tar.xz"
+    archive_ext="${archive_url//*.}"
+    tar_file="$archive_name.tar.$archive_ext"
 
     # Create output directory
     [[ -d "$cwd/$archive_name" ]] && sudo rm -fr "$cwd/$archive_name"
     mkdir -p "$cwd/$archive_name/build"
-
-    if [[ -n "$sed_version" ]]; then
-        archive_url="https://ftp.gnu.org/gnu/sed/sed-$sed_version.tar.xz"
-        archive_ext="${archive_url//*.}"
-    else
-        archive_url="https://ftp.gnu.org/gnu/sed/sed-$version.tar.xz"
-        archive_ext="${archive_url//*.}"
-    fi
-    tar_file="$archive_name.tar.$archive_ext"
 
     required_packages
     set_compiler_flags
@@ -181,10 +190,15 @@ main_menu() {
     configure_build
     compile_build
     install_build
-    ld_linker_path
+    [[ -d "$install_dir/$archive_name/lib" ]] && ld_linker_path
     create_soft_links
     cleanup
     exit_function
 }
+
+if [[ "$EUID" -eq 0 ]]; then
+    echo "You must run this script without root or sudo."
+    exit 1
+fi
 
 main_menu "$@"
