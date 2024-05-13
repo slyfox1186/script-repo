0637f519286f6c048ab24959bbbf59d6a5243022
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-05-13 09:07:47 -0400

    Update build-bash.sh

--- Bash/Installer Scripts/GNU Software/build-bash.sh
+++ Bash/Installer Scripts/GNU Software/build-bash.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
 # GitHub: https://github.com/slyfox1186/script-repo/blob/main/Bash/Installer%20Scripts/GNU%20Software/build-bash.sh
-# Purpose: build gnu bash from source.
-# Updated: 05.08.24
-# Script version: 2.1
-
-if [[ "$EUID" -eq 0 ]]; then
-    echo "You must run this script without root or sudo."
-    exit 1
-fi
+# Purpose: build gnu bash from source
+# Updated: 05.13.24
+# Script version: 2.2
 
 CYAN='\033[0;36m'
 GREEN='\033[0;32m'
@@ -17,15 +12,12 @@ YELLOW='\033[0;33m'
 NC='\033[0m'
 
 # Set the variables
-script_ver=2.1
+script_ver=2.2
 prog_name="bash"
-version=$(curl -fsS "https://ftp.gnu.org/gnu/$prog_name/" | grep -oP 'bash-\K([0-9.]{3,6})[a-z-]*' | grep -Eiv 'alpha|beta|rc|patches' | sort -ruV |head -n1)
-archive_name="$prog_name-$version"
-archive_url="https://ftp.gnu.org/gnu/$prog_name/$prog_name-$version.tar.gz"
-archive_ext="${archive_url//*.}"
-tar_file="$archive_name.tar.$archive_ext"
-install_dir="/usr/local/$archive_name"
-cwd="$PWD/$archive_name-build-script"
+default_version=$(curl -fsS "https://ftp.gnu.org/gnu/$prog_name/" | grep -oP '\d\.[\d.]+(?=\.tar\.[a-z]+)' | sort -ruV | head -n1)
+install_dir="/usr/local"
+cwd="$PWD/$prog_name-build-script"
+compiler="gcc"
 
 # Enhanced logging and error handling
 log() {
@@ -38,12 +30,16 @@ fail() {
     exit 1
 }
 
-echo "$prog_name build script - version $script_ver"
-echo "================================================="
-echo
+print_usage() {
+    echo "Usage: $0 [OPTIONS]"
+    echo "  -v, --version VERSION       Set the version of $prog_name to install (default: $default_version)"
+    echo "  -l, --list                  List available versions of $prog_name"
+    echo "  -u, --uninstall             Uninstall $prog_name"
+    echo "  -c, --compiler COMPILER     Set the compiler to use (clang) instead of the default: $compiler"
+    echo "  -h, --help                  Display this help and exit"
+}
 
-# Create functions
-exit_fn() {
+exit_function() {
     echo
     log "The script has completed"
     log "${GREEN}Make sure to ${YELLOW}star ${GREEN}this repository to show your support!${NC}"
@@ -59,9 +55,9 @@ required_packages() {
     local -a missing_pkgs pkgs
     local pkg
     pkgs=(
-        autoconf autoconf-archive automake binutils bison build-essential
-        ccache curl gettext libncursesw5-dev libpth-dev libreadline-dev
-        libticonv-dev libtool lzip m4 tar
+        autoconf automake build-essential gettext libacl1-dev
+        libattr1-dev libbz2-dev libintl-perl liblzma-dev libtool
+        lzip lzop libzstd-dev libticonv9 xz-utils zlib1g-dev
     )
 
     missing_pkgs=()
@@ -71,22 +67,21 @@ required_packages() {
         fi
     done
 
-    if [ ${#missing_pkgs[@]} -gt 0 ]; then
+    if [[ "${#missing_pkgs[@]}" -gt 0 ]]; then
         sudo apt update
         sudo apt install "${missing_pkgs[@]}"
     fi
 }
 
 set_compiler_flags() {
-    CC="gcc"
-    CXX="g++"
+    CC="$compiler"
+    CXX="$compiler++"
     CFLAGS="-O2 -pipe -march=native"
     CXXFLAGS="$CFLAGS"
-    CPPFLAGS="-D_FORTIFY_SOURCE=2"
-    LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now,-rpath,$install_dir/lib"
-    PATH="/usr/lib/ccache:$HOME/perl5/bin:$HOME/.cargo/bin:$HOME/.local/bin:/usr/local/bin:/usr/bin:/bin"
-    PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:/usr/lib/pkgconfig"
-    export CC CXX CFLAGS CPPFLAGS CXXFLAGS LDFLAGS PATH PKG_CONFIG_PATH
+    LDFLAGS="-Wl,-rpath,$install_dir/lib"
+    PATH="/usr/lib/ccache:$PATH"
+    PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig:/usr/lib/pkgconfig"
+    export CC CXX CFLAGS CXXFLAGS LDFLAGS PATH PKG_CONFIG_PATH
 }
 
 download_archive() {
@@ -98,16 +93,11 @@ extract_archive() {
 }
 
 configure_build() {
-    local libiconv_prefix
-
-    # Locate the latest installed libiconv prefix
-    libiconv_prefix=$(sudo find /usr/local -maxdepth 1 -type d -name "libiconv-*" -print -quit 2>/dev/null || echo /usr)
-
     cd "$cwd/$archive_name" || fail "Failed to cd into $cwd/$archive_name. Line: $LINENO"
 
     autoreconf -fi
     cd build || exit 1
-    ../configure --prefix="$install_dir" --disable-nls --disable-profiling --enable-brace-expansion \
+    ../configure --prefix="$install_dir/$archive_name" --disable-nls --disable-profiling --enable-brace-expansion \
                  --enable-history --enable-separate-helpfiles --enable-threads=posix --with-bash-malloc \
                  --with-libiconv-prefix="$libiconv_prefix" --with-libintl-prefix=/usr --with-libpth-prefix=/usr \
                  --without-included-gettext || fail "Failed to execute: configure. Line: $LINENO"
@@ -121,15 +111,76 @@ install_build() {
     sudo make install || fail "Failed execute: make install. Line: $LINENO"
 }
 
+ld_linker_path() {
+    echo "$install_dir/$archive_name/lib" | sudo tee "/etc/ld.so.conf.d/custom_$prog_name.conf" >/dev/null
+    sudo ldconfig
+}
+
 create_soft_links() {
-    sudo ln -sf "$install_dir/bin/"* "/usr/local/bin/"
-    sudo ln -sf "$install_dir/lib/pkgconfig/"*.pc "/usr/local/lib/pkgconfig/"
-    sudo ln -sf "$install_dir/include/bash/"* "/usr/local/include/"
-    sudo ln -sf "$install_dir/include/bash/include/"* "/usr/local/include/"
-    sudo ln -sf "$install_dir/include/bash/builtins/"* "/usr/local/include/"
+    [[ -d "$install_dir/$archive_name/bin" ]] && sudo ln -sf "$install_dir/$archive_name/bin/"* "/usr/local/bin/"
+    [[ -d "$install_dir/$archive_name/lib/pkgconfig" ]] && sudo ln -sf "$install_dir/$archive_name/lib/pkgconfig/"*.pc "/usr/local/lib/pkgconfig/"
+    [[ -d "$install_dir/$archive_name/include" ]] && sudo ln -sf "$install_dir/$archive_name/include/"* "/usr/local/include/"
+}
+
+uninstall_bash() {
+    local bash_dir
+    bash_dir="$install_dir/$archive_name"
+    if [[ -d "$bash_dir" ]]; then
+        log "Uninstalling $prog_name from $bash_dir"
+        sudo rm -rf "$bash_dir"
+        sudo rm "/etc/ld.so.conf.d/custom_$prog_name.conf"
+        sudo ldconfig
+        log "$prog_name has been uninstalled"
+    else
+        log "$prog_name is not installed"
+    fi
+}
+
+list_versions() {
+    log "Available versions of $prog_name:"
+    curl -fsS "https://ftp.gnu.org/gnu/$prog_name/" | grep -oP '\d\.[\d.]+(?=\.tar\.[a-z]+)' | sort -ruV
 }
 
 main_menu() {
+    # Parse command-line arguments
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            -v|--version)
+                version="$2"
+                shift 2
+                ;;
+            -l|--list)
+                list_versions
+                exit 0
+                ;;
+            -u|--uninstall)
+                uninstall_bash
+                exit 0
+                ;;
+            -c|--compiler)
+                compiler="$2"
+                shift 2
+                ;;
+            -h|--help)
+                print_usage
+                exit 0
+                ;;
+            *)
+                fail "Invalid option: $1"
+                ;;
+        esac
+    done
+
+    if [[ -z "$version" ]]; then
+        version="$default_version"
+        log "No version specified, using default version: $version"
+    fi
+
+    archive_name="$prog_name-$version"
+    archive_url="https://ftp.gnu.org/gnu/$prog_name/$prog_name-$version.tar.gz"
+    archive_ext="${archive_url//*.}"
+    tar_file="$archive_name.tar.$archive_ext"
+
     # Create output directory
     [[ -d "$cwd/$archive_name" ]] && sudo rm -fr "$cwd/$archive_name"
     mkdir -p "$cwd/$archive_name/build"
@@ -141,9 +192,15 @@ main_menu() {
     configure_build
     compile_build
     install_build
+    [[ -d "$install_dir/$archive_name/lib" ]] && ld_linker_path
     create_soft_links
     cleanup
-    exit_fn
+    exit_function
 }
+
+if [[ "$EUID" -eq 0 ]]; then
+    echo "You must run this script without root or sudo."
+    exit 1
+fi
 
 main_menu "$@"
