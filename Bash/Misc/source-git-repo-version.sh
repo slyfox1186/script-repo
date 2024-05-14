eff26a580bded4f9968278177475cbd93a90b5de
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-05-14 13:30:00 -0400

    Update source-git-repo-version.sh

--- Bash/Misc/source-git-repo-version.sh
+++ Bash/Misc/source-git-repo-version.sh
@@ -2,40 +2,30 @@
 
 # This script will return the latest release version of a GitHub repository.
 # Example usage: ./source-git-repo-version.sh "https://github.com/rust-lang/rust.git"
-# Expected Output: e.g., 1.77.2
 
 # Regex patterns
-first_match='href="[^"]*/releases/tag/\K[^"]*'
+first_match='href="[^"]*/tag/\K[^"]*'
 second_match='(?:[a-z-]+-)?\K([0-9]+(?:[._-][0-9]+)*(?:-[a-zA-Z0-9]+)?)'
-exclude_words='alpha|beta|early|init|M2|next|pending|pre|rc|tentative|^.$'
+exclude_words='alpha|beta|DEV|early|init|M[0-9]+|next|pending|pre|rc|tentative|^.$'
 trim_this='s/-$//'
 
 # Function to fetch and parse the latest release version
 get_latest_release_version() {
-    local tags_url url
+    local url tags_url html_content main_content tags_content
     url="$1"
-    tags_url="${url%.*}/tags"  # Adjust the URL to point to the tags page
+    releases_url="${url%.*}/releases"
+    tags_url="${url%.*}/tags"
 
-    # Correct usage of the pipe with curl and grep commands
-    latest_version=$(curl -fsSL "$url" | parse_version)
-    [[ -z "$latest_version" ]] && latest_version=$(curl -fsSL "$tags_url" | parse_version)
-    echo "$latest_version"
-}
+    # Fetch HTML content from both URLs
+    main_content=$(curl -fsSL "$url")
+    releases_content=$(curl -fsSL "$releases_url")
+    tags_content=$(curl -fsSL "$tags_url")
+    html_content="$releases_content $main_content $tags_content"
 
-# Parse the version from HTML content
-parse_version() {
-    grep -oP "$first_match" |
-    grep -oP "$second_match" |
-    grep -Eiv "$exclude_words" |
-    sort -rV | head -n1 |
-    sed "$trim_this" | sed 's/^v//'
+    echo "$html_content" | grep -oP "$first_match" | grep -oP "$second_match" | grep -Eiv "$exclude_words" | sort -rV | head -n1 | sed "$trim_this" | sed 's/^v//'
 }
 
 # Main script execution
-if [[ -z "$1" ]]; then
-    echo "Usage: $0 <url>"
-    exit 1
-fi
+[[ -z "$1" ]] && { echo "Usage: $0 <url>"; exit 1; }
 
-version=$(get_latest_release_version "$1")
-echo "$version"
+get_latest_release_version "$1"
