7f33e0a597e8959d370796246a537ccf614fa32c
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-05-27 11:29:21 -0400

    Update build-menu.sh

--- Bash/Installer Scripts/SlyFox1186 Scripts/build-menu.sh
+++ Bash/Installer Scripts/SlyFox1186 Scripts/build-menu.sh
@@ -2,8 +2,6 @@
 
 clear
 
-user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
-
 # Menu color functions
 green='\e[32m'
 blue='\e[34m'
