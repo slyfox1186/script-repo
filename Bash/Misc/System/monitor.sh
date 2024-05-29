985c73e551045a6413e361b8240bd93d5023868a
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-05-29 11:10:07 -0400

    Update monitor.sh

--- Bash/Misc/System/monitor.sh
+++ Bash/Misc/System/monitor.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # GitHub: https://github.com/slyfox1186/script-repo/blob/main/Bash/Misc/System/monitor.sh
-# Script version: 1.5
-# Last update: 05-28-24
+# Script version: 1.6
+# Last update: 05-29-24
 
 ## Important information
 # Arguments take priority over hardcoded variables
@@ -12,25 +12,17 @@ monitor_dir="${PWD}"  # Default directory to monitor
 include_access=false  # Flag to include access events
 log_file=""           # Log file path
 
-# Define colors
-cyan='\033[36m'       # Cyan for access events
-green='\033[32m'      # Green for create events
-red='\033[31m'        # Red for delete events
-yellow='\033[33m'     # Yellow for modify events
-magenta='\033[35m'    # Magenta for move events
-reset='\033[0m'       # Resets the color to none
-
 # Function to display help
 display_help() {
-    echo -e "${yellow}Usage${magenta}:${reset} ${0} [options]"
+    echo -e "${eventcolors[MODIFY]}Usage${eventcolors[MOVE]}:${reset} ${0} [options]"
     echo
-    echo -e "${yellow}Options${magenta}:${reset}"
+    echo -e "${eventcolors[MODIFY]}Options${eventcolors[MOVE]}:${reset}"
     echo "  -a, --access             Include \"access\" events"
     echo "  -d, --directory <path>   Specify the directory to monitor"
     echo "  -l, --log <path>         Specify the log file to write events"
     echo "  -h, --help               Display this help message"
     echo
-    echo -e "${yellow}Examples${magenta}:${reset}"
+    echo -e "${eventcolors[MODIFY]}Examples${eventcolors[MOVE]}:${reset}"
     echo "./monitor.sh --help"
     echo "./monitor.sh --directory \"/path/to/folder\""
     echo "./monitor.sh -a -d \"/path/to/folder\""
@@ -69,37 +61,21 @@ parse_arguments() {
 # Function to check if inotifywait is installed
 check_command() {
     if ! command -v inotifywait &>/dev/null; then
-        echo -e "${red}[ERROR]${reset} The command inotifywait is not installed."
-        echo -e "${green}[INFO]${reset} This is commonly installed by your package manager inside the package inotify-tools"
+        echo -e "${eventcolors[DELETE]}[ERROR]${reset} The command inotifywait is not installed."
+        echo -e "${eventcolors[CREATE]}[INFO]${reset} This is commonly installed by your package manager inside the package inotify-tools"
         exit 1
     fi
 }
 
-# Function to get the color for an event
-get_color_for_event() {
-    local event
-    event="${1}"
-    case "${event}" in
-        *ACCESS*)
-            echo "${cyan}"
-            ;;
-        *CREATE*)
-            echo "${green}"
-            ;;
-        *DELETE*)
-            echo "${red}"
-            ;;
-        *MODIFY*)
-            echo "${yellow}"
-            ;;
-        *MOVE*)
-            echo "${magenta}"
-            ;;
-        *)
-            echo "${reset}"
-            ;;
-    esac
-}
+# Define colors
+declare -A eventcolors=(
+    [ACCESS]=$'\033[36m'   # Cyan for access events
+    [CREATE]=$'\033[32m'   # Green for create events
+    [DELETE]=$'\033[31m'   # Red for delete events
+    [MODIFY]=$'\033[33m'   # Yellow for modify events
+    [MOVE]=$'\033[35m'     # Magenta for move events
+)
+reset='\033[0m'       # Resets the color to none
 
 # Main function to monitor directory
 monitor_directory() {
@@ -110,7 +86,7 @@ monitor_directory() {
     fi
 
     if [[ ! -d "${monitor_dir}" ]]; then
-        echo -e "${red}[ERROR]${reset} The directory to monitor does not exist."
+        echo -e "${eventcolors[DELETE]}[ERROR]${reset} The directory to monitor does not exist."
         exit 1
     fi
 
@@ -118,7 +94,8 @@ monitor_directory() {
     inotifywait -mre "${events}" "${monitor_dir}" |
     while read -r event; do
         printf -v timestamp '%(%m-%d-%Y %I:%M:%S %p)T' -1
-        color=$(get_color_for_event "${event}")
+        event_type=$(echo "$event" | awk '{print $2}')
+        color=${eventcolors[$event_type]}
         event_log="[${timestamp}] ${event}"
         echo -e "${color}${event_log}${reset}"
         
