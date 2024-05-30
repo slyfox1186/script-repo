393fb659c5cdf71499eb257d4061c49c5bb5a75e
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-05-30 12:57:39 -0400

    Update convert.sh

--- Bash/Installer Scripts/ImageMagick/scripts/convert.sh
+++ Bash/Installer Scripts/ImageMagick/scripts/convert.sh
@@ -1,18 +1,37 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2086 source=/dev/null
 
 # Color setup
+CYAN='\033[0;36m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
+YELLOW='\033[0;33m'
 NC='\033[0m'
-
-# Icon sizes
-ICON_SIZES="256,128,96,64,48,32,20,16"
+export CYAN GREEN RED YELLOW NC
 
 # Default values
 output_dir="output"
-quality=83
+quality=82
 additional_args=""
 max_file_size=0
+log_file="convert.log"
+recursive=false
+parallel=false
+verbose=false
+test_run=false
+delete_input=false
+webp_sizes=(1.5 2 3 4)
+icon_sizes="256,128,96,64,48,32,20,16"
+
+MAGICK_AREA_LIMIT="1GP"
+MAGICK_DISK_LIMIT="128GiB"
+MAGICK_FILE_LIMIT="1536"
+MAGICK_HEIGHT_LIMIT="512MP"
+MAGICK_MAP_LIMIT="32GiB"
+MAGICK_MEMORY_LIMIT="32GiB"
+MAGICK_THREAD_LIMIT="$(nproc --all)"
+MAGICK_WIDTH_LIMIT="512MP"
+export MAGICK_AREA_LIMIT MAGICK_DISK_LIMIT MAGICK_FILE_LIMIT MAGICK_HEIGHT_LIMIT MAGICK_MAP_LIMIT MAGICK_MEMORY_LIMIT MAGICK_THREAD_LIMIT MAGICK_WIDTH_LIMIT
 
 # Function to display the help menu
 display_help() {
@@ -24,16 +43,22 @@ display_help() {
     echo
     echo "Options:"
     echo "  -h, --help                    Display the help menu"
-    echo "  -q, --quality <value>         Set the quality level (default is 83)"
+    echo "  -q, --quality <value>         Set the quality level (default is 82)"
     echo "  -o, --output <dir>            Set the output directory (default is 'output')"
     echo "  -a, --additional <args>       Set additional command-line arguments for ImageMagick"
     echo "  -m, --max-size <size>         Set the maximum output file size (e.g., 500KB or 1.5MB)"
+    echo "  -l, --log-file <file>         Set the log file (default is 'convert.log')"
+    echo "  -r, --recursive               Search for image files recursively"
+    echo "  -p, --parallel                Convert images in parallel"
+    echo "  -v, --verbose                 Enable verbose mode"
+    echo "  -t, --test-run                Perform a test run (show actions without executing)"
+    echo "  -d, --delete                  Delete input files after processing"
 }
 
 # Function to check for required dependencies
 check_dependencies() {
-    local -a missing_deps=() dependencies=()
-    dependencies=(convert identify)
+    local missing_deps dependencies dep
+    dependencies=(convert gifsicle identify optipng parallel pngcrush)
 
     for dep in "${dependencies[@]}"; do
         if ! command -v "$dep" &>/dev/null; then
@@ -52,45 +77,190 @@ get_file_type() {
     identify -format '%m' "$1" | tr '[:upper:]' '[:lower:]'
 }
 
+# Function to log messages
+log_message() {
+    local message
+    message=$1
+    echo "$message" >> "$log_file"
+}
+
+warn() {
+    echo -e "${YELLOW}[WARN]${NC} $1"
+}
+
+# Function to resize images exceeding the limits
+resize_if_needed() {
+    local file height height_limit width width_limit
+    file=$1
+    width_limit=$2
+    height_limit=$3
+
+    width=$(identify -ping -format '%w' "$file")
+    height=$(identify -ping -format '%h' "$file")
+
+    if (( width > width_limit || height > height_limit )); then
+        echo -e "${GREEN}[INFO]${NC} Resizing $file to fit within limits."
+        log_message "Resizing $file to fit within limits."
+        convert "$file" -resize "${width_limit}x${height_limit}" "$file"
+    fi
+}
+
 # Function to convert image files
-convert_file() {
-    local file="$1"
-    local output_type="$2"
-    local output_file="$3"
-    local quality="$4"
-    local additional_args="$5"
-    local max_size="$6"
-    local temp_file
-    local file_size_kb
+process_image() {
+    local base_name file img_height max_quality mid_quality min_quality mpc_file output_file
+    local output_type size_kb sized_file sizes_dir small_height temp_dir temp_file
+    local -a convert_base_opts=() png_opts=()
 
-    echo -e "${GREEN}[INFO]${NC} Converting $file to $output_file with quality $quality and additional args $additional_args"
+    file=$1
+    output_type=$2
+    base_name="${file##*/}"
+    output_file="$output_dir/${base_name%.*}.$output_type"
+    temp_dir=$(mktemp -d)
 
-    temp_file=$(mktemp)
-    convert "$file" -quality "$quality" $additional_args "$temp_file"
-    file_size_kb=$(du -k "$temp_file" | cut -f1)
+    resize_if_needed "$file" 512000 512000
 
-    # Adjust quality to fit the max size
-    while [[ "$file_size_kb" -gt "$max_size" && "$quality" -gt 10 ]]; do
-        quality=$((quality - 5))
-        convert "$file" -quality "$quality" $additional_args "$temp_file"
-        file_size_kb=$(du -k "$temp_file" | cut -f1)
-    done
-
-    # Final conversion to output file
-    mv "$temp_file" "$output_file"
-
-    if [[ "$file_size_kb" -gt "$max_size" ]]; then
-        echo -e "${RED}[ERROR]${NC} Unable to compress $file to below the maximum size limit."
-        rm -f "$output_file"
+    if [[ "$file" =~ \.gif ]]; then
+        warn "This script does not support the processing of a GIF to WEBP."
         return 1
     fi
 
+    if [[ "$test_run" == "true" ]]; then
+        echo -e "${GREEN}[INFO] ${CYAN}[TEST RUN]${NC} $file to $output_file with quality $quality and additional args $additional_args"
+        log_message "[TEST RUN] $file to $output_file with quality $quality and additional args $additional_args"
+        return 0
+    fi
+
+    echo -e "${GREEN}[INFO]${NC} Converting $file to $output_file with quality $quality and additional args $additional_args"
+    log_message "Converting $file to $output_file with quality $quality and additional args $additional_args"
+
+    convert_base_opts=(
+        -thumbnail "$(identify -ping -format '%wx%h' "$file")"
+        -strip -unsharp '0.25x0.08+8.3+0.045' -dither None -posterize 136 -quality "$quality"
+        -define jpeg:fancy-upsampling=off -auto-level -enhance -interlace none
+        -colorspace sRGB
+    )
+
+    png_opts=(
+        -define png:compression-filter=5 -define png:compression-level=9
+        -define png:compression-strategy=1 -define png:exclude-chunk=all
+    )
+
+    convert_image() {
+        local qual
+        qual=$1
+        convert "$file" "${convert_base_opts[@]}" -quality "$qual" "${png_opts[@]}" "$temp_file"
+        optipng -o7 "$temp_file"
+        size_kb=$(du -k "$temp_file" | cut -f1)
+    }
+
+    case "$output_type" in
+        jpg)
+            mpc_file="$temp_dir/${base_name%.*}.mpc"
+            if ! convert "$file" "${convert_base_opts[@]}" -sampling-factor 2x2 -limit area 0 "$mpc_file"; then
+                [[ "$verbose" == "true" ]] && log_message "First attempt failed, retrying without '-sampling-factor 2x2 -limit area 0'..."
+                if ! convert "$file" "${convert_base_opts[@]}" "$mpc_file"; then
+                    [[ "$verbose" == "true" ]] && log_message "Error: Second attempt failed as well."
+                    rm -fr "$temp_dir"
+                    return 1
+                fi
+            fi
+            convert "$mpc_file" "$output_file"
+            ;;
+        png)
+            if [[ "$max_file_size" -gt 0 ]]; then
+                temp_file="$temp_dir/temp.png"
+                size_kb=0
+                min_quality=10
+                max_quality="$quality"
+                mid_quality=0
+
+                convert_image "$quality"
+                while [[ "$size_kb" -gt "$max_file_size" ]] && [[ "$min_quality" -lt "$max_quality" ]]; do
+                    mid_quality=$(( (min_quality + max_quality) / 2 ))
+                    max_quality=$(( mid_quality - 1 ))
+                    min_quality=$(( mid_quality + 1 ))
+                    convert_image "$mid_quality"
+                done
+                mv "$temp_file" "$output_file"
+            else
+                convert "$file" "${convert_base_opts[@]}" "${png_opts[@]}" "$output_file"
+            fi
+            ;;
+        bmp)
+            convert "$file" -strip -compress none -quality "$quality" "$output_file"
+            ;;
+        gif)
+            if [[ "$output_type" == "gif" ]] && [[ "$(get_file_type "$file")" == "gif" ]]; then
+                cp "$file" "$output_file"
+                echo -e "${GREEN}[INFO]${NC} Copying $file to $output_file (no conversion needed)"
+                log_message "Copying $file to $output_file (no conversion needed)"
+                return 0
+            fi
+
+            temp_file="$(mktemp).gif"
+            convert "$file" -strip -quality "$quality" "$temp_file"
+            gifsicle --colors 256 -O3 "$temp_file" -o "$output_file"
+            rm "$temp_file"
+            ;;
+        tiff)
+            if [[ "$output_type" =~ \.jpg ]]; then
+                convert "$file" -format tif -compress jpeg "$output_file"
+            else
+                convert "$file" -format tif "$output_file"
+            fi
+            ;;
+        webp)
+            small_height="$(identify -ping -format '%h' "$file")"
+            sizes_dir="$output_dir/sizes"
+            mkdir -p "$sizes_dir"
+            convert -quality "$quality" -define webp:method=6 -resize "x${small_height}" "$file" "$output_file"
+            for webp_size in "${webp_sizes[@]}"; do
+                img_height=$(awk "BEGIN {print $webp_size*$small_height}")
+                sized_file="${output_file%.webp}@${webp_size}x.webp"
+                convert -quality "$quality" -define webp:method=6 -resize "x${img_height}" "$file" "$sized_file"
+            done
+            [[ -d "$sizes_dir" ]] && [[ -z "$(ls -A "$sizes_dir")" ]] && rm -fr "$sizes_dir"
+            ;;
+        ico)
+            convert -background none "$file" -define icon:auto-resize="$icon_sizes" "$output_file"
+            ;;
+        *)
+            convert "$file" "${convert_base_opts[@]}" "$additional_args" "$output_file"
+            ;;
+    esac
+
     echo -e "${GREEN}[INFO]${NC} Convert success: $file to $output_file"
-    return 0
+    log_message "Convert success: $file to $output_file"
+
+    if [[ "$delete_input" == "true" ]]; then
+        rm -f "$file"
+        echo -e "${GREEN}[INFO]${NC} Deleted input file: $file"
+        log_message "Deleted input file: $file"
+    fi
+
+    rm -fr "$temp_dir"
 }
+
+export -f process_image warn
+export output_dir quality additional_args max_file_size log_file test_run verbose delete_input
+
+# Function to find files
+find_files() {
+    local files find_files
+    find_files='( -name *.bmp -o -name *.gif -o -name *.ico -o -name *.jfif -o -name *.jpg -o -name *.png -o -name *.tiff -o -name *.webp )'
+    if [[ "$recursive" == "true" ]]; then
+        files=$(find ./ -type f $find_files ! -path "./$output_dir/*")
+    else
+        files=$(find ./ -maxdepth 1 -type f $find_files ! -path "./$output_dir/*")
+    fi
+    echo "$files"
+}
+
 
 # Main script
 main() {
+    local img_files output_choices output_types gif_to_bmp
+
     # Parse arguments
     while [[ "$#" -gt 0 ]]; do
         case "$1" in
@@ -99,15 +269,15 @@ main() {
                 exit 0
                 ;;
             -q|--quality)
-                quality="$2"
+                quality=$2
                 shift
                 ;;
             -o|--output)
-                output_dir="$2"
+                output_dir=$2
                 shift
                 ;;
             -a|--additional)
-                additional_args="$2"
+                additional_args=$2
                 shift
                 ;;
             -m|--max-size)
@@ -119,6 +289,25 @@ main() {
                     max_file_size=$(echo "$size" | sed 's/kb$//;s/[[:space:]]//g')
                 fi
                 shift
+                ;;
+            -l|--log-file)
+                log_file=$2
+                shift
+                ;;
+            -r|--recursive)
+                recursive=true
+                ;;
+            -p|--parallel)
+                parallel=true
+                ;;
+            -v|--verbose)
+                verbose=true
+                ;;
+            -t|--test-run)
+                test_run=true
+                ;;
+            -d|--delete)
+                delete_input=true
                 ;;
             *)
                 echo -e "${RED}[ERROR]${NC} Invalid option: $1"
@@ -136,51 +325,67 @@ main() {
     mkdir -p "$output_dir"
 
     # Determine input files
-    mapfile -t files < <(find . -maxdepth 1 -type f \( -name "*.png" -o -name "*.jpg" -o -name "*.webp" -o -name "*.jfif" -o -name "*.tiff" -o -name "*.bmp" -o -name "*.gif" \) ! -path "./$output_dir/*")
+    img_files=$(find_files)
 
-    if [[ ${#files[@]} -eq 0 ]]; then
+    if [[ -z "$img_files" ]]; then
         echo -e "${RED}[ERROR]${NC} No supported input files found."
         exit 1
     fi
 
     # Prompt user for output file types
-    echo "Select output file types (comma-separated, e.g., bmp,gif,ico,jpg,png,tiff,webp):"
-    read -p "Enter your choices: " output_choices
+    echo "Select output file types (comma-separated, e.g., bmp,gif,ico,jpg,png,tiff,webp, or all):"
+    read -r -p "Enter your choices: " output_choices
     IFS=',' read -ra output_types <<< "$output_choices"
+
+    if [[ " ${output_types[*]} " == *" all "* ]]; then
+        output_types=(bmp gif ico jpg png tiff webp)
+    fi
 
     # Validate output types
     for output_type in "${output_types[@]}"; do
-        case "$output_type" in
-            bmp|gif|ico|jfif|jpg|png|tiff|webp) ;;
-            *)
-                echo -e "${RED}[ERROR]${NC} Invalid output type: $output_type"
-                exit 1
-                ;;
-        esac
+        if [[ ! " bmp gif ico jfif jpg png tiff webp " =~ $output_type ]]; then
+            echo -e "${RED}[ERROR]${NC} Invalid output type: $output_type"
+            exit 1
+        fi
     done
+
+    # Check for GIF to BMP conversion
+    gif_to_bmp=false
+    for img in $img_files; do
+        if [[ "$(get_file_type "$img")" == "gif" ]] && [[ " ${output_types[*]} " == *" bmp "* ]]; then
+            echo -e "${RED}[ERROR]${NC} Conversion from GIF to BMP is not possible with this script."
+            gif_to_bmp=true
+            break
+        fi
+    done
+
+    if [[ "$gif_to_bmp" == "true" ]]; then
+        if [[ "${#output_types[@]}" -eq 1 ]]; then
+            exit 1
+        else
+            read -r -p "Do you want to continue processing the other file types? [y/N]: " continue_choice
+            if [[ "${continue_choice,,}" != "y" ]]; then
+                exit 0
+            fi
+        fi
+    fi
 
     # Convert files
-    for file in "${files[@]}"; do
-        input_type=$(get_file_type "$file")
+    if [[ "$parallel" == "true" ]]; then
+        export -f get_file_type log_message process_image resize_if_needed
         for output_type in "${output_types[@]}"; do
-            if [[ "$input_type" == "$output_type" ]]; then
-                echo -e "${GREEN}[INFO]${NC} Skipping conversion of $file to the same format."
-                continue
-            fi
-            output_file="$output_dir/$(basename "${file%.*}").$output_type"
-            convert_file "$file" "$output_type" "$output_file" "$quality" "$additional_args" "$max_file_size"
+            echo "$img_files" | tr ' ' '\n' | parallel --lb -j 16 process_image {} "$output_type"
         done
-    done
-
-    # Prompt to delete input files
-    echo
-    read -p "Do you want to delete the input files? [y/N]: " delete_choice
-    if [[ ${delete_choice,,} == "y" ]]; then
-        rm -f "${files[@]}"
-        echo -e "${GREEN}[INFO]${NC} Input files deleted."
     else
-        echo -e "${GREEN}[INFO]${NC} Input files not deleted."
+        for img in $img_files; do
+            for output_type in "${output_types[@]}"; do
+                process_image "$img" "$output_type"
+            done
+        done
     fi
 }
 
 main "$@"
+
+# Unset exported variables
+unset CYAN GREEN RED NC
