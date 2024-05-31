179d0aa33b28da22cc1feaea2fd4fd455bbe3001
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-05-31 08:58:08 -0400

    Update raspi-mirrors.sh

--- Bash/Raspbian OS/raspi-mirrors.sh
+++ Bash/Raspbian OS/raspi-mirrors.sh
@@ -7,7 +7,7 @@ fi
 
 fname=/etc/apt/sources.list
 
-# Make a backup of the file
+# Make a backup of the sources list
 if [[ ! -f "${fname}.bak" ]]; then
     cp -f "${fname}" "${fname}.bak"
 fi
@@ -26,13 +26,16 @@ deb http://deb.debian.org/debian-security/ bookworm-security main contrib non-fr
 EOF
 
 # Open an editor to view the changes
-if command -v nano &>/dev/null; then
-    nano "${fname}"
+if command -v gnome-text-editor &>/dev/null; then
+    sudo gnome-text-editor "${fname}"
+elif command -v gedit &>/dev/null; then
+    sudo gedit "${fname}"
+elif command -v nano &>/dev/null; then
+    sudo nano "${fname}"
+elif command -v vim &>/dev/null; then
+    sudo vim "${fname}"
+elif command -v vi &>/dev/null; then
+    sudo vi "${fname}"
 else
-    printf "\n%s\n" "The script failed to locate nano to open the file for viewing."
+    printf "\n%s\n" "Unable to open the sources.list file because no text editor was found."
 fi
-
-script_path=$(readlink -f "${BASH_SOURCE[0]}")
-script_name="${script_path##*/}"
-
-[[ -f "${script_name}" ]] && rm "${script_name}"
