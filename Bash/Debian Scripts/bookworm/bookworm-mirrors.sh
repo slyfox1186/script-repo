486fcc7cc9bd88d95af19c687087f7934267a6dd
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-05-31 08:55:11 -0400

    Update bookworm-mirrors.sh

--- Bash/Debian Scripts/bookworm/bookworm-mirrors.sh
+++ Bash/Debian Scripts/bookworm/bookworm-mirrors.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
 
-if [[ "$EUID" -ne 0 ]]; then
+if [[ "${EUID}" -ne 0 ]]; then
     echo "You must run this script with root or sudo."
     exit 1
 fi
 
-script_path=$(readlink -f "${BASH_SOURCE[0]}")
-script_name=$(basename "$script_path")
+fname=/etc/apt/sources.list
 
-fname="/etc/apt/sources.list"
-
-# Make a backup of the file
+# Make a backup of the sources list
 if [[ ! -f "${fname}.bak" ]]; then
-    cp -f "$fname" "${fname}.bak"
+    cp -f "${fname}" "${fname}.bak"
 fi
 
-cat > "$fname" <<EOF
+cat > "${fname}" <<'EOF'
 deb https://security.debian.org/debian-security/ bookworm-security main contrib non-free non-free-firmware
 deb https://atl.mirrors.clouvider.net/debian/ bookworm main contrib non-free non-free-firmware
 deb https://atl.mirrors.clouvider.net/debian/ bookworm-updates main contrib non-free non-free-firmware
@@ -23,10 +20,14 @@ deb https://atl.mirrors.clouvider.net/debian/ bookworm-backports main contrib no
 EOF
 
 # Open an editor to view the changes
-if command -v nano &>/dev/null; then
-    nano "$fname"
+if command -v gedit &>/dev/null; then
+    sudo gedit "${fname}"
+elif command -v nano &>/dev/null; then
+    sudo nano "${fname}"
+elif command -v vim &>/dev/null; then
+    sudo vim "${fname}"
+elif command -v vi &>/dev/null; then
+    sudo vi "${fname}"
 else
-    echo -e "\\nThe script failed to locate nano to open the file...\\n"
+    printf "\n%s\n" "Unable to open the sources.list file because no text editor was found."
 fi
-
-rm "$script_name"
