4b5b7c2e3a0484a362d53340c42d413c4e87ecc0
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-05-31 08:35:24 -0400

    Update jammy-mirrors.sh

--- Bash/Ubuntu Scripts/jammy/jammy-mirrors.sh
+++ Bash/Ubuntu Scripts/jammy/jammy-mirrors.sh
@@ -6,16 +6,17 @@ if [[ "$EUID" -ne 0 ]]; then
 fi
 
 script_path=$(readlink -f "${BASH_SOURCE[0]}")
-script_name=$(basename "$script_path")
+script_name="${script_path##*/}"
 
-fname="/etc/apt/sources.list"
+fname=/etc/apt/sources.list
+backup_fname="${fname}.bak"
 
-# Make a backup of the file
-if [[ ! -f "${list}.bak" ]]; then
-    cp -f "$list" "${list}.bak"
+# Make a backup of the sources list
+if [[ ! -f "$backup_fname" ]]; then
+    cp -f "$fname" "$backup_fname"
 fi
 
-cat > "$fname" <<EOF
+cat > "$fname" <<'EOF'
 deb https://atl.mirrors.clouvider.net/ubuntu/ jammy main restricted universe multiverse
 deb https://atl.mirrors.clouvider.net/ubuntu/ jammy-updates main restricted universe multiverse
 deb https://atl.mirrors.clouvider.net/ubuntu/ jammy-backports main restricted universe multiverse
@@ -26,7 +27,5 @@ EOF
 if command -v nano &>/dev/null; then
     nano "$fname"
 else
-    echo -e "\\nThe script failed to locate nano to open the file...\\n"
+    printf "\n%s\n" "The script failed to locate nano to open the file."
 fi
-
-rm "$script_name"
