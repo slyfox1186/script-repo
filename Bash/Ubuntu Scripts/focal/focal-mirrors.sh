787dc587e745bd8aeac2f7a4bcbd3aa9aaa26d9e
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-05-31 08:53:39 -0400

    Update focal-mirrors.sh

--- Bash/Ubuntu Scripts/focal/focal-mirrors.sh
+++ Bash/Ubuntu Scripts/focal/focal-mirrors.sh
@@ -1,29 +1,33 @@
 #!/usr/bin/env bash
 
-clear
-
-list='/etc/apt/sources.list'
-
-if [ ! -f "$list.bak" ]; then
-    cp -f "$list" "$list.bak"
+if [[ "${EUID}" -ne 0 ]]; then
+    echo "You must run this script with root or sudo."
+    exit 1
 fi
 
-cat > "$list" <<EOF
+fname=/etc/apt/sources.list
+
+# Make a backup of the sources list
+if [[ ! -f "${fname}.bak" ]]; then
+    cp -f "${fname}" "${fname}.bak"
+fi
+
+cat > "${fname}" <<'EOF'
 deb https://atl.mirrors.clouvider.net/ubuntu/ focal main restricted universe multiverse
 deb https://atl.mirrors.clouvider.net/ubuntu/ focal-updates main restricted universe multiverse
 deb https://atl.mirrors.clouvider.net/ubuntu/ focal-backports main restricted universe multiverse
 deb http://security.ubuntu.com/ubuntu/ focal-security main restricted universe multiverse
 EOF
 
-if which gedit &>/dev/null; then
-    sudo gedit "$list"
-elif which nano &>/dev/null; then
-    sudo nano "$list"
-elif which vi &>/dev/null; then
-    sudo vi "$list"
+# Open an editor to view the changes
+if command -v gedit &>/dev/null; then
+    sudo gedit "${fname}"
+elif command -v nano &>/dev/null; then
+    sudo nano "${fname}"
+elif command -v vim &>/dev/null; then
+    sudo vim "${fname}"
+elif command -v vi &>/dev/null; then
+    sudo vi "${fname}"
 else
-    printf "\n%s\n\n" "Could not find an EDITOR to open the file: $list"
-    exit 1
+    printf "\n%s\n" "Unable to open the sources.list file because no text editor was found."
 fi
-
-sudo rm "$0"
