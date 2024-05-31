fbb587fe6c9bd2b54c08e3ad60e7b4d7499e90a8
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-05-31 08:53:09 -0400

    Update jammy-mirrors.sh

--- Bash/Ubuntu Scripts/jammy/jammy-mirrors.sh
+++ Bash/Ubuntu Scripts/jammy/jammy-mirrors.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
-if [[ "$EUID" -ne 0 ]]; then
+if [[ "${EUID}" -ne 0 ]]; then
     echo "You must run this script with root or sudo."
     exit 1
 fi
 
 fname=/etc/apt/sources.list
-backup_fname="${fname}.bak"
 
 # Make a backup of the sources list
-if [[ ! -f "$backup_fname" ]]; then
-    cp -f "$fname" "$backup_fname"
+if [[ ! -f "${fname}.bak" ]]; then
+    cp -f "${fname}" "${fname}.bak"
 fi
 
-cat > "$fname" <<'EOF'
+cat > "${fname}" <<'EOF'
 deb https://atl.mirrors.clouvider.net/ubuntu/ jammy main restricted universe multiverse
 deb https://atl.mirrors.clouvider.net/ubuntu/ jammy-updates main restricted universe multiverse
 deb https://atl.mirrors.clouvider.net/ubuntu/ jammy-backports main restricted universe multiverse
@@ -22,15 +21,15 @@ EOF
 
 # Open an editor to view the changes
 if command -v gnome-text-editor &>/dev/null; then
-    sudo gnome-text-editor "$list"
+    sudo gnome-text-editor "${fname}"
 elif command -v gedit &>/dev/null; then
-    sudo gedit "$list"
+    sudo gedit "${fname}"
 elif command -v nano &>/dev/null; then
-    sudo nano "$list"
+    sudo nano "${fname}"
 elif command -v vim &>/dev/null; then
-    sudo vim "$list"
+    sudo vim "${fname}"
 elif command -v vi &>/dev/null; then
-    sudo vi "$list"
+    sudo vi "${fname}"
 else
     printf "\n%s\n" "Unable to open the sources.list file because no text editor was found."
 fi
