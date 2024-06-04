1cf1a258baf38f533067bac2d3cac035afea1a18
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-06-03 23:02:53 -0400

    Update focal-mirrors.sh

--- Bash/Ubuntu Scripts/focal/focal-mirrors.sh
+++ Bash/Ubuntu Scripts/focal/focal-mirrors.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ "${EUID}" -ne 0 ]]; then
+if [[ "$EUID" -ne 0 ]]; then
     echo "You must run this script with root or sudo."
     exit 1
 fi
@@ -8,26 +8,39 @@ fi
 fname=/etc/apt/sources.list
 
 # Make a backup of the sources list
-if [[ ! -f "${fname}.bak" ]]; then
-    cp -f "${fname}" "${fname}.bak"
+if [[ ! -f "$fname.bak" ]]; then
+    cp -f "$fname" "${fname}.bak"
 fi
 
-cat > "${fname}" <<'EOF'
+cat > "$fname" <<'EOF'
+# clouvider.net - Atlanta, GA
 deb https://atl.mirrors.clouvider.net/ubuntu/ focal main restricted universe multiverse
 deb https://atl.mirrors.clouvider.net/ubuntu/ focal-updates main restricted universe multiverse
 deb https://atl.mirrors.clouvider.net/ubuntu/ focal-backports main restricted universe multiverse
+
+# Ubuntu Security
 deb http://security.ubuntu.com/ubuntu/ focal-security main restricted universe multiverse
+
+# pilotfiber.com
+# deb https://mirror.pilotfiber.com/ubuntu/ focal main restricted universe multiverse
+# deb https://mirror.pilotfiber.com/ubuntu/ focal-updates main restricted universe multiverse
+# deb https://mirror.pilotfiber.com/ubuntu/ focal-backports main restricted universe multiverse
+
+# teraswitch.com
+# deb http://mirror.pit.teraswitch.com/ubuntu/ focal main restricted universe multiverse
+# deb http://mirror.pit.teraswitch.com/ubuntu/ focal-updates main restricted universe multiverse
+# deb http://mirror.pit.teraswitch.com/ubuntu/ focal-backports main restricted universe multiverse
 EOF
 
 # Open an editor to view the changes
 if command -v gedit &>/dev/null; then
-    sudo gedit "${fname}"
+    sudo gedit "$fname"
 elif command -v nano &>/dev/null; then
-    sudo nano "${fname}"
+    sudo nano "$fname"
 elif command -v vim &>/dev/null; then
-    sudo vim "${fname}"
+    sudo vim "$fname"
 elif command -v vi &>/dev/null; then
-    sudo vi "${fname}"
+    sudo vi "$fname"
 else
     printf "\n%s\n" "Unable to open the sources.list file because no text editor was found."
 fi
