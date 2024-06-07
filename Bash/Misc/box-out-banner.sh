724a972d08658bb2996e8482c54d569918b11851
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-06-07 12:17:47 -0400

    Update box-out-banner.sh

--- Bash/Misc/box-out-banner.sh
+++ Bash/Misc/box-out-banner.sh
@@ -3,15 +3,15 @@
 clear
 
 box_out_banner() {
-    input_char=$(echo "${@}" | wc -c)
-    line=$(for i in $(seq 0 $input_char); do printf '-'; done)
+    input_char=$(echo "$@" | wc -c)
+    line=$(for i in $(seq 0 "$input_char"); do printf "-"; done)
     tput bold
     line="$(tput setaf 3)$line"
-    space=$line//-/ 
+    space="${line//-/ }"
     echo " $line"
-    printf '|' ; echo -n "$space" ; printf "%s\n" '|';
-    printf '| ' ;tput setaf 4; echo -n "${@}"; tput setaf 3 ; printf "%s\n" ' |';
-    printf '|' ; echo -n "$space" ; printf "%s\n" '|';
+    printf "|" ; echo -n "$space" ; printf "%s\n" "|";
+    printf "| " ;tput setaf 4; echo -n "$@"; tput setaf 3 ; printf "%s\n" " |";
+    printf "|" ; echo -n "$space" ; printf "%s\n" "|";
     echo " $line"
     tput sgr 0
 }
