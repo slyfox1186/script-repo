903a8e108e368d420ebbf2999f97e53be96b428e
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-06-07 05:13:10 -0400

    Update untar.sh

--- Bash/Misc/Functions/untar.sh
+++ Bash/Misc/Functions/untar.sh
@@ -1,43 +1,39 @@
 #!/usr/bin/env bash
 
 untar() {
-    local archive dirname ext USER=$(whoami) approved="7z zip gz tgz bz2 xz lz"
+    local archive dirname ext flag USER=$(whoami) supported_ext="7z bz2 gz lz tgz xz zip"
 
-    for archive in *.*; do
+    for archive in *; do
         ext="${archive##*.}"
-        if [[ ! " $approved " =~ " $ext " ]]; then
-            continue
-        fi
+        [[ ! " $supported_ext " =~ " $ext " ]] && continue
 
         dirname="${archive%.*}"
-        if [[ "$archive" =~ \.tar\.(gz|bz2|xz|lz)$ ]]; then
-            dirname="${dirname%.*}"
-        fi
-
+        [[ "$archive" =~ \.tar\.(gz|bz2|lz|xz)$ ]] && dirname="${dirname%.*}"
         mkdir -p "$dirname"
 
         case "$ext" in
             7z) sudo 7z x -y "$archive" -o"$dirname" ;;
             zip) temp_dir=$(mktemp -d)
                  sudo unzip "$archive" -d "$temp_dir"
-                 items=("$temp_dir"/* "$temp_dir"/.*) # Include hidden files
-                 if [[ ${#items[@]} -eq 3 && -d "${items[0]}" && "${items[0]##*/}" == "$dirname" ]]; then
-                     sudo mv "${items[0]}"/* "${items[0]}/".* "$dirname" 2>/dev/null
+                 items=("$temp_dir"/*)
+                 item_dirname="${items[0]##*/}"
+                 if [[ "${#items[@]}" -eq 1 && -d "${items[0]}" && "$item_dirname" == "$dirname" ]]; then
+                     sudo mv "${items[0]}"/* "$dirname"
                  else
-                     sudo mv "$temp_dir"/* "$temp_dir"/.* "$dirname" 2>/dev/null
+                     sudo mv "$temp_dir"/* "$dirname"
                  fi
-                 sudo rm -rf "$temp_dir"
+                 sudo rm -fr "$temp_dir"
                  ;;
             gz|tgz|bz2|xz|lz)
-                 sudo tar -xf "$archive" -C "$dirname" --strip-components 1 ;;
+                sudo tar -xf "$archive" -C "$dirname" --strip-components 1 ;;
         esac
-    done
 
-    for dir in *; do
-        if [ -d "$dir" ]; then
-            sudo chown -R "$USER":"$USER" "$dir"
-            sudo chmod -R 755 "$dir"
-        fi
+        for dir in *; do
+            if [[ -d "$dir" ]]; then
+                sudo chown -R "$USER":"$USER" "$dir"
+                sudo chmod -R 755 "$dir"
+            fi
+        done
     done
 }
 
