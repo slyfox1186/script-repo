936be64133ec15c58e27f4215b04cca3d8bfbd61
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-06-19 20:16:30 -0400

    Update fix-start-of-video.sh

--- Bash/Installer Scripts/FFmpeg/run-scripts/fix-start-of-video.sh
+++ Bash/Installer Scripts/FFmpeg/run-scripts/fix-start-of-video.sh
@@ -154,8 +154,7 @@ while true; do
 
         temp_output_dir="$(dirname "$input_file")"
         if [[ $overwrite -eq 1 ]]; then
-            temp_output="$temp_output_dir/ffmpeg.$(basename "$input_file").XXXXXX"
-            temp_output=$(mktemp "$temp_output")
+            temp_output=$(mktemp "$temp_output_dir/ffmpeg.XXXXXX.$extension")
             command="ffmpeg -hide_banner $trim_start_cmd -y -i \"$input_file\" $trim_end_cmd -c copy \"$temp_output\""
             if eval $command && mv "$temp_output" "$input_file"; then
                 echo -e "${GREEN}Successfully processed and overwritten $input_file${NC}\\n"
