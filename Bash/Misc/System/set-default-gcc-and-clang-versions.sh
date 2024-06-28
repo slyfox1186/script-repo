f1c1d50168fda8f7a87fa8643143e722949ff565
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-06-27 21:47:05 -0400

    Update set-default-gcc-and-clang-versions.sh

--- Bash/Misc/System/set-default-gcc-and-clang-versions.sh
+++ Bash/Misc/System/set-default-gcc-and-clang-versions.sh
@@ -94,18 +94,23 @@ fi
 
 # Function to detect available compiler versions and return the highest
 find_highest_version() {
-    local compiler highest_version
-    local -a available_versions=()
-    compiler="$1"
-    highest_version=""
+    local compiler="$1"
+    local highest_version=""
+    local -a versions=()
 
+    # Check APT versions
     if [[ "$compiler" == "gcc" && "$enable_gcc14" == false ]]; then
-        available_versions=($(apt-cache search "^${compiler}-[0-9]+$" | grep -oP "${compiler}-\K\d+" | grep -v "^14$" | sort -nr))
+        versions+=($(apt-cache search "^${compiler}-[0-9]+$" | grep -oP "${compiler}-\K\d+" | grep -v "^14$" | sort -Vr))
     else
-        available_versions=($(apt-cache search "^${compiler}-[0-9]+$" | grep -oP "${compiler}-\K\d+" | sort -nr))
+        versions+=($(apt-cache search "^${compiler}-[0-9]+$" | grep -oP "${compiler}-\K\d+" | sort -Vr))
     fi
 
-    [[ ${#available_versions[@]} -gt 0 ]] && highest_version="${available_versions[0]}"
+    # Check manually installed versions in /usr/local and /usr/local/bin
+    local manual_versions=($(find /usr/local /usr/local/bin -name "${compiler}-[0-9]*" \( -type f -executable -o -type l \) | grep -oP "${compiler}-\K[0-9]+(\.[0-9]+)*" | sort -Vr))
+    versions+=("${manual_versions[@]}")
+
+    # Get the highest version
+    highest_version=$(printf '%s\n' "${versions[@]}" | sort -Vr | head -n1)
 
     echo "$highest_version"
 }
@@ -128,29 +133,57 @@ install_and_set_compiler() {
     pkg_manager="$3"
     binary_base_path="/usr/bin/$compiler"
 
-    [[ -z "$version" ]] && version=$(find_highest_version "$compiler" "$pkg_manager")
+    [[ -z "$version" ]] && version=$(find_highest_version "$compiler")
 
     echo
-    if run_command "apt install -y $compiler-$version"; then
-        if [[ "$compiler" == "gcc" ]]; then
-            echo
-            run_command "apt install -y g++-$version"
+
+    # Check for manually installed version first
+    local manual_install_path=$(find /usr/local /usr/local/bin -name "${compiler}-${version}*" \( -type f -executable -o -type l \) | head -n1)
+    if [[ -n "$manual_install_path" ]]; then
+        log "Found manually installed $compiler-$version: $manual_install_path"
+        binary_base_path="$manual_install_path"
+    elif [[ -x "/usr/bin/$compiler-$version" ]]; then
+        log "Found APT installed $compiler-$version in /usr/bin"
+        binary_base_path="/usr/bin/$compiler-$version"
+    else
+        if run_command "apt install -y $compiler-$version"; then
+            if [[ "$compiler" == "gcc" ]]; then
+                echo
+                run_command "apt install -y g++-$version"
+            fi
+            binary_base_path="/usr/bin/$compiler-$version"
         fi
     fi
 
     # Set alternatives
     echo
-    run_command "update-alternatives --install $binary_base_path $compiler $binary_base_path-$version 50"
-    run_command "update-alternatives --set $compiler $binary_base_path-$version"
+    run_command "update-alternatives --install /usr/bin/$compiler $compiler $binary_base_path 50"
+    run_command "update-alternatives --set $compiler $binary_base_path"
     echo
 
     # Set alternatives for the compiler++
     if [[ "$compiler" == "gcc" ]]; then
-        run_command "update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-$version 50"
-        run_command "update-alternatives --set g++ /usr/bin/g++-$version"
+        local gpp_path="${binary_base_path/gcc/g++}"
+        if [[ ! -x "$gpp_path" ]]; then
+            gpp_path="$(dirname "$binary_base_path")/g++-$version"
+        fi
+        if [[ ! -x "$gpp_path" ]]; then
+            gpp_path=$(find /usr/local /usr/local/bin /usr/bin -name "g++-${version}*" \( -type f -executable -o -type l \) | head -n1)
+        fi
+        if [[ -x "$gpp_path" ]]; then
+            run_command "update-alternatives --install /usr/bin/g++ g++ $gpp_path 50"
+            run_command "update-alternatives --set g++ $gpp_path"
+        else
+            error "g++ not found. Please ensure g++-$version is installed."
+        fi
     elif [[ "$compiler" == "clang" ]]; then
-        run_command "update-alternatives --install $binary_base_path++ clang++ $binary_base_path++-$version 50"
-        run_command "update-alternatives --set clang++ $binary_base_path++-$version"
+        local clangpp_path="${binary_base_path%%-*}++-${binary_base_path##*-}"
+        if [[ -x "$clangpp_path" ]]; then
+            run_command "update-alternatives --install /usr/bin/clang++ clang++ $clangpp_path 50"
+            run_command "update-alternatives --set clang++ $clangpp_path"
+        else
+            error "clang++ not found at $clangpp_path"
+        fi
     fi
 }
 
