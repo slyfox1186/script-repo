d503a3e354d2153e68d7e99a95a709dad2fc2174
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-12-27 10:58:38 -0500

    Update llama-cpp-python_installer.sh

--- Bash/Misc/Conda/llama-cpp-python_installer.sh
+++ Bash/Misc/Conda/llama-cpp-python_installer.sh
@@ -18,15 +18,9 @@ echo "CUDA_HOME is set to: $CUDA_HOME"
 echo "LD_LIBRARY_PATH is set to: $LD_LIBRARY_PATH"
 echo "PATH is set to: $PATH"
 
-# Install conda packages
-printf "\n%s\n\n" "Installing conda packages."
-conda install -y bs4 markdown2 nltk psutil pytest-asyncio pytest python-dotenv \
-                 scikit-learn sentencepiece spacy textblob tqdm transformers unidecode \
-                 peft apscheduler redis
-
-# Install pip packages
-printf "\n%s\n\n" "Installing pip packages."
-pip install accelerate bitsandbytes cloud-tpu-client fake_useragent flask langdetect "numpy<2.1.0,>=2.0.0" tiktoken
+# Install required pip packages
+printf "\n%s\n\n" "Installing required pip packages."
+pip install apscheduler redis flask
 
 # Clone and install llama-cpp-python
 printf "\n%s\n\n" "Cloning and installing llama-cpp-python."
@@ -42,7 +36,7 @@ CMAKE_ARGS="-DGGML_CUDA=ON \
            -DCMAKE_CUDA_HOST_COMPILER=$(type -P gcc-12)" \
 CUDACXX="$CUDA_HOME/bin/nvcc" \
 CUDA_PATH="$CUDA_HOME" \
-pip install -e . || (
+pip install . --force-reinstall --no-cache-dir --upgrade --verbose || (
     echo "Failed to install llama-cpp-python. Exiting..."
     exit 1
 )
