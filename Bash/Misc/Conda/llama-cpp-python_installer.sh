85e03c56801b92c8d4cb888a17c3c63629c1ef51
Jeff <37704019+slyfox1186@users.noreply.github.com>
2025-03-11 11:07:30 -0400

    Update llama-cpp-python_installer.sh

--- Bash/Misc/Conda/llama-cpp-python_installer.sh
+++ Bash/Misc/Conda/llama-cpp-python_installer.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+if [[ "$EUID" -eq 0 ]]; then
+    echo "You must run this script without sudo or as root."
+    exit 1
+fi
+
+if ! type -P conda &>/dev/null; then
+    echo "You must install conda to run this script."
+    echo
+    read -p "Do you want to install conda (y/n): install_conda.sh ?" answer
+    if [[ -n "$answer" ]]; then
+        bash <(curl -fsSL https://raw.githubusercontent.com/slyfox1186/script-repo/refs/heads/main/Bash/Misc/Conda/install_conda.sh)
+    fi
+fi
+
 # Set CUDA environment variables
 CUDA_HOME="/usr/local/cuda"
 PATH="$PATH:/usr/lib/x86_64-linux-gnu:$CUDA_HOME/bin"
@@ -23,21 +37,20 @@ conda install -c pytorch pytorch torchvision torchaudio -y
 
 # Install pip packages
 printf "%s\n\n" "Installing pip packages."
-pip install bs4 markdown2 nltk psutil pytest \
-python-dotenv sentencepiece spacy textblob tqdm \
-unidecode peft apscheduler redis fake_useragent \
-flask flask_cors langdetect "numpy<2.0.0,>=1.25.0" \
-"scipy>=1.6.0" "threadpoolctl>=3.1.0" "tzdata>=2022.7" \
-"python-dateutil>=2.8.2"
+pip install aiohttp apscheduler bs4 fake_useragent fastapi flask flask_cors \
+langdetect markdown2 nltk "numpy<2.0.0,>=1.25.0" peft psutil pytest \
+"python-dateutil>=2.8.2" python-dotenv pytz redis "scipy>=1.6.0" sentencepiece \
+spacy textblob "threadpoolctl>=3.1.0" tqdm "tzdata>=2022.7" unidecode utils uvicorn
 
 # Install with CUDA support in editable mode
 CMAKE_ARGS="-DGGML_CUDA=ON \
-           -DCMAKE_CUDA_ARCHITECTURES=native \
-           -DCMAKE_LIBRARY_PATH=$CUDA_HOME/lib64:/usr/lib/x86_64-linux-gnu \
-           -DCMAKE_CUDA_HOST_COMPILER=$(type -P gcc-12)" \
+-DCMAKE_CUDA_ARCHITECTURES=native \
+-DCMAKE_LIBRARY_PATH=$CUDA_HOME/lib64:/usr/lib/x86_64-linux-gnu \
+-DCMAKE_CUDA_HOST_COMPILER=$(type -P gcc-12)" \
 CUDACXX="$CUDA_HOME/bin/nvcc" \
 CUDA_PATH="$CUDA_HOME" \
 pip install llama-cpp-python --force-reinstall --no-cache-dir --upgrade --verbose || (
-    echo "Failed to install llama-cpp-python. Exiting..."
+    printf "\n%s\n%s\n" "[ERROR] Failed to install llama-cpp-python." \
+                    "Exiting the script"
     exit 1
 )
