3e1e87d71e8bdba4e0131b49cc813d7ab88b7764
Jeff <37704019+slyfox1186@users.noreply.github.com>
2025-03-14 12:03:03 -0400

    Update 05_environment.sh

--- Bash/Ubuntu Scripts/jammy/user-scripts/.bashrc.d/05_environment.sh
+++ Bash/Ubuntu Scripts/jammy/user-scripts/.bashrc.d/05_environment.sh
@@ -16,12 +16,3 @@ export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quo
 
 # PostgreSQL configuration
 export PG_CONFIG="/usr/bin/pg_config"
-
-# ====================
-# API KEYS
-# ====================
-# Google API's - Note: Consider moving these to a separate private file
-GOOGLE_API_KEY="AIzaSyD9xQLbEB6924BbgxnS1FvXpeKBmCIFyaY"
-GOOGLE_CSE_ID="f2a9a9431a797430a"
-OPENWEATHER_API_KEY="092269b7aa1069ab41a0210bca856b16"
-export GOOGLE_API_KEY GOOGLE_CSE_ID OPENWEATHER_API_KEY
